8a8d62fd8b178e374cafa8cdf7cc5c8fb9f25618
Michael Sharpe <m.sharpe@project89.org>
2023-01-03 15:16:21 -0800

    Clear some cruft

--- archive/netlify/plugins/staging-canary/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  onPreBuild: ({ netlifyConfig, utils: { git } }) => {
-    netlifyConfig.build.command = `cd client && yarn add @thothai/core@canary && cd .. && ${netlifyConfig.build.command}`
-  },
-}
