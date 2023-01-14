3f1c4a43e4a3f7f2231c8b76038b34334486293d
Michael Sharpe <m.sharpe@project89.org>
2023-01-14 00:14:35 -0800

    Allow env file to overwrite prisma client

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -3,12 +3,10 @@ import { DATABASE_URL } from '@magickml/server-config'
 
 console.log('DAtabase URL', DATABASE_URL)
 
-export const prisma = new PrismaClient()
-
-// {
-//   datasources: {
-//     db: {
-//       url: DATABASE_URL,
-//     },
-//   },
-// }
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+})
