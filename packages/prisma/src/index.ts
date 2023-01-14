cd03f9a3cf357c2505fc462a77f7d7f19382e478
Michael Sharpe <m.sharpe@project89.org>
2023-01-13 22:37:04 -0800

    Swap sqlite prisma to postgres with shadow supabase

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { DATABASE_URL } from '@magickml/server-config'
+
+console.log('DAtabase URL', DATABASE_URL)
 
 export const prisma = new PrismaClient()
+
+// {
+//   datasources: {
+//     db: {
+//       url: DATABASE_URL,
+//     },
+//   },
+// }
