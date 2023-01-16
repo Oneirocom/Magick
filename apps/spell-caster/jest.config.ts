ad229f2180c9a2dd1a6f40d0773e14e80704cc35
Michael Sharpe <m.sharpe@project89.org>
2023-01-15 21:20:54 -0800

    Add empty spellcaster application

---
+++ apps/spell-caster/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'spell-caster',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/spell-caster'
+};
