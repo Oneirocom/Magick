4c03f35e8ecca2cf363e65b0409e13305367b5ea
Michael Sharpe <m.sharpe@project89.org>
2023-01-16 21:40:31 -0800

    Clean up files

--- packages/core/src/components/io/JupyterNotebook.ts
+++ packages/core/src/components/io/JupyterNotebook.ts
@@ -20,29 +20,26 @@ type WorkerReturn = {
   output: any
 }
 
-const removeProtocol = (url) => {
+const removeProtocol = url => {
   if (!url) {
-      return '';
+    return ''
   }
 
-  return url.replace(/(^\w+:|^)\/\//, '');
+  return url.replace(/(^\w+:|^)\/\//, '')
 }
 
 function getPromiseFromEvent(item, event) {
-
-  return new Promise<void>((resolve) => {
-    const listener = (e) => {
+  return new Promise<void>(resolve => {
+    const listener = e => {
       e = JSON.parse(e.data)
-      if (e['msg_type'] == "stream") {
-        item.removeEventListener(event, listener);
-        resolve();
+      if (e['msg_type'] == 'stream') {
+        item.removeEventListener(event, listener)
+        resolve()
       }
-      
     }
-    item.addEventListener(event, listener);
+    item.addEventListener(event, listener)
   })
 }
-
 
 export class JupyterComms extends MagickComponent<Promise<WorkerReturn>> {
   constructor() {
@@ -95,18 +92,21 @@ export class JupyterComms extends MagickComponent<Promise<WorkerReturn>> {
       icon: 'moon',
     })
 
-    node.inspector.add(nameControl).add(inputGenerator).add(url).add(file_name).add(authorization_key)
+    node.inspector
+      .add(nameControl)
+      .add(inputGenerator)
+      .add(url)
+      .add(file_name)
+      .add(authorization_key)
 
     return node.addInput(dataInput).addOutput(dataOutput).addOutput(outp)
   }
-  
 
   async worker(
     node: NodeData,
     rawInputs: MagickWorkerInputs,
     _outputs: MagickWorkerOutputs
   ) {
-
     const name = node.data.name as string
     node.name = name
 
@@ -118,48 +118,68 @@ export class JupyterComms extends MagickComponent<Promise<WorkerReturn>> {
     //URL of the Jupyter Server Eg: http://localhost:8888 (No Slash at the end)
     let url = node?.data?.url as string
     let authorization_key = node?.data?.url as string
-    authorization_key = "Token " + authorization_key
+    authorization_key = 'Token ' + authorization_key
     let config = {
       headers: {
-        "Authorization": authorization_key,
+        Authorization: authorization_key,
       },
-    };
+    }
     //Gets the Active Kernel from the Jupyter Tornado Server (REST/POST)
-    let kernel = await axios.post(url + '/api/kernels' + "?timestamp=" + Math.random().toString(36), config)
-    let notebook_path = "/" + node?.data?.file_name as string
-    
+    let kernel = await axios.post(
+      url + '/api/kernels' + '?timestamp=' + Math.random().toString(36),
+      config
+    )
+    let notebook_path = ('/' + node?.data?.file_name) as string
+
     //Gets the Cell Contents of the notebook of which the filename is specified (REST/GET)
-    let file = await axios.get(url + '/api/contents' + notebook_path + "?timestamp=" + Math.random().toString(36), config)
-    
+    let file = await axios.get(
+      url +
+        '/api/contents' +
+        notebook_path +
+        '?timestamp=' +
+        Math.random().toString(36),
+      config
+    )
+
     //Selects the last cell
     let code = file['data']['content']['cells']
-    if (code.slice(-1)[0]['source'] == "") {
+    if (code.slice(-1)[0]['source'] == '') {
       code = code.slice(-2)
     } else {
       code = code.slice(-1)
     }
 
     //Create a UUID for exectuion
-    let uniqueId = Date.now().toString(36) + Math.random().toString(36).substring(2);
-    
+    let uniqueId =
+      Date.now().toString(36) + Math.random().toString(36).substring(2)
+
     //Initializing the WebSocket link with the Jupyter Server
-    let ws = new WebSocket("ws://" + removeProtocol(url) + '/api/kernels/' + kernel['data']['id'] + "/channels"+"?timestamp=" + Math.random().toString(36));
-    
+    let ws = new WebSocket(
+      'ws://' +
+        removeProtocol(url) +
+        '/api/kernels/' +
+        kernel['data']['id'] +
+        '/channels' +
+        '?timestamp=' +
+        Math.random().toString(36)
+    )
+
     //Specifc Headers Required for Code Execution
-    let msg_type = 'execute_request';
-    let content = { 'code': code['0']['source'], 'silent': false }
+    let msg_type = 'execute_request'
+    let content = { code: code['0']['source'], silent: false }
     let hdr = {
-      'msg_id': parseInt(uniqueId, 10).toString(16),
-      'username': 'test',
-      'session': parseInt(uniqueId, 10).toString(16),
-      'data': Date.now(),
-      'msg_type': msg_type,
-      'version': '5.0'
+      msg_id: parseInt(uniqueId, 10).toString(16),
+      username: 'test',
+      session: parseInt(uniqueId, 10).toString(16),
+      data: Date.now(),
+      msg_type: msg_type,
+      version: '5.0',
     }
     let msg = {
-      'header': hdr, 'parent_header': hdr,
-      'metadata': {},
-      'content': content
+      header: hdr,
+      parent_header: hdr,
+      metadata: {},
+      content: content,
     }
 
     //Event Listner for Socket Connection open event
@@ -167,17 +187,17 @@ export class JupyterComms extends MagickComponent<Promise<WorkerReturn>> {
       this.send(JSON.stringify(msg))
     }
 
-    var code_output = "Nothing"
+    var code_output = 'Nothing'
     //Event Listener when the server responds back with the code
     ws.onmessage = async function (e) {
       e = JSON.parse(e.data)
-      code_output = "Hello This is text"
-      if (e['msg_type'] == "stream") {
+      code_output = 'Hello This is text'
+      if (e['msg_type'] == 'stream') {
         //Wait till the message with 'stream' status is obtained then the promise is resolved
-        code_output = await e['content']["text"];
+        code_output = await e['content']['text']
       }
     }
-    await getPromiseFromEvent(ws, "message")
+    await getPromiseFromEvent(ws, 'message')
     return {
       output: code_output ? (code_output as any) : '',
     }
