426e44feebbd16a51fc5f07725e9dd2e5c64b9b9
Shivaditya Shivganesh <sshivaditya@gmail.com>
2023-01-17 21:27:35 +0530

    Added Question Answer Node

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -52,6 +52,19 @@ export type Event = {
   date?: string
 }
 
+export type SemanticSearch = {
+  concept?: string
+  postive?: string
+  negative?: string
+  distance?: number
+  postive_distance?: number
+  negative_distance?: number
+}
+
+export type QAArgs = {
+  question: string
+}
+
 export type CreateEventArgs = Event
 
 export type GetEventArgs = Event & {
@@ -125,6 +138,7 @@ export type EngineContext = {
   ) => Promise<string | string[] | null | Record<string, any>>
   storeEvent: (args: CreateEventArgs) => Promise<any>
   storeEventWeaviate: (args: CreateEventArgs) => Promise<any>
+  eventQAWeaviate: (args: QAArgs) => Promise<string | string[] >
   getWikipediaSummary: (keyword: string) => Promise<Record<string, any> | null>
 }
 
