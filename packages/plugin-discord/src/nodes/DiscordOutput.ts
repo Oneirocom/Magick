216637e66db29ec44e9f0ce5b4cae98303ed1e44
shawticus <shawmakesmagic@gmail.com>
2023-02-04 21:15:43 -0800

    Fix plugin dependencies

--- packages/plugin-discord/src/nodes/DiscordOutput.ts
+++ packages/plugin-discord/src/nodes/DiscordOutput.ts
@@ -5,7 +5,7 @@ import {
     anySocket, EditorContext, InputControl, MagickComponent, MagickNode,
     MagickWorkerInputs,
     MagickWorkerOutputs, NodeData, triggerSocket, SwitchControl
-} from '@magickml/engine'
+  } from "@magickml/engine";
 
 const info = `The output component will pass values out from your spell.  You can have multiple outputs in a spell and all output values will be collected. It also has an option to send the output to the playtest area for easy testing.`
 
