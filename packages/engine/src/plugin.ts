f0ce4d4846b0ddd217a86920a69a6b5ec16bb56d
shawticus <shawmakesmagic@gmail.com>
2023-02-08 18:42:54 -0800

    Fix merge conflicts with weaviate and discord plugins

--- packages/engine/src/plugin.ts
+++ packages/engine/src/plugin.ts
@@ -1,12 +1,12 @@
 export class Plugin {
     name: string;
     nodes: any;
-    services:any;
+    services: any;
     windowComponents: []
     agentComponents: []
     setup: Function;
     teardown: Function;
-    agentMethods: {
+    agentMethods?: {
         start: Function,
         stop: Function
     }
@@ -35,18 +35,18 @@ export class Plugin {
 class PluginManager {
     static pluginList: Array<Plugin>;
     static componentList: Object;
-    constructor(){
+    constructor() {
         PluginManager.pluginList = new Array<Plugin>();
     }
 
-    register(plugin: Plugin){
+    register(plugin: Plugin) {
         PluginManager.pluginList.push(plugin)
         plugin.setup()
     }
     /*
     Gets All Agent Components from all the registered plugins 
     */
-    getAgentComponents(){
+    getAgentComponents() {
         let agentComp = []
         PluginManager.pluginList.forEach((plugin) => {
             plugin.agentComponents.forEach((component) => {
@@ -56,23 +56,27 @@ class PluginManager {
         return agentComp
     }
 
-    getAgentStartMethods(){
+    getAgentStartMethods() {
         let agentStartMethods = {}
         PluginManager.pluginList.forEach((plugin) => {
-            let obj = {}
-            obj[plugin.name] = plugin.agentMethods.start
-            agentStartMethods = {...agentStartMethods, ...obj}
+            if (plugin.agentMethods) {
+                let obj = {}
+                obj[plugin.name] = plugin.agentMethods.start
+                agentStartMethods = { ...agentStartMethods, ...obj }
+            }
         })
         console.log('getAgentStartMethods', agentStartMethods)
         return agentStartMethods
     }
 
-    getAgentStopMethods(){
+    getAgentStopMethods() {
         let agentStopMethods = {}
         PluginManager.pluginList.forEach((plugin) => {
-            let obj = {}
-            obj[plugin.name] = plugin.agentMethods.stop
-            agentStopMethods = {...agentStopMethods, ...obj}
+            if (plugin.agentMethods) {
+                let obj = {}
+                obj[plugin.name] = plugin.agentMethods.stop
+                agentStopMethods = { ...agentStopMethods, ...obj }
+            }
         })
         return agentStopMethods
     }
@@ -80,7 +84,7 @@ class PluginManager {
     /*
     Gets All Services from all the registered plugins 
     */
-    getServices(){
+    getServices() {
         let services_list = []
         PluginManager.pluginList.forEach((plugin) => {
             plugin.services.forEach((service) => {
@@ -90,26 +94,26 @@ class PluginManager {
         return services_list
     }
 
-    async teardown(plugin: Plugin){
+    async teardown(plugin: Plugin) {
         plugin.teardown()
         PluginManager.pluginList.pop()
     }
     /*
     Gets All Nodes from all the registered plugins 
     */
-    getNodes(){
+    getNodes() {
         let nodes = {}
-        
+
         PluginManager.pluginList.forEach((plugin) => {
-            
+
             let plug_nodes = {}
             plugin.nodes.forEach((node) => {
                 let id = Math.random().toString(36).slice(2, 7);
                 let obj = {}
                 obj[id] = () => new node()
-                plug_nodes = {...plug_nodes, ...obj}
+                plug_nodes = { ...plug_nodes, ...obj }
             })
-            nodes = {...nodes, ...plug_nodes}
+            nodes = { ...nodes, ...plug_nodes }
         })
 
         return nodes;
