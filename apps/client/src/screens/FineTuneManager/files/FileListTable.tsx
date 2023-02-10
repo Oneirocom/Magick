ff736fa37a0440bf0b9636f625f83b4776c6ed60
Thomage Anderson <thomageanderson@gmail.com>
2023-02-10 00:00:55 -0500

    client: Update FileListTable.tsx to use mui Table components

--- apps/client/src/screens/FineTuneManager/files/FileListTable.tsx
+++ apps/client/src/screens/FineTuneManager/files/FileListTable.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import useSWR from 'swr'
 import type { OpenAI } from '../../../../../../@types/openai'
 import DeleteFileButton from './DeleteFileButton'
+import { Table, TableCell } from '@mui/material'
 
 export default function FileListTable({
   linkTo,
@@ -27,36 +28,34 @@ export default function FileListTable({
   }
 
   return (
-    <table className="w-full border-collapse">
-      <tbody>
-        {files
-          .sort((a, b) => b.created_at - a.created_at)
-          .map((file, index) => (
-            <tr className={index % 2 === 0 ? 'bg-gray-100' : ''} key={file.id}>
-              <td className="p-2 max-w-0 truncate" title={file.id}>
-                {linkTo ? (
-                  <a href={linkTo(file)}>
-                    <a>{file.id}</a>
-                  </a>
-                ) : (
-                  file.id
-                )}
-              </td>
-              <td className="p-2 max-w-0 truncate" title={file.filename}>
-                {file.filename}
-              </td>
-              <td
-                className="p-2 max-w-0 truncate"
-                title={new Date(file.created_at * 1000).toISOString()}
-              >
-                {new Date(file.created_at * 1000).toLocaleString()}
-              </td>
-              <td className="p-2 w-8">
-                <DeleteFileButton id={file.id} />
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <Table>
+      {files
+        .sort((a, b) => b.created_at - a.created_at)
+        .map((file, index) => (
+          <tr className={index % 2 === 0 ? 'bg-gray-100' : ''} key={file.id}>
+            <TableCell className="p-2 max-w-0 truncate" title={file.id}>
+              {linkTo ? (
+                <a href={linkTo(file)}>
+                  <a>{file.id}</a>
+                </a>
+              ) : (
+                file.id
+              )}
+            </TableCell>
+            <TableCell className="p-2 max-w-0 truncate" title={file.filename}>
+              {file.filename}
+            </TableCell>
+            <TableCell
+              className="p-2 max-w-0 truncate"
+              title={new Date(file.created_at * 1000).toISOString()}
+            >
+              {new Date(file.created_at * 1000).toLocaleString()}
+            </TableCell>
+            <TableCell className="p-2 w-8">
+              <DeleteFileButton id={file.id} />
+            </TableCell>
+          </tr>
+        ))}
+    </Table>
   )
 }
