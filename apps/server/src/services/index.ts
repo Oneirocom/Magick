6fc563e2faba5ecab9c6c804265d8c3566a35a67
shawticus <shawmakesmagic@gmail.com>
2023-02-11 04:41:59 -0800

    Remove users service-- not needed

--- apps/server/src/services/index.ts
+++ apps/server/src/services/index.ts
@@ -1,6 +1,5 @@
 import { spell } from './spells/spells'
 import { agent } from './agents/agents'
-import { user } from './users/users'
 import { spellRunner } from './spell-runner/spell-runner'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
@@ -9,7 +8,6 @@ export const services = (app: Application) => {
   const service_list = pluginManager.getServices()
   app.configure(spell)
   app.configure(agent)
-  app.configure(user)
   app.configure(spellRunner)
   service_list.forEach((service)=>{
       app.use(service[0], new service[1])
