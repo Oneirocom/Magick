138e540ccdef68da2a268b24115fa779fb06a4d7
Thomage Anderson <thomageanderson@gmail.com>
2023-02-12 18:06:18 -0500

    client: Add InfoCard wrappers to CompletionList.tsx

--- apps/client/src/screens/FineTuneManager/completions/CompletionList.tsx
+++ apps/client/src/screens/FineTuneManager/completions/CompletionList.tsx
@@ -8,63 +8,69 @@ import { useNavigate } from 'react-router-dom'
 // TODO @thomageanderson: resolve the type warning on this markdown import
 import { html as CompletionInstructions } from './instructions.md'
 import { Box, Typography } from '@mui/material'
+import InfoCard from '../components/InfoCard'
 
 export default function ClassificationList() {
   const navigate = useNavigate()
   return (
     <main className="mx-auto space-y-12 max-w-4xl">
-      <Box component={'div'} style={{ width: '100%' }}>
-        <Box
-          component={'div'}
-          display={'flex'}
-          justifyContent={'space-between'}
-          flexWrap={'nowrap'}
-          flexDirection={'row'}
-          padding={1}
-        >
-          {/* TODO @thomageanderson: remove hardcoded color when global mui themes are supported */}
-          <Typography variant="h4" component="h2" color="white">
-            Completions
-          </Typography>
-          <Button
-            size="small"
-            variant="contained"
-            onClick={() => navigate('/fineTuneManager/fine-tunes/new')}
+      <InfoCard>
+        <Box component={'div'} style={{ width: '100%' }}>
+          <Box
+            component={'div'}
+            display={'flex'}
+            justifyContent={'space-between'}
+            flexWrap={'nowrap'}
+            flexDirection={'row'}
+            padding={1}
           >
-            New Model
-          </Button>
+            {/* TODO @thomageanderson: remove hardcoded color when global mui themes are supported */}
+            <Typography variant="h4" component="h2" color="white">
+              Completions
+            </Typography>
+            <Button
+              size="small"
+              variant="contained"
+              onClick={() => navigate('/fineTuneManager/fine-tunes/new')}
+            >
+              New Model
+            </Button>
+          </Box>
         </Box>
-      </Box>
-      <FineTuneList />
-
-      <Box component={'div'} style={{ width: '100%' }}>
-        <Box
-          component={'div'}
-          display={'flex'}
-          justifyContent={'space-between'}
-          flexWrap={'nowrap'}
-          flexDirection={'row'}
-          padding={1}
-        >
-          {/* TODO @thomageanderson: remove hardcoded color when global mui themes are supported */}
-          <Typography variant="h4" component="h2" color="white">
-            Training Files
-          </Typography>
-          <UploadFileButton
-            purpose="fine-tune"
-            enforce={{
-              required: ['prompt', 'completion'],
-              count: ['prompt', 'completion'],
-              maxTokens: 2048,
-            }}
-          />
+        <FineTuneList />
+      </InfoCard>
+      <InfoCard>
+        <Box component={'div'} style={{ width: '100%' }}>
+          <Box
+            component={'div'}
+            display={'flex'}
+            justifyContent={'space-between'}
+            flexWrap={'nowrap'}
+            flexDirection={'row'}
+            padding={1}
+          >
+            {/* TODO @thomageanderson: remove hardcoded color when global mui themes are supported */}
+            <Typography variant="h4" component="h2" color="white">
+              Training Files
+            </Typography>
+            <UploadFileButton
+              purpose="fine-tune"
+              enforce={{
+                required: ['prompt', 'completion'],
+                count: ['prompt', 'completion'],
+                maxTokens: 2048,
+              }}
+            />
+          </Box>
         </Box>
-      </Box>
 
-      <FileListTable purpose="fine-tune" />
-      <ExpansionDetails title={'Usage Instructions'}>
-        <div dangerouslySetInnerHTML={{ __html: CompletionInstructions }} />
-      </ExpansionDetails>
+        <FileListTable purpose="fine-tune" />
+      </InfoCard>
+      <InfoCard>
+        <ExpansionDetails title={'Usage Instructions'}>
+          <div dangerouslySetInnerHTML={{ __html: CompletionInstructions }} />
+        </ExpansionDetails>
+      </InfoCard>
     </main>
   )
 }
