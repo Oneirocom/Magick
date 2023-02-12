31f2668accab7d8ce32afff2b73558a29cd601a6
Thomage Anderson <thomageanderson@gmail.com>
2023-02-12 18:05:34 -0500

    client: Remove InfoCard wrapper from ShowRequestExample.tsx

--- apps/client/src/screens/FineTuneManager/components/ShowRequestExample.tsx
+++ apps/client/src/screens/FineTuneManager/components/ShowRequestExample.tsx
@@ -2,7 +2,6 @@ import Tooltip from '@mui/material/Tooltip'
 import { useState } from 'react'
 import { useCopyToClipboard } from 'usehooks-ts'
 import ExpansionDetails from './ExpansionDetails'
-import InfoCard from './InfoCard'
 
 export default function ShowRequestExample({
   reference,
@@ -21,29 +20,27 @@ export default function ShowRequestExample({
   const code = JSON.stringify(request, null, 2)
 
   return (
-    <InfoCard>
-      <ExpansionDetails title={'Request JSON'}>
-        <div className="relative prose">
-          <Tooltip
-            className="absolute top-0 right-0 p-2"
-            title={copied ? 'Copied!' : 'Click to copy'}
-            placement="left"
-            onClose={() => setCopied(false)}
-            onClick={async () => {
-              await copy(code)
-              setCopied(true)
-            }}
-          >
-            <></>
-          </Tooltip>
-          <pre>{code}</pre>
-        </div>
-        <p>
-          <a href={reference} target="_blank">
-            OpenAI Reference
-          </a>
-        </p>
-      </ExpansionDetails>
-    </InfoCard>
+    <ExpansionDetails title={'Request JSON'}>
+      <div className="relative prose">
+        <Tooltip
+          className="absolute top-0 right-0 p-2"
+          title={copied ? 'Copied!' : 'Click to copy'}
+          placement="left"
+          onClose={() => setCopied(false)}
+          onClick={async () => {
+            await copy(code)
+            setCopied(true)
+          }}
+        >
+          <></>
+        </Tooltip>
+        <pre>{code}</pre>
+      </div>
+      <p>
+        <a href={reference} target="_blank">
+          OpenAI Reference
+        </a>
+      </p>
+    </ExpansionDetails>
   )
 }
