9427e951c9c3a96d304d110cdcbf58da6a94f822
Thomage Anderson <thomageanderson@gmail.com>
2023-02-12 15:52:30 -0500

    client: Connect completions button to navigation in fineTuneManager NavBar

--- apps/client/src/screens/FineTuneManager/components/NavBar/index.tsx
+++ apps/client/src/screens/FineTuneManager/components/NavBar/index.tsx
@@ -2,15 +2,19 @@ import MagickLogo from './Magick-purple-logo.png'
 import css from './tabBar.module.css'
 import Button from '@mui/material/Button'
 import useAuthentication from '../../account/useAuthentication'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
   const { isSignedIn, signOut } = useAuthentication()
+  const navigate = useNavigate()
   return (
     <div className={css['th-tabbar']}>
       <img style={{ height: 16, marginLeft: 10 }} src={MagickLogo} alt="" />
       {isSignedIn && (
         <div className={css['tabbar-section']}>
-          <Button>Completions</Button>
+          <Button onClick={() => navigate('/fineTuneManager/completions')}>
+            Completions
+          </Button>
           <Button onClick={() => signOut()}>Clear Credentials</Button>
         </div>
       )}
