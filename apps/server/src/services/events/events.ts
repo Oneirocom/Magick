6e39936f55d48c57d2f87718a67b17276c6c333f
shawticus <shawmakesmagic@gmail.com>
2023-02-12 16:49:48 -0800

    Add events to service backend

---
+++ apps/server/src/services/events/events.ts
@@ -0,0 +1,63 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
+import { authenticate } from '@feathersjs/authentication'
+
+import { hooks as schemaHooks } from '@feathersjs/schema'
+
+import {
+  eventDataValidator,
+  eventPatchValidator,
+  eventQueryValidator,
+  eventResolver,
+  eventExternalResolver,
+  eventDataResolver,
+  eventPatchResolver,
+  eventQueryResolver
+} from './events.schema'
+
+import type { Application } from '../../declarations'
+import { EventService, getOptions } from './events.class'
+
+export * from './events.class'
+export * from './events.schema'
+
+// A configure function that registers the service and its hooks via `app.configure`
+export const event = (app: Application) => {
+  // Register our service on the Feathers application
+  app.use('events', new EventService(getOptions(app)), {
+    // A list of all methods this service exposes externally
+    methods: ['find', 'get', 'create', 'patch', 'remove'],
+    // You can add additional custom events to be sent to clients here
+    events: []
+  })
+  // Initialize hooks
+  app.service('events').hooks({
+    around: {
+      all: [
+        process.env.USE_AUTH !== 'true' && process.env.NODE_ENV === 'development' ? authenticate('jwt') : (context: any, next: any) => next(),
+        schemaHooks.resolveExternal(eventExternalResolver),
+        schemaHooks.resolveResult(eventResolver),
+      ]
+    },
+    before: {
+      all: [schemaHooks.validateQuery(eventQueryValidator), schemaHooks.resolveQuery(eventQueryResolver)],
+      find: [],
+      get: [],
+      create: [schemaHooks.validateData(eventDataValidator), schemaHooks.resolveData(eventDataResolver)],
+      patch: [schemaHooks.validateData(eventPatchValidator), schemaHooks.resolveData(eventPatchResolver)],
+      remove: []
+    },
+    after: {
+      all: []
+    },
+    error: {
+      all: []
+    }
+  })
+}
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    events: EventService
+  }
+}
