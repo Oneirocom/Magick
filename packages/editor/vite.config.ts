cadf3713e499354a482075741b3946b1f99996fa
moon <shawmakesmagic@gmail.com>
2023-02-16 17:17:14 -0800

    client-core -> editor

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -10,6 +10,7 @@ import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import mdPlugin, { Mode } from 'vite-plugin-markdown'
 
 export default defineConfig({
+  cacheDir: '../../node_modules/.vite/editor',
   assetsInclude: ['**/*.vrm', '**/*.svg'],
   resolve: {
     alias: {
@@ -19,20 +20,16 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
-    mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.REACT] }),
     dts({
       tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
       // Faster builds by skipping tests. Set this to false to enable type checking.
       skipDiagnostics: true,
     }),
-
+    react(),
     viteTsConfigPaths({
       root: '../../',
     }),
+    mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.REACT] })
   ],
 
   // Uncomment this if you are using workers.
@@ -62,16 +59,16 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'client-core',
+      name: 'editor',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: [],
       plugins: [rollupNodePolyFill()],
+      // External packages that should not be bundled into your library.
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
 })
