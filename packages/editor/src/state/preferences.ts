97c75a5ce13e6e9e572f209aeba237996c6f7e92
moon <shawmakesmagic@gmail.com>
2023-02-19 07:18:00 -0800

    Make spell autosave faster, hide playtest trigger on input since it doesnt work atm

--- packages/editor/src/state/preferences.ts
+++ packages/editor/src/state/preferences.ts
@@ -2,29 +2,23 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export interface Preference {
   autoSave: boolean
-  doNotShowUnlockWarning: boolean
 }
 
 export const preferenceSlice = createSlice({
   name: 'preferences',
   initialState: {
     autoSave: true,
-    doNotShowUnlockWarning: false,
   },
   reducers: {
     toggleAutoSave: state => {
       const newState = state.autoSave === true ? false : true
       state.autoSave = newState
     },
-    toggleDoNotShowUnlockWarning: state => {
-      const newState = state.doNotShowUnlockWarning === true ? false : true
-      state.doNotShowUnlockWarning = newState
-    },
   },
 })
 
 // actions
-export const { toggleAutoSave, toggleDoNotShowUnlockWarning } =
+export const { toggleAutoSave } =
   preferenceSlice.actions
 
 export default preferenceSlice.reducer
