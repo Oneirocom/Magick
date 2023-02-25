e7db953e4732bb019b456ef8bfbf4aae480ded2c
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-02-25 01:28:11 +0000

    add plugin imports

--- apps/client/src/plugins.ts
+++ apps/client/src/plugins.ts
@@ -1,4 +1,6 @@
 const plugins = {}
 import {default as plugin0} from '@magickml/plugin-discord';
+import {default as plugin1} from '@magickml/plugin-ethereum';
 plugins['@magickml/plugin-discord'] = plugin0;
+plugins['@magickml/plugin-ethereum'] = plugin1;
 export default plugins;
