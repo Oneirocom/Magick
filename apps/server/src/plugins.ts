db2ec56c84bd13ac36ab5a0a54b44bead9d67115
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-02-25 03:55:27 +0000

    initial eth plugin code

--- apps/server/src/plugins.ts
+++ apps/server/src/plugins.ts
@@ -1,6 +1,6 @@
 const plugins = {}
 import {default as plugin0} from '@magickml/plugin-discord';
-import {default as plugin1} from '@magickml/plugin-ethereum';
 plugins['@magickml/plugin-discord'] = plugin0;
+import {default as plugin1} from '@magickml/plugin-ethereum';
 plugins['@magickml/plugin-ethereum'] = plugin1;
 export default plugins;
---
+++ packages/plugin-ethereum/src/components/utils.tsx
@@ -0,0 +1,32 @@
+//@ts-ignore
+import React from 'react'
+
+export const KeyInput = ({ value, setValue, secret }: { value: string, setValue: any, secret: boolean }) => {
+
+    const addKey = (str: string) => {
+      // discount random key presses, could def have better sense checking
+      // ethereum addresses are 42 chars
+      if (str.length > 31) {
+        setValue(str)
+      }
+    }
+  
+    const removeKey = () => {
+      setValue('')
+    }
+  
+    const obfuscateKey = (str: string) => {
+      const first = str.substring(0, 6)
+      const last = str.substring(str.length - 4, str.length)
+      return `${first}....${last}`
+    }
+  
+    return value ? (
+      <>
+        <p>{secret ? obfuscateKey(value) : value}</p>
+        <button onClick={removeKey}>remove</button>
+      </>
+    ) : (
+      <input type={secret ? "password" : "input"} defaultValue={value} onChange={e => { addKey(e.target.value) }} />
+    )
+  }
