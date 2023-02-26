bdcef1df317af463c93b061c61dd1bef7d46b04b
moon <shawmakesmagic@gmail.com>
2023-02-25 23:32:56 -0800

    ignore plugins

--- apps/server/src/plugins.ts
+++
@@ -1,8 +0,0 @@
-const plugins = {}
-import {default as plugin0} from '@magickml/plugin-discord';
-plugins['@magickml/plugin-discord'] = plugin0;
-import {default as plugin1} from '@magickml/plugin-rest';
-plugins['@magickml/plugin-rest'] = plugin1;
-import {default as plugin2} from '@magickml/plugin-loop';
-plugins['@magickml/plugin-loop'] = plugin2;
-export default plugins;
