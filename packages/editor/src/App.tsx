e8210f8577efde6e727b5ddc6e1f8c03c2bafef7
codingsh <codingsh@pm.me>
2023-02-27 01:32:22 +0000

    chore(editor): add gitignore

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import './App.css'
 
-import { Routes, Route } from 'react-router-dom'
-import { useRouter } from 'next/router' // importando o useRouter do next/router
+import { useRoutes, RouteObject } from 'react-router-dom';
+import { useRouter } from 'next/router'; 
 
 import MagickPageLayout from './components/MagickPageLayout/MagickPageLayout'
 import FineTuneManagerLayout from './screens/FineTuneManager/PageLayout'
@@ -21,75 +21,54 @@ import NewFineTune from './screens/FineTuneManager/screens/NewFineTune'
 import CompletionDetails from './screens/FineTuneManager/completions/CompletionDetails'
 import RequestWindow from './workspaces/spells/windows/RequestWindow'
 
-const singleUserMode = import.meta.env.VITE_APP_SINGLE_USER_MODE || true
+const singleUserMode = import.meta.env.VITE_APP_SINGLE_USER_MODE || true;
 
-let router: any;
+const fineTuneRoutes: RouteObject[] = [
+  {
+    path: '/fineTuneManager',
+    element: <FineTuneManager />,
+    children: [
+      { path: '/completions', element: <Completions /> },
+      { path: '/fine-tunes/new', element: <NewFineTune /> },
+      { path: '/fine-tune/:fineTuneId', element: <CompletionDetails /> },
+    ],
+  },
+];
 
-try {
-  require.resolve('next/router');
-  router = require('next/router');
-} catch (e) {
-  // next/router not found, try react-router-dom
-}
+const magickRoutes: RouteObject[] = [
+  { path: '/home/*', element: <HomeScreen /> },
+  { path: '/', element: <Magick /> },
+  { path: '/magick/*', element: <Magick /> },
+  { path: '/magick/:spellName', element: <Magick /> },
+  { path: '/contract/:chain/:address/:function', element: <Contract /> },
+  { path: '/*', element: <Magick /> },
+];
 
-if (!router) {
-  try {
-    require.resolve('react-router-dom');
-    router = require('react-router-dom');
-  } catch (e) {    
-    console.error('Neither next/router nor react-router-dom was found in the user\'s dependencies');
-    return null;
-  }
-}
-
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/events', element: <EventWindow /> },
+      { path: '/requests', element: <RequestWindow /> },
+      { path: '/agents', element: <AgentManagerWindow /> },
+      ...(singleUserMode ? [{ path: '/settings', element: <SettingsWindow /> }] : []),
+      { path: '/fineTuneManager', children: fineTuneRoutes },
+      { path: '/', children: magickRoutes },
+    ],
+  },
+];
 
 function App() {
-  const router = useRouter(); 
-  const routerType = router ? "next" : "react-router-dom";
+  const routerType = typeof useRouter === 'function' ? 'next' : 'react-router-dom';
+  const routing = useRoutes(routes);
 
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/events" element={<EventWindow />} />
-        <Route path="/requests" element={<RequestWindow />} />
-        <Route path="/agents" element={<AgentManagerWindow />} />
-        {singleUserMode && <Route path="/settings" element={<SettingsWindow />} />}
-        <Route element={<FineTuneManagerLayout />}>
-          <Route path="/fineTuneManager" element={<FineTuneManager />} />
-          <Route
-            path="/fineTuneManager/completions"
-            element={<Completions />}
-          />
-          <Route
-            path="/fineTuneManager/fine-tunes/new"
-            element={<NewFineTune />}
-          />
-          <Route
-            path="/fineTuneManager/fine-tune/:fineTuneId"
-            element={<CompletionDetails />}
-          />
-        </Route>
-        <Route element={<MagickPageLayout />}>
-          {/* todo search corpus component */}
-          <Route path="/home/*" element={<HomeScreen />} />
-
-          <Route path="/" element={<Magick />} />
-          <Route path="/magick/*" element={<Magick />} />
-          <Route path="/magick/:spellName" element={<Magick />} />
-          <Route
-            path="/contract/:chain/:address/:function"
-            element={<Contract />}
-          />
-          {/* Condicional para escolher o sistema de roteamento */}
-          {routerType === "next" ? (
-            <Route path="/*" element={<Magick />} />
-          ) : (
-            <Route path="/*" element={<Magick />} />
-          )}
-        </Route>
-      </Route>
-    </Routes>
-  )
+    <>
+      {routerType === 'next' && <Head>...</Head>}
+      {routing}
+    </>
+  );
 }
 
-export default App
+export default App;
