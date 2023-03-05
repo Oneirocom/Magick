52efde5cb8d04c85d3651060d78e02a1193848d2
Q <stackedq@gmail.com>
2023-03-05 20:17:38 +0330

    test: setting up jest and test harness

---
+++ packages/engine/test/utils/runSpell.ts
@@ -0,0 +1,36 @@
+import { buildMagickInterface } from './buildMagickInterface'
+import { GraphData, Spell } from '../../src/lib/types'
+import { SpellRunner } from '../../src/lib/spellManager'
+
+export type RunSpellArgs = {
+  id: string
+  inputs: Record<string, unknown>
+  inputFormatter?: (graph: GraphData) => Record<string, unknown>
+  projectId: string
+  spell: Spell
+}
+
+export const runSpell = async ({ spell, inputs, inputFormatter, projectId }: RunSpellArgs) => {
+  const graph = spell.graph as unknown as GraphData
+  const magickInterface = buildMagickInterface() as any
+
+  const formattedInputs = inputFormatter ? inputFormatter(graph) : inputs
+
+  const spellToRun = {
+    // TOTAL HACK HERE
+    ...spell
+  }
+
+  // Initialize the spell runner
+  const spellRunner = new SpellRunner({ magickInterface })
+
+  // Load the spell in to the spell runner
+  await spellRunner.loadSpell(spellToRun as unknown as Spell)
+
+  // Get the outputs from running the spell
+  const outputs = await spellRunner.runComponent({
+    inputs: formattedInputs
+  })
+
+  return { outputs, name: spell.name }
+}
