759f970d94b3c61153ba166ec8092a61f02bc650
Michael <m.sharpe@project89.org>
2023-03-05 21:08:19 -0800

    Add hook s for spell manager stuff

---
+++ packages/server-core/src/hooks/spellmanagerHooks.ts
@@ -0,0 +1,53 @@
+import { otJson0 } from 'ot-json0';
+import { Spell } from "../client"
+import { HookContext } from "../declarations"
+import { getSpell } from "../helpers/getSpell"
+
+
+export const checkForSpellInManager = async (context: HookContext) => {
+  const { app, params, id, data } = context
+  const { user } = params
+  const { projectId } = data
+
+  if (!user) throw new Error('No user is present in service')
+  // Here we get the users spellManagerApp
+  const spellManager = app.userSpellManagers.get(user.id.toString())
+
+  if (!spellManager) throw new Error('No spell manager created for user!')
+  const decodedId = (id as string).length > 36 ? (id as string).slice(0, 36) : id as string
+
+  if (!spellManager) throw new Error('No spell manager found for user!')
+
+  if (!spellManager.hasSpellRunner(decodedId)) {
+    const spell = await getSpell({ app, id: decodedId, projectId })
+    await spellManager.load(spell as Spell)
+  }
+}
+
+// When the spell updates on the server, we need to update the spell in the spell manager
+export const updateSpellInManager = async (context: HookContext) => {
+  const { app, params, id, data } = context
+  const { user } = params
+  const { spellUpdate, diff } = data
+
+  // Here we get the users spellManagerApp
+  const spellManager = app.userSpellManagers.get(user.id.toString())
+  const decodedId = (id as string).length > 36 ? (id as string).slice(0, 36) : id as string
+
+  const spellRunner = spellManager.getSpellRunner(decodedId)
+  if (!spellRunner) throw new Error('No spell runner found!')
+
+  if (diff) {
+    const spell = spellRunner.currentSpell
+    const updatedSpell = otJson0.type.apply(spell, diff)
+    spellManager.load(updatedSpell, true)
+    return updatedSpell
+  }
+
+  if (spellUpdate) {
+    spellManager.load(spellUpdate, true)
+    return spellUpdate
+  }
+}
+
+
