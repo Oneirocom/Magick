426d1e438afaa4ecc484d222c8f76320a34880c0
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-03-06 04:32:34 +0000

    plugin(ethereum): add worker for deploycontract node

--- packages/plugin-ethereum/shared/src/nodes/DeployContract.ts
+++ packages/plugin-ethereum/shared/src/nodes/DeployContract.ts
@@ -1,6 +1,7 @@
 import { isEmpty } from 'lodash'
 import Rete from 'rete'
 import { v4 as uuidv4 } from 'uuid'
+import * as ethers from 'ethers'
 
 import {
   anySocket,
@@ -114,11 +115,56 @@ export class DeployContract extends MagickComponent<InputReturn> {
   // @ts-ignore
   async worker(
     node: NodeData,
-    _inputs: MagickWorkerInputs,
+    inputs: MagickWorkerInputs,
     outputs: MagickWorkerOutputs,
     { data }: { data: string | undefined }
   ) {
     this._task.closed = ['trigger']
+
+    const defaultNetwork = {
+      name: 'maticmaticmum',
+      chainId: 80001,
+      _defaultProvider: (providers) => new providers.JsonRpcProvider('https://rpc.ankr.com/polygon_mumbai')
+    };
+
+    let chainId = defaultNetwork.chainId
+    if (node.data?.chain_id) {
+      const parsed = parseInt(node.data?.chain_id as string);
+      if (!isNaN(parsed)) {
+        chainId = parsed
+      }
+    }
+    if (inputs['chain_id']) {
+      const parsed = parseInt(inputs['chain_id'][0] as string);
+      if (!isNaN(parsed)) {
+        chainId = parsed
+      }
+    }
+
+    let provider = ethers.getDefaultProvider(defaultNetwork)
+    if (node.data?.rpc_http) {
+      provider = new ethers.providers.JsonRpcProvider(node.data?.rpc_http as string, chainId)
+    }
+    if (inputs['rpc_http']) {
+      provider = new ethers.providers.JsonRpcProvider(inputs['rpc_http'][0] as string, chainId)
+    }
+
+    const privateKey = (inputs['privatekey'] && inputs['privatekey'][0]) as string
+    const contractAbi = (inputs['abi'] && inputs['abi'][0]) as string
+    const contractByteCode = (inputs['bytecode'] && inputs['bytecode'][0]) as string
+
+    // TODO: check if privateKey is valid
+    const wallet = new ethers.Wallet(privateKey, provider)
+    const factory = new ethers.ContractFactory(contractAbi, contractByteCode, wallet);
+
+    const balance = await provider.getBalance(wallet.address)
+    const balanceInEth = ethers.utils.formatEther(balance).toString()
+
+    const contract = await factory.deploy();
+    await contract.deployTransaction.wait()
+
+    const balanceAfter = await provider.getBalance(wallet.address)
+    const balanceAfterInEth = ethers.utils.formatEther(balanceAfter).toString()
 
     // handle data subscription.  If there is data, this is from playtest
     if (data && !isEmpty(data)) {
@@ -126,6 +172,10 @@ export class DeployContract extends MagickComponent<InputReturn> {
 
       return {
         output: data,
+        balance: balanceInEth,
+        balance_after: balanceAfterInEth,
+        tx: contract.deployTransaction.hash,
+        contract: contract.address
       }
     }
   }
