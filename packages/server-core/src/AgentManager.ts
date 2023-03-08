5e98545bdb3c866c63cac1fcfb2b2494c36f9ef1
shawticus <shawmakesmagic@gmail.com>
2023-03-07 16:49:55 -0800

    Agent starts and stops

--- packages/server-core/src/AgentManager.ts
+++ packages/server-core/src/AgentManager.ts
@@ -3,10 +3,14 @@ import { app } from './app'
 
 export class AgentManager {
   agents: { [id: string]: any } = {}
-  lastAgents: any
+  currentAgents: any = []
   newAgents: any
   addHandlers: any = []
   removeHandlers: any = []
+
+  getAgent({ agent }) {
+    return this.agents[agent.id]
+  }
 
   constructor() {
     setInterval(async () => {
@@ -14,11 +18,11 @@ export class AgentManager {
     }, 1000)
   }
 
-  registerAddAgentHandler(handler: Function) {
+  registerAddAgentHandler(handler) {
     this.addHandlers.push(handler)
   }
 
-  registerRemoveAgentHandler(handler: Function) {
+  registerRemoveAgentHandler(handler) {
     this.removeHandlers.push(handler)
   }
 
@@ -26,43 +30,46 @@ export class AgentManager {
   async updateAgents() {
     this.newAgents = (await app.service('agents').find()).data
     if (
-      JSON.stringify(this.newAgents) === JSON.stringify(this.lastAgents ?? [])
+      JSON.stringify(this.newAgents) === JSON.stringify(this.currentAgents)
     )
       return // They are the same
-    console.log('Updating agents')
 
-    // If an entry exists in lastAgents but not in newAgents, it has been deleted
-    for (const i in this.lastAgents ?? []) {
+    // If an entry exists in currentAgents but not in newAgents, it has been deleted
+    for (const i in this.currentAgents ?? []) {
       if (
-        this.newAgents.filter((x: any) => x.id === this.lastAgents[i].id)[0] ===
-        undefined
+        this.newAgents.filter((x: any) => x.id === this.currentAgents[i].id)[0] === undefined
       ) {
-        const id = this.lastAgents[i].id
+        const id = this.currentAgents[i].id
         if(this.agents[id] !== undefined){
           const agent = this.agents[id]
-          this.addHandlers.forEach((handler: Function) => handler(agent))
+          this.removeHandlers.forEach((handler) => handler({agent}))
           await this.agents[id]?.onDestroy()
           this.agents[id] = null
           delete this.agents[id]
+          console.log('deleted agent', id)
         }
       }
     }
 
-    for (const i in this.newAgents) {
-      if (
-        this.lastAgents?.filter(
-          (x: any) => x.id === this.lastAgents[i].id
-        )[0] === undefined
-      ) {
+    this.newAgents.forEach(async (agent: any) => {
+      if(!agent.enabled) return console.log('Agent is disabled', agent.id)      
+      if(!agent.rootSpell) return console.log('Agent has no root spell', agent.id)
+      
+      const oldAgent = this.agents[agent.id]
+      if(!oldAgent || !oldAgent.enabled || JSON.stringify(oldAgent) !== JSON.stringify(agent)){
         const data = {
-          ...this.newAgents[i],
+          ...agent,
           dirty: false,
-          updatedAt: this.newAgents[i].updatedAt,
+          updatedAt: new Date().toISOString(),
         }
         this.agents[data.id] = new Agent(data, this)
-      }
-    }
+        this.addHandlers.forEach((handler) => handler({agent}))
 
-    this.lastAgents = this.newAgents
+        // tell the feathers 'agents' service to update the agent and set dirty to false
+        await app.service('agents').patch(data.id, { dirty: false })
+      }
+    })
+
+    this.currentAgents = this.newAgents
   }
 }
