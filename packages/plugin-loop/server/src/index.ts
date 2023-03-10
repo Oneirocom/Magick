4d5bf09fa7b4c9e37c001b95c4903470631cb683
moon <shawmakesmagic@gmail.com>
2023-03-10 01:18:29 -0800

    Loop not erroring

--- packages/plugin-loop/server/src/index.ts
+++ packages/plugin-loop/server/src/index.ts
@@ -8,21 +8,25 @@ type StartLoopArgs = {
 
 class LoopManager {
   agentManager: any
-  constructor(agentManager) {
+  loopHandlers = new Map<string, any>();
+  spellRunner: any
+  constructor(agentManager, spellRunner) {
+    this.spellRunner = spellRunner
     this.agentManager = agentManager
-    this.agentManager.registerAddAgentHandler(({agent}) => this.addAgent({ agent }))
+    this.agentManager.registerAddAgentHandler(({agent, agentData}) => this.addAgent({ agent, agentData }))
     this.agentManager.registerRemoveAgentHandler(({agent}) => this.removeAgent({ agent }))
   } 
 
-  addAgent({ agent }) {
-    if(!agent.data) return console.log("No data for this agent", agent.id)
-    if(!agent.data.loop_enabled) return console.log("Loop is not enabled for this agent")
-    const loopInterval = parseInt(agent.data.loop_interval) * 1000
+  addAgent({ agent, agentData }) {
+    console.log('addAgent', agent)
+    if(!agentData) return console.log("No data for this agent", agent.id)
+    if(!agentData.data.loop_enabled) return console.log("Loop is not enabled for this agent")
+    const loopInterval = parseInt(agentData.data.loop_interval) * 1000
     if (!loopInterval) {
       return console.error('Loop Interval must be a number greater than 0')
     }
-    agent.loopHandler = setInterval(async () => {
-      const resp = await agent.spellRunner({
+    const loopHandler = setInterval(async () => {
+      const resp = await this.spellRunner({
         content: 'loop',
         sender: 'loop',
         observer: agent.name,
@@ -33,13 +37,15 @@ class LoopManager {
         entities: [],
       })
     }, loopInterval)
+    this.loopHandlers[agent.id] = loopHandler
     console.log('Added agent to loop', agent.id)
   }
 
   removeAgent({ agent }) {
     const _agent = this.agentManager.getAgent({ agent })
-    if(!_agent || !_agent.loopHandler) return
-    clearInterval(_agent.loopHandler)
+    if(!_agent || !this.loopHandlers.get(agent.id)) return
+    clearInterval(this.loopHandlers.get(agent.id))
+    this.loopHandlers.delete(agent.id)
   }
 }
 
@@ -47,14 +53,12 @@ class LoopManager {
 function getAgentMethods() {
   let loopManager: LoopManager | null = null;
   return {
-    start: async ({ spellRunner, agent, agentManager }: StartLoopArgs) => {
-      if(!loopManager) loopManager = new LoopManager(agentManager)
-      agent.data.spellRunner = spellRunner
-      loopManager.addAgent({ agent })
+    start: async ({ spellRunner, agentManager }: StartLoopArgs) => {
+      if(!loopManager) loopManager = new LoopManager(agentManager, spellRunner)
     },
-    stop: async ({ agent }) => {
+    stop: async () => {
       if(!loopManager) return console.error('Loop Manager not initialized')
-      loopManager.removeAgent({ agent })
+      return console.log('Stopping loop manager')
     },
   }
 }
