7e15e2f13e91779d90c2628044d09844d3307cb2
Michael <m.sharpe@project89.org>
2023-03-09 20:50:04 -0800

    Improve spell manager hooks

--- packages/server-core/src/hooks/spellmanagerHooks.ts
+++ packages/server-core/src/hooks/spellmanagerHooks.ts
@@ -1,21 +1,24 @@
-import { otJson0 } from 'ot-json0';
-import { Spell } from "../client"
-import { HookContext } from "../declarations"
-import { getSpell } from "../helpers/getSpell"
+import { SpellManager } from '@magickml/engine'
+import { otJson0 } from 'ot-json0'
+import { Spell } from '../client'
+import { HookContext } from '../declarations'
+import { getSpell } from '../helpers/getSpell'
 
 export const checkForSpellInManager = async (context: HookContext) => {
   const { app, params, data } = context
   const { user } = params
-  const { projectId, id } = data
+  const { id } = data
+
+  const projectId = data.projectId || params.query.projectId
 
   if (!user) throw new Error('No user is present in service')
   // Here we get the users spellManagerApp
+  console.log('Gettig spell manager for user', user.id.toString())
   const spellManager = app.userSpellManagers.get(user.id.toString())
 
   if (!spellManager) throw new Error('No spell manager created for user!')
-  const decodedId = (id as string).length > 36 ? (id as string).slice(0, 36) : id as string
-
-  if (!spellManager) throw new Error('No spell manager found for user!')
+  const decodedId =
+    (id as string).length > 36 ? (id as string).slice(0, 36) : (id as string)
 
   if (!spellManager.hasSpellRunner(decodedId)) {
     const spell = await getSpell({ app, id: decodedId, projectId })
@@ -31,7 +34,8 @@ export const updateSpellInManager = async (context: HookContext) => {
 
   // Here we get the users spellManagerApp
   const spellManager = app.userSpellManagers.get(user.id.toString())
-  const decodedId = (id as string).length > 36 ? (id as string).slice(0, 36) : id as string
+  const decodedId =
+    (id as string).length > 36 ? (id as string).slice(0, 36) : (id as string)
 
   const spellRunner = spellManager.getSpellRunner(decodedId)
   if (!spellRunner) throw new Error('No spell runner found!')
@@ -48,5 +52,3 @@ export const updateSpellInManager = async (context: HookContext) => {
     return spellUpdate
   }
 }
-
-
