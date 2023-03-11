a66e705b6a119e5953813291768943b2be980d8c
moon <shawmakesmagic@gmail.com>
2023-03-11 15:24:57 -0800

    Default to ignore auth flow

--- packages/editor/src/contexts/ConfigProvider.tsx
+++ packages/editor/src/contexts/ConfigProvider.tsx
@@ -1,4 +1,4 @@
-import { projectId } from '@magickml/engine'
+import { DEFAULT_PROJECT_ID } from '@magickml/engine'
 import { useContext, createContext, useState } from 'react'
 
 export type AppConfig = {
@@ -21,7 +21,8 @@ export const useConfig = () => useContext(Context)
 export const defaultConfig: AppConfig = {
   // add props here
   apiUrl: 'http://localhost:3030',
-  projectId,
+  projectId: DEFAULT_PROJECT_ID,
+  token: ''
 }
 
 // Might want to namespace these
