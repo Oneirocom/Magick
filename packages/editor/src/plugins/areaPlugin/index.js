5c31090cc35d6ff5035236003199091b22292c5d
BMO <bmo@skypress.io>
2023-03-12 22:28:41 -0500

    fix: snap enabled by default

--- packages/editor/src/plugins/areaPlugin/index.js
+++ packages/editor/src/plugins/areaPlugin/index.js
@@ -8,14 +8,14 @@ import { zoomAt } from './zoom-at'
 function install(editor, params) {
   console.log('installing', params)
   const background = params.background || false
-  const snap = params.snap || false
+  const snap = params.snap || true
   const scaleExtent = params.scaleExtent || false
   const translateExtent = params.translateExtent || false
 
   if (background) {
     this._background = new Background(editor, background)
   }
-  
+
   if (scaleExtent || translateExtent) {
     this._restrictor = new Restrictor(editor, scaleExtent, translateExtent)
   }
