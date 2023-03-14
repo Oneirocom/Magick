a89d80ebd912c55ba2123f427f086b7c20b3fca2
0xpetra <franco.petraz@gmail.com>
2023-03-14 16:41:38 +0100

    Basic chat route and scaffolding

--- packages/editor/src/routes.tsx
+++ packages/editor/src/routes.tsx
@@ -14,7 +14,10 @@ import DocumentWindow from './windows/DocumentWindow'
 import EventWindow from './windows/EventWindow'
 import RequestWindow from './windows/RequestWindow'
 import SettingsWindow from './windows/settings/SettingsWindow'
-import ProjectWindow from './windows/ProjectWindow'
+import Chat from './screens/Chat/Chat'
+
+import 'flexlayout-react/style/dark.css'
+import './design-globals/design-globals.css'
 
 const RenderComp = props => {
   return <props.element props={props} />
@@ -23,41 +26,42 @@ const RenderComp = props => {
 const MyRoutes = () => (
   <Routes>
     <Route element={<MainLayout />}>
-    {pluginManager.getGroupedClientRoutes().map(pluginRouteGroup => {
-      const ClientPageLayout = pluginRouteGroup.layout ?? MagickPageLayout
-      return (
-        <Route
-          key={pluginRouteGroup.routes[0].path}
-          element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <ClientPageLayout />
-            </Suspense>
-          }
-        >
-          {pluginRouteGroup.routes.map(route => {
-            return (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={<RenderComp element={route.component} />}
-              />
-            )
-          })}
-        </Route>
-      )
-    })}
-    <Route path="/documents" element={<DocumentWindow />} />
-    <Route path="/events" element={<EventWindow />} />
-    <Route path="/requests" element={<RequestWindow />} />
-    <Route path="/agents" element={<AgentManagerWindow />} />
-    <Route path="/settings" element={<SettingsWindow />} />
+      {pluginManager.getGroupedClientRoutes().map(pluginRouteGroup => {
+        const ClientPageLayout = pluginRouteGroup.layout ?? MagickPageLayout
+        return (
+          <Route
+            key={pluginRouteGroup.routes[0].path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ClientPageLayout />
+              </Suspense>
+            }
+          >
+            {pluginRouteGroup.routes.map(route => {
+              return (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={<RenderComp element={route.component} />}
+                />
+              )
+            })}
+          </Route>
+        )
+      })}
+      <Route path="/documents" element={<DocumentWindow />} />
+      <Route path="/events" element={<EventWindow />} />
+      <Route path="/requests" element={<RequestWindow />} />
+      <Route path="/agents" element={<AgentManagerWindow />} />
+      <Route path="/settings" element={<SettingsWindow />} />
 
-    <Route element={<MagickPageLayout />}>
-      <Route path="/home/*" element={<HomeScreen />} />
-      <Route path="/" element={<Magick />} />
-      <Route path="/magick/*" element={<Magick />} />
-      <Route path="/magick/:URI" element={<Magick />} />
-    </Route>
+      <Route element={<MagickPageLayout />}>
+        <Route path="/home/*" element={<HomeScreen />} />
+        <Route path="/" element={<Magick />} />
+        <Route path="/magick/*" element={<Magick />} />
+        <Route path="/magick/:URI" element={<Magick />} />
+        <Route path="/chat/*" element={<Chat />} />
+      </Route>
     </Route>
   </Routes>
 )
---
+++ packages/editor/src/screens/Chat/Chat.tsx
@@ -0,0 +1,174 @@
+import { RootState } from '../../state/store'
+import {
+  activeTabSelector,
+  selectAllTabs,
+  openTab,
+  closeTab,
+} from '../../state/tabs'
+import { useEffect } from 'react'
+import { useConfig } from '../../contexts/ConfigProvider'
+import { useHotkeys } from 'react-hotkeys-hook'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSnackbar } from 'notistack'
+import { usePubSub } from '../../contexts/PubSubProvider'
+import { LoadingScreen, TabLayout } from '@magickml/client-core'
+import ChatWindow from '../../chatwindow'
+import TabBar from '../../components/TabBar/TabBar'
+import axios from 'axios'
+
+
+const Chat = ({ empty = false }) => {
+  const config = useConfig()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { enqueueSnackbar } = useSnackbar()
+  const tabs = useSelector((state: RootState) => selectAllTabs(state.tabs))
+  const activeTab = useSelector(activeTabSelector)
+  const pubSub = usePubSub()
+  const { URI } = useParams()
+  const { events, publish, subscribe } = pubSub
+
+  // Handle open tab events
+  useEffect(() => {
+    return subscribe(events.OPEN_TAB, (_event, tabData) => {
+      dispatch(openTab(tabData))
+    }) as () => void
+  })
+
+  useEffect(() => {
+    if (!tabs) return
+    //Check If exists
+    if (activeTab) {
+      //If No spell Exist on Project id
+      axios.get(config.apiUrl + '/spells', {
+        params: {
+          projectId: config.projectId
+        }
+      }).then(function (response) {
+        if (response.data.total == 0) {
+          navigate('/home')
+        }
+      })
+      //Check on Page load if the spell exsists
+      //Redux Query gives undefined
+      axios.get(config.apiUrl + '/spells', {
+        params: {
+          id: activeTab.id
+        }
+      })
+        .then(function (response) {
+          if (response.data.total == 0) {
+            dispatch(closeTab(activeTab.id))
+
+            const temp_tabs = tabs.filter((item) => item.id !== activeTab.id)
+            console.log(temp_tabs)
+            if (temp_tabs.length === 0) navigate('/home')
+            const idx = Math.floor(Math.random() * temp_tabs.length)
+            if (temp_tabs.length > 0) navigate(`/chat/${temp_tabs[idx].URI}`)
+            dispatch(
+              openTab({
+                name: temp_tabs[idx].URI,
+                openNew: false,
+                type: 'spell',
+              })
+            )
+            enqueueSnackbar('You are trying to access spell, that no longer exists ', {
+              variant: 'error',
+            })
+
+          }
+        })
+    }
+    // If there are still tabs, grab one at random to open to for now.
+    // We should do better at this.  Probably with some kind of tab ordering.
+    // Could fit in well with drag and drop for tabs
+    if (tabs.length > 0 && !activeTab) {
+      navigate(`/chat/${tabs[Math.floor(Math.random() * tabs.length)].URI}`)
+    }
+
+    if (tabs.length === 0 && !activeTab && !URI) navigate('/home')
+  }, [tabs])
+
+  useEffect(() => {
+    if (!URI) return
+    // Return if navigating to the spell that is already active
+    if (activeTab && activeTab.URI === URI) return
+    // Close spell tab if it is exists
+    const spellNameTab = tabs.filter(tab => tab.URI === URI)
+    const isSpellNameTabPresent = spellNameTab.length
+    if (isSpellNameTabPresent) dispatch(closeTab(spellNameTab[0].id))
+    dispatch(
+      openTab({
+        name: URI,
+        openNew: false,
+        type: 'spell',
+      })
+    )
+  }, [URI])
+
+  useHotkeys(
+    'Control+z',
+    () => {
+      if (!pubSub || !activeTab) return
+
+      publish(events.$UNDO(activeTab.id))
+    },
+    [pubSub, activeTab]
+  )
+
+  useHotkeys(
+    'Control+Shift+z',
+    () => {
+      if (!pubSub || !activeTab) return
+      publish(events.$REDO(activeTab.id))
+    },
+    [pubSub, activeTab]
+  )
+
+  useHotkeys(
+    'Control+Delete',
+    () => {
+      if (!pubSub || !activeTab) return
+      publish(events.$DELETE(activeTab.id))
+    },
+    [pubSub, activeTab]
+  )
+
+  useHotkeys(
+    'Control+c',
+    () => {
+      if (!pubSub || !activeTab) return
+      publish(events.$MULTI_SELECT_COPY(activeTab.id))
+    },
+    [pubSub, activeTab]
+  )
+
+  useHotkeys(
+    'Control+v',
+    () => {
+      if (!pubSub || !activeTab) return
+      publish(events.$MULTI_SELECT_PASTE(activeTab.id))
+    },
+    [pubSub, activeTab]
+  )
+
+  if (!activeTab) return <LoadingScreen />
+
+
+
+  console.log("🚀 ~ file: Chat.tsx:164 ~ Chat ~ tabs:", tabs)
+  return (
+    <>
+      {/* TODO: Add Export chat (CSV in OpenAI's fine tunning format). Delete chat.  */}
+      <TabBar tabs={tabs} activeTab={activeTab} />
+      <TabLayout>
+        {!empty && (
+          <ChatWindow tabs={tabs} pubSub={pubSub} activeTab={activeTab} />
+        )}
+      </TabLayout>
+    </>
+  )
+}
+
+export default Chat
