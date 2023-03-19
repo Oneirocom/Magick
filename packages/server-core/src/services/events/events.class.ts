36dd8b8759970b9e4472dd9f75ac2c72f4fc6ad7
Thomage Anderson <thomageanderson@gmail.com>
2023-03-19 10:21:40 -0400

    Lint event service class and update unmodified lets to constants

--- packages/server-core/src/services/events/events.class.ts
+++ packages/server-core/src/services/events/events.class.ts
@@ -10,26 +10,23 @@ import { app } from '../../app'
 export type EventParams = KnexAdapterParams<EventQuery>
 
 // By default calls the standard Knex adapter service methods but can be customized with your own functionality.
-export class EventService<ServiceParams extends Params = EventParams> extends KnexService<
-  Event,
-  EventData,
-  ServiceParams,
-  EventPatch
-> {
+export class EventService<
+  ServiceParams extends Params = EventParams
+> extends KnexService<Event, EventData, ServiceParams, EventPatch> {
   async find(params?: ServiceParams) {
     const db = app.get('dbClient')
     if (params.query.embedding) {
-      const blob = atob(params.query.embedding);
-      const ary_buf = new ArrayBuffer(blob.length);
-      const dv = new DataView(ary_buf);
-      for (let i = 0; i < blob.length; i++) dv.setUint8(i, blob.charCodeAt(i));
-      const f32_ary = new Float32Array(ary_buf);
+      const blob = atob(params.query.embedding)
+      const ary_buf = new ArrayBuffer(blob.length)
+      const dv = new DataView(ary_buf)
+      for (let i = 0; i < blob.length; i++) dv.setUint8(i, blob.charCodeAt(i))
+      const f32_ary = new Float32Array(ary_buf)
       //const result = await findSimilarEventByEmbedding(db, "'[" + f32_ary.toString() + "]'")
-      let vectordb = app.get('vectordb')
-      const query = f32_ary as unknown as number[];
-      const k = 2;
-      const results = vectordb.search(query, k);
-      let result_s = await db('events').where('id', results[0]).first()
+      const vectordb = app.get('vectordb')
+      const query = f32_ary as unknown as number[]
+      const k = 2
+      const results = vectordb.search(query, k)
+      const result_s = await db('events').where('id', results[0]).first()
       console.log(result_s)
       if (result_s) {
         return result_s
@@ -37,14 +34,12 @@ export class EventService<ServiceParams extends Params = EventParams> extends Kn
     }
     return super.find(params)
   }
-
 }
-
 
 export const getOptions = (app: Application): KnexAdapterOptions => {
   return {
     paginate: app.get('paginate'),
     Model: app.get('dbClient'),
-    name: 'events'
+    name: 'events',
   }
 }
