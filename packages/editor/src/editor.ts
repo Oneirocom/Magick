3fda8816c439d34d524ebfc5758946921d8f30f1
Pietro <pietro@palmesi.com>
2023-03-19 15:40:19 -0100

    Finish usage of Function in types.ts

--- packages/editor/src/editor.ts
+++ packages/editor/src/editor.ts
@@ -4,7 +4,7 @@ import { Plugin } from 'rete/types/core/plugin'
 import ContextMenuPlugin from './plugins/contextMenu'
 import { Data } from 'rete/types/core/data'
 import CommentPlugin from './plugins/commentPlugin'
-import { SelectionPlugin, SubspellUpdatedCallback } from '@magickml/engine'
+import { PubSubCallback, SelectionPlugin } from '@magickml/engine'
 import ReactRenderPlugin, {
   ReactRenderPluginOptions,
 } from './plugins/reactRenderPlugin'
@@ -200,7 +200,7 @@ export const initEditor = function ({
   // ██╔═══╝ ██║   ██║██╔══██╗██║     ██║██║
   // ██║     ╚██████╔╝██████╔╝███████╗██║╚██████╗
   // ╚═╝      ╚═════╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝
-  editor.onSpellUpdated = (spellName: string, callback: SubspellUpdatedCallback) => {
+  editor.onSpellUpdated = (spellName: string, callback: PubSubCallback) => {
     return magick.onSubspellUpdated(spellName, callback)
   }
 
