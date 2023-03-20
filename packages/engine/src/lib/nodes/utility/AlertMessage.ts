c98cdc2e077f6ca23392b6715d1f67c1d1b96782
Pietro <pietro@palmesi.com>
2023-03-20 10:58:53 -0100

    Fix TextInputControl typings

--- packages/engine/src/lib/nodes/utility/AlertMessage.ts
+++ packages/engine/src/lib/nodes/utility/AlertMessage.ts
@@ -5,6 +5,7 @@ import { TextInputControl } from '../../dataControls/TextInputControl'
 import { TaskOptions } from '../../plugins/taskPlugin/task'
 import { triggerSocket } from '../../sockets'
 import { MagickComponent } from '../../magick-component'
+import _ from 'lodash'
 
 const info = `When the alert component is triggered, it will fire an alert with the message in the input box.`
 
@@ -25,7 +26,8 @@ export class Alert extends MagickComponent<void> {
     // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const dataInput = new Rete.Input('trigger', 'Trigger', triggerSocket, true)
 
-    const value = node.data.text ? node.data.text : 'Input text here'
+    const value = node.data.text && typeof node.data.text === 'string' 
+      ? node.data.text : 'Input text here'
 
     const input = new TextInputControl({
       editor: this.editor,
@@ -41,6 +43,7 @@ export class Alert extends MagickComponent<void> {
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connected components
   worker(node: NodeData) {
-    alert(node.data.text)
+    const text = _.get(node, 'data.text', `node has no data: ${JSON.stringify(node)}`)
+    alert(text)
   }
 }
