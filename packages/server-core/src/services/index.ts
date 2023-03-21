2746cbc28fa791cc2a0545856bead45470176ef8
Thomage Anderson <thomageanderson@gmail.com>
2023-03-20 23:39:24 -0400

    Configure datasets service in server-core

--- packages/server-core/src/services/index.ts
+++ packages/server-core/src/services/index.ts
@@ -3,6 +3,7 @@ import { spell } from './spells/spells'
 import { agent } from './agents/agents'
 import { event } from './events/events'
 import { spellRunner } from './spell-runner/spell-runner'
+import { datasets } from './datasets/datasets'
 import { UploadService } from './Upload.class'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
@@ -13,10 +14,11 @@ export const services = async (app: Application) => {
   app.configure(agent)
   app.configure(event)
   app.configure(spellRunner)
+  app.configure(datasets)
   app.use('upload' as any, new UploadService())
   // TODO: handle this stupid race condition
-  await new Promise((resolve) => setTimeout(resolve, 1))
-  pluginManager.getServices().forEach((service) => {
+  await new Promise(resolve => setTimeout(resolve, 1))
+  pluginManager.getServices().forEach(service => {
     app.configure(service[1])
   })
 }
