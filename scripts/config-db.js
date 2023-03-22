b12515986c0eafc8a0bcc4e51f86b1236c149f49
moon <shawmakesmagic@gmail.com>
2023-03-22 11:59:45 -0700

    Remove installation of linux deps

--- scripts/config-db.js
+++ scripts/config-db.js
@@ -1,8 +1,6 @@
 const dotenv = require('dotenv-flow')
 dotenv.config('../')
 
-const os = require('os')
-
 function configDb() {
   const dbType = process.env.DATABASE_TYPE
   const nodeEnv = process.env.NODE_ENV
@@ -23,25 +21,4 @@ function configDb() {
   }
 }
 
-function installDependenciesOnLinux() {
-  // if we are on linux, we need to run sudo apt-get install -y libgomp1 libatlas-base-dev liblapack-dev 
-  // dont forget ARM64 M1 macs
-  const isLinux = os.platform() === 'linux'
-  if (isLinux) {
-    console.log('Installing dependencies for linux')
-    const { exec } = require('child_process')
-    exec('sudo apt-get install -y libgomp1 libatlas-base-dev liblapack-dev', (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        return
-      }
-
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`)
-      console.log(`stderr: ${stderr}`)
-    })
-  }
-}
-
 configDb()
-installDependenciesOnLinux()
