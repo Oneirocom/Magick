4abc6b8d09c01ee11eabbe67a1275aac41d0ead0
moon <shawmakesmagic@gmail.com>
2023-03-22 11:55:58 -0700

    Add projects

--- packages/editor/src/windows/ProjectWindow/index.tsx
+++ packages/editor/src/windows/ProjectWindow/index.tsx
@@ -1,16 +1,30 @@
+import { API_ROOT_URL } from '@magickml/engine'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import TreeItem from '@mui/lab/TreeItem'
 import TreeView from '@mui/lab/TreeView'
 import { Button, Grid } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { useConfig } from '../../contexts/ConfigProvider'
 
 const ProjectWindow = () => {
   const config = useConfig()
 
-  const agents = []
-  const spells = []
-  const documents = []
+  const [data, setData] = useState({agents: [], spells: [], documents: []})
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    if(loaded) return
+    setLoaded(true)
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        `${API_ROOT_URL}/projects?projectId=${config.projectId}`
+      )
+      setData(data)
+      console.log('data', data)
+    }
+    fetchData()
+  }, [])
 
   return (
     <div
@@ -27,27 +41,28 @@ const ProjectWindow = () => {
         <Grid item xs={12}>
           <Button variant="contained" style={{float:'right', margin: '1em', color: 'white', backgroundColor: 'purple'}}>Import</Button>
           <Button variant="contained" style={{float:'right', margin: '1em', color: 'white', backgroundColor: 'purple'}}>Export</Button>
-          <h1 style={{margin: '1em'}}>Project ID: {config.projectId}</h1>
+          <p style={{margin: '1em'}}>Project ID: {config.projectId}</p>
         </Grid>
         {/* show tree view of project - Agents, Spells, Documents */}
         <Grid item xs={6} style={{padding:'1em'}}>
           <TreeView
             defaultCollapseIcon={<ExpandMoreIcon />}
             defaultExpandIcon={<ChevronRightIcon />}
+            sx={{ flexGrow: 1, width: '100%' }}
           >
-            <TreeItem nodeId="1" label="Agents">
-              {agents.map((agent, index) => (
-                <TreeItem nodeId={index} label={agent.name} />
+            <TreeItem nodeId="0" label="Agents">
+              {data.agents.map((agent, index) => (
+                <TreeItem key={index} style={{width:'100%'}} nodeId={10+index.toString()} label={agent.name} />
               ))}
             </TreeItem>
-            <TreeItem nodeId="2" label="Spells">
-              {spells.map((spell, index) => (
-                <TreeItem nodeId={index} label={spell.name} />
-              ))}
+            <TreeItem nodeId="10" label="Spells">
+              {data.spells.map((spell, index) => (
+                <TreeItem key={index} style={{width:'100%'}} nodeId={20+index.toString()} label={spell.name} />
+                ))}
             </TreeItem>
-            <TreeItem nodeId="3" label="Documents">
-              {documents.map((document, index) => (
-                <TreeItem nodeId={index} label={document.name} />
+            <TreeItem nodeId="20" label="Documents">
+              {data.documents.map((document, index) => (
+                <TreeItem key={index} style={{width:'100%'}} nodeId={30+index.toString()} label={document.name} />
               ))}
             </TreeItem>
           </TreeView>
@@ -55,7 +70,7 @@ const ProjectWindow = () => {
         {/* show details of selected item */}
         <Grid item xs={6} style={{padding:'1em'}}>
           <h2>Details</h2>
-        </Grid>
+          </Grid>
       </Grid>
     </div>
   )
