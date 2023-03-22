79bc5cc5489f1e0e4e054ad5f8d0979b5036e7be
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2023-03-22 17:13:47 +0200

    improved helper modal for better UX

--- packages/client-core/src/components/Drawer/SetAPIKeys.tsx
+++ packages/client-core/src/components/Drawer/SetAPIKeys.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import InfoDialog from '../InfoDialog'
 
 const title = 'Set Your API Keys'
@@ -5,16 +6,28 @@ const body =
   'Please set your API keys in the editor settings to use external APIs. You will need to do this before you can use any AI features'
 
 export const SetAPIKeys = () => {
+  const [showWarning, setShowWarning] = useState(false)
+  useEffect(() => {
+    if (!window.localStorage.getItem('first_time_user')) {
+      window.localStorage.setItem(
+        'first_time_user',
+        JSON.stringify({ has_seen_api_key_warning: true })
+      )
+      setShowWarning(true)
+    }
+  }, [])
   return (
-    <InfoDialog
-      title={title}
-      body={body}
-      style={{
-        width: '100%',
-        height: '1px',
-        marginLeft: '89%',
-        marginTop: '-40%',
-      }}
-    />
+    showWarning && (
+      <InfoDialog
+        title={title}
+        body={body}
+        style={{
+          width: '100%',
+          height: '1px',
+          marginLeft: '89%',
+          marginTop: '-40%',
+        }}
+      />
+    )
   )
 }
