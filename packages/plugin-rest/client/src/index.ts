e211917389c010185545c9c1e620050cdbaa3513
moon <shawmakesmagic@gmail.com>
2023-03-23 01:43:55 -0700

    Fix spell templates for discord and api

--- packages/plugin-rest/client/src/index.ts
+++ packages/plugin-rest/client/src/index.ts
@@ -1,9 +1,12 @@
 import { ClientPlugin, eventSocket, anySocket } from '@magickml/engine'
 import { RestAgentWindow } from './components/rest.component'
 
+import RestSpellTemplate from './templates/spells/REST API.spell.json'
+
 const RestPlugin = new ClientPlugin({
   name: 'RestPlugin',
   agentComponents: [RestAgentWindow],
+  spellTemplates: [RestSpellTemplate],
   inputTypes: [
     { name: 'REST API (GET)', trigger: true, socket: eventSocket },
     { name: 'REST API (POST)', trigger: true, socket: eventSocket },
