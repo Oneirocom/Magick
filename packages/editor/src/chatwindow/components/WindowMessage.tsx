380e8be1dc24cf3173cf350a3f066454ba349f3e
0xpetra <franco.petraz@gmail.com>
2023-03-24 00:24:39 +0100

    Cleanup chatwindow

--- packages/editor/src/chatwindow/components/WindowMessage.tsx
+++
@@ -1,7 +0,0 @@
-import css from './windowMessage.module.css'
-
-const WindowMessage = ({ content = 'No component selected' }) => {
-  return <p className={css['message']}>{content}</p>
-}
-
-export default WindowMessage
