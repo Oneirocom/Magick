1803f544d69e3de81fdc8d7f7b58c1224d96a723
Shivaditya Shivganesh <53160903+sshivaditya2019@users.noreply.github.com>
2023-03-26 21:25:46 +0530

    Update app.ts

--- packages/server-core/src/app.ts
+++ packages/server-core/src/app.ts
@@ -40,7 +40,7 @@ if (!globalThis.Headers) globalThis.Headers = Headers
 if (!globalThis.Request) globalThis.Request = Request
 if (!globalThis.Response) globalThis.Response = Response
 
-const OPENAI_API_KEY = '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'
+const OPENAI_API_KEY = 'API_KEY_HERE'
 const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.9 })
 
 const app: Application = koa(feathers())
