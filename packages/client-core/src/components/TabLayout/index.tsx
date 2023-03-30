64210012f672a0c5e1ca3676b21d71a419bc2076
BMO <bmo@skypress.io>
2023-03-29 23:38:39 -0500

    chore: autofixed with linter

--- packages/client-core/src/components/TabLayout/index.tsx
+++ packages/client-core/src/components/TabLayout/index.tsx
@@ -3,10 +3,8 @@ import css from './TabLayout.module.css'
 
 export const TabLayout = ({ children }) => {
   return (
-    <>
-      <div className={css['view-container']}>
+    <div className={css['view-container']}>
         <div style={{ position: 'relative', height: '100%' }}>{children}</div>
       </div>
-    </>
   )
 }
