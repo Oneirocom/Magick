2ce8efe586f6d81c4735779044af1c44a116dbf5
moon <shawmakesmagic@gmail.com>
2023-03-30 15:00:01 -0700

    Add more autodoced files

--- packages/client-core/src/templates/index.ts
+++ packages/client-core/src/templates/index.ts
@@ -1,11 +1,33 @@
-import Starter from './spells/Starter.spell.json'
-import Blank from './spells/Blank.spell.json'
+// GENERATED 
+/**
+ * An array of spell templates in JSON format.
+ * 
+ * @typedef {Object[]} SpellTemplates
+ */
 
-const spellTemplates = [Starter, Blank]
+import Starter from './spells/Starter.spell.json';
+import Blank from './spells/Blank.spell.json';
 
-const projectTemplates = []
+/**
+ * An array of spell templates in JSON format.
+ * 
+ * @type {SpellTemplates}
+ */
+const spellTemplates = [Starter, Blank];
 
-export default {
+/**
+ * An empty array to hold project templates.
+ * 
+ * @type {any[]}
+ */
+const projectTemplates = [];
+
+/**
+ * An object that exports spellTemplates and projectTemplates.
+ */
+const templateData = {
   spells: spellTemplates,
   projects: projectTemplates,
-}
+};
+
+export default templateData;
