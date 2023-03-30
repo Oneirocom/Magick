b109e08884f09f7c405cb89371fd4cf098d26be6
moon <shawmakesmagic@gmail.com>
2023-03-30 15:19:00 -0700

    Add more components

--- packages/client-core/src/components/TabLayout/index.tsx
+++ packages/client-core/src/components/TabLayout/index.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react'
-import css from './TabLayout.module.css'
+// GENERATED 
+import * as React from 'react';
+import css from './TabLayout.module.css';
 
-export const TabLayout = ({ children }) => {
+/**
+ * TabLayout component that wraps children in a styled container.
+ *
+ * @param {React.ReactNode} children - The child components to be wrapped.
+ * @returns {JSX.Element} A styled container with the children.
+ */
+export const TabLayout: React.FC<{ children: React.ReactNode }> = ({ children }): JSX.Element => {
   return (
     <div className={css['view-container']}>
-        <div style={{ position: 'relative', height: '100%' }}>{children}</div>
-      </div>
-  )
-}
+      {/* Child components are wrapped in a relatively positioned div */}
+      <div style={{ position: 'relative', height: '100%' }}>{children}</div>
+    </div>
+  );
+};
