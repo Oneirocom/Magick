7733d831c4e01ce6c93caedef44efdd6f3f7844c
moon <shawmakesmagic@gmail.com>
2023-03-30 18:34:40 -0700

    Add search

--- packages/plugin-search/server/src/services/google-search/google-search.class.ts
+++ packages/plugin-search/server/src/services/google-search/google-search.class.ts
@@ -1,40 +1,54 @@
-import { Params } from '@feathersjs/feathers'
-import unirest from 'unirest'
-import cheerio from 'cheerio'
-import serp from 'serp'
+// GENERATED 
+import { Params } from '@feathersjs/feathers';
+import unirest from 'unirest';
+import cheerio from 'cheerio';
+import serp from 'serp';
 
+/**
+ * Interface for Google Search parameters.
+ */
 export interface GoogleSearchParams extends Params {
-  query: any
+  query: any;
 }
 
+/**
+ * Queries Google Search directly and returns the featured result or snippets.
+ *
+ * @param searchTerm - The search term to query.
+ * @returns A Promise that resolves to the search result string.
+ */
+async function searchDirectly(searchTerm: string): Promise<string> {
+  console.log('searching directly');
+  const query = searchTerm.split(' ').join('+');
+  const response = await unirest
+    .get(`https://www.google.com/search?q=${query}&gl=us&hl=en`)
+    .headers({
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
+    });
+  const $ = cheerio.load(response.body);
+
+  const el = $('.hgKElc');
+  const featuredResult = $(el).text();
+
+  if (featuredResult) return featuredResult;
+
+  const snippets = $('.g .VwiC3b');
+  let text = '';
+  snippets.each((i, el) => {
+    text += $(el).text();
+  });
+
+  return text;
+}
+
+/**
+ * Queries Google Search and returns structured results.
+ *
+ * @param searchTerm - The search term to query.
+ * @returns An object with a summary and links.
+ */
 export const queryGoogleSearch = async (searchTerm: string) => {
-  async function searchDirectly(searchTerm) {
-    console.log('searching directly')
-    const query = searchTerm.split(' ').join('+')
-    const response = await unirest
-      .get(`https://www.google.com/search?q=${query}&gl=us&hl=en`)
-      .headers({
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
-      })
-    const $ = cheerio.load(response.body)
-
-    const el = $('.hgKElc')
-    const featuredResult = $(el).text()
-
-    if (featuredResult) return featuredResult
-
-    const snippets = $('.g .VwiC3b ')
-      let text = ''
-    snippets.each((i, el) => {
-      text += $(el).text()
-    })
-
-
-    return text
-  }
-
-  // promise.all serpSearch and searchDirectly
   const [serpSearch, searchDirectResponse] = await Promise.all([
     serp.search({
       host: 'google.com',
@@ -46,41 +60,42 @@ export const queryGoogleSearch = async (searchTerm: string) => {
       num: 10,
     }),
     searchDirectly(searchTerm),
-  ])
+  ]);
 
-  // example serpSearch
-  /*
-  [{"url":"https://www.youtube.com/watch?v=mHONNcZbwDY","title":"Lionel Richie - Hello (Official Music Video) - YouTube"},{"url":"https://en.wikipedia.org/wiki/Hello#:~:text=Hello%20might%20be%20derived%20from,publications%20as%20early%20as%201803.","title":"Hello - Wikipedia"},{"url":"https://mollysmusic.org/blog/hard-songs-to-sing-hello-by-adele/#:~:text=The%20belt%20notes%20in%20%E2%80%9CHello,makes%20the%20belt%20more%20precarious.","title":"Hard Songs to Sing: Hello, by Adele - Molly's Music"}]
-  */
-  // for each item in serpSearch, format into a string, with the title and url
   const formattedSerpSearch = serpSearch?.map(item => {
-    return `${item.title} | URL: ${item.url}`
-  }) ?? []
+    return `${item.title} | URL: ${item.url}`;
+  }) ?? [];
 
-  const output = `${searchDirectResponse}\n${formattedSerpSearch.join('\n')}`
-  
-  console.log('output', output)
-  
+  const output = `${searchDirectResponse}\n${formattedSerpSearch.join('\n')}`;
+
+  console.log('output', output);
+
   return {
     summary: searchDirectResponse,
     links: formattedSerpSearch,
-  }
-}
+  };
+};
 
-// By default calls the standard Knex adapter service methods but can be customized with your own functionality.
-export class GoogleSearchService<
-  ServiceParams extends Params = GoogleSearchParams
-> {
+/**
+ * Google Search service for Feathers.
+ */
+export class GoogleSearchService<ServiceParams extends Params = GoogleSearchParams> {
+  /**
+   * Returns a Google Search result summary and links.
+   *
+   * @param params - Google Search parameters.
+   * @returns An object with a summary and links.
+   */
   async find(
     params: GoogleSearchParams
-  ): Promise<{ summary: string; links: string }> {
-    console.log('params', params)
-    const query = params.query.query as string
-    console.log('query', query)
+  ): Promise<{ summary: string; links: string[] }> {
+    console.log('params', params);
+    const query = params.query.query as string;
+    console.log('query', query);
 
-    const data = await queryGoogleSearch(query)
+    const data = await queryGoogleSearch(query);
 
-    const { summary, links } = data
-    return { summary, links }
+    const { summary, links } = data;
+    return { summary, links };
   }
 }
