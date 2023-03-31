59350e9f4cd174ac36c7ef0c37cfeb570ff803a5
moon <shawmakesmagic@gmail.com>
2023-03-30 18:36:09 -0700

    Comment agent windows

--- packages/editor/src/windows/agents/AgentWindow/AgentItem.tsx
+++ packages/editor/src/windows/agents/AgentWindow/AgentItem.tsx
@@ -1,7 +1,18 @@
+// GENERATED
 import { Icon, IconBtn } from '@magickml/client-core'
 import { Avatar, Typography } from '@mui/material'
 import styles from './index.module.scss'
 
+/**
+ * Represents an agent item component.
+ * @param {Object} props - The properties for this component.
+ * @param {string} props.keyId - The unique key for this agent item.
+ * @param {Object} props.agent - The agent object.
+ * @param {Function} props.onDelete - The function to be called when the delete button is clicked.
+ * @param {Function} props.onClick - The function to be called when the agent item is clicked.
+ * @param {Object} [props.style] - Inline CSS styles if needed.
+ * @returns {React.Element} The rendered agent item component.
+ */
 const AgentItem = ({ keyId, agent, onDelete, onClick, style }) => {
   return (
     <div
