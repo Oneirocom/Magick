86887960357e551df579ac0f2b8ca284973ddaf9
moon <shawmakesmagic@gmail.com>
2023-03-30 18:25:51 -0700

    Fix issue and add more docs

--- packages/editor/src/components/EventHandler.tsx
+++ packages/editor/src/components/EventHandler.tsx
@@ -1,67 +1,74 @@
-import { useEffect, useRef } from 'react'
-import { useSnackbar } from 'notistack'
-import { GraphData, SpellInterface } from '@magickml/engine'
+// GENERATED 
+import { useEffect, useRef } from 'react';
+import { useSnackbar } from 'notistack';
+import { GraphData, SpellInterface } from '@magickml/engine';
 
-import md5 from 'md5'
+import md5 from 'md5';
 
 import {
   useLazyGetSpellByIdQuery,
   useSaveSpellMutation,
-} from '../state/api/spells'
-import { useLayout } from '../contexts/LayoutProvider'
-import { useEditor } from '../contexts/EditorProvider'
-import { diff } from '../utils/json0'
-import { useFeathers } from '../contexts/FeathersProvider'
+} from '../state/api/spells';
+import { useLayout } from '../contexts/LayoutProvider';
+import { useEditor } from '../contexts/EditorProvider';
+import { diff } from '../utils/json0';
+import { useFeathers } from '../contexts/FeathersProvider';
 
-import { useConfig } from '../contexts/ConfigProvider'
+import { useConfig } from '../contexts/ConfigProvider';
 
+/**
+ * Event Handler component for handling various events in the editor
+ * @param {object} pubSub - PubSub object
+ * @param {object} tab - The current editor's tab object
+ * @returns - null, this is a functional component used for managing side effects
+ */
 const EventHandler = ({ pubSub, tab }) => {
-  const config = useConfig()
+  const config = useConfig();
 
   // only using this to handle events, so not rendering anything with it.
-  const { createOrFocus, windowTypes } = useLayout()
-  const { enqueueSnackbar } = useSnackbar()
+  const { createOrFocus, windowTypes } = useLayout();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const [saveSpellMutation] = useSaveSpellMutation()
+  const [saveSpellMutation] = useSaveSpellMutation();
   // TODO: is this a bug?
   const [getSpell, { data: spell }] = useLazyGetSpellByIdQuery({
     spellName: tab.name.split('--')[0],
     id: tab.id,
     projectId: config.projectId,
-  } as any)
-  // Spell ref because callbacks cant hold values from state without them
-  const spellRef = useRef<SpellInterface | null>(null)
+  } as any);
+  // Spell ref because callbacks can't hold values from state without them
+  const spellRef = useRef<SpellInterface | null>(null);
 
-  const FeathersContext = useFeathers()
-  const client = FeathersContext.client
+  const FeathersContext = useFeathers();
+  const client = FeathersContext.client;
 
   useEffect(() => {
     getSpell({
       spellName: tab.name,
       id: tab.id,
       projectId: config.projectId,
-    })
-    spellRef.current = spell?.data[0]
-  }, [config.projectId, getSpell, spell, tab.id, tab.name])
+    });
+    spellRef.current = spell?.data[0];
+  }, [config.projectId, getSpell, spell, tab.id, tab.name]);
 
   useEffect(() => {
-    if (!client.io || !tab.id || !enqueueSnackbar) return
+    if (!client.io || !tab.id || !enqueueSnackbar) return;
 
-    const listener = data => {
-      //publish($DEBUG_PRINT(tab.id), (data.error.message))
-      console.error('Error in spell execution')
+    const listener = (data) => {
+      // publish($DEBUG_PRINT(tab.id), (data.error.message))
+      console.error('Error in spell execution');
       enqueueSnackbar('Error Running the spell. Please Check the Console', {
         variant: 'error',
-      })
-    }
+      });
+    };
 
-    client.io.on(`${tab.id}-error`, listener)
+    client.io.on(`${tab.id}-error`, listener);
 
     // Handle cleaning up the subscription
     return () => {
-      client.io.off(`${tab.id}-error`, listener)
-    }
-  }, [client.io, tab.id, enqueueSnackbar])
+      client.io.off(`${tab.id}-error`, listener);
+    };
+  }, [client.io, tab.id, enqueueSnackbar]);
 
   const {
     serialize,
@@ -71,9 +78,9 @@ const EventHandler = ({ pubSub, tab }) => {
     del,
     multiSelectCopy,
     multiSelectPaste,
-  } = useEditor()
+  } = useEditor();
 
-  const { events, subscribe } = pubSub
+  const { events, subscribe } = pubSub;
 
   const {
     $DELETE,
@@ -93,197 +100,249 @@ const EventHandler = ({ pubSub, tab }) => {
     $CLOSE_EDITOR,
     $PROCESS,
     $RUN_SPELL,
-  } = events
+  } = events;
 
+  /**
+   * Save the current spell
+   */
   const saveSpell = async () => {
-    const currentSpell = spellRef.current
-    const graph = serialize() as GraphData
-    if (!currentSpell) return
+    const currentSpell = spellRef.current;
+    const graph = serialize() as GraphData;
+    if (!currentSpell) return;
 
     const updatedSpell = {
       ...currentSpell,
       graph,
       hash: md5(JSON.stringify(graph)),
-    }
+    };
 
     const response = await saveSpellMutation({
       spell: updatedSpell,
       projectId: config.projectId,
-    })
+    });
 
     if ('error' in response) {
-      console.log('UPDATED SPELL', updatedSpell)
-      console.error(response.error)
+      console.log(' UPDATED SPELL', updatedSpell);
+      console.error(response.error);
       enqueueSnackbar('Error saving spell', {
         variant: 'error',
-      })
-      return
+      });
+      return;
     }
 
     enqueueSnackbar('Spell saved', {
       variant: 'success',
-    })
+    });
 
-    onProcess()
-  }
+    onProcess();
+  };
 
+  /**
+   * Save an incremental diff of changes made in editor to the server
+   * @param {object} event - The onSaveDiff event object
+   * @param {object} update - The updated spell object
+   */
   const onSaveDiff = async (event, update) => {
-    if (!spellRef.current) return
+    if (!spellRef.current) return;
 
-    const currentSpell = spellRef.current
+    const currentSpell = spellRef.current;
     const updatedSpell = {
       ...currentSpell,
       ...update,
-    }
+    };
 
-    const jsonDiff = diff(currentSpell, updatedSpell)
+    const jsonDiff = diff(currentSpell, updatedSpell);
 
     // no point saving if nothing has changed
-    if (jsonDiff.length === 0) return
-    //While Importing spell, the graph is first created, then the imported graph is loaded
-    //This might be causing issue at the server end.
-    if (updatedSpell.graph.nodes.length === 0) return
+    if (jsonDiff.length === 0) return;
+    // While Importing spell, the graph is first created, then the imported graph is loaded
+    // This might be causing issue at the server end.
+    if (updatedSpell.graph.nodes.length === 0) return;
 
-    updatedSpell.hash = md5(JSON.stringify(updatedSpell.graph.nodes))
+    updatedSpell.hash = md5(JSON.stringify(updatedSpell.graph.nodes));
 
     try {
-      // We save the diff.  Doing this via feathers but may want to switch to rtk query
+      // We save the diff. Doing this via feathers but may want to switch to rtk query
       const diffResponse = await client.service('spells').saveDiff({
         projectId: config.projectId,
         diff: jsonDiff,
         name: currentSpell.name,
         id: currentSpell.id,
-      })
+      });
 
       // refresh the spell after saving
       getSpell({
         spellName: tab.name,
         id: tab.id,
         projectId: config.projectId,
-      })
+      });
 
       if ('error' in diffResponse) {
         enqueueSnackbar('Error Updating spell', {
           variant: 'error',
-        })
-        return
+        });
+        return;
       }
     } catch (err) {
       enqueueSnackbar('Error saving spell', {
         variant: 'error',
-      })
-      return
+      });
+      return;
     }
 
-    onProcess()
-  }
+    onProcess();
+  };
 
+  /**
+   * Create a new project window or focus on the existing one
+   */
   const createProjectWindow = () => {
-    createOrFocus(windowTypes.PROJECT, 'Project Window')
-  }
+    createOrFocus(windowTypes.PROJECT, 'Project Window');
+  };
 
+  /**
+   * Create a new message reaction editor window or focus on the existing one
+   */
   const createMessageReactionEditor = () => {
     createOrFocus(
       windowTypes.MESSAGE_REACTION_EDITOR,
       'Message Reaction Editor'
-    )
-  }
+    );
+  };
 
+  /**
+   * Create a new playtest window or focus on the existing one
+   */
   const createPlaytest = () => {
-    createOrFocus(windowTypes.PLAYTEST, 'Playtest')
-  }
+    createOrFocus(windowTypes.PLAYTEST, 'Playtest');
+  };
 
+  /**
+   * Create a new inspector window or focus on the existing one
+   */
   const createInspector = () => {
-    createOrFocus(windowTypes.INSPECTOR, 'Inspector')
-  }
+    createOrFocus(windowTypes.INSPECTOR, 'Inspector');
+  };
 
+  /**
+   * Create a new text editor window or focus on the existing one
+   */
   const createTextEditor = () => {
-    createOrFocus(windowTypes.TEXT_EDITOR, 'Text Editor')
-  }
+    createOrFocus(windowTypes.TEXT_EDITOR, 'Text Editor');
+  };
 
+  /**
+   * Create a new console window or focus on the existing one
+   */
   const createConsole = () => {
-    createOrFocus(windowTypes.CONSOLE, 'Console')
-  }
+    createOrFocus(windowTypes.CONSOLE, 'Console');
+  };
 
+  /**
+   * Trigger the processing of the graph in the editor
+   */
   const onProcess = () => {
-    const editor = getEditor()
-    if (!editor) return
+    const editor = getEditor();
+    if (!editor) return;
 
-    editor.runProcess()
-  }
+    editor.runProcess();
+  };
 
+  /**
+   * Trigger the undo action in the editor
+   */
   const onUndo = () => {
-    undo()
-  }
+    undo();
+  };
 
+  /**
+   * Trigger the redo action in the editor
+   */
   const onRedo = () => {
-    redo()
-  }
+    redo();
+  };
 
+  /**
+   * Trigger the delete action in the editor
+   */
   const onDelete = () => {
-    del()
-  }
+    del();
+  };
 
+  /**
+   * Trigger the multi-select copy action in the editor
+   */
   const onMultiSelectCopy = () => {
-    multiSelectCopy()
-  }
+    multiSelectCopy();
+  };
 
+  /**
+   * Trigger the multi-select paste action in the editor
+   */
   const onMultiSelectPaste = () => {
-    multiSelectPaste()
-  }
+    multiSelectPaste();
+  };
 
+  /**
+   * Export the current spell to a JSON file
+   */
   const onExport = async () => {
     // refetch spell from local DB to ensure it is the most up to date
-    const spell = { ...spellRef.current }
-    spell.graph = serialize() as GraphData
+    const spell = { ...spellRef.current };
+    spell.graph = serialize() as GraphData;
 
     // remove secrets, if there are any
     function recurse(obj) {
       for (const key in obj) {
         if (key === 'secrets') {
-          obj[key] = {}
-          console.log('removed secrets')
+          obj[key] = {};
+          console.log('removed secrets');
         }
         if (typeof obj[key] === 'object') {
-          recurse(obj[key])
+          recurse(obj[key]);
         }
       }
     }
 
-    recurse(spell)
+    recurse(spell);
 
     // traverse the json. replace any
-    const json = JSON.stringify(spell)
+    const json = JSON.stringify(spell);
 
-    const blob = new Blob([json], { type: 'application/json' })
-    const url = window.URL.createObjectURL(new Blob([blob]))
-    const link = document.createElement('a')
-    link.href = url
-    link.setAttribute('download', `${spell.name}.spell.json`)
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = window.URL.createObjectURL(new Blob([blob]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${spell.name}.spell.json`);
 
     // Append to html link element page
-    document.body.appendChild(link)
+    document.body.appendChild(link);
 
     // Start download
-    link.click()
+    link.click();
 
-    if (!link.parentNode) return
+    if (!link.parentNode) return;
 
     // Clean up and remove the link
-    link.parentNode.removeChild(link)
-  }
+    link.parentNode.removeChild(link);
+  };
 
   // clean up anything inside the editor which we need to shut down.
   // mainly subscriptions, etc.
   const onCloseEditor = () => {
-    const editor = getEditor() as Record<string, any>
-    if (editor.moduleSubscription) editor.moduleSubscription.unsubscribe()
-  }
+    const editor = getEditor() as Record<string, any>;
+    if (editor.moduleSubscription) editor.moduleSubscription.unsubscribe();
+  };
 
+  /**
+   * Run the current spell
+   * @param {object} event - The run Spell event object
+   * @param {object} data - The data object for running the spell
+   */
   const runSpell = async (event, data) => {
     // run the spell in the spell runner service
-    client.service('spell-runner').create(data)
-  }
+    client.service('spell-runner').create(data);
+  };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handlerMap = {
@@ -304,24 +363,24 @@ const EventHandler = ({ pubSub, tab }) => {
     [$PROCESS(tab.id)]: onProcess,
     [$SAVE_SPELL_DIFF(tab.id)]: onSaveDiff,
     [$RUN_SPELL(tab.id)]: runSpell,
-  }
+  };
 
   useEffect(() => {
-    if (!tab && !spell && !client) return
+    if (!tab && !spell && !client) return;
 
     const subscriptions = Object.entries(handlerMap).map(([event, handler]) => {
-      return subscribe(event, handler)
-    })
+      return subscribe(event, handler);
+    });
 
     // unsubscribe from all subscriptions on unmount
     return () => {
-      subscriptions.forEach(unsubscribe => {
-        unsubscribe()
-      })
-    }
-  }, [tab, client, spell, handlerMap, subscribe])
+      subscriptions.forEach((unsubscribe) => {
+        unsubscribe();
+      });
+    };
+  }, [tab, client, spell, handlerMap, subscribe]);
 
-  return null
-}
+  return null;
+};
 
-export default EventHandler
+export default EventHandler;
