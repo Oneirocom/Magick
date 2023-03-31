99706162e59a90cd460f37567e7feecc81c69d07
moon <shawmakesmagic@gmail.com>
2023-03-30 18:34:22 -0700

    Mor egpt updates

--- packages/plugin-search/shared/src/nodes/SearchGoogle.ts
+++ packages/plugin-search/shared/src/nodes/SearchGoogle.ts
@@ -1,28 +1,31 @@
-/* eslint-disable @nrwl/nx/enforce-module-boundaries */
-import Rete from 'rete'
+// GENERATED 
+/**
+ * @file Search Google Component
+ * @module SearchGoogle
+ */
 
+import Rete from 'rete';
 import {
   MagickNode,
   MagickWorkerInputs,
-  TaskOptions,
   stringSocket,
   triggerSocket,
   MagickComponent,
   API_ROOT_URL,
   WorkerData,
-} from '@magickml/engine'
-// import { queryGoogleSearch } from '../../functions/queryGoogle'
+} from '@magickml/engine';
 
 const info = `When the alert component is triggered, it will fire an alert with the message in the input box.`
 
-type WorkerReturn = {
-  summary: string,
-  links: string[]
-}
-
+/**
+ * Search Google component.
+ * @extends MagickComponent<Promise<WorkerReturn>>
+ */
 export class SearchGoogle extends MagickComponent<Promise<WorkerReturn>> {
+  /**
+   * Constructor for SearchGoogle component.
+   */
   constructor() {
-    // Name of the component
     super('Search Google', {
       outputs: {
         summary: 'output',
@@ -31,44 +34,50 @@ export class SearchGoogle extends MagickComponent<Promise<WorkerReturn>> {
       },
     }, 'Search', info)
   }
-  // the builder is used to "assemble" the node component.
 
+  /**
+   * Builds the SearchGoogle node.
+   * @param {MagickNode} node - The node to build.
+   * @returns {MagickNode} - A MagickNode with configured inputs and outputs.
+   */
   builder(node: MagickNode): MagickNode {
-    // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
-    const query = new Rete.Input('query', 'Query', stringSocket)
-    const triggerIn = new Rete.Input('trigger', 'Trigger', triggerSocket, true)
-    const triggerOut = new Rete.Output('trigger', 'Trigger', triggerSocket)
-    const summary = new Rete.Output('summary', 'Summary', stringSocket)
-    const links = new Rete.Output('links', 'Links', stringSocket)
+    const query = new Rete.Input('query', 'Query', stringSocket);
+    const triggerIn = new Rete.Input('trigger', 'Trigger', triggerSocket, true);
+    const triggerOut = new Rete.Output('trigger', 'Trigger', triggerSocket);
+    const summary = new Rete.Output('summary', 'Summary', stringSocket);
+    const links = new Rete.Output('links', 'Links', stringSocket);
 
     return node
       .addInput(triggerIn)
       .addInput(query)
       .addOutput(triggerOut)
       .addOutput(summary)
-      .addOutput(links)
+      .addOutput(links);
   }
 
-  // the worker contains the main business logic of the node.  It will pass those results
-  // to the outputs to be consumed by any connected components
+  /**
+   * Main worker function, executes the business logic of the component.
+   * @param {WorkerData} _node - The worker data.
+   * @param {MagickWorkerInputs} inputs - The inputs to the worker.
+   * @returns {Promise<WorkerReturn>} - A promise resolving to the worker result.
+   */
   async worker(
     _node: WorkerData,
     inputs: MagickWorkerInputs,
-  ) {
-    const url = `${API_ROOT_URL}/google-search?query=${inputs.query[0]}`
+  ): Promise<WorkerReturn> {
+    const url = `${API_ROOT_URL}/google-search?query=${inputs.query[0]}`;
 
-    // write a fetch post to SERVER_URL/query_google with the parameter query in the body
-    const response = await fetch(url)
+    const response = await fetch(url);
 
-    const json = await response.json()
+    const json = await response.json();
 
-    console.log('json', json)
+    console.log('json', json);
 
-    const { summary, links } = json
+    const { summary, links } = json;
 
     return {
       summary,
       links,
-    }
+    };
   }
 }
