c84b35eb7a4b5603b20c35279d460c3d513ee526
moon <shawmakesmagic@gmail.com>
2023-04-11 23:23:50 -0700

    Search google

--- plugins/search/server/src/index.ts
+++ plugins/search/server/src/index.ts
@@ -7,7 +7,7 @@ import { ServerPlugin } from '@magickml/core'
 /**
  * Nodes that are shared between search plugins
  */
-import Nodes from '@magickml/plugin-search-shared'
+import { getNodes } from '@magickml/plugin-search-shared'
 
 /**
  * Service for Google search functionality
@@ -19,7 +19,7 @@ import { googleSearch } from './services/google-search/google-search'
  */
 const SearchPlugin = new ServerPlugin({
   name: 'SearchPlugin',
-  nodes: Nodes(),
+  nodes: getNodes(),
   services: [googleSearch],
 })
 
--- plugins/search/shared/src/index.ts
+++ plugins/search/shared/src/index.ts
@@ -10,6 +10,6 @@ import { SearchGoogle } from './nodes/SearchGoogle'
  * Export an array of all nodes used in the app.
  * @returns MagickComponent[]
  */
-export default function getNodes(): MagickComponent<any>[] {
-  return [new SearchGoogle()]
+export function getNodes(): MagickComponent<any>[] {
+  return [SearchGoogle]
 }
