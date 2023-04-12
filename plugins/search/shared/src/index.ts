8a3f890fcfa6822b61adc2fbde86fc948e79d5c7
moon <shawmakesmagic@gmail.com>
2023-04-12 00:20:24 -0700

    pin 0.1.0 and fix node ts

--- plugins/search/shared/src/index.ts
+++ plugins/search/shared/src/index.ts
@@ -10,6 +10,6 @@ import { SearchGoogle } from './nodes/SearchGoogle'
  * Export an array of all nodes used in the app.
  * @returns MagickComponent[]
  */
-export function getNodes(): MagickComponent<any>[] {
+export function getNodes(): MagickComponent<Promise<any>>[] {
   return [SearchGoogle]
 }
