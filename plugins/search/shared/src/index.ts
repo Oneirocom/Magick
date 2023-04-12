936d147dc1c7a016c1d7c89cb0022bd7cc413ed3
BMO <bmo@skypress.io>
2023-04-11 22:11:43 -0500

    fix: type errors

--- plugins/search/shared/src/index.ts
+++ plugins/search/shared/src/index.ts
@@ -1,17 +1,15 @@
-// DOCUMENTED 
+// DOCUMENTED
 /**
  * This is the main file exporting the nodes used in the app
  */
 
- import { MagickComponent } from '@magickml/core';
- import { SearchGoogle } from './nodes/SearchGoogle';
+import { MagickComponent } from '@magickml/core'
+import { SearchGoogle } from './nodes/SearchGoogle'
 
- /**
-  * Export an array of all nodes used in the app.
-  * @returns MagickComponent[]
-  */
- export default function getNodes(): MagickComponent[] {
-   return [
-     SearchGoogle
-   ];
- }
+/**
+ * Export an array of all nodes used in the app.
+ * @returns MagickComponent[]
+ */
+export default function getNodes(): MagickComponent<any>[] {
+  return [new SearchGoogle()]
+}
