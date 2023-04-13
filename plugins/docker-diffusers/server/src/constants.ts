7a2be6bdacc67883ca3ff079890b75b461e16b36
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-04-13 14:26:28 -0400

    initial plugin, wip

---
+++ plugins/docker-diffusers/server/src/constants.ts
@@ -0,0 +1,11 @@
+// DOCUMENTED
+/**
+ * The BANANA endpoint URL.
+ * Attempts to retrieve from various environment variables, falls back to default endpoint if not found.
+ */
+export const BANANA_ENDPOINT =
+  process.env['VITE_BANANA_ENDPOINT'] ||
+  process.env['NEXT_BANANA_ENDPOINT'] ||
+  process.env['REACT_APP_BANANA_ENDPOINT'] ||
+  process.env['BANANA_ENDPOINT'] ||
+  'https://api.banana.dev/start/v4'
