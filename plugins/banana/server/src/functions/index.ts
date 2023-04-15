58370896febdad6da36189f6f581add6d88eeb12
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-04-15 05:25:59 -0400

    banana-server plugin setup

---
+++ plugins/banana/server/src/functions/index.ts
@@ -0,0 +1,6 @@
+// DOCUMENTED 
+/** 
+ * Exporting necessary modules for external use.
+ */
+export * from './makeImageCompletion';
+export * from './makeSpeechCompletion';
--- plugins/banana/server/src/index.ts
+++ plugins/banana/server/src/index.ts
@@ -1,17 +1,42 @@
-// DOCUMENTED 
+// DOCUMENTED
 /**
- * A ServerPlugin that adds the BananaPlugin with a single secret - API key.
+ * A plugin for the @magickml/core that adds banana.dev completion functionality
+ *
+ * @remarks
+ * The plugin uses handlers for text2image and speech2text which are defined in the 'makeImageCompletion and 'makeSpeechCompletion' functions respectively.',
+ *
+ * @packageDocumentation
  */
-import { ServerPlugin } from '@magickml/core';
 
+import { ServerPlugin } from '@magickml/core'
+import shared from '@magickml/plugin-banana-shared'
+import { makeImageCompletion, makeSpeechCompletion } from './functions'
+
+/**
+ * The secrets used by the banana.dev API
+ */
+const { secrets } = shared
+
+/**
+ * The handlers for each type of banan.dev completion
+ */
+const completionHandlers = {
+  image: {
+    text2image: makeImageCompletion,
+  },
+  speech: {
+    speech2text: makeSpeechCompletion,
+  },
+}
 const BananaPlugin = new ServerPlugin({
-    name: 'BananaPlugin',
-    // The list of secrets required by the 'BananaPlugin'.
-    secrets: [{
-        name: 'Banana API Key',
-        key: 'banana_api_key',
-        global: true,
-    }]
-});
+  name: 'BananaPlugin',
+  secrets,
+  completionProviders: shared.completionProviders.map(provider => {
+    return {
+      ...provider,
+      handler: completionHandlers[provider.type][provider.subtype],
+    }
+  }),
+})
 
-export default BananaPlugin;
+export default BananaPlugin
