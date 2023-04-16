87eda317b507febbe186c822c83f555fb3985777
M̵̞̗̝̼̅̏̎͝Ȯ̴̝̻̊̃̋̀Õ̷̼͋N̸̩̿͜ ̶̜̠̹̼̩͒ <shawmakesmagic@gmail.com>
2023-04-16 10:57:48 -0700

    Revert "Fix: Removed Supabase Client"

--- packages/core/shared/src/plugin.ts
+++ packages/core/shared/src/plugin.ts
@@ -238,20 +238,7 @@ class PluginManager {
     })
     return secrets
   }
-  getCompletionProviders(
-    type: string | null = null,
-    subtypes: null | string[] = null
-  ): CompletionProvider[] {
-    const completionProviders: CompletionProvider[] = []
-    this.pluginList.forEach(plugin => {
-      plugin.completionProviders.forEach(provider => {
-        if (type && provider.type !== type) return
-        if (subtypes && !subtypes.includes(provider.subtype)) return
-        completionProviders.push(provider)
-      })
-    })
-    return completionProviders
-  }
+
   getCompletionProviders(
     type: string | null = null,
     subtypes: null | string[] = null
