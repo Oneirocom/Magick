72edca1768afbfef4f2f51c4ba799109c13c4db1
Thomage Anderson <thomageanderson@gmail.com>
2023-04-17 01:13:50 -0400

    Add AccountProvider wrapper to NewFineTune.tsx and lint file

--- plugins/finetune/client/src/screens/NewFineTune.tsx
+++ plugins/finetune/client/src/screens/NewFineTune.tsx
@@ -1,17 +1,20 @@
-// DOCUMENTED 
+// DOCUMENTED
 /**
  * This component is responsible to render the page for creating a new fine tune.
  * It wraps the NewFineTuneForm component into a LoginRequired component to ensure the user has permission.
  */
 
-import React from 'react';
-import LoginRequired from '../account/LoginRequired';
-import NewFineTuneForm from '../fine-tunes/NewFineTuneForm';
+import React from 'react'
+import LoginRequired from '../account/LoginRequired'
+import NewFineTuneForm from '../fine-tunes/NewFineTuneForm'
+import Account from '../account/Account'
 
 export default function NewFineTunePage(): JSX.Element {
   return (
-    <LoginRequired>
-      <NewFineTuneForm />
-    </LoginRequired>
-  );
+    <Account>
+      <LoginRequired>
+        <NewFineTuneForm />
+      </LoginRequired>
+    </Account>
+  )
 }
