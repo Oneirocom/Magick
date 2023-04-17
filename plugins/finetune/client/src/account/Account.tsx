b7b8ddda62d5666d33320bbe93c66cccd18b8127
Thomage Anderson <thomageanderson@gmail.com>
2023-04-17 01:11:25 -0400

    Update Account.tsx to work with centralized secrets key from LocalStorage and lint file

--- plugins/finetune/client/src/account/Account.tsx
+++ plugins/finetune/client/src/account/Account.tsx
@@ -1,29 +1,33 @@
-// DOCUMENTED 
-import React, { useCallback, useMemo, FC, ReactNode } from 'react';
-import { toast } from 'react-toastify';
-import { SWRConfig } from 'swr';
-import requestHeaders from './requestHeaders';
+// DOCUMENTED
+import React, { useCallback, useMemo, FC, ReactNode } from 'react'
+import { toast } from 'react-toastify'
+import { SWRConfig } from 'swr'
+import requestHeaders from './requestHeaders'
 
-import { OPENAI_ENDPOINT } from '../constants';
-import { useLocalStorage } from 'usehooks-ts';
-import { OpenAI } from '../types/openai';
+import { OPENAI_ENDPOINT } from '../constants'
+import { useLocalStorage } from 'usehooks-ts'
+import { OpenAI } from '../types/openai'
 
 interface IAccountProvider {
-  headers?: { [key: string]: string };
-  isSignedIn: boolean;
-  signIn: (apiKey: string, organizationId: string) => void;
-  signOut: () => void;
+  headers?: { [key: string]: string }
+  isSignedIn: boolean
+  signIn: (apiKey: string, organizationId: string) => void
+  signOut: () => void
 }
 
 interface IAccountProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 export const AccountContext = React.createContext<IAccountProvider>({
   isSignedIn: false,
-  signIn: () => { /* null */},
-  signOut: () => { /* null */},
-});
+  signIn: () => {
+    /* null */
+  },
+  signOut: () => {
+    /* null */
+  },
+})
 
 /**
  * Provides User Authentication and sets up the SWR hook
@@ -32,37 +36,45 @@ export const AccountContext = React.createContext<IAccountProvider>({
  */
 const Account: FC<IAccountProps> = ({ children }): JSX.Element => {
   const [account, setAccount] = useLocalStorage<{
-    apiKey: string;
-    organizationId: string;
-  } | null>('openai', null);
+    openai_api_key: string
+  } | null>('secrets', null)
 
   const headers = useMemo(
-    () => (account ? requestHeaders(account) : undefined),
+    () =>
+      account
+        ? requestHeaders({
+            apiKey: account?.openai_api_key,
+            organizationId: undefined,
+          })
+        : undefined,
     [account]
-  );
+  )
 
   /**
    * @param {string} path - url path
    * @returns {Promise<void>} - returns a JSON object on success or throws an error with message if unsuccessful
    */
-  const fetcher = useCallback(async (path: string): Promise<void> => {
-    if (!headers) {
-      return null;
-    }
-    const response = await fetch(`${OPENAI_ENDPOINT}/${path}`, {
-      headers,
-    });
-    if (response.ok) {
-      return await response.json();
-    } else {
-      const { error } = (await response.json()) as OpenAI.ErrorResponse;
-      throw new Error(error.message);
-    }
-  }, [headers]);
+  const fetcher = useCallback(
+    async (path: string): Promise<void> => {
+      if (!headers) {
+        return null
+      }
+      const response = await fetch(`${OPENAI_ENDPOINT}/${path}`, {
+        headers,
+      })
+      if (response.ok) {
+        return await response.json()
+      } else {
+        const { error } = (await response.json()) as OpenAI.ErrorResponse
+        throw new Error(error.message)
+      }
+    },
+    [headers]
+  )
 
   const onError = useCallback((error: Error): void => {
-    toast.error(String(error));
-  }, []);
+    toast.error(String(error))
+  }, [])
 
   /**
    * @param {string} apiKey - User's apiKey on OpenAI
@@ -70,21 +82,20 @@ const Account: FC<IAccountProps> = ({ children }): JSX.Element => {
    * @returns {void}
    */
   const signIn = useCallback(
-    (apiKey: string, organizationId: string): void => {
-      setAccount({ apiKey, organizationId });
+    (apiKey: string): void => {
+      setAccount({ openai_api_key: apiKey })
     },
     [setAccount]
-  );
+  )
 
-  const signOut = useCallback(() => setAccount(null), [setAccount]);
-
+  const signOut = useCallback(() => setAccount(null), [setAccount])
   return (
     <AccountContext.Provider
       value={{ isSignedIn: !!account, headers, signIn, signOut }}
     >
       <SWRConfig value={{ fetcher, onError }}>{children}</SWRConfig>
     </AccountContext.Provider>
-  );
-};
+  )
+}
 
-export default Account;
+export default Account
