89cf27491d93b77aefe6a22c2a711929052844ee
Thomage Anderson <thomageanderson@gmail.com>
2023-04-17 01:12:57 -0400

    Add AccountProvider wrapper to CompletionList.tsx and todo note about handling plugin specific providers going forward

--- plugins/finetune/client/src/completions/CompletionList.tsx
+++ plugins/finetune/client/src/completions/CompletionList.tsx
@@ -1,29 +1,30 @@
-// DOCUMENTED 
+// DOCUMENTED
 // Import required libraries and components
-import Button from '@mui/material/Button';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import ExpansionDetails from '../components/ExpansionDetails';
-import FileListTable from '../files/FileListTable';
-import UploadFileButton from '../files/UploadFileButton';
-import FineTuneList from '../fine-tunes/FineTuneList';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Box, Typography } from '@mui/material';
-import InfoCard from '../components/InfoCard';
+import Button from '@mui/material/Button'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import ExpansionDetails from '../components/ExpansionDetails'
+import FileListTable from '../files/FileListTable'
+import UploadFileButton from '../files/UploadFileButton'
+import FineTuneList from '../fine-tunes/FineTuneList'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+import { Box, Typography } from '@mui/material'
+import InfoCard from '../components/InfoCard'
+import Account from '../account/Account'
 
 /**
  * Component for displaying the classification list.
  * @returns JSX element containing the classification list.
  */
-export default function ClassificationList() {
+const ClassificationList = () => {
   // Use the navigate hook from react-router-dom
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // Return the JSX element
   return (
     <main className="mx-auto space-y-12 max-w-4xl">
       <InfoCard>
-        <Box component={'div'} style={{ width: '100%' }}> 
+        <Box component={'div'} style={{ width: '100%' }}>
           <Box
             component={'div'}
             display={'flex'}
@@ -39,7 +40,7 @@ export default function ClassificationList() {
             <Button
               size="small"
               variant="contained"
-              style={{backgroundColor: 'purple', color: 'white' }}
+              style={{ backgroundColor: 'purple', color: 'white' }}
               onClick={() => navigate('/fineTuneManager/fine-tunes/new')}
               startIcon={<AddCircleOutlineIcon />}
             >
@@ -78,20 +79,45 @@ export default function ClassificationList() {
       <InfoCard>
         <h1>Instructions</h1>
         <p>
-          To fine-tune the model, first upload a file containing prompts and completions.
+          To fine-tune the model, first upload a file containing prompts and
+          completions.
         </p>
         <p>
-          The file can be CSV, Excel, or JSONL. It must contain two columns, "prompt" and "completion". For Excel, the first column is "prompt", and the second column is "completion". Combined they cannot have more than 2048 tokens. <a href="https://beta.openai.com/docs/guides/fine-tuning/preparing-your-dataset">Learn more</a>.
+          The file can be CSV, Excel, or JSONL. It must contain two columns,
+          "prompt" and "completion". For Excel, the first column is "prompt",
+          and the second column is "completion". Combined they cannot have more
+          than 2048 tokens.{' '}
+          <a href="https://beta.openai.com/docs/guides/fine-tuning/preparing-your-dataset">
+            Learn more
+          </a>
+          .
         </p>
         <p>
-          Then create a new model using that training file. You can use a second file for validating the model. Read more about <a href="https://beta.openai.com/docs/guides/completion/prompt-design">prompt design</a> and <a href="https://beta.openai.com/docs/guides/completion/evaluation">evaluation</a>.
+          Then create a new model using that training file. You can use a second
+          file for validating the model. Read more about{' '}
+          <a href="https://beta.openai.com/docs/guides/completion/prompt-design">
+            prompt design
+          </a>{' '}
+          and{' '}
+          <a href="https://beta.openai.com/docs/guides/completion/evaluation">
+            evaluation
+          </a>
+          .
         </p>
         <h4>For example:</h4>
         <p>
           {`{ "prompt": "Company: BHFF cars\nProduct: cars\nAd:One stop shop!\nSupported:", "completion": "yes" }
-          { "prompt": "Company: Loft teeth\nProduct: -\nAd:Straight teeth in weeks!\nSupported:", "completion":"no" }`} 
+          { "prompt": "Company: Loft teeth\nProduct: -\nAd:Straight teeth in weeks!\nSupported:", "completion":"no" }`}
         </p>
       </InfoCard>
     </main>
-  );
+  )
+}
+// @thomageanderson: What's a better pattern for plugins with their own providers?
+export default function ClassificationListWrapped() {
+  return (
+    <Account>
+      <ClassificationList />
+    </Account>
+  )
 }
