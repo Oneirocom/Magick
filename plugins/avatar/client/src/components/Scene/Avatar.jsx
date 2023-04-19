1b83c3517ff8303d62412bebc7361c6e0e34cf8f
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-04-18 19:00:17 -0700

    load avatar

---
+++ plugins/avatar/client/src/components/Scene/Avatar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { useZustand } from '../../store/useZustand'
+
+
+export const Avatar = () => {
+  const { avatarInitPos } = useZustand()
+
+  const gltf = useGLTF('/models/avatar2.vrm')
+  return (
+    <primitive
+      object={gltf.scene}
+      position={avatarInitPos}
+    />
+  )
+}
