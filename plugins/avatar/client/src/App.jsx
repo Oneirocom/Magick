0ce80f892b46df3ab0ee0269d6fc6ec2f7325b12
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-05-02 11:10:02 -0400

    use drag drop

--- plugins/avatar/client/src/App.jsx
+++ plugins/avatar/client/src/App.jsx
@@ -1,15 +1,44 @@
-import styles from './App.module.css'
-import ChatBox from './components/Chat/ChatBox'
+import { useCallback, useEffect, useState, useFrame } from "react"
+import { useFBX } from '@react-three/drei'
+import { useVrm } from './hooks/useVrm'
+import { customDebug } from "./utils/custom.debug"
 import { Scene } from './components/Scene/Scene'
+import DragDrop from './components/DragDrop/DragDrop'
+import ChatBox from './components/Chat/ChatBox'
+import { useZustand } from "./store/useZustand"
+import styles from './App.module.css'
 
 const App = () => {
+
+  const {
+    setAvatarVrm,
+  } = useZustand()
+
+  const [vrmURL, setVrmUrl] = useState('/models/avatar2.vrm')
+
+  const fbx = useFBX('/models/magic idle.fbx')
+
+  const avatar = useVrm(vrmURL)
+  
+  // customDebug().log('Avatar: ', avatar)
+  // customDebug().log('vrmURL: ', vrmURL)
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0]
+    if (!file) return
+    setVrmUrl(URL.createObjectURL(file))
+  }, []);
+
+  useEffect(()=>{
+    setAvatarVrm(avatar)
+  }, [avatar])
+
   return (
     <div className={styles.container}>
-      <Scene />
+      <Scene onDrop={onDrop} className={styles.dragdrop} noClick={false}/>
       <ChatBox />
     </div>
   )
 }
-
 
 export default App
