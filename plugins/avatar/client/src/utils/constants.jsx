b5252c173e8ca65b6cb3ee6c12fa6cf7859c2298
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-05-02 05:27:01 -0400

    charater position,  fix style

--- plugins/avatar/client/src/utils/constants.jsx
+++ plugins/avatar/client/src/utils/constants.jsx
@@ -4,7 +4,7 @@ export const CAMERA_INIT_POS = [0, 0, 2]
 
 export const CAMERA_DES_POS = CAMERA_INIT_POS
 
-export const AVATAR_INIT_POS = [-0.4, -0.8, -0.3]
+export const AVATAR_INIT_POS = [-0.6, -0.8, -0.3]
 
 export const AVATAR_DES_POS = CAMERA_INIT_POS
 
