94dd8156980e975ed37ed7e1881c1bc349b18591
moon <shawmakesmagic@gmail.com>
2023-05-03 02:47:16 -0700

    Avatar window almost calls out

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,11 +1,3 @@
-// DOCUMENTED 
-/**
- * This exports the AppConfig interface from the './contexts/ConfigProvider' file.
- */
-export type {
-  AppConfig
-} from '../../core/client/src/providers/ConfigProvider';
-
 /**
  * This exports the MagickIDE class from './main' file.
  */
