5ff48d143f1b65edb8630e118961e493e1547659
ronplaymeta <rondoor124@gmail.com>
2023-05-12 11:48:18 +0900

    create github plugn

---
+++ plugins/github/client/src/index.ts
@@ -0,0 +1,52 @@
+import {
+  ClientPlugin,
+  eventSocket,
+  triggerSocket,
+} from '@magickml/core'
+import { GithubAgentWindow } from './components/agent.component'
+
+
+const inputSockets = [
+  {
+    socket: 'output',
+    name: 'output',
+    type: eventSocket,
+  },
+  {
+    socket: 'trigger',
+    name: 'trigger',
+    type: triggerSocket,
+  }
+]
+
+const outputSockets = [
+  {
+    socket: 'output',
+    name: 'output',
+    type: eventSocket,
+  }
+]
+
+const GithubPlugin = new ClientPlugin({
+  name: 'GithubPlugin',
+  agentComponents: [GithubAgentWindow],
+  inputTypes: [
+    { name: 'Github (Feed)', sockets: inputSockets},
+    // { name: 'Github (DM)', trigger: true, socket: eventSocket },
+    // { name: 'Github (Mention)', trigger: true, socket: eventSocket },
+  ],
+  outputTypes: [
+    { name: 'Github (Feed)', sockets: outputSockets },
+    // { name: 'Github (DM)', trigger: false, socket: eventSocket },
+    // { name: 'Github (Mention)', trigger: false, socket: eventSocket },
+  ],
+  secrets: [
+    {
+    name: 'Access Token',
+    key: 'github_access_token',
+    global: false
+  }
+]
+})
+
+export default GithubPlugin
