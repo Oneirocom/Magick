cb4519261ee93eca4c270a3a87c346946f5ba53b
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2023-05-12 18:40:40 +0200

    implementing event UI

---
+++ packages/core/client/src/components/Table/TableSortLabel.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react'
+
+import {
+  TableSortLabel as MuiTableSortLabel,
+  TableSortLabelProps,
+} from '@mui/material'
+
+const TableSortLabel = ({ children, ...props }: TableSortLabelProps) => (
+  <MuiTableSortLabel {...props}>{children}</MuiTableSortLabel>
+)
+
+TableSortLabel.displayName = 'TableSortLabel'
+
+TableSortLabel.defaultProps = {
+  children: null,
+}
+
+export default TableSortLabel
