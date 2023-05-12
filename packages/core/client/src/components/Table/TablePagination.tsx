2b5ed676fd21a53bd051fe97ac6f91eab6914d40
moon <shawmakesmagic@gmail.com>
2023-05-12 15:46:23 -0700

    Fix linting

--- packages/core/client/src/components/Table/TablePagination.tsx
+++ packages/core/client/src/components/Table/TablePagination.tsx
@@ -1,5 +1,3 @@
-import React, { ReactNode } from 'react'
-
 import {
   TablePagination as MuiTablePagination,
   TablePaginationProps,
--- packages/core/client/src/components/Table/TableSortLabel.tsx
+++ packages/core/client/src/components/Table/TableSortLabel.tsx
@@ -1,5 +1,3 @@
-import React, { ReactNode } from 'react'
-
 import {
   TableSortLabel as MuiTableSortLabel,
   TableSortLabelProps,
