1178de6e6a2fe423e09b97c1cc4e595b9c66653e
moon <shawmakesmagic@gmail.com>
2023-05-14 16:54:37 -0700

    lint expandVetor

--- packages/core/shared/src/functions/expandVector.ts
+++ packages/core/shared/src/functions/expandVector.ts
@@ -2,22 +2,22 @@ export default function expandVector(
   inputVector: number[],
   newLength: number
 ): number[] {
-  let outputVector: number[] = []
-  let inputLength = inputVector.length
+  const outputVector: number[] = []
+  const inputLength = inputVector.length
 
   for (let i = 0; i < newLength; i++) {
     // calculate the proportional index in the inputVector
-    let index = (i * (inputLength - 1)) / (newLength - 1)
+    const index = (i * (inputLength - 1)) / (newLength - 1)
 
     // find the indices of the two points to interpolate in the inputVector
-    let index1 = Math.floor(index)
-    let index2 = Math.min(index1 + 1, inputLength - 1)
+    const index1 = Math.floor(index)
+    const index2 = Math.min(index1 + 1, inputLength - 1)
 
     // calculate the interpolation weight for the second point
-    let weight = index - index1
+    const weight = index - index1
 
     // linearly interpolate between the two points
-    let value =
+    const value =
       (1 - weight) * inputVector[index1] + weight * inputVector[index2]
 
     outputVector.push(value)
