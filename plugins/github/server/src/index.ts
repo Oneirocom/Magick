81ce3bd440dd3db32b4710de2a6455f7d6e7e10b
moon <shawmakesmagic@gmail.com>
2023-05-16 21:39:23 -0700

    Remove world manager

--- plugins/github/server/src/index.ts
+++ plugins/github/server/src/index.ts
@@ -1,30 +1,26 @@
-import { eventSocket, ServerPlugin, triggerSocket, WorldManager } from '@magickml/core'
+import { eventSocket, ServerPlugin, triggerSocket } from '@magickml/core'
 
 import { GithubConnector } from './connectors/github'
 
 type StartGithubArgs = {
   agent: any
   spellRunner: any
-  worldManager: WorldManager
 }
 
 function getAgentMethods() {
-  async function startGithub({
-    agent,
-    spellRunner,
-    worldManager,
-  }: StartGithubArgs) {
+  async function startGithub({ agent, spellRunner }: StartGithubArgs) {
     const { data } = agent.data
-    if(!data) return console.log("No data for this agent")
-    if(!data.github_enabled) return console.log("Github is not enabled for this agent")
-    if (!data.github_access_token) return console.log("Github Access Token is not set for this agent")
+    if (!data) return console.log('No data for this agent')
+    if (!data.github_enabled)
+      return console.log('Github is not enabled for this agent')
+    if (!data.github_access_token)
+      return console.log('Github Access Token is not set for this agent')
     console.log('starting github connect')
     try {
       const github = new GithubConnector({
         ...data,
         agent,
         spellRunner,
-        worldManager,
       })
       agent.github = github
     } catch (err) {
@@ -32,8 +28,9 @@ function getAgentMethods() {
     }
   }
 
-  async function stopGithub({agent}) {
-    if (!agent.github) return console.warn("Github isn't running, can't stop it")
+  async function stopGithub({ agent }) {
+    if (!agent.github)
+      return console.warn("Github isn't running, can't stop it")
     try {
       await agent.github.destroy()
       agent.github = null
@@ -49,19 +46,12 @@ function getAgentMethods() {
   }
 }
 
-async function handleResponse(
-  {
-    output,
-    agent,
-    event
-  }
-) {
+async function handleResponse({ output, agent, event }) {
   if (!output || output === '')
     return console.warn('No output to send to github')
   await agent.github.sendMessageToChannel(event.channel, output)
   console.log('RESPONSE HANDLED')
 }
-
 
 const inputSockets = [
   {
@@ -73,7 +63,7 @@ const inputSockets = [
     socket: 'trigger',
     name: 'trigger',
     type: triggerSocket,
-  }
+  },
 ]
 
 const outputSockets = [
@@ -81,22 +71,28 @@ const outputSockets = [
     socket: 'output',
     name: 'output',
     type: eventSocket,
-  }
+  },
 ]
 
 const GithubPlugin = new ServerPlugin({
   name: 'GithubPlugin',
   inputTypes: [
-    { name: 'Github (Feed)', sockets: inputSockets, defaultResponseOutput: 'Github (Feed)' },
+    {
+      name: 'Github (Feed)',
+      sockets: inputSockets,
+      defaultResponseOutput: 'Github (Feed)',
+    },
     // { name: 'Github (DM)', trigger: true, socket: eventSocket, defaultResponseOutput: 'Github (DM)' },
     // { name: 'Github (Mention', trigger: true, socket: eventSocket, defaultResponseOutput: 'Github (Mention'}
   ],
   outputTypes: [
-    { name: 'Github (Feed)', sockets: outputSockets, handler: async ({
-      output, agent, event
-    }) => {
-      await handleResponse({output, agent, event})
-    }},
+    {
+      name: 'Github (Feed)',
+      sockets: outputSockets,
+      handler: async ({ output, agent, event }) => {
+        await handleResponse({ output, agent, event })
+      },
+    },
     // { name: 'Github (DM)', trigger: true, socket: eventSocket, handler: async ({
     //   output, agent, event
     // }) => {
@@ -105,12 +101,12 @@ const GithubPlugin = new ServerPlugin({
   ],
   agentMethods: getAgentMethods(),
   secrets: [
-  {
-    name: 'Access Token',
-    key: 'github_access_token',
-    global: false
-  }
-]
+    {
+      name: 'Access Token',
+      key: 'github_access_token',
+      global: false,
+    },
+  ],
 })
 
 export default GithubPlugin
