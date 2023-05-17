80f3f01cbde0748ae338d618139f1a5927f5af64
Michael <m.sharpe@project89.org>
2023-05-17 12:04:55 -0700

    Pass emit down into engine in spellRunnner and add events

--- packages/core/shared/src/spellManager/SpellRunner.ts
+++ packages/core/shared/src/spellManager/SpellRunner.ts
@@ -67,6 +67,22 @@ class SpellRunner {
     })
   }
 
+  publish(event, message) {
+    if (!this.agent) return
+    this.agent.publishEvent(`spell:${this.currentSpell.id}`, {
+      ...message,
+      event,
+    })
+  }
+
+  emit(event, message) {
+    if (!this.agent) return
+    this.agent.publishEvent(`spell:${this.currentSpell.id}:${event}`, {
+      ...message,
+      event,
+    })
+  }
+
   constructor({ app, socket, agent, spellManager }: SpellRunnerConstructor) {
     this.agent = agent
     this.spellManager = spellManager
@@ -77,6 +93,7 @@ class SpellRunner {
       components: getNodes(),
       server: true,
       socket: socket || undefined,
+      emit: this.emit.bind(this),
     }) as MagickEngine
     this.app = app
 
@@ -242,7 +259,6 @@ class SpellRunner {
     inputs,
     componentName = 'Input',
     runSubspell = false,
-    agent,
     secrets,
     publicVariables,
     app,
@@ -250,7 +266,9 @@ class SpellRunner {
     // This should break us out of an infinite loop if we have circular spell dependencies.
     if (runSubspell && this.ranSpells.includes(this.currentSpell.name)) {
       this._clearRanSpellCache()
-      return this.logger.error('Infinite loop detected in SpellRunner. Exiting.')
+      return this.logger.error(
+        'Infinite loop detected in SpellRunner. Exiting.'
+      )
     }
     // Set the current spell into the cache of spells that have run now.
     if (runSubspell) this.ranSpells.push(this.currentSpell.name)
@@ -263,7 +281,6 @@ class SpellRunner {
     this.module.read({
       inputs: this._formatInputs(inputs),
       secrets,
-      agent,
       publicVariables,
       app,
     })
