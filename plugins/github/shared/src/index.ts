c774d4f43da5c1d9e1bd450128604deb6a24e21d
Ron <rondoor124@gmail.com>
2023-05-18 18:00:00 -0700

    Update GithubIssueSearch   - add 'github_access_token' to secrets   - add getNodes to GithubPlugin   - implement githubSearchIssue function   - refactor code

--- plugins/github/shared/src/index.ts
+++ plugins/github/shared/src/index.ts
@@ -3,11 +3,15 @@
  * Imports
  */
 
+import { MagickComponent } from '@magickml/core'
 import { GithubIssueSearch } from './nodes/GithubIssueSearch';
 
 /**
- * Export an array of nodes
+ * Export an array of all nodes used in the app.
+ * @returns MagickComponent[]
  */
-export default [
-  GithubIssueSearch
-];
+export function getNodes(): (typeof MagickComponent<any>)[] {
+  return [GithubIssueSearch]
+}
+
+export default [GithubIssueSearch]
