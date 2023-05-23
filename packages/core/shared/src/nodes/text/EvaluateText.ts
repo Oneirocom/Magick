1921bd4f59bebbdf94492675fb905c16b79dc8c5
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2023-05-23 19:02:58 +0200

    fixed info

--- packages/core/shared/src/nodes/text/EvaluateText.ts
+++ packages/core/shared/src/nodes/text/EvaluateText.ts
@@ -11,7 +11,7 @@ const fewshot = ''
 
 /** Information string for Evaluate Text component */
 const info =
-  'Evaluate Text - options: includes, not includes, equals, not equals, starts with, not starts with, ends with, not ends with'
+  'Takes a string input and evaluates it against the value specified in the text editor using the operation specified in the Operation Type property, then triggers the appropriate output based on the result. Valid values for the Operation Type: includes, not includes, equals, not equals, starts with, not starts with, ends with, not ends with.'
 
 /**
  * A class that represents an Evaluate Text component.
