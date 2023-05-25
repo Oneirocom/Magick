a092484a9417caaf6026cd7b631d9aca188afbb2
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-05-25 01:10:00 -0400

    supabase server

---
+++ plugins/supabase/server/src/functions/index.ts
@@ -0,0 +1,10 @@
+// UNDOCUMENTED
+/**
+ * Exporting necessary modules for external use.
+ */
+
+export * from './db/select'
+export * from './db/insert'
+export * from './db/update'
+export * from './db/upsert'
+export * from './db/delete'
