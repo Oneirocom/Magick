a167f2c7014aa5f6e978cb431a6ef0f93ced1931
sshivaditya2019 <s.shivaditya@gmail.com>
2023-05-31 19:21:31 +0530

    Feat: Local Model Support

---
+++ plugins/localmodel/server/src/index.ts
@@ -0,0 +1,50 @@
+// DOCUMENTED
+/**
+ * A plugin for the @magickml/core that adds LocalModel completion functionality
+ *
+ * @remarks
+ * The plugin uses handlers for text, chat and text embedding which are defined in the 'makeTextCompletion',
+ * 'makeChatCompletion' and 'makeTextEmbedding' functions respectively.
+ *
+ * @packageDocumentation
+ */
+
+import { ServerPlugin } from '@magickml/core'
+import shared from '@magickml/plugin-localmodel-shared'
+import {
+  makeChatCompletion,
+  makeTextCompletion,
+  makeTextEmbedding,
+} from './functions'
+
+/**
+ * The secrets used by the OpenAI API
+ */
+const { secrets } = shared
+
+/**
+ * The handlers for each type of OpenAI completion
+ */
+const completionHandlers = {
+  text: {
+    text: makeTextCompletion,
+    chat: makeChatCompletion,
+    embedding: makeTextEmbedding,
+  },
+}
+
+/**
+ * A server plugin for the @magickml/core that adds OpenAI completion functionality
+ */
+const LocalModelPlugin = new ServerPlugin({
+  name: 'LocalModelPlugin',
+  secrets,
+  completionProviders: shared.completionProviders.map(provider => {
+    return {
+      ...provider,
+      handler: completionHandlers[provider.type][provider.subtype],
+    }
+  }),
+})
+
+export default LocalModelPlugin
