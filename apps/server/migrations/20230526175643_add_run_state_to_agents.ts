980e5bd6edab03b68cf698471a757eb104836add
benbot <bbotwin@atlassian.com>
2023-05-31 17:50:37 -0400

    Added pgnotify reporter for cloud agents

--- apps/server/migrations/20230526175643_add_run_state_to_agents.ts
+++ apps/server/migrations/20230526175643_add_run_state_to_agents.ts
@@ -3,14 +3,14 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('agents', table => {
-        table.enum('run_state', ['running', 'stopped', 'paused', 'failed', 'starting']).defaultTo('stopped').notNullable()
+        table.enum('runState', ['running', 'stopped', 'paused', 'failed', 'starting']).defaultTo('stopped').notNullable()
     })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.alterTable('agents', table => {
-        table.dropColumn('run_state')
+        table.dropColumn('runState')
     })
 }
 
