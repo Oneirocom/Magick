e2cf30f9edaf6bdd0f7afbb227aaa991416912f3
benbot <bbotwin@atlassian.com>
2023-05-31 19:35:06 -0400

    Cloud agents can now be started and stopped

--- packages/cloud-agent-worker/src/lib/cloud-agent-worker.ts
+++ packages/cloud-agent-worker/src/lib/cloud-agent-worker.ts
@@ -1,16 +1,62 @@
 import { Worker, Job } from 'bullmq'
-import { hostname } from 'os'
-import pino from 'pino'
-import { getLogger } from '@magickml/core'
+import { bullMQConnection } from '@magickml/core'
 
 import { app } from '@magickml/server-core'
+import { Agent, AgentManager } from '@magickml/agents'
 
-const runningAgents = []
+export class CloudAgentWorker extends AgentManager {
+  constructor() {
+    super(app, false)
+  }
 
-export class CloudAgentWorker {
-  logger: pino.Logger = getLogger()
+  async addAgent(agentId: string) {
+    this.logger.info(`Creating agent ${agentId}...`)
+    const agentDBResult = (
+      await app.service('agents').find({
+        query: {
+          id: agentId
+        }
+      })
+    )?.data
 
-  constructor() {}
+    if (agentDBResult.length == 0 || !agentDBResult) {
+      this.logger.error(`Agent ${agentId} not found when creating agent`)
+      throw new Error(`Agent ${agentId} not found when creating agent`)
+    }
+
+    const agentData = {
+      ...agentDBResult[0],
+      pingedAt: new Date().toISOString()
+    }
+
+    if (!agentData.enabled) {
+      this.logger.info(`Agent ${agentId} is disabled, skipping creation`)
+      return
+    }
+
+    const agent = new Agent(agentData, this, this.app)
+    await agent.initialize({})
+
+    this.logger.debug(`Running agent add handlers for ${agentId}`)
+    this.addHandlers.forEach(handler => {
+      handler({ agent, agentData })
+    })
+
+    this.currentAgents[agentId] = agent
+    this.logger.debug(`Finished running agent add handlers for ${agentId}`)
+
+    this.logger.info(`Created agent ${agentId}`)
+  }
+
+  async removeAgent(agentId: string) {
+    this.logger.info(`Removing agent ${agentId}...`)
+
+    this.removeHandlers.forEach(handler => {
+      handler({ agent: this.currentAgents[agentId] })
+    })
+
+    await this.currentAgents[agentId]?.onDestroy()
+  }
 
   async agentUpdated(agentId: string) {
     this.logger.info(`Creating agent ${agentId}`)
@@ -22,8 +68,6 @@ export class CloudAgentWorker {
       })
     )?.data
 
-    console.log(agentDBResult)
-
     if (agentDBResult.length == 0 || !agentDBResult) {
       this.logger.error(`Agent ${agentId} not found when creating agent`)
       throw new Error(`Agent ${agentId} not found when creating agent`)
@@ -32,21 +76,25 @@ export class CloudAgentWorker {
     const agent = agentDBResult[0]
 
     if (agent.enabled) {
-      switch (agent.runState) {
-        case 'stopped':
-        case 'failed':
-          this.logger.info(`Starting agent ${agentId}`)
-          await app.service('agents').patch(agentId, {
-            runState: 'starting'
-          })
-        break;
-      }
+      await this.addAgent(agentId)
+    // TODO: care about runState for restarting agents
+    //   switch (agent.runState) {
+    //     case 'stopped':
+    //     case 'failed':
+    //       this.logger.info(`Starting agent ${agentId}`)
+    //       await app.service('agents').patch(agentId, {
+    //         runState: 'starting'
+    //       })
+    //     break;
+    //   }
+    } else {
+      await this.removeAgent(agentId)
     }
   }
 
   async work() {
     this.logger.info('waiting for jobs')
-    const worker = new Worker("agent:updates", async (job: Job) => {
+    new Worker("agent:updates", async (job: Job) => {
       switch(job.name) {
           case 'agent:updated':
               this.agentUpdated(job.data.agentId)
@@ -54,15 +102,11 @@ export class CloudAgentWorker {
           default:
               this.logger.error(`Received unknown job ${job.name} from queue ${job.queueName}`)
               throw new Error(`Received unknown job ${job.name} from queue ${job.queueName}`)
-              break
       }
 
     },
     {
-      connection: {
-        host: 'localhost',
-        port: 6379
-      }
+      connection: bullMQConnection
     })
   }
 }
