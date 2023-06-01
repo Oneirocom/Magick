4ab47df1980aef2a1c5d332a11ab7dabd3c194ce
moon <shawmakesmagic@gmail.com>
2023-06-01 09:04:57 -0700

    Add ngrok script and env var

---
+++ scripts/ngrok.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const ngrok = require('ngrok');
+
+(async function () {
+  const authToken = process.env.NGROK_AUTH_TOKEN;
+
+  if (!authToken) {
+    console.error('NGROK_AUTH_TOKEN is not defined in the .env file');
+    process.exit(1);
+  }
+
+  // Connect to ngrok with your auth token
+  await ngrok.authtoken(authToken);
+
+  // Start ngrok tunnel on port 3000 (you can change this to your desired port)
+  const url = await ngrok.connect(3000);
+
+  console.log(`ngrok tunnel started at ${url}`);
+})().catch(error => {
+  console.error('Error while starting ngrok:', error);
+  process.exit(1);
+});
