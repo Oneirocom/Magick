8192cefbf6fc94fe59b517dfae9e941e567f25d8
moon <shawmakesmagic@gmail.com>
2023-05-31 20:31:15 -0700

    add-github

--- plugins/github/server/src/index.ts
+++ plugins/github/server/src/index.ts
@@ -9,14 +9,13 @@ type StartGithubArgs = {
 }
 
 function getAgentMethods() {
-  async function startGithub({
-    agent,
-    spellRunner,
-  }: StartGithubArgs) {
+  async function startGithub({ agent, spellRunner }: StartGithubArgs) {
     const { data } = agent.data
-    if (!data) return console.log("No data for this agent")
-    if (!data.github_enabled) return console.log("Github is not enabled for this agent")
-    if (!data.github_access_token) return console.log("Github Access Token is not set for this agent")
+    if (!data) return console.log('No data for this agent')
+    if (!data.github_enabled)
+      return console.log('Github is not enabled for this agent')
+    if (!data.github_access_token)
+      return console.log('Github Access Token is not set for this agent')
     console.log('starting github connect')
     try {
       const github = new GithubConnector({
@@ -30,7 +29,8 @@ function getAgentMethods() {
   }
 
   async function stopGithub({ agent }) {
-    if (!agent.github) return console.warn("Github isn't running, can't stop it")
+    if (!agent.github)
+      return console.warn("Github isn't running, can't stop it")
     try {
       await agent.github.destroy()
       agent.github = null
@@ -64,7 +64,7 @@ const inputSockets = [
     socket: 'trigger',
     name: 'trigger',
     type: triggerSocket,
-  }
+  },
 ]
 
 const outputSockets = [
@@ -72,26 +72,50 @@ const outputSockets = [
     socket: 'output',
     name: 'output',
     type: eventSocket,
-  }
+  },
 ]
 
 const GithubPlugin = new ServerPlugin({
   name: 'GithubPlugin',
   nodes: getNodes(),
   inputTypes: [
-    { name: 'Github (New Issues)', sockets: inputSockets, defaultResponseOutput: 'Github (New Issues)' },
-    { name: 'Github (New PRs)', sockets: inputSockets, defaultResponseOutput: 'Github (New PRs)' },
-    { name: 'Github (Issue Response)', sockets: inputSockets, defaultResponseOutput: 'Github (Issue Response)' },
+    {
+      name: 'Github (Issue)',
+      sockets: inputSockets,
+      defaultResponseOutput: 'Github (Issue Comment)',
+    },
+    {
+      name: 'Github (Pull Request)',
+      sockets: inputSockets,
+      defaultResponseOutput: 'Github (Pull Request Comment)',
+    },
+    {
+      name: 'Github (Comment)',
+      sockets: inputSockets,
+      defaultResponseOutput: 'Github (Comment Reply)',
+    },
   ],
   outputTypes: [
     {
-      name: 'Github (New Issues)',
+      name: 'Github (Issue Comment)',
       sockets: outputSockets,
-      handler: async ({
-        output, agent, event
-      }) => {
-        await handleResponse({ output, agent, event })
-      }
+      handler: async ({ output, agent, event }) => {
+        await agent.github.handleMessage(event, output, 'issue')
+      },
+    },
+    {
+      name: 'Github (Pull Request Comment)',
+      sockets: outputSockets,
+      handler: async ({ output, agent, event }) => {
+        await agent.github.handleMessage(event, output, 'pull_request')
+      },
+    },
+    {
+      name: 'Github (Comment Reply)',
+      sockets: outputSockets,
+      handler: async ({ output, agent, event }) => {
+        await agent.github.handleMessage(event, output, 'comment')
+      },
     },
   ],
   agentMethods: getAgentMethods(),
@@ -99,9 +123,9 @@ const GithubPlugin = new ServerPlugin({
     {
       name: 'Access Token',
       key: 'github_access_token',
-      global: true
-    }
-  ]
+      global: true,
+    },
+  ],
 })
 
 export default GithubPlugin
