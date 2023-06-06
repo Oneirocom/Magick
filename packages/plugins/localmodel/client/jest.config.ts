f026b791e3fe72af2411524b307e6c7d15924401
moon <shawmakesmagic@gmail.com>
2023-06-06 14:40:57 -0700

    Move localmodel to packages/plugins and lint

--- packages/plugins/localmodel/client/jest.config.ts
+++ packages/plugins/localmodel/client/jest.config.ts
@@ -13,5 +13,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/plugins/localmodel/client',
+  coverageDirectory: '../../coverage/packages/plugins/localmodel/client',
 }
