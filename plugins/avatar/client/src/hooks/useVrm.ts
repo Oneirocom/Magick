8e24ba8ecf2d8bdbf59410d9562fa76d9f19cde0
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-06-08 10:24:54 -0400

    add log for errors

--- plugins/avatar/client/src/hooks/useVrm.ts
+++ plugins/avatar/client/src/hooks/useVrm.ts
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { VRMLoaderPlugin, VRMUtils } from '@pixiv/three-vrm'
 import { DEFAULT_MODEL } from '../utils/constants'
+import { initLogger, getLogger } from '@magickml/core'
 
 export const useVrm = (vrmUrl) => {
   const [vrm, setVrm] = useState(null)
+  initLogger({ name: "AIDE" })
+  const logger = getLogger()
 
   useEffect(() => {
     (async () => {
@@ -18,9 +21,11 @@ export const useVrm = (vrmUrl) => {
         gltf = await gltfLoader.loadAsync(vrmUrl)
       } catch (e) {
         //default vrm ( Ref! drag/drop model )
+        logger.error('non standard model %s', vrmUrl)
         gltf = await gltfLoader.loadAsync(DEFAULT_MODEL)
       }
       if (!gltf) {
+        logger.error('non existing model %s', vrmUrl)
         return
       }
 
