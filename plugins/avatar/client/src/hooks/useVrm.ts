896ffecc413b686e7754cfdb32cd2d527a28e415
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-06-07 21:13:32 -0400

    load default model when non-standard one is loaded with try-catch

--- plugins/avatar/client/src/hooks/useVrm.ts
+++ plugins/avatar/client/src/hooks/useVrm.ts
@@ -8,13 +8,27 @@ export const useVrm = (vrmUrl) => {
 
   useEffect(() => {
     (async () => {
+      let gltf;
+
       if (!vrmUrl) {
         return
       }
-      customDebug().log('useVrm#useEffect: vrmUrl: ', vrmUrl)
-      const gltf = await gltfLoader.loadAsync(vrmUrl)
-      const newVrm = gltf.userData.vrm; // VRMを制御するためのクラス `VRM` が `gltf.userData.vrm` に入っています
-      VRMUtils.rotateVRM0(newVrm); // 読み込んだモデルがVRM0.0の場合は回す
+
+      //load vrm file
+      try {
+        gltf = await gltfLoader.loadAsync(vrmUrl)
+      } catch (e) {
+        //default vrm ( Ref! drag/drop model )
+        gltf = await gltfLoader.loadAsync('/models/bbb.vrm')
+      }
+
+      if (!gltf) {
+        return
+      }
+
+      //set avatar with new gltf tranform
+      const newVrm = gltf.userData.vrm; 
+      VRMUtils.rotateVRM0(newVrm);
       setVrm(newVrm)
     })()
   }, [vrmUrl])
@@ -22,5 +36,5 @@ export const useVrm = (vrmUrl) => {
   return vrm
 }
 
-const gltfLoader = new GLTFLoader(); // GLTFを読み込むLoader
-gltfLoader.register((parser) => new VRMLoaderPlugin(parser)); // GLTFLoaderにVRMLoaderPluginをインストール
+const gltfLoader = new GLTFLoader();
+gltfLoader.register((parser) => new VRMLoaderPlugin(parser));
