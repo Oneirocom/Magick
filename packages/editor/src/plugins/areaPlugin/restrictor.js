728fa178775ab26e1737ca9ee28388cceb757a32
moon <shawmakesmagic@gmail.com>
2023-06-12 16:59:48 -0700

    Better lerpage

--- packages/editor/src/plugins/areaPlugin/restrictor.js
+++ packages/editor/src/plugins/areaPlugin/restrictor.js
@@ -4,29 +4,26 @@ export class Restrictor {
     this.scaleExtent = scaleExtent
     this.zoomLerpFactor = zoomLerpFactor
     this.translateExtent = translateExtent
-
+    this.lastZoom = 2.0
     if (scaleExtent) editor.on('zoom', this.restrictZoom.bind(this))
     if (translateExtent)
       editor.on('translate', this.restrictTranslate.bind(this))
   }
   lastZoom = null
   restrictZoom(data) {
-    if(!this.lastZoom) {
-      this.lastZoom = data.zoom
-    }
-
-    // lerp from lastZoom to zoom, weighted 1/3 toward zoom
-    const avgZoom = (data.zoom * this.zoomLerpFactor) + (this.lastZoom * (1 - this.zoomLerpFactor))
-
-    this.lastZoom = data.zoom
-    data.zoom = avgZoom
-
-
     const se =
       typeof this.scaleExtent === 'boolean'
         ? { min: 0.1, max: 1 }
         : this.scaleExtent
-    // const tr = data.transform;
+
+    if (this.lastZoom < se.min) this.lastZoom = se.min
+    else if (this.lastZoom > se.max) this.lastZoom = se.max
+
+    // lerp from lastZoom to zoom, weighted 1/3 toward zoom
+    const avgZoom = (data.zoom * this.zoomLerpFactor) + (this.lastZoom * (1 - this.zoomLerpFactor))
+
+    this.lastZoom = avgZoom
+    data.zoom = avgZoom
 
     if (data.zoom < se.min) data.zoom = se.min
     else if (data.zoom > se.max) data.zoom = se.max
