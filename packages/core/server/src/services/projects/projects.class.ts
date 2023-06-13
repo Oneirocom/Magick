6132703c96b6cec532b776844265a1d76b278b35
moon <shawmakesmagic@gmail.com>
2023-06-13 16:16:07 -0700

    Import and replace are working, consolidated some code

--- packages/core/server/src/services/projects/projects.class.ts
+++ packages/core/server/src/services/projects/projects.class.ts
@@ -98,7 +98,7 @@ export class ProjectsService {
 
 
     // Map agents, documents, and spells with updated information for the new project
-    const mappedAgents = agents.map(agent => {
+    const mappedAgents = (agents ?? []).map(agent => {
       delete agent.id
       if (!agent.data) agent.data = '{}'
       if ('spells' in agent) delete agent.spells // <-- Updated to fix eliza import
@@ -108,7 +108,7 @@ export class ProjectsService {
       return agent
     })
 
-    const mappedDocuments = documents.map(doc => {
+    const mappedDocuments = (documents ?? []).map(doc => {
       delete doc.id
       doc.projectId = projectId
       return doc
@@ -117,7 +117,7 @@ export class ProjectsService {
     // Create a key value of old IDs to new IDs for spells
     const spellKeys = {}
 
-    const mappedSpells = spells.map(spell => {
+    const mappedSpells = (spells ?? []).map(spell => {
       delete spell.updatedAt
 
       // generate new uuid
@@ -145,7 +145,8 @@ export class ProjectsService {
     const agentResponse: any[] = []
     if (mappedAgents.length > 0) {
       mappedAgents.forEach(async agent => {
-        console.log('creating agent', agent)
+        // todo: clear this up so validation works
+        delete(agent.runState)
 
         const r = await app.service('agents').create(agent)
         agentResponse.push(r)
