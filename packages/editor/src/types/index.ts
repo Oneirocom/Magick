ce9772c82ad5350489eb336094a80c36da52af54
moon <shawmakesmagic@gmail.com>
2023-06-25 12:12:09 -0700

    Remove unnecessary types

--- packages/editor/src/types/index.ts
+++ packages/editor/src/types/index.ts
@@ -20,22 +20,6 @@ export interface OpenAIModel {
   name: string
 }
 
-export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
-}
-
-export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'Default (GPT-3.5)',
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-  },
-}
-
 export interface Message {
   role: Role
   content: string
