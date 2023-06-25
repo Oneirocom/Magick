ecb9fbe4c218dcf3dec29691c6951238e1aa1e70
moon <shawmakesmagic@gmail.com>
2023-06-24 17:18:38 -0700

    More things broken out into separate PRs

--- packages/editor/src/screens/EventWindow/event.ts
+++ packages/editor/src/screens/EventWindow/event.ts
@@ -7,7 +7,6 @@ export interface Column {
     | 'content'
     | 'type'
     | 'channel'
-    | 'channelType'
     | 'observer'
     | 'date'
   label: string
@@ -24,7 +23,6 @@ export const columns: Column[] = [
   { id: 'type', label: 'Type', minWidth: 65 },
   { id: 'channel', label: 'Channel', minWidth: 65 },
   { id: 'observer', label: 'Observer', minWidth: 65 },
-  { id: 'channelType', label: 'Channel Type', minWidth: 65 },
   { id: 'date', label: 'Date', minWidth: 65 },
 ]
 
@@ -36,7 +34,6 @@ export interface EventData {
   content: string
   type: string
   channel: string
-  channelType: string
   observer: string
   date: string
 }
