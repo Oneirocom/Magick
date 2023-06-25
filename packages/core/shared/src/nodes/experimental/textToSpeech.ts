046418560d441a1a7e35319b547f84b394fd0603
moon <shawmakesmagic@gmail.com>
2023-06-25 12:14:16 -0700

    Fix textTospeech -> textToSpeech

--- packages/core/shared/src/nodes/experimental/textToSpeech.ts
+++ packages/core/shared/src/nodes/experimental/textToSpeech.ts
@@ -53,7 +53,7 @@ export class TextToSpeech extends MagickComponent<Promise<WorkerReturn>> {
 
     // get completion providers for text and chat categories
     const completionProviders = pluginManager.getCompletionProviders('audio', [
-      'textTospeech',
+      'textToSpeech',
     ]) as CompletionProvider[]
 
     // get the models from the completion providers and flatten into a single array
@@ -160,7 +160,7 @@ export class TextToSpeech extends MagickComponent<Promise<WorkerReturn>> {
     }
   ) {
     const completionProviders = pluginManager.getCompletionProviders('audio', [
-      'textTospeech',
+      'textToSpeech',
     ]) as CompletionProvider[]
   
     const model = (node.data as { model: string }).model as string
