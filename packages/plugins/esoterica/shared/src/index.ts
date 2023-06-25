85c322369a4c45ff9f2e8fee162316af541edfb0
moon <shawmakesmagic@gmail.com>
2023-06-25 12:20:03 -0700

    Remove unused plugins

--- packages/plugins/esoterica/shared/src/index.ts
+++
@@ -1,9 +0,0 @@
-// DOCUMENTED 
-/**
- * This module exports an array containing a single class which is the ComputeNatalChart class.
- * It is used for computing natal charts.
- */
-
-import { ComputeNatalChart } from './nodes/ComputeNatalChart';
-
-export default [ComputeNatalChart];
