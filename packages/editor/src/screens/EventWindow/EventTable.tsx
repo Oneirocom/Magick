b01baabd30604113163c0713131b4870af241cb9
benbot <bbotwin@atlassian.com>
2023-06-29 14:06:39 -0400

    swapped from fetch to feathers client

--- packages/editor/src/screens/EventWindow/EventTable.tsx
+++ packages/editor/src/screens/EventWindow/EventTable.tsx
@@ -2,6 +2,7 @@
 // Import statements kept as-is
 import { TableComponent } from '@magickml/client-core'
 import { API_ROOT_URL } from '@magickml/config'
+import { useFeathers  } from '@magickml/client-core'
 import { Delete, MoreHoriz, Refresh } from '@mui/icons-material'
 import {
   Button,
@@ -79,6 +80,8 @@ function EventTable({ events, updateCallback }) {
   const [selectedRow, setSelectedRow] = useState(null)
   const [selectedRows, setSelectedRows] = useState<string[]>([])
   const [currentPage, setCurrentPage] = useState(0)
+
+  const { client: feathersClient } = useFeathers();
 
   const handleActionClick = (event, row) => {
     setAnchorEl(event.currentTarget)
@@ -227,16 +230,12 @@ function EventTable({ events, updateCallback }) {
 
   // Handle events deletion
   const handleDeleteMany = async (event: any) => {
-    const isDeleted = await fetch(`${API_ROOT_URL}/events`, {
-      method: 'DELETE',
-      headers: {
-        Authorization: `Bearer ${token}`,
+    const isDeleted = await feathersClient.service('events').remove(null, {
+      query: {
+        id: {
+          $in: selectedRows,
+        },
       },
-      body: JSON.stringify({
-        _id: {
-          $in: selectedRows
-        }
-      })
     });
   
     if (isDeleted) {
