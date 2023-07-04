287e4c3f42c13c55b7c5fc11b738667eff2dc987
Michael <m.sharpe@project89.org>
2023-07-04 16:22:08 -0700

    Fix event store type socket input

--- packages/core/shared/src/nodes/events/EventStore.ts
+++ packages/core/shared/src/nodes/events/EventStore.ts
@@ -91,7 +91,7 @@ export class EventStore extends MagickComponent<Promise<void>> {
     const { projectId } = context
 
     const event = inputs['event'][0] as Event
-    const typeSocket = inputs['type'] && inputs['type'][0]
+    const typeSocket = inputs['type'] && (inputs['type'][0] as string)
     const sender = (inputs['sender'] ? inputs['sender'][0] : null) as string
     let content = (inputs['content'] ? inputs['content'][0] : null) as string
     let embedding = (
@@ -104,9 +104,10 @@ export class EventStore extends MagickComponent<Promise<void>> {
 
     const typeData = node?.data?.type as string
     const type =
-      typeSocket ?? (typeData !== undefined && typeData.length > 0)
+      typeSocket ??
+      ((typeData !== undefined && typeData.length > 0
         ? typeData.toLowerCase().trim()
-        : 'none'
+        : 'none') as string)
 
     if (!content) {
       content = (event as Event).content || 'Error'
