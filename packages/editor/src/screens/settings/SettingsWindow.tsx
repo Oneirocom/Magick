4a74869811c1e697d0bfdae0844eebbe0536cc1f
emmyn5600 <emmy66418@gmail.com>
2023-07-04 22:26:00 +0200

    adding button functionality to save a button

--- packages/editor/src/screens/settings/SettingsWindow.tsx
+++ packages/editor/src/screens/settings/SettingsWindow.tsx
@@ -1,23 +1,11 @@
-// DOCUMENTED
-import { Tooltip } from '@magickml/client-core'
-import { pluginManager } from '@magickml/core'
-import { Clear } from '@mui/icons-material/'
-import { IconButton, Input, Button } from '@mui/material'
-import { useState } from 'react'
-import { useSnackbar } from 'notistack'
-import styles from './styles.module.scss'
+import { Tooltip } from '@magickml/client-core';
+import { pluginManager } from '@magickml/core';
+import { Clear } from '@mui/icons-material/';
+import { IconButton, Input, Button } from '@mui/material';
+import { useState } from 'react';
+import { useSnackbar } from 'notistack';
+import styles from './styles.module.scss';
 
-/**
- * This function component renders an input form for entering an API key and buttons to copy and clear the key.
- *
- * @param {Object} props - The properties of this component.
- * @param {string} props.displayName - The display name for the service key.
- * @param {string} props.keyName - The key name of the service key.
- * @param {string} props.getUrl - The URL where the user can find the API key.
- * @param {(key: string, value: string) => void} props.setKey - Function to set service key in local storage.
- * @param {(key: string) => string} props.getKey - Function to get service key from local storage.
- * @return {JSX.Element} The rendered component.
- */
 const SettingsWindowChild = ({
   displayName,
   keyName,
@@ -25,10 +13,8 @@ const SettingsWindowChild = ({
   setKey,
   getKey,
 }) => {
-  // State for copy and clear button labels
-  // const [copy, setCopy] = useState('Copy')
-  const [clear, setClear] = useState('Clear')
-  // Snackbar for showing notifications
+  const [clear, setClear] = useState('Clear');
+ 
 
   return (
     <div className={styles['child']}>
@@ -41,36 +27,21 @@ const SettingsWindowChild = ({
         id={keyName}
         name={keyName}
         value={getKey(keyName) || ''}
-        onChange={e => {
-          setKey(keyName, e.target.value)
+        onChange={(e) => {
+          setKey(keyName, e.target.value);
         }}
       />
       {getKey(keyName) && getKey(keyName) !== '' && (
         <>
-          {/* CANNOT COPY SECRET KEYS  */}
-          {/* <Tooltip title={copy}>
-            <IconButton
-              className={styles['icon']}
-              onClick={() => {
-                navigator.clipboard.writeText(getKey(keyName))
-                setCopy('Copied')
-                setTimeout(() => {
-                  setCopy('Copy')
-                }, 1000)
-              }}
-            >
-              <FileCopy />
-            </IconButton>
-          </Tooltip> */}
           <Tooltip title={clear}>
             <IconButton
               className={styles['icon']}
               onClick={() => {
-                setKey(keyName, '')
-                setClear('Cleared')
+                setKey(keyName, '');
+                setClear('Cleared');
                 setTimeout(() => {
-                  setClear('Clear')
-                }, 1000)
+                  setClear('Clear');
+                }, 1000);
               }}
             >
               <Clear />
@@ -85,50 +56,35 @@ const SettingsWindowChild = ({
           available here.
         </a>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-/**
- * This function component renders the settings window.
- *
- * @return {JSX.Element} The rendered component.
- */
 const SettingsWindow = () => {
-  // State for nonce
-  const [nonce, setNonce] = useState(0)
-  const { enqueueSnackbar } = useSnackbar()
+  const { enqueueSnackbar } = useSnackbar();
 
+  // State for keeping track of secret key values
+  const [secretKeys, setSecretKeys] = useState(() => {
+    const secrets = window.localStorage.getItem('secrets');
+    return secrets ? JSON.parse(secrets) : {};
+  });
 
   // Function to get service key from local storage
-  const getKey = key => {
-    if (!window.localStorage.getItem('secrets')) {
-      window.localStorage.setItem('secrets', JSON.stringify({}))
-    }
+  const getKey = (key) => {
+    return secretKeys[key] || '';
+  };
 
-    const secrets = window.localStorage.getItem('secrets')
-
-    return JSON.parse(secrets)[key]
-  }
-
-  // Function to set service key in local storage
+  // Function to update secret key in state
   const setKey = (newKey, newValue) => {
-    const secrets = window.localStorage.getItem('secrets')
-    const json = secrets ? JSON.parse(secrets) : {}
-    const newJsonString = JSON.stringify({ ...json, [newKey]: newValue })
-    window.localStorage.setItem('secrets', newJsonString)
-    setNonce(nonce + 1)
-  }
+    setSecretKeys((prevSecretKeys) => ({
+      ...prevSecretKeys,
+      [newKey]: newValue,
+    }));
+  };
 
   const handleSaveKey = () => {
-    const settings = pluginManager.getSecrets(true).reduce((acc, value) => {
-      const keyValue = getKey(value.key);
-      return { ...acc, [value.key]: keyValue };
-    }, {});
-
-    // Perform validation on the settings
-    const hasValue = Object.values(settings).some(value => value !== '');
+    // Perform validation on the secretKeys object
+    const hasValue = Object.values(secretKeys).some((value) => value !== '');
 
     if (!hasValue) {
       // Show an error message if no setting has a value
@@ -138,12 +94,12 @@ const SettingsWindow = () => {
       return;
     }
 
-    localStorage.setItem('settings', JSON.stringify(settings));
+    localStorage.setItem('secrets', JSON.stringify(secretKeys));
 
-    pluginManager.getSecrets(true).forEach(value => {
-      const { name, displayName } = value;
+    pluginManager.getSecrets(true).forEach((value) => {
+      const { name, displayName }:any = value;
       const secretName = displayName || name;
-      const valueName = settings[value.key];
+      const valueName = secretKeys[value.key];
 
       if (valueName) {
         enqueueSnackbar(`${secretName.toLowerCase()} set successfully`, {
@@ -153,47 +109,42 @@ const SettingsWindow = () => {
     });
   };
 
-
-
-  const globalSecrets = pluginManager.getSecrets(true)
+  const globalSecrets = pluginManager.getSecrets(true);
 
   return (
-    nonce !== null && (
-      <>
-        <div className={styles['settings-editor']}>
-          {globalSecrets.map((value, index) => {
-            return (
-              <SettingsWindowChild
-                key={value.key}
-                displayName={value.name}
-                keyName={value.key}
-                getUrl={value.getUrl}
-                setKey={setKey}
-                getKey={getKey}
-              />
-            )
-          })}
-          <div className={styles['child']}>
-            <p>
-              We do not keep your API keys. They are stored in your browser's
-              local storage.
-            </p>
-          </div>
-          <div>
-            <Button
-              onClick={handleSaveKey}
-              className={styles.btn}
-              variant="outlined"
-              style={{ marginLeft: '1rem' }}
-            >
-              Save
-            </Button>
-          </div>
+    <>
+      <div className={styles['settings-editor']}>
+        {globalSecrets.map((value) => {
+          return (
+            <SettingsWindowChild
+              key={value.key}
+              displayName={value.name}
+              keyName={value.key}
+              getUrl={value.getUrl}
+              setKey={setKey}
+              getKey={getKey}
+            />
+          );
+        })}
+        <div className={styles['child']}>
+          <p>
+            We do not keep your API keys. They are stored in your browser's
+            local storage.
+          </p>
         </div>
+        <div>
+          <Button
+            onClick={handleSaveKey}
+            className={styles.btn}
+            variant="outlined"
+            style={{ marginLeft: '1rem' }}
+          >
+            Save
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-      </>
-    )
-  )
-}
-
-export default SettingsWindow
+export default SettingsWindow;
