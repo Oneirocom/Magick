3c090c26e0b755aaa5b72ebe31e4e1b0c5ec7781
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-07-05 16:01:52 -0400

    cog client

---
+++ packages/plugins/cog/client/src/index.ts
@@ -0,0 +1,33 @@
+// DOCUMENTED
+/**
+ * A plugin for interacting with cog's API.
+ * @class
+ */
+import { ClientPlugin, InputControl } from '@magickml/core'
+import shared from '@magickml/plugin-cog-shared'
+
+// Importing shared variables from plugin-cog-shared module
+const { secrets, completionProviders } = shared
+
+// Input controls for text completion
+const textCompletionControls = []
+
+// Object containing all input controls for different completion types
+const inspectorControls = {
+  text: textCompletionControls,
+}
+
+// Creating a new cogPlugin instance
+const cogPlugin = new ClientPlugin({
+  name: 'cogPlugin',
+  secrets, // API Key and Model ID secrets
+  completionProviders: completionProviders.map(provider => {
+    // Adding custom input controls for each completion type
+    return {
+      ...provider,
+      inspectorControls: inspectorControls[provider.subtype],
+    }
+  }),
+})
+
+export default cogPlugin
