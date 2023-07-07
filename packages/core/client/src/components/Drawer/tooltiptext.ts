cbb116fd790582cf15dd9101d9b1fc860c9e35dc
emmyn5600 <emmy66418@gmail.com>
2023-07-07 15:09:39 +0200

    adding tooltips on sidemenus

---
+++ packages/core/client/src/components/Drawer/tooltiptext.ts
@@ -0,0 +1,12 @@
+// all drawer tooltip text
+
+export const drawerTooltipText = {
+ spells: "Spells are the basic unit of work in MagickML. ",
+ agents: "Agents are the basic unit of work in MagickML. ",
+ documents: "Documents are the basic unit of work in MagickML. ",
+ events: "Events are the basic unit of work in MagickML.",
+ requests: "Requests are the basic unit of work in MagickML. ",
+ tasks: "Tasks are the basic unit of work in MagickML.",
+ avatar: "Avatars are the basic unit of work in MagickML.",
+ settings: "Settings are the basic unit of work in MagickML.",
+}
