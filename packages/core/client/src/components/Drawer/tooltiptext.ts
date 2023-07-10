d6010f87111b116003caa9a0cf97f271f59233ec
nsabimana emmanuel <emmy@nsabimanas-MacBook-Pro.local>
2023-07-10 19:41:24 +0200

    Add descriptive text for each navbar menu

--- packages/core/client/src/components/Drawer/tooltiptext.ts
+++ packages/core/client/src/components/Drawer/tooltiptext.ts
@@ -1,12 +1,12 @@
 // all drawer tooltip text
 
 export const drawerTooltipText = {
- spells: "Spells are the basic unit of work in MagickML. ",
- agents: "Agents are the basic unit of work in MagickML. ",
- documents: "Documents are the basic unit of work in MagickML. ",
- events: "Events are the basic unit of work in MagickML.",
- requests: "Requests are the basic unit of work in MagickML. ",
- tasks: "Tasks are the basic unit of work in MagickML.",
- avatar: "Avatars are the basic unit of work in MagickML.",
- settings: "Settings are the basic unit of work in MagickML.",
+ spells: "Create and manage spell node graphs for agents to use. ",
+ agents: "Create and manage autonomous agents powered by spells. ",
+ documents: "Information vectorized as embeddings for quick search and retrieval. ",
+ events: "Data stored for use as short-term memory between spell runs.",
+ requests: "Historical outbound requests made to LLM web services. ",
+ tasks: "Objectives for agents to iterate through and complete.",
+ avatar: "Chat with your agents embodied with a 3D avatar.",
+ settings: "Global settings used in the Playtest window.",
 }
