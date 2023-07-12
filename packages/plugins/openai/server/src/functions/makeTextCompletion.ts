a55032dba864bf6f9b5a03e191024a8ddb1ea4ae
Knar <knarxr@protonmail.com>
2023-07-12 14:26:34 -0400

    fix order of operations for authorization header

--- packages/plugins/openai/server/src/functions/makeTextCompletion.ts
+++ packages/plugins/openai/server/src/functions/makeTextCompletion.ts
@@ -64,7 +64,7 @@ export async function makeTextCompletion(
   // Set up headers for the API request.
   const headers = {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + openaiKey ? openaiKey : DEFAULT_OPENAI_KEY,
+    Authorization: 'Bearer ' + (openaiKey ? openaiKey : DEFAULT_OPENAI_KEY),
   }
 
   // Make the API request and handle the response.
