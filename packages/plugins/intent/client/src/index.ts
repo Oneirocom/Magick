f638cb603a9ef078b836f06916b1e7e97cb622db
Knar <knarxr@protonmail.com>
2023-07-19 19:45:29 -0400

    remove unused input/output sockets

--- packages/plugins/intent/client/src/index.ts
+++ packages/plugins/intent/client/src/index.ts
@@ -1,27 +1,6 @@
 import { ClientPlugin, eventSocket, triggerSocket } from '@magickml/core'
 import { getNodes } from '@magickml/plugin-intent-shared'
 
-const inputSockets = [
-  {
-    socket: 'output',
-    name: 'output',
-    type: eventSocket,
-  },
-  {
-    socket: 'trigger',
-    name: 'trigger',
-    type: triggerSocket,
-  },
-]
-
-const outputSockets = [
-  {
-    socket: 'output',
-    name: 'output',
-    type: eventSocket,
-  },
-]
-
 const IntentPlugin = new ClientPlugin({
   name: 'IntentPlugin',
   nodes: getNodes(),
