7ea573b08e33fdc52477f423912b6ec056cb82af
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-07-20 13:28:19 -0400

    cog/google seperate functions

--- packages/core/server/src/metering/openaiMetering.ts
+++ packages/core/server/src/metering/openaiMetering.ts
@@ -1,47 +1,4 @@
-import { sendEvent, Event } from './meteringClient'
-import { CompletionHandlerInputData, getLogger } from '@magickml/core'
-import { v4 as uuidv4 } from 'uuid'
-import { OPENMETER_ENABLED } from '@magickml/config'
-
-/**
- * Sends a metering event to the OpenMeter server.
- *
- * @param {string} projectId - The id of the project.
- * @param {number} totalTokens - The total number of tokens.
- * @param {string} model - The model used for the completion.
- */
-async function sendMeteringEvent(
-  projectId: string,
-  totalTokens: number,
-  model: string
-) {
-  if (!OPENMETER_ENABLED) {
-    return
-  }
-  const logger = getLogger()
-  const event: Event = {
-    specversion: '1.0',
-    id: uuidv4(),
-    source: 'magick-cloud',
-    type: model,
-    subject: projectId,
-    time: new Date().toISOString(),
-    data: {
-      total_tokens: totalTokens.toString(),
-      model,
-      project_id: projectId,
-    },
-  }
-
-  try {
-    logger.info('Sending metering event: %o', event)
-    await sendEvent(event)
-    return true
-  } catch (err) {
-    logger.error('Error sending metering event: %o', err)
-    return false
-  }
-}
+import { sendMeteringEvent, MeteringEvent } from './meteringClient'
 
 type OpenAIMeterData = {
   projectId: string
@@ -49,6 +6,28 @@ type OpenAIMeterData = {
   model: string
 }
 
+type GoogleAIMeterData = {
+  projectId: string
+  totalTokens: number
+  model: string
+}
+
+type CogMeterData = {
+  projectId: string
+  durationMs: number
+  model: string
+}
+
+
+
 export async function trackOpenAIUsage(data: OpenAIMeterData) {
-  return await sendMeteringEvent(data.projectId, data.totalTokens, data.model)
+  return await sendMeteringEvent(data.projectId, data.model, data.totalTokens)
+}
+
+export async function trackGoogleAIUsage(data: GoogleAIMeterData) {
+  return await sendMeteringEvent(data.projectId, data.model, data.totalTokens)
+}
+
+export async function trackCogUsage(data: CogMeterData) {
+  return await sendMeteringEvent(data.projectId, data.model, undefined, data.durationMs)
 }
