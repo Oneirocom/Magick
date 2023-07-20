73a217a126cb3de26ab6ccde745a478d02173783
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-07-20 16:05:46 -0400

    update metering input structure

--- packages/core/server/src/metering/meters.ts
+++ packages/core/server/src/metering/meters.ts
@@ -2,32 +2,41 @@ import { sendMeteringEvent } from './meteringClient'
 
 type OpenAIMeterData = {
   projectId: string
-  totalTokens: number
   model: string
+  totalTokens: number
 }
 
 type GoogleAIMeterData = {
   projectId: string
-  totalTokens: number
   model: string
+  count: number
 }
 
 type CogMeterData = {
   projectId: string
-  durationMs: number
   model: string
+  durationMs: number
 }
 
-
-
 export async function trackOpenAIUsage(data: OpenAIMeterData) {
-  return await sendMeteringEvent(data.projectId, data.model, data.totalTokens)
+  return await sendMeteringEvent({
+    project_id: data.projectId,
+    model: data.model,
+    total_tokens: data.totalTokens,
+  })
 }
 
 export async function trackGoogleAIUsage(data: GoogleAIMeterData) {
-  return await sendMeteringEvent(data.projectId, data.model, data.totalTokens)
+  return await sendMeteringEvent({
+    project_id: data.projectId,
+    model: data.model,
+    count: data.count,
+  })
 }
-
 export async function trackCogUsage(data: CogMeterData) {
-  return await sendMeteringEvent(data.projectId, data.model, undefined, data.durationMs)
+  return await sendMeteringEvent({
+    project_id: data.projectId,
+    model: data.model,
+    duration_ms: data.durationMs,
+  })
 }
