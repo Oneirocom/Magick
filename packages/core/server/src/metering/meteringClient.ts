d0e696d617540cd224bf049836272fe6afac756a
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-07-20 16:05:46 -0400

    update metering input structure

--- packages/core/server/src/metering/meteringClient.ts
+++ packages/core/server/src/metering/meteringClient.ts
@@ -7,10 +7,11 @@ import { v4 as uuidv4 } from 'uuid'
  * Represents the event data.
  */
 type MeteringEventData = {
-  total_tokens?: string
-  duration_ms?: string
-  model: string
   project_id: string
+  model: string
+  total_tokens?: number
+  duration_ms?: number
+  count?: number
 }
 
 /**
@@ -30,17 +31,13 @@ type MeteringEvent = {
  * Sends a metering event to the OpenMeter server.
  *
  * @param {string} projectId - The id of the project.
+ * @param {string} model - The model used for the completion.
  * @param {number} totalTokens - The total number of tokens, optional.
  * @param {number} durationMs - The duration in milliseconds, optional.
- * @param {string} model - The model used for the completion.
+ * @param {number} count - The number of completions, optional.
  * @return {Promise<boolean>} Indicates whether the event was sent successfully.
  */
-async function sendMeteringEvent(
-  projectId: string,
-  model: string,
-  totalTokens?: number,
-  durationMs?: number
-): Promise<boolean> {
+async function sendMeteringEvent(data: MeteringEventData): Promise<boolean> {
   if (!OPENMETER_ENABLED) {
     return Promise.resolve(false)
   }
@@ -50,14 +47,15 @@ async function sendMeteringEvent(
     specversion: '1.0',
     id: uuidv4(),
     source: 'magick-cloud',
-    type: model,
-    subject: projectId,
+    type: data.model,
+    subject: data.project_id,
     time: new Date().toISOString(),
     data: {
-      total_tokens: totalTokens?.toString(),
-      duration_ms: durationMs?.toString(),
-      model,
-      project_id: projectId,
+      project_id: data.project_id,
+      model: data.model,
+      total_tokens: data.total_tokens,
+      duration_ms: data.duration_ms,
+      count: data.count,
     },
   }
 
