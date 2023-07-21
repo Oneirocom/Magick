c436c964444929c2b23d1df886643324f864735a
emmyn5600 <emmy66418@gmail.com>
2023-07-21 10:11:49 +0200

    adding agent dropdown

--- packages/core/client/src/components/Drawer/AgentMenu.tsx
+++ packages/core/client/src/components/Drawer/AgentMenu.tsx
@@ -7,6 +7,7 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import { Popper } from '@mui/material';
 import Fade from '@mui/material/Fade';
@@ -15,7 +16,6 @@ import Menu from '@mui/material/Menu';
 import Paper from '@mui/material/Paper';
 import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
-import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -27,7 +27,9 @@ function AgentMenu() {
   `;
 
   const [open, setOpen] = React.useState(false);
+  const [sopen, setSOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
+  const anchorSRef = React.useRef(null);
 
   const handleToggle = () => {
     setOpen((prevOpen) => !prevOpen);
@@ -40,12 +42,24 @@ function AgentMenu() {
     setOpen(false);
   };
 
+  const handleSClose = (event) => {
+    if (anchorSRef.current && anchorSRef.current.contains(event.target)) {
+      return;
+    }
+    setSOpen(false);
+  };
+
+  const handleSToggle = () => {
+    setSOpen((prevOpen) => !prevOpen);
+  };
+
+
   return (
     <div>
       <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         <ListItem alignItems="flex-center">
           <ListItemAvatar>
-            <BorderedAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <BorderedAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 30, height: 30 }} />
           </ListItemAvatar>
           <ListItemText primary="Agent name" />
           <IconButton
@@ -59,38 +73,64 @@ function AgentMenu() {
           </IconButton>
         </ListItem>
       </List>
-
-      <MenuList
+      {/* select agent modal */}
+      <Menu
         id="positioned-demo-menu"
         anchorEl={anchorRef.current}
         open={open}
         onClose={handleClose}
         aria-labelledby="positioned-demo-button"
-       
-
       >
-        <MenuItem onClick={handleClose}>
-<ListItemAvatar>
-            <BorderedAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <MenuItem sx={{
+          px: 1,
+          py: 0,
+          width: 200,
+          justifyContent: 'space-between',
+        }}>
+          <ListItemAvatar sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}>
+            <BorderedAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 24, height: 24 }} />
+            <ListItemText primary="Agent1" sx={{
+              ml: 2,
+            }} />
           </ListItemAvatar>
-          <ListItemText>Copy</ListItemText>
-          <ListItemIcon>
-            <MoreIcon fontSize="small" />
+          <ListItemIcon >
+            <MoreIcon fontSize="small" onClick={handleSToggle}
+              ref={anchorSRef} />
           </ListItemIcon>
         </MenuItem>
-        <MenuItem >
-
-          Draft post
+        <Divider />
+        <MenuItem sx={{
+          px: 1,
+          py: 0,
+          width: 200,
+          justifyContent: 'space-between',
+        }}>
+          <ListItemAvatar sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}>
+            <BorderedAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 24, height: 24 }} />
+            <ListItemText primary="Agent2" sx={{
+              ml: 2,
+            }} />
+          </ListItemAvatar>
+          <ListItemIcon >
+            <MoreIcon fontSize="small" onClick={handleSToggle}
+              ref={anchorSRef} />
+          </ListItemIcon>
         </MenuItem>
         <Divider />
         <MenuItem onClick={handleClose}>
           <List sx={{
-                px: 0,
-                py: 0,
-              }}>
+            px: 0,
+            py: 0,
+          }}>
             <ListItem
               sx={{
-              
+
                 px: 0,
                 py: 0,
               }}
@@ -102,7 +142,31 @@ function AgentMenu() {
             </ListItem>
           </List>
         </MenuItem>
-      </MenuList>
+      </Menu>
+      {/* second mini menu */}
+      <Menu
+        id="sub_menu"
+        anchorEl={anchorSRef.current}
+        open={sopen}
+        onClose={handleSClose}
+        aria-labelledby="sub_menu"
+      >
+        <MenuItem sx={{ py: 0 , width:180 }}  >
+          Rename
+        </MenuItem>
+        <Divider />
+        <MenuItem sx={{ py: 0 }} >
+          Delete
+        </MenuItem>
+        <Divider />
+        <MenuItem sx={{ py: 0 }} >
+          Change Image
+        </MenuItem>
+        <Divider />
+        <MenuItem sx={{ py: 0 }} >
+          Other Options
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
