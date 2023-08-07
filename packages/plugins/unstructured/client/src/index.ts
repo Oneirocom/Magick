07369c6fde9d612060bc5a00798f587f0acf6f6d
Knar <knarxr@protonmail.com>
2023-08-07 15:49:25 -0400

    copied intent plugin

---
+++ packages/plugins/unstructured/client/src/index.ts
@@ -0,0 +1,36 @@
+import { ClientPlugin } from '@magickml/core'
+import { getNodes } from '@magickml/plugin-intent-shared'
+import MediationIcon from '@mui/icons-material/Mediation'
+import IntentWindow from './windows/IntentWindow'
+
+class TaskIntentPluginPlugin extends ClientPlugin {
+  /**
+   * Constructs a new instance of the `TaskPlugin` class.
+   */
+  constructor() {
+    super({
+      name: 'IntentPlugin',
+      agentComponents: [],
+      inputTypes: [],
+      spellTemplates: [],
+      nodes: getNodes(),
+      drawerItems: [
+        {
+          path: '/intents',
+          icon: MediationIcon,
+          text: 'Intents',
+          tooltip: 'Create and manage intent stories for intent detection.',
+        },
+      ],
+      clientRoutes: [
+        {
+          path: '/intents',
+          component: IntentWindow,
+          plugin: 'IntentPlugin',
+        },
+      ],
+    })
+  }
+}
+
+export default new TaskIntentPluginPlugin()
