eafdb5df79d522c18a5958fb478ef142b3d28b9d
Knar <knarxr@protonmail.com>
2023-08-07 15:49:25 -0400

    copied intent plugin

---
+++ packages/plugins/unstructured/server/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'intent',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/plugin-intent/server',
+}
