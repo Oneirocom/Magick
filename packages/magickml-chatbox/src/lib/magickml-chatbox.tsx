0fd325fff082696a93c4ff473c575b96271bd214
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-08-18 14:33:59 -0400

    update chatbot icon and add default avatar image

--- packages/magickml-chatbox/src/lib/magickml-chatbox.tsx
+++ packages/magickml-chatbox/src/lib/magickml-chatbox.tsx
@@ -2,6 +2,7 @@ import { useFeatureFlagEnabled } from 'posthog-js/react'
 import styles from './magickml-chatbox.module.css'
 import { Widget } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
+import chatbot from '../assets/chatbot.png'
 
 /* eslint-disable-next-line */
 export interface MagickmlChatboxProps {}
@@ -15,6 +16,12 @@ export function MagickmlChatbox(props: MagickmlChatboxProps) {
     console.log(`New message incoming! ${newMessage}`);
   };
 
+  const getCustomLauncher = (handleToggle) => {
+    console.log('handleToggle = ', handleToggle.isWidgetOpened)
+    return <button className={styles['defaultCloseButton']} onClick={handleToggle}><img src={chatbot} /></button>
+  }
+    
+
   return (
     <div className={styles['container']}>
       {
@@ -23,7 +30,10 @@ export function MagickmlChatbox(props: MagickmlChatboxProps) {
             handleNewUserMessage={handleNewUserMessage}
             // profileAvatar={logo}
             title="Magick Bot"
+            emojis="true"
             subtitle="Welcome to Magick!"
+            showCloseButton="true"
+            launcher={handleToggle => getCustomLauncher(handleToggle)}
           />
         ) : ''
       }
