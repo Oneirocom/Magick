b8a808466b83b6090c341d8d745aeabf7439a161
ultrabot2022 <sayhellometaverse2022@gmail.com>
2023-08-23 16:56:50 -0400

    update chatbot style to match design

--- packages/magickml-chatbox/src/lib/magickml-chatbox.tsx
+++ packages/magickml-chatbox/src/lib/magickml-chatbox.tsx
@@ -3,6 +3,7 @@ import styles from './magickml-chatbox.module.css'
 import { Widget } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 import chatbot from '../assets/chatbot.png'
+import defaultAvatar from '../assets/defaultAvatar.jpg'
 
 /* eslint-disable-next-line */
 export interface MagickmlChatboxProps {}
@@ -17,8 +18,8 @@ export function MagickmlChatbox(props: MagickmlChatboxProps) {
   };
 
   const getCustomLauncher = (handleToggle) => {
-    console.log('handleToggle = ', handleToggle.isWidgetOpened)
-    return <button className={styles['defaultCloseButton']} onClick={handleToggle}><img src={chatbot} /></button>
+    // console.log('handleToggle = ', handleToggle.isWidgetOpened)
+    return <button className={styles['defaultCloseButton']} onClick={handleToggle}><img src={chatbot} alt=""/></button>
   }
     
 
@@ -28,12 +29,16 @@ export function MagickmlChatbox(props: MagickmlChatboxProps) {
         showChatBoxFlag ? (
           <Widget
             handleNewUserMessage={handleNewUserMessage}
-            // profileAvatar={logo}
+            // profileAvatar={defaultAvatar}
+            titleAvatar={defaultAvatar}
+            autofocus={true}
             title="Magick Bot"
             emojis="true"
             subtitle="Welcome to Magick!"
-            showCloseButton="true"
-            launcher={handleToggle => getCustomLauncher(handleToggle)}
+            showCloseButton={true}
+            // launcher={handleToggle => getCustomLauncher(handleToggle)}
+            launcherOpenImg={chatbot}
+            launcherCloseImg={chatbot}
           />
         ) : ''
       }
