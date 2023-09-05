9e9870a862da13f5c5bbeefda8e3e56a43f71947
Michael <m.sharpe@project89.org>
2023-09-04 23:41:17 -0700

    Fix build ts errors

--- packages/plugins/avatar/client/src/hooks/useLipSync.ts
+++ packages/plugins/avatar/client/src/hooks/useLipSync.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
-import { LipSync } from "../utils/lip.sync"
+import { LipSync } from '../utils/lip.sync'
 
-export const useLipSync = (avatarVrm) => {
-  const [lipSync, setLipSync] = useState(null)
+export const useLipSync = avatarVrm => {
+  const [lipSync, setLipSync] = useState<any>(null)
 
   useEffect(() => {
     if (!avatarVrm) {
       return
     }
-    const newLipSync = new LipSync(avatarVrm);
+    const newLipSync = new LipSync(avatarVrm)
     setLipSync(newLipSync)
   }, [avatarVrm])
 
