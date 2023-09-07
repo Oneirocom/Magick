528968082d3927998fda0d39060efcae0d202a11
Knar <knarxr@protonmail.com>
2023-09-07 16:03:53 -0400

    add up/down functions to migration

--- apps/server/migrations/20230907195051_embeddings.ts
+++ apps/server/migrations/20230907195051_embeddings.ts
@@ -1,10 +1,24 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-}
+  await knex.schema.alterTable('documents', (table) => {
+    table.dropColumn('content')
+    table.dropColumn('embedding')
+  })
 
+  await knex.schema.createTable('embeddings', (table) => {
+    table.increments('id').primary()
+    table.string('documentId').notNullable()
+    table.text('content').notNullable()
+    table.specificType('embedding', 'vector(1536)')
+  })
+}
 
 export async function down(knex: Knex): Promise<void> {
-}
+  await knex.schema.alterTable('documents', (table) => {
+    table.text('content')
+    table.specificType('embedding', 'vector(1536)')
+  })
 
+  await knex.schema.dropTable('embeddings')
+}
