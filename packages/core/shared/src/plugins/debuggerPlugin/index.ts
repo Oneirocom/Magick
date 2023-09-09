bdeb53ef1489af022d8e213a041ef5693eb1bf6c
Michael <m.sharpe@project89.org>
2023-09-09 01:02:51 -0700

    Toggle console logging from switch

---
+++ packages/core/shared/src/plugins/debuggerPlugin/index.ts
@@ -0,0 +1,31 @@
+import { IRunContextEditor, MagickNode } from '../../types'
+import { MagickComponent } from '../../engine'
+import { SwitchControl } from '../../dataControls/SwitchControl'
+
+function install(editor: IRunContextEditor) {
+  editor.on('componentregister', (component: MagickComponent<unknown>) => {
+    const builder = component.builder
+
+    // we are going to override the default builder with our own, and will invoke the original builder inside it.
+    component.builder = (node: MagickNode) => {
+      const switchControl = new SwitchControl({
+        dataKey: 'debug',
+        name: 'debug',
+        icon: 'bug',
+        label: 'Debug',
+        defaultValue: false,
+      })
+
+      node.inspector.add(switchControl)
+
+      builder.call(component, node)
+    }
+  })
+}
+
+const defaultExport = {
+  name: 'debuggerPlugin',
+  install,
+}
+
+export default defaultExport
