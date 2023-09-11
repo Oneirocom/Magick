f219c8800b6db8fc9021f6fd1ca6ba64d3cfaa8d
Benjamin Botwin <Benj@minbot.win>
2023-09-11 13:43:12 -0400

    Revert "Revert "Merge pull request #1292 from Oneirocom/cloud-agents""

--- packages/plugins/rest/server/src/services/api/api.schema.ts
+++ packages/plugins/rest/server/src/services/api/api.schema.ts
@@ -1,10 +1,10 @@
-// DOCUMENTED 
-import { resolve } from '@feathersjs/schema';
-import { Type, getValidator } from '@feathersjs/typebox';
-import type { Static } from '@feathersjs/typebox';
+// DOCUMENTED
+import { resolve } from '@feathersjs/schema'
+import { Type, getValidator } from '@feathersjs/typebox'
+import type { Static } from '@feathersjs/typebox'
 
-import type { HookContext } from '@magickml/server-core';
-import { dataValidator, queryValidator } from '@magickml/server-core';
+import type { HookContext } from '@magickml/server-core'
+import { dataValidator, queryValidator } from '@magickml/server-core'
 
 /**
  * Main data model schema
@@ -15,42 +15,43 @@ export const apiSchema = Type.Object(
     agentId: Type.String(),
     content: Type.String(),
     apiKey: Type.String(),
+    isCloud: Type.Optional(Type.Boolean()),
   },
-  { $id: 'Api', additionalProperties: false },
-);
+  { $id: 'Api', additionalProperties: false }
+)
 
-export type Api = Static<typeof apiSchema>;
-export const apiValidator = getValidator(apiSchema, dataValidator);
-export const apiResolver = resolve<Api, HookContext>({});
+export type Api = Static<typeof apiSchema>
+export const apiValidator = getValidator(apiSchema, dataValidator)
+export const apiResolver = resolve<Api, HookContext>({})
 
-export const apiExternalResolver = resolve<Api, HookContext>({});
+export const apiExternalResolver = resolve<Api, HookContext>({})
 
 export const apiDataSchema = Type.Object(
   {
     spellId: Type.Optional(Type.String()),
     agentId: Type.String(),
     content: Type.String(),
+    isCloud: Type.Optional(Type.Boolean()),
   },
   {
     $id: 'ApiData',
-  },
-);
+  }
+)
 
-
-export type ApiData = Static<typeof apiDataSchema>;
-export const apiDataValidator = getValidator(apiDataSchema, dataValidator);
-export const apiDataResolver = resolve<Api, HookContext>({});
+export type ApiData = Static<typeof apiDataSchema>
+export const apiDataValidator = getValidator(apiDataSchema, dataValidator)
+export const apiDataResolver = resolve<Api, HookContext>({})
 
 /**
  * Schema for updating existing entries
  */
 export const apiPatchSchema = Type.Partial(apiSchema, {
   $id: 'ApiPatch',
-});
+})
 
-export type ApiPatch = Static<typeof apiPatchSchema>;
-export const apiPatchValidator = getValidator(apiPatchSchema, dataValidator);
-export const apiPatchResolver = resolve<Api, HookContext>({});
+export type ApiPatch = Static<typeof apiPatchSchema>
+export const apiPatchValidator = getValidator(apiPatchSchema, dataValidator)
+export const apiPatchResolver = resolve<Api, HookContext>({})
 
 /**
  * Schema for allowed query properties
@@ -60,16 +61,17 @@ export const apiQueryProperties = Type.Pick(apiSchema, [
   'spellId',
   'content',
   'apiKey',
-]);
+  'isCloud',
+])
 
 export const apiQuerySchema = Type.Object(
   {
     spellId: Type.Optional(Type.String()),
     content: Type.String(),
   },
-  { additionalProperties: false },
-);
+  { additionalProperties: false }
+)
 
-export type ApiQuery = Static<typeof apiQuerySchema>;
-export const apiQueryValidator = getValidator(apiQuerySchema, queryValidator);
-export const apiQueryResolver = resolve<ApiQuery, HookContext>({});
+export type ApiQuery = Static<typeof apiQuerySchema>
+export const apiQueryValidator = getValidator(apiQuerySchema, queryValidator)
+export const apiQueryResolver = resolve<ApiQuery, HookContext>({})
