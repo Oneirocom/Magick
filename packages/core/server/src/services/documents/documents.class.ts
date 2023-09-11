35f9ca7652e5a1b4c70e9b37883d871807a67887
Michael <m.sharpe@project89.org>
2023-09-11 16:39:22 -0700

    Fix another document embedding bug

--- packages/core/server/src/services/documents/documents.class.ts
+++ packages/core/server/src/services/documents/documents.class.ts
@@ -60,6 +60,7 @@ export class DocumentService<
     }
 
     for (const element of elements) {
+      if (!element.content) continue
       if (data.hasOwnProperty('secrets')) {
         await docdb.fromString(element.content, element, {
           modelName,
