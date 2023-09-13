ee54d3787d5f66841b3a7885a25123075f38fd8c
Knar <knarxr@protonmail.com>
2023-09-13 12:13:14 -0400

    update UP function

--- apps/server/migrations/20230907195051_embeddings.ts
+++ apps/server/migrations/20230907195051_embeddings.ts
@@ -1,16 +1,27 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('documents', (table) => {
-    table.dropColumn('content')
-    table.dropColumn('embedding')
-  })
-
   await knex.schema.createTable('embeddings', (table) => {
     table.increments('id').primary()
     table.string('documentId').notNullable()
     table.text('content').notNullable()
     table.specificType('embedding', 'vector(1536)')
+    table.integer('index')
+  })
+
+  await knex
+    .insert(
+      knex
+        .select(
+          knex.raw(`content, embedding, COALESCE(metadata->>'elementNumber', 0) as index, id as documentId`)
+        )
+        .from('documents')
+    )
+    .into('embeddings')
+
+  await knex.schema.alterTable('documents', (table) => {
+    table.dropColumn('content')
+    table.dropColumn('embedding')
   })
 }
 
