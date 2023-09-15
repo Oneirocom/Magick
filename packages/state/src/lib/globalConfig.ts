aa7855d9c5ac38a9b05899cdd1688cf4070fc2ab
Michael <m.sharpe@project89.org>
2023-09-14 21:37:13 -0700

    Add state package

---
+++ packages/state/src/lib/globalConfig.ts
@@ -0,0 +1,54 @@
+// DOCUMENTED 
+/**
+ * Module that exports a Redux slice for the global config that carries the authentication, project id and API url information.
+ * @module globalConfigSlice
+ */
+
+import { createSlice, PayloadAction, Slice } from '@reduxjs/toolkit'
+
+/**
+ * Interface that defines the Global Config type.
+ */
+export interface GlobalConfig {
+  apiUrl: string
+  token: string
+  projectId: string
+}
+
+/**
+ * Slice of the GlobalConfig to be used in Redux store.
+ * @type {Slice<GlobalConfig>}
+ */
+export const globalConfigSlice: Slice<GlobalConfig> = createSlice({
+  name: 'globalConfig',
+  initialState: {
+    apiUrl: '',
+    token: '',
+    projectId: '',
+  },
+  reducers: {
+    /**
+     * Reducer method for setting the global config.
+     * @param {GlobalConfig} state - The current global config state.
+     * @param {PayloadAction<GlobalConfig>} action - Config to set on the global state.
+     * @return {void}
+     */
+    setConfig: (state: GlobalConfig, action: PayloadAction<GlobalConfig>): void => {
+      const { apiUrl, token, projectId } = action.payload
+      state.apiUrl = apiUrl
+      state.token = token
+      state.projectId = projectId
+    },
+  },
+})
+
+// Actions
+/**
+ * Action to set the global configuration.
+ */
+export const { setConfig } = globalConfigSlice.actions
+
+/**
+ * Export GlobalConfigSlice reducer.
+ */
+export default globalConfigSlice.reducer
