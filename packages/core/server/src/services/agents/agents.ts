82cedc51479d5884a3265f1ae9e2348f36e75e46
Michael <m.sharpe@project89.org>
2023-09-14 20:50:28 -0700

    Add subscribe method to agent methods array

--- packages/core/server/src/services/agents/agents.ts
+++ packages/core/server/src/services/agents/agents.ts
@@ -58,7 +58,16 @@ const AGENT_EVENTS = ['log', 'result', 'spell', 'run']
 export const agent = (app: Application) => {
   // Register the agent service on the Feathers application
   app.use('agents', new AgentService(getOptions(app), app), {
-    methods: ['find', 'get', 'create', 'patch', 'remove', 'run', 'ping'],
+    methods: [
+      'find',
+      'get',
+      'create',
+      'patch',
+      'remove',
+      'run',
+      'ping',
+      'subscribe',
+    ],
     events: AGENT_EVENTS,
   })
 
