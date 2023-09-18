62ad1b2319e80a3c2c9a6bec6711467991b8a6e2
Michael <m.sharpe@project89.org>
2023-09-18 10:41:02 -0700

    Add agent command job type

--- packages/core/shared/src/communication/agentJobTypes.ts
+++ packages/core/shared/src/communication/agentJobTypes.ts
@@ -1,8 +1,9 @@
 export const AGENT_RUN_JOB = (id: string) => `agent:run:${id}`
+export const AGENT_COMMAND_JOB = (id: string) => `agent:command:${id}`
 export const AGENT_UPDATE_JOB = (id: string) => `agent:update:${id}`
 export const AGENT_DELETE_JOB = (id: string) => `agent:delete:${id}`
 
 export interface CreateAgentJob {
-    jobId: string
-    agentId: string
+  jobId: string
+  agentId: string
 }
