87ee30157b68f6d1ac11813ad1885766705ae25b
Michael <m.sharpe@project89.org>
2023-09-19 16:05:47 -0700

    move agent command events into agent events file

--- packages/core/shared/src/communication/agentJobTypes.ts
+++ packages/core/shared/src/communication/agentJobTypes.ts
@@ -1,5 +1,4 @@
 export const AGENT_RUN_JOB = (id: string) => `agent:run:${id}`
-export const AGENT_COMMAND_JOB = (id: string) => `agent:command:${id}`
 export const AGENT_UPDATE_JOB = (id: string) => `agent:update:${id}`
 export const AGENT_DELETE_JOB = (id: string) => `agent:delete:${id}`
 
