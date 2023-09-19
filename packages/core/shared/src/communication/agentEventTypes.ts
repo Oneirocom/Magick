0f70ebf7b3a559f0a5d42f0be6135db1c8f09edc
Michael <m.sharpe@project89.org>
2023-09-19 16:05:47 -0700

    move agent command events into agent events file

--- packages/core/shared/src/communication/agentEventTypes.ts
+++ packages/core/shared/src/communication/agentEventTypes.ts
@@ -5,4 +5,7 @@ export const AGENT_SPELL = (agentId: string) => `agent:${agentId}:spell`
 export const AGENT_LOG = (agentId: string) => `agent:${agentId}:log`
 export const AGENT_WARN = (agentId: string) => `agent:${agentId}:warn`
 export const AGENT_ERROR = (agentId: string) => `agent:${agentId}:error`
+export const AGENT_COMMAND = (agentId: string) => `agent:${agentId}:command`
+export const AGENT_COMMAND_PROJECT = (projectId: string) =>
+  `agent:${projectId}:command`
 export const AGENT_DELETE = `agent:delete`
