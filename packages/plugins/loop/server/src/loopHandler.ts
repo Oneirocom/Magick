786c9aed2b41a048a2f45ef2ff6a8de1aa4cb53e
Michael <m.sharpe@project89.org>
2023-09-21 14:44:09 -0700

    Add loop handler class for controlable loop

---
+++ packages/plugins/loop/server/src/loopHandler.ts
@@ -0,0 +1,29 @@
+export class LoopHandler {
+  private interval: NodeJS.Timeout
+  private isPaused: boolean
+  private fn: Function
+
+  constructor(fn: Function, delay: number) {
+    this.fn = fn
+    this.isPaused = false
+    this.interval = setInterval(async () => {
+      if (!this.isPaused) {
+        await this.fn()
+      }
+    }, delay)
+  }
+
+  pause() {
+    this.isPaused = true
+  }
+
+  resume() {
+    this.isPaused = false
+  }
+
+  async step() {
+    if (this.isPaused) {
+      await this.fn()
+    }
+  }
+}
