761f33e67c265972fc438858308d2f45e11034f8
Michael <m.sharpe@project89.org>
2023-09-23 23:09:09 -0700

    Move route mapping to inside workspaces

--- packages/editor/src/components/Workspaces.tsx
+++ packages/editor/src/components/Workspaces.tsx
@@ -2,16 +2,15 @@
 import { memo, useEffect } from 'react';
 import isEqual from 'lodash/isEqual';
 import WorkspaceProvider from '../contexts/WorkspaceProvider';
+import { ClientPluginManager, pluginManager } from '@magickml/core'
 import Composer from './Workspace';
 import { Tab } from '@magickml/state';
+import Events from '../screens/EventWindow'
+import Requests from '../screens/RequestWindow'
+import Settings from '../screens/settings/SettingsWindow'
+import Documents from '../screens/DocumentWindow'
+import Agents from '../screens/agents/AgentManagerWindow';
 
-/**
- * A mapping of workspace types to their respective components.
- */
-const workspaceMap = {
-  spell: Composer,
-  module: Composer,
-};
 
 type WorkspaceProps = {
   pubSub: any;  // You might want to provide a more detailed type based on your usage.
@@ -19,8 +18,35 @@ type WorkspaceProps = {
 };
 
 const WorkspaceComponent = ({ tab, pubSub, activeTabId }) => {
-  // const { active, ...tabWithoutActive } = tab;
-  const Workspace = workspaceMap[tab.type];
+  const pluginComponents = []
+
+    ; (pluginManager as ClientPluginManager)
+      .getGroupedClientRoutes()
+      .forEach(plugin => {
+        plugin.routes.map(route => {
+          pluginComponents.push({
+            name: route.path.charAt(1).toUpperCase() + route.path.slice(2),
+            component: route.component,
+          })
+        })
+      })
+
+  const componentMapping = {
+    spell: Composer,
+    module: Composer,
+    Events,
+    Requests,
+    Settings,
+    Documents,
+    Agents,
+    ...pluginComponents.reduce((acc, obj) => {
+      acc[obj.name] = obj.component
+      return acc
+    }, {}),
+  }
+
+
+  const Workspace = componentMapping[tab.type];
   const props = {
     pubSub,
     tab
@@ -30,12 +56,18 @@ const WorkspaceComponent = ({ tab, pubSub, activeTabId }) => {
     return null;
   }
 
+  console.log(componentMapping)
+
   return (
     <div
       key={tab.name}
       style={{
         visibility: tab.id !== activeTabId ? 'hidden' : undefined,
-        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
       }}
     >
       <WorkspaceProvider {...props}>
@@ -45,12 +77,13 @@ const WorkspaceComponent = ({ tab, pubSub, activeTabId }) => {
   )
 }
 
+// We want to render all workspaces and components together so we can hold them in memory
+// and not have to re-render them when switching between tabs.
 const Workspaces = ({ tabs, pubSub, activeTabId }) => {
-
   return (
     <>
       {tabs.map((tab) => {
-        console.log("TAB!", tab)
+        // render the component if it is a component type
         return <WorkspaceComponent key={tab.id} tab={tab} pubSub={pubSub} activeTabId={activeTabId} />;
       })}
     </>
