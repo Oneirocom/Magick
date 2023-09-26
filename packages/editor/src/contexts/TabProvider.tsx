310a4309b1f701d7c936ea93debfe2463ab8a219
Michael <m.sharpe@project89.org>
2023-09-26 11:58:52 -0700

    Add layout persistance

--- packages/editor/src/contexts/TabProvider.tsx
+++ packages/editor/src/contexts/TabProvider.tsx
@@ -1,14 +1,39 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import {
-  DockviewReact, DockviewReadyEvent, IDockviewPanelProps,
+  DockviewApi,
+  DockviewReact, DockviewReadyEvent, IDockviewPanelProps, SerializedDockview,
 } from 'dockview';
+
+function loadDefaultLayout(api: DockviewApi) {
+  api.addPanel({
+    id: 'panel_1',
+    component: 'default',
+  });
+
+  api.addPanel({
+    id: 'panel_2',
+    component: 'default',
+  });
+
+  api.addPanel({
+    id: 'panel_3',
+    component: 'default',
+  });
+}
 
 type DockviewTheme = 'dockview-theme-abyss'
 
 type DocviewContext = {
   theme: DockviewTheme,
-  setTheme: (theme: DockviewTheme) => void
+  setTheme: (theme: DockviewTheme) => void,
+  api: DockviewApi,
+  setApi: (api: DockviewApi) => void,
+  getLayout: () => SerializedDockview | null,
+  setLayout: (layout: SerializedDockview) => void
+
 }
+
+const TAB_LAYOUT_KEY = 'tab-layout'
 
 // Creating the context
 const Context = createContext<DocviewContext>(undefined!)
@@ -16,25 +41,67 @@ const Context = createContext<DocviewContext>(undefined!)
 // Helper hook to use Layout context
 export const useLayout = () => useContext(Context)
 
-export const TabLayout = ({ children }) => {
-  const { theme, setTheme } = useLayout()
+export const TabProvider = ({ children }) => {
+  const [theme, setTheme] = useState<DockviewTheme>('dockview-theme-abyss')
+  const [api, setApi] = useState<DockviewApi>();
 
+  const getLayout = () => {
+    const layout = localStorage.getItem(TAB_LAYOUT_KEY)
+
+    if (!layout) {
+      return null
+    }
+    return JSON.parse(localStorage.getItem(TAB_LAYOUT_KEY)) as SerializedDockview
+  }
+
+  const setLayout = (layout: SerializedDockview) => {
+    localStorage.setItem(TAB_LAYOUT_KEY, JSON.stringify(layout))
+  }
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    // set up API event handlers
+    api.onDidLayoutChange(() => {
+      const layout = api.toJSON();
+
+      setLayout(layout)
+    });
+
+  }, [api]);
+
+  const publicInterface = {
+    theme,
+    setTheme,
+    api,
+    setApi,
+    getLayout,
+    setLayout
+  }
+  return <Context.Provider value={publicInterface}>{children}</Context.Provider>
+}
+
+export const TabLayout = ({ children }) => {
+  const { theme, setTheme, setApi, api, setLayout, getLayout } = useLayout()
 
   const onReady = (event: DockviewReadyEvent) => {
-    event.api.addPanel({
-      id: 'panel_1',
-      component: 'default',
-    });
 
-    event.api.addPanel({
-      id: 'panel_2',
-      component: 'default',
-    });
+    const layout = getLayout()
 
-    event.api.addPanel({
-      id: 'panel_3',
-      component: 'default',
-    });
+    let success = false;
+
+    if (layout) {
+      event.api.fromJSON(layout);
+      success = true;
+    }
+
+    if (!success) {
+      loadDefaultLayout(event.api);
+    }
+
+    setApi(event.api)
   };
 
   return (<DockviewReact
@@ -58,14 +125,4 @@ const components = {
       </div>
     );
   }
-};
-
-export const TabProvider = ({ children }) => {
-  const [theme, setTheme] = useState<DockviewTheme>('dockview-theme-abyss')
-
-  const publicInterface = {
-    theme,
-    setTheme
-  }
-  return <Context.Provider value={publicInterface}>{children}</Context.Provider>
 }
