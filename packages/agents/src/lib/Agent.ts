4130fdb49b542ba4e40eece1d37e9a1c020949cd
Michael <m.sharpe@project89.org>
2023-09-30 10:30:05 -0700

    HOTFIX: Disable agent interval

--- packages/agents/src/lib/Agent.ts
+++ packages/agents/src/lib/Agent.ts
@@ -125,18 +125,18 @@ export class Agent implements AgentInterface {
       const outputTypes = pluginManager.getOutputTypes()
       this.outputTypes = outputTypes
 
-      this.updateInterval = setInterval(async () => {
-        // every second, update the agent, set pingedAt to now
-        try {
-          await app.service('agents').ping(this.id)
-        } catch (err) {
-          if (err instanceof Error && err?.name === 'NotFound') {
-            this.logger.warn('Agent not found: %s', this.id)
-            app.get('agentCommander').removeAgent(this.id)
-            clearInterval(this.updateInterval)
-          }
-        }
-      }, PING_AGENT_TIME_MSEC)
+      // this.updateInterval = setInterval(async () => {
+      //   // every second, update the agent, set pingedAt to now
+      //   try {
+      //     await app.service('agents').ping(this.id)
+      //   } catch (err) {
+      //     if (err instanceof Error && err?.name === 'NotFound') {
+      //       this.logger.warn('Agent not found: %s', this.id)
+      //       app.get('agentCommander').removeAgent(this.id)
+      //       clearInterval(this.updateInterval)
+      //     }
+      //   }
+      // }, PING_AGENT_TIME_MSEC)
       this.logger.info('New agent created: %s | %s', this.name, this.id)
       this.ready = true
     })()
