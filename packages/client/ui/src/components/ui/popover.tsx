de36036045e22d099252dd0255fd0734e5a4f178
Michael <m.sharpe@project89.org>
2023-10-03 23:45:41 -0700

    Fix some issues moving ui folder

--- packages/client/ui/src/components/ui/popover.tsx
+++ packages/client/ui/src/components/ui/popover.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as PopoverPrimitive from '@radix-ui/react-popover'
 
-import { cn } from 'client/ui/utils'
+import { cn } from '@magickml/ui'
 
 const Popover = PopoverPrimitive.Root
 
