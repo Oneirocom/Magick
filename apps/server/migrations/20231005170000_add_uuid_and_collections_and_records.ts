82472dd85a5b1c0c191610f586d47be04006e308
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-10-06 05:15:06 -0400

    add collections + records table, enable uuid-ossp

---
+++ apps/server/migrations/20231005170000_add_uuid_and_collections_and_records.ts
@@ -0,0 +1,33 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
+  await knex.schema.createTable('collections', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string('name')
+    table.string('projectId').notNullable()
+    table.text('description').nullable()
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
+    table.timestamp('deletedAt').nullable()
+    table.unique(['name', 'projectId'])
+  })
+
+  await knex.schema.createTable('records', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string('key').notNullable()
+    table.uuid('collectionId').references('id').inTable('collections')
+    table.json('data')
+    table.json('metadata').nullable()
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
+    table.timestamp('deletedAt').nullable()
+    table.unique(['key', 'collectionId'])
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('records')
+  await knex.schema.dropTable('collections')
+}
