2d05148839acb8551420e9f4c972767131e7e406
Michael <m.sharpe@project89.org>
2023-10-13 17:03:53 -0700

    Initial migration of documents to redux

--- packages/client/editor/src/screens/DocumentWindow/index.tsx
+++ packages/client/editor/src/screens/DocumentWindow/index.tsx
@@ -1,55 +1,15 @@
 // DOCUMENTED
-import { API_ROOT_URL } from 'shared/config'
-import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { LoadingScreen } from 'client/core'
-import { useConfig } from '@magickml/providers'
 import DocumentTable from './DocumentTable'
+import { useGetDocumentsQuery } from 'client/state'
 
 /**
  * DocumentWindow component displays the documents of a project.
  * @returns JSX Element
  */
 const DocumentWindow = (): JSX.Element => {
-  const globalConfig = useSelector((state: any) => state.globalConfig)
-  const token = globalConfig?.token
-  const config = useConfig()
-  const [documents, setDocuments] = useState<Document[] | null>(null)
-  const [loading, setLoading] = useState<boolean>(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetchDocuments()
-  }, [])
-
-  /**
-   * Resets the events and fetches the updated events.
-   */
-  const resetDocuments = async (): Promise<void> => {
-    await fetchDocuments()
-  }
-
-  /**
-   * Fetches the events of the current project.
-   */
-  const fetchDocuments = async (): Promise<void> => {
-    try {
-      const response = await fetch(
-        `${API_ROOT_URL}/documents?projectId=${config.projectId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-
-      const data = await response.json()
-      setLoading(false)
-      setDocuments(data.data)
-    } catch (error) {
-      console.error('ERROR', error)
-    }
-  }
+  const { data: documents, isLoading } = useGetDocumentsQuery({})
 
   return (
     <div
@@ -61,9 +21,9 @@ const DocumentWindow = (): JSX.Element => {
         overflow: 'scroll',
       }}
     >
-      {loading && <LoadingScreen />}
+      {isLoading && <LoadingScreen />}
       {documents && (
-        <DocumentTable documents={documents} updateCallback={resetDocuments} />
+        <DocumentTable documents={documents.data} />
       )}
     </div>
   )
