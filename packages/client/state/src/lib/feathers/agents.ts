6c773f1ae1e7efa5b32614abaead7ed48a495dd5
Michael <m.sharpe@project89.org>
2023-10-17 16:31:37 -0700

    Create feathers redux service for agents

---
+++ packages/client/state/src/lib/feathers/agents.ts
@@ -0,0 +1,11 @@
+import root from './root'
+
+const feathersService = root.injectService<
+  'agents',
+  ['log', 'results', 'spell']
+>({
+  name: 'agents',
+  events: ['log', 'results', 'spell'],
+})
+
+export const { useSelectAgentsLog } = feathersService.selectors
