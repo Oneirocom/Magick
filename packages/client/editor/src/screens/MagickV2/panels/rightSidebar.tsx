e95032e6f737e9f6e120d8a05a19014dfcc14a62
Michael <m.sharpe@project89.org>
2023-10-17 23:23:08 -0700

    Merge data streams in right sidebar

--- packages/client/editor/src/screens/MagickV2/panels/rightSidebar.tsx
+++ packages/client/editor/src/screens/MagickV2/panels/rightSidebar.tsx
@@ -2,26 +2,39 @@ import { IGridviewPanelProps } from "dockview"
 import { useEffect, useState } from "react"
 import { useGlobalLayout } from "../../../contexts/GlobalLayoutProvider"
 import { usePanelControls } from "../hooks/usePanelControls"
-import { useSelectAgentsLog } from "client/state"
+import { useSelectAgentsLog, useSelectAgentsSpell } from "client/state"
+import LogWindow from "../components/logWindow"
 
 const RightSidebar = (props: IGridviewPanelProps<{ title: string, id: string }>) => {
   usePanelControls(props, 'none', 'ctrl+l');
-  const { data } = useSelectAgentsLog()
+  const { data: LogData } = useSelectAgentsLog()
+  const { data: spellData } = useSelectAgentsSpell()
+  const [combinedData, setCombinedData] = useState([]);
 
   useEffect(() => {
-    if (!data) return
-    console.log('LOG DATA!!!!!!!!!!!!', data)
-  }, [data])
+    if (!spellData || !LogData) return;
 
+    const merged = [...LogData, ...spellData].sort((a, b) => {
+      const dateA = new Date(a.timestamp);
+      const dateB = new Date(b.timestamp);
+
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    console.log("Merged", merged)
+
+    setCombinedData(merged);
+  }, [spellData, LogData]);
   return (
     <div
       style={{
         height: '100%',
-        padding: '20px',
         background: 'var(--dv-group-view-background-color)',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
-      {JSON.stringify(props.params)}
+      <LogWindow logs={combinedData} />
     </div>
   )
 }
