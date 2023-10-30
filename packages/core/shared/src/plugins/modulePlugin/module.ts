afdf2db6f2a033e8988290cdb67a06ca4fdf6984
Michael <m.sharpe@project89.org>
2023-10-30 00:40:31 -0700

    Add sessionId to module

--- packages/core/shared/src/plugins/modulePlugin/module.ts
+++ packages/core/shared/src/plugins/modulePlugin/module.ts
@@ -3,6 +3,7 @@ export class Module {
   publicVariables?: Record<string, string>
   inputs: Record<string, unknown>
   outputs: Record<string, unknown>
+  sessionId?: string
   app?: any // set to App, but move App to engine first
   constructor() {
     this.inputs = {}
@@ -12,11 +13,12 @@ export class Module {
     this.app = null
   }
 
-  read({ inputs, secrets, publicVariables, app }) {
+  read({ inputs, secrets, publicVariables, app, sessionId = undefined }) {
     this.inputs = inputs
     this.secrets = secrets || ({} as Record<string, string>)
     this.publicVariables = publicVariables || ({} as Record<string, string>)
     this.app = app
+    this.sessionId = sessionId
   }
 
   write(outputs: Record<string, unknown>) {
