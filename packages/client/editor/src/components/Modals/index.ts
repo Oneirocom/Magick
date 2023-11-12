80c670a44cec8a6f0e800c57c95f968c63c6fae3
Michael <m.sharpe@project89.org>
2023-11-11 22:42:27 -0800

    Clean up unused state and old tab stuff

--- packages/client/editor/src/components/Modals/index.ts
+++ packages/client/editor/src/components/Modals/index.ts
@@ -3,7 +3,6 @@
  * Represents a collection of modals for handling different interactions.
  */
 import InfoModal from './InfoModal'
-import EditSpellModal from './EditSpellModal'
 import SaveAsModal from './SaveAsModal'
 import DeleteModal from './DeleteModal'
 import CreateSpellModal from './CreateSpellModal'
@@ -11,7 +10,6 @@ import CreateSpellModal from './CreateSpellModal'
 // A dictionary of modals with their names and components
 const modals = {
   infoModal: InfoModal,
-  editSpellModal: EditSpellModal,
   saveAsModal: SaveAsModal,
   deleteModal: DeleteModal,
   createSpellModal: CreateSpellModal,
