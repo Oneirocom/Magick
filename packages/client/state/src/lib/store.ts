e308595ec778e0cf0462b8ed8d6072ebfd6ee40c
Michael <m.sharpe@project89.org>
2023-11-11 22:43:37 -0800

    Add support for dynamically injecting tab routes

--- packages/client/state/src/lib/store.ts
+++ packages/client/state/src/lib/store.ts
@@ -1,15 +1,20 @@
 // DOCUMENTED
-import { configureStore, ThunkAction, Action, Dispatch } from '@reduxjs/toolkit'
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  Dispatch,
+  EnhancedStore,
+} from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query/react'
 
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import { combineReducers } from 'redux'
+import { Reducer, combineReducers } from 'redux'
 
 import { spellApi } from './api/spells'
 import { rootApi } from './api/api'
 import { agentApi } from './api/agents'
-import tabReducer from './tabs'
 import localStateReducer from './localState'
 import preferencesReducer from './preferences'
 import globalConfigReducer from './globalConfig'
@@ -17,22 +22,30 @@ import tabLayoutReducer from './tabLayoutState'
 import statusBarReducer from './statusBarState'
 import rootFeathers, { configureFeathersStore } from './feathers/root'
 import { feathersEventMiddleware } from '@magickml/feathersRedux'
+import { tabReducer } from './tabs/tabReducer'
+import { stringSocket } from 'shared/core'
 
 // import { AppConfig } from '@magickml/client-core'
 
 /**
  * Combine all reducers into the root reducer.
  */
-const rootReducer = combineReducers({
+
+const initialReducers = {
   [rootApi.reducerPath]: rootApi.reducer,
   [rootFeathers.rootReducerPath]: rootFeathers.reducer,
   tabLayout: tabLayoutReducer,
   globalConfig: globalConfigReducer,
-  tabs: tabReducer,
   preferences: preferencesReducer,
   localState: localStateReducer,
   statusBar: statusBarReducer,
-})
+}
+const rootReducer = combineReducers(initialReducers)
+
+// Extend the store type
+interface ExtendedStore extends EnhancedStore {
+  asyncReducers: Record<string, Reducer>
+}
 
 // Store instance placeholder
 let _store: any = null
@@ -42,7 +55,7 @@ let _store: any = null
  * @param config - Optional configuration for the store.
  * @returns The created store.
  */
-export const createStore = (config: any) => {
+export const createStore = (config?: any): ExtendedStore => {
   if (_store) return _store
 
   const persistConfig = {
@@ -57,7 +70,7 @@ export const createStore = (config: any) => {
     ],
   }
 
-  const store = configureFeathersStore({
+  const store: ExtendedStore = configureFeathersStore({
     reducer: persistReducer(persistConfig, rootReducer),
     preloadedState: {
       globalConfig: config,
@@ -68,19 +81,40 @@ export const createStore = (config: any) => {
       })
         .concat(rootApi.middleware)
         .concat(feathersEventMiddleware),
-  })
+  }) as ExtendedStore
 
   setupListeners(store.dispatch)
   persistStore(store)
+
+  store.asyncReducers = {}
 
   _store = store
 
   return store
 }
 
+export const injectReducer = (
+  store: ExtendedStore,
+  key: string,
+  asyncReducer: Reducer
+) => {
+  store.asyncReducers[key] = asyncReducer
+
+  store.replaceReducer(
+    combineReducers({
+      ...initialReducers,
+      ...store.asyncReducers,
+    })
+  )
+}
+
+interface DynamicState {
+  [key: string]: typeof tabReducer | undefined
+}
+
 // Export types for the application's Redux store
 export type AppDispatch = Dispatch
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer> & DynamicState
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
