94949aa853e0cfbfa15c06b63acb32493941f022
Michael <m.sharpe@project89.org>
2023-11-12 22:20:15 -0800

    Load behave graph from spell data

--- packages/client/editor/src/components/GraphWindow/GraphWindow.tsx
+++ packages/client/editor/src/components/GraphWindow/GraphWindow.tsx
@@ -6,13 +6,31 @@ import { Flow } from "../react-flow/Flow";
 
 import graph from '../../graphs/graph.json';
 import Branch from '../../graphs/core/flow/Branch.json';
-import { Tab } from "@magickml/providers";
+import { Tab, useConfig } from "@magickml/providers";
+import { spellApi } from "client/state";
+import { SpellInterface } from "shared/core";
 
 const GraphWindow = (props: IDockviewPanelProps<{ tab: Tab, spellId: string }>) => {
   const examples = {
     branch: Branch as unknown as GraphJSON,
   } as Record<string, GraphJSON>;
+
+  const config = useConfig()
   const parentRef = useRef();
+  const [loadSpell, { data: spell }] = spellApi.useLazyGetSpellByIdQuery()
+  const { tab, spellId } = props.params
+
+  useEffect(() => {
+    // If there is no tab, or we already have a spell, return early
+    if (!tab || !tab.name) return
+
+    loadSpell({
+      spellName: tab.name,
+      projectId: config.projectId,
+      id: spellId,
+    })
+  }, [tab])
+
 
   const [height, setHeight] = useState(0)
   const [width, setWidth] = useState(0)
@@ -30,8 +48,17 @@ const GraphWindow = (props: IDockviewPanelProps<{ tab: Tab, spellId: string }>) 
 
   const registry = useRegistry();
 
+  if (!spell?.data) return null
+
+  console.log('SPELL', spell)
+
   return (<div style={{ height, width }} ref={parentRef}>
-    <Flow registry={registry} initialGraph={graph} examples={examples} parentRef={parentRef} tab={props.params.tab} />;
+    <Flow
+      registry={registry}
+      parentRef={parentRef}
+      tab={props.params.tab}
+      spell={spell.data[0]}
+    />;
   </div>)
 }
 
