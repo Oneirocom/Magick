4c715f63e576e4607982d7355ae57f8c914149d0
Knar <knarxr@protonmail.com>
2023-11-17 15:28:28 -0500

    added chat table

---
+++ apps/server/migrations/20231115170320_create_chat_table.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('chat_messages', (table) => {
+    table.uuid('id').primary()
+    table.string('connector')
+    table.string('agentId').references('id').inTable('agents').notNullable()
+    table.string('message').notNullable()
+    table.dateTime('createdAt')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('chat_messages')
+}
