69c61baef7584e0720c7f28fa3675a4319dcdc25
Michael <m.sharpe@project89.org>
2023-11-21 00:33:07 -0800

    Add comment to update toggle live handling

--- packages/server/agents/src/lib/Agent.ts
+++ packages/server/agents/src/lib/Agent.ts
@@ -180,6 +180,7 @@ export class Agent implements AgentInterface {
     this.commandHub.registerDomain('agent', 'core', {
       toggleLive: async (data: any) => {
         this.spellManager.toggleLive(data)
+        // update this to use new spellCaster
       },
     })
   }
