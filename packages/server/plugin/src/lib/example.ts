98c8265c709fbcf4a87bfdaaa99f425d57e7210e
Michael <m.sharpe@project89.org>
2023-11-24 23:52:52 -0800

    Update example discord plugin

--- packages/server/plugin/src/lib/example.ts
+++ packages/server/plugin/src/lib/example.ts
@@ -1,10 +1,19 @@
 import Discord, { GatewayIntentBits, MessagePayload } from 'discord.js'
 import Redis from 'ioredis'
 
-import BasePlugin from './basePlugin'
+import { EventFormat } from './basePlugin'
+import { CorePlugin } from './corePlugin'
 // import { BullQueue } from 'server/core'
 
-class DiscordPlugin extends BasePlugin {
+type DiscordPayload = {
+  content: string
+  sender: string
+  channelId: string
+  rawData: unknown
+  observer: string
+}
+
+class DiscordPlugin extends CorePlugin<DiscordPayload> {
   private client: Discord.Client
 
   constructor(connection: Redis) {
@@ -38,26 +47,68 @@ class DiscordPlugin extends BasePlugin {
       eventName: 'discord:messageReceived',
       displayName: 'Message Received', // Define MessagePayload type
     })
-
     // ... register other Discord-specific events
   }
 
+  /**
+   * Formats the full event payload from the plugin payload.
+   * This method can be used to format a message event.
+   * @param event The event name.
+   * @param payload The payload to format.
+   * @returns Formatted event payload.
+   * @example
+   * formatPayload('discord:messageReceived', {
+   *   content: 'Hello World',
+   *   sender: 'John Doe',
+   *   channelId: '1234567890',
+   *   rawData: { ... },
+   *   observer: 'Discord'
+   * });
+   */
+  formatPayload(event, payload: DiscordPayload) {
+    const eventPayload: EventFormat = {
+      ...payload,
+      client: 'Discord',
+      observer: this?.client?.user?.username || 'Discord',
+      channelType: event,
+      data: {},
+    }
+    return this.formatMessageEvent(event, eventPayload)
+  }
+
+  /**
+   * Formats to an event payload for a message.
+   * @param messageDetails Details of the message to format.
+   * @returns Formatted message event payload.
+   */
+  makePayloadFromMessage(message: Discord.Message): DiscordPayload {
+    return {
+      content: message.content,
+      sender: message.author.username,
+      channelId: message.channel.id,
+      rawData: message,
+      observer: this?.client?.user?.username || 'Discord',
+    }
+  }
+
+  /**
+   * Handles the Discord message creation event.
+   * @param message The message that was created.
+   */
   private handleMessageCreate(message: Discord.Message) {
     // add a whitelist option here probably.
     if (message.author.bot) return
 
     const eventName = 'discord:messageReceived'
+    const pluginPayload = this.makePayloadFromMessage(message)
 
     // Emitting a custom event for message creation
-    const payload = this.formatMessageEvent(eventName, {
-      client: 'Discord',
-      content: message.content,
-      channelId: message.channel.id,
-      rawData: message.toJSON(),
-      channelType: eventName,
-      sender: message.author.id,
-      observer: this?.client?.user?.username || 'Discord',
-    })
+    const payload = this.formatPayload(
+      eventName,
+      this.makePayloadFromMessage(message)
+    )
+
+    this.onMessage(pluginPayload)
 
     this.emitEvent(eventName, payload)
   }
