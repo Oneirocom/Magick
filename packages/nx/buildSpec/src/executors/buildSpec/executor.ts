c3ad27559459f734dde6f7d2d318785b74e3508d
Michael <m.sharpe@project89.org>
2023-11-27 15:49:55 -0800

    Custom NX executor to build the node spec from plugins

---
+++ packages/nx/buildSpec/src/executors/buildSpec/executor.ts
@@ -0,0 +1,32 @@
+import type { ExecutorContext } from '@nx/devkit'
+import { BuildSpecExecutorSchema } from './schema'
+import { writeNodeSpecsToFile } from '../../tools/generateNodeSpecs'
+import path from 'path'
+
+export default async function runExecutor(
+  options: BuildSpecExecutorSchema,
+  context: ExecutorContext
+) {
+  const filePath = path.join(
+    context.root,
+    options.specFileLocation,
+    options.specFileName
+  )
+
+  console.log('FILE PATH', filePath)
+
+  // get the path for a relative fole in ../tools/generateNodeSpec using path
+
+  try {
+    writeNodeSpecsToFile(filePath)
+    return {
+      success: true,
+    }
+  } catch (err) {
+    console.error('Error running script to update node specs')
+    console.error(err)
+    return {
+      success: false,
+    }
+  }
+}
