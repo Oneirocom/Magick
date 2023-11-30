db8f3e3a5a60c779e20fd98cba2ea4c56c6c755d
benbot <bbotwin@atlassian.com>
2023-11-30 14:48:29 -0500

    brough back channelTypes

--- packages/plugins/discord/shared/src/types/ChannelType.ts
+++ packages/plugins/discord/shared/src/types/ChannelType.ts
@@ -55,4 +55,30 @@ export enum ChannelType {
      * A channel that can only contain threads
      */
     GuildForum = 15,
+    /**
+     * A channel that users can follow and crosspost into their own guild
+     *
+     * @deprecated This is the old name for {@apilink ChannelType#GuildAnnouncement}
+     *
+     * See https://support.discord.com/hc/articles/360032008192
+     */
+    GuildNews = 5,
+    /**
+     * A temporary sub-channel within a Guild Announcement channel
+     *
+     * @deprecated This is the old name for {@apilink ChannelType#AnnouncementThread}
+     */
+    GuildNewsThread = 10,
+    /**
+     * A temporary sub-channel within a Guild Text channel
+     *
+     * @deprecated This is the old name for {@apilink ChannelType#PublicThread}
+     */
+    GuildPublicThread = 11,
+    /**
+     * A temporary sub-channel within a Guild Text channel that is only viewable by those invited and those with the Manage Threads permission
+     *
+     * @deprecated This is the old name for {@apilink ChannelType#PrivateThread}
+     */
+    GuildPrivateThread = 12
   }
