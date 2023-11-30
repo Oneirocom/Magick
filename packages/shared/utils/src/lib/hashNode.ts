e1c86d100a7a1dd0c198cdf20644eb9ed9438fad
Michael <m.sharpe@project89.org>
2023-11-30 00:32:52 -0800

    Refactpr hash node helpers

--- packages/shared/utils/src/lib/hashNode.ts
+++ packages/shared/utils/src/lib/hashNode.ts
@@ -1,10 +1,26 @@
 import { INode } from '@magickml/behave-graph'
 import crypto from 'crypto'
 
-export function generateNodeHash(node: INode) {
+type HashProperties = {
+  nodeType: string
+  positionX: number
+  positionY: number
+}
+
+export function formatBehaveNodeForHash(node: INode): HashProperties {
+  return {
+    nodeType: node.nodeType,
+    positionX: node.metadata?.positionX ?? 0,
+    positionY: node.metadata?.positionY ?? 0,
+  }
+}
+
+export function generateNodeHash(node: HashProperties) {
   const hash = crypto.createHash('sha256')
-  hash.update(
-    `${node.nodeType}-${node.description.typeName}-${node.metadata.positionX}-${node.metadata.positionY}`
-  )
+  hash.update(`${node.nodeType}-${node.positionX}-${node.positionY}`)
   return hash.digest('hex')
+}
+
+export function generateBehaveNodeHash(node: INode) {
+  return generateNodeHash(formatBehaveNodeForHash(node))
 }
