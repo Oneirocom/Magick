7b5fe5925a318acaf365230e01ae8e5832168013
Jakob <haruhunab1320@gmail.com>
2023-12-01 14:06:32 -0800

    add a fetchAllPages utility

--- packages/shared/utils/src/lib/feathersHelpers.ts
+++ packages/shared/utils/src/lib/feathersHelpers.ts
@@ -9,8 +9,43 @@ export const checkPaginated = <T>(thing: Paginated<T> | T[]) => {
     total: thing.length,
     limit: 0,
     skip: 0,
-    data: thing 
+    data: thing,
   }
 
-  return page;
+  return page
+}
+
+// Define a function type that matches the signature of your find method
+type FindFunction<T> = (params: any) => Promise<Paginated<T>>
+
+export const fetchAllPages = async <T>(
+  findFunction: FindFunction<T>,
+  params: any,
+  skip = 0,
+  previousData: T[] = []
+): Promise<T[]> => {
+  try {
+    // Make the request for the current page
+    const result = await findFunction({ ...params, $skip: skip })
+
+    // Concatenate the new data with the previous data
+    const allData = previousData.concat(result.data)
+
+    // Check if there are more pages to fetch
+    if (allData.length < result.total) {
+      // If there are more pages, call this function recursively
+      return fetchAllPages(
+        findFunction,
+        params,
+        skip + (params.$limit || result.limit),
+        allData
+      )
+    }
+
+    // If there are no more pages, or all data has been fetched, return all the data
+    return allData
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
