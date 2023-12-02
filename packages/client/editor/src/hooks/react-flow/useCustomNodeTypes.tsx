7af84d77f1037655418daca5518990ea0c346c47
Michael <m.sharpe@project89.org>
2023-12-01 22:52:26 -0800

    Drill spell down into node

--- packages/client/editor/src/hooks/react-flow/useCustomNodeTypes.tsx
+++ packages/client/editor/src/hooks/react-flow/useCustomNodeTypes.tsx
@@ -4,25 +4,29 @@ import { useEffect, useState } from 'react';
 import { NodeTypes } from 'reactflow';
 
 import { Node } from '../../components/react-flow/Node';
+import { Tab } from '@magickml/providers';
+import { SpellInterface } from 'server/schemas';
 
-const getCustomNodeTypes = (allSpecs: NodeSpecJSON[]) => {
+const getCustomNodeTypes = (allSpecs: NodeSpecJSON[], spell: SpellInterface) => {
   return allSpecs.reduce((nodes: NodeTypes, node) => {
     nodes[node.type] = (props) => (
-      <Node spec={node} allSpecs={allSpecs} {...props} />
+      <Node spec={node} allSpecs={allSpecs} spell={spell} {...props} />
     );
     return nodes;
   }, {});
 };
 
 export const useCustomNodeTypes = ({
-  specJson
+  specJson,
+  spell
 }: {
-  specJson: NodeSpecJSON[] | undefined;
+  specJson: NodeSpecJSON[] | undefined
+  spell: SpellInterface
 }) => {
   const [customNodeTypes, setCustomNodeTypes] = useState<NodeTypes>();
   useEffect(() => {
     if (!specJson) return;
-    const customNodeTypes = getCustomNodeTypes(specJson);
+    const customNodeTypes = getCustomNodeTypes(specJson, spell);
 
     setCustomNodeTypes(customNodeTypes);
   }, [specJson]);
