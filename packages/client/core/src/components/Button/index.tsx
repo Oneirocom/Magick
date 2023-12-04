a2e0d7b2d7ec668ad906d604da8ae3f2335320fe
Jakob <haruhunab1320@gmail.com>
2023-12-04 15:10:22 -0800

    build out button component

--- packages/client/core/src/components/Button/index.tsx
+++ packages/client/core/src/components/Button/index.tsx
@@ -1,28 +1,61 @@
-// DOCUMENTED
-import React, { MouseEventHandler, ButtonHTMLAttributes } from 'react'
-import styles from './button.module.css'
+import React, { ButtonHTMLAttributes, ReactNode, useState, MouseEvent, CSSProperties } from 'react';
+// import styles from './button.module.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
+  hoverStyle?: React.CSSProperties;
+}
+
 
 /**
- * Button component. Provides onClick functionality and allows for props to be passed in.
+ * Button component with hover style support.
  *
  * @param {object} props - The component properties.
- * @param {function} [onClick] - The click event handler function. Optional.
  */
-export const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
-  onClick = (e?: React.MouseEvent<HTMLButtonElement>) => {
-    /* null */
-  },
+export const Button = ({
+  onClick,
+  className,
+  children,
+  hoverStyle,
+  disabled,
+  style,
   ...props
-}) => {
+}: ButtonProps): JSX.Element => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const disabledStyle: React.CSSProperties = {
+    backgroundColor: '#3c3c3cba', // Grey background
+    color: '#909090', // Darker grey text
+    cursor: 'not-allowed',
+    filter: 'none',
+  };
+
+
+  // Function to merge styles
+  const getButtonStyle = (): CSSProperties | undefined => {
+    if (disabled) {
+      return { ...style, ...disabledStyle };
+    }
+    if (isHovered && hoverStyle) {
+      return { ...style, ...hoverStyle };
+    }
+    return style;
+  };
+
   return (
     // Render a button with the provided onClick handler and spread props
     <button
-      onClick={onClick as MouseEventHandler<HTMLButtonElement>}
-      className={styles['default']}
+      onClick={onClick}
+      className={className}
+      disabled={disabled}
+      onMouseEnter={() => !disabled && setIsHovered(true)}
+      onMouseLeave={() => !disabled && setIsHovered(false)}
+      style={getButtonStyle()}
       {...props}
     >
-      {/* Render children within the button */}
-      {props.children}
+      {children}
     </button>
   )
 }
