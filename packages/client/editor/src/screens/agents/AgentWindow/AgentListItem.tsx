e9c0fd2eb0528f69e3b1050b1d2d92a993af671e
Jakob <haruhunab1320@gmail.com>
2023-12-04 15:12:00 -0800

    add agentListItem component

---
+++ packages/client/editor/src/screens/agents/AgentWindow/AgentListItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { MenuItem, Avatar, ListItemText, Box } from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
+const AgentListItem = ({ agent, onSelectAgent }) => {
+  const agentImage = agent.image
+    ? `https://pub-58d22deb43dc48e792b7b7468610b5f9.r2.dev/magick-dev/agents/${agent.image}`
+    : undefined;
+
+
+  const formatDate = (date: string) => {
+    if (!date) return false
+    return formatDistanceToNow(new Date(date), { addSuffix: true })
+  }
+
+  return (
+    <MenuItem sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }} onClick={() => onSelectAgent(agent)}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar
+          alt={agent.name.at(0) || 'A'}
+          src={agentImage}
+          sx={{ width: 24, height: 24 }}
+        >
+          {agent.name.at(0) || 'A'}
+        </Avatar>
+        <ListItemText
+          primary={agent.name}
+          secondary={`${agent.updatedAt ? 'Updated:' : 'Created:'} ${formatDate(agent.updatedAt || agent.createdAt)}`}
+          sx={{
+            ml: 1,
+            maxWidth: '200px',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap'
+          }} />
+      </Box>
+      <LockIcon />
+    </MenuItem>
+  );
+};
+
+export default AgentListItem;
