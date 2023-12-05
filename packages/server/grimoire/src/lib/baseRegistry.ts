12661b20398c7ee8e7e4735b907d6bd9e05feb75
Michael <m.sharpe@project89.org>
2023-12-05 15:30:04 -0800

    Add init method for base registry to

--- packages/server/grimoire/src/lib/baseRegistry.ts
+++ packages/server/grimoire/src/lib/baseRegistry.ts
@@ -1,22 +1,30 @@
 import {
+  IGraph,
   IRegistry,
   ManualLifecycleEventEmitter,
   NodeDefinition,
   ValueType,
 } from '@magickml/behave-graph'
 import { AgentLoggingService, IAgentLogger } from 'server/agents'
-import DefaultStateService from './services/defaultStateService'
+import { DefaultStateService } from './services/defaultStateService'
+import { EventStore } from './services/eventStore'
+
 export class BaseRegistry {
   values: ValueType[] = []
   nodes: NodeDefinition[] = []
   dependencies: Record<string, any> = {
     ILifecycleEventEmitter: new ManualLifecycleEventEmitter(),
-    IStateService: new DefaultStateService(),
+    IEventStore: new EventStore(),
   }
 
   constructor(agent: IAgentLogger) {
     // Create the agent logger for the core registry
+    this.dependencies.IStateServive = new DefaultStateService()
     this.dependencies.ILogger = new AgentLoggingService(agent)
+  }
+
+  init(graph: IGraph) {
+    this.dependencies.IStateServive.init(graph)
   }
 
   getRegistry(): IRegistry {
