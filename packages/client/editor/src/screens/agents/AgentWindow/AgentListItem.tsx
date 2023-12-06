763b2180abdd2764bd985415ba448e111a39ece9
Jakob <haruhunab1320@gmail.com>
2023-12-05 16:56:13 -0800

    add checkbox back in list item

--- packages/client/editor/src/screens/agents/AgentWindow/AgentListItem.tsx
+++ packages/client/editor/src/screens/agents/AgentWindow/AgentListItem.tsx
@@ -1,32 +1,73 @@
 import React from 'react';
-import { MenuItem, Avatar, ListItemText, Box } from '@mui/material';
+import { MenuItem, Avatar, ListItemText, Box, Checkbox } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
-import { AgentInterface } from 'server/core';
+import { AgentInterface } from 'server/core'; // Adjust the import path as necessary
 
 const AgentListItem = ({
   agent,
   onSelectAgent,
-  isDraft = false
+  onRemoveSelectedAgent,
+  onAddSelectedAgent,
+  isDraft = false,
+  selectedAgents,
+  onCheckboxChange,
 }: {
-  agent: AgentInterface
-  onSelectAgent: (agent: AgentInterface) => void
-  isDraft?: boolean
+  agent: AgentInterface;
+  onSelectAgent?: (agent: AgentInterface) => void;
+  onAddSelectedAgent?: (agent: string) => void;
+  onRemoveSelectedAgent?: (agent: string) => void;
+  isDraft?: boolean;
+  selectedAgents?: string[];
+  onCheckboxChange?: (agentId: string, checked: boolean) => void;
 }) => {
   const agentImage = agent.image
     ? `https://pub-58d22deb43dc48e792b7b7468610b5f9.r2.dev/magick-dev/agents/${agent.image}`
     : undefined;
 
-
   const formatDate = (date: string) => {
-    if (!date) return false
-    return formatDistanceToNow(new Date(date), { addSuffix: true })
-  }
+    if (!date) return '';
+    return formatDistanceToNow(new Date(date), { addSuffix: true });
+  };
+
+  const handleCheckboxClick = (event: React.MouseEvent) => {
+    event.stopPropagation(); // Prevent menu close
+  };
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+    if (onCheckboxChange) {
+      onCheckboxChange(agent.id, checked);
+    }
+  };
 
   return (
-    <MenuItem sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }} onClick={() => onSelectAgent(agent)}>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <MenuItem
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+      }}
+      onClick={() => onSelectAgent && onSelectAgent(agent)}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }} onClick={handleCheckboxClick}>
+        {!isDraft && (
+          <Checkbox
+            size='small'
+            checked={selectedAgents?.includes(agent.id)}
+            onChange={handleCheckboxChange}
+            sx={{
+              ":hover": {
+                backgroundColor: 'transparent',
+                cursor: 'pointer',
+              },
+              marginRight: 0,
+              marginLeft: 0,
+              paddingLeft: 0,
+            }}
+          />
+        )}
         <Avatar
           alt={agent.name.at(0) || 'A'}
           src={agentImage}
@@ -39,13 +80,18 @@ const AgentListItem = ({
           secondary={`${agent.updatedAt ? 'Updated:' : 'Created:'} ${formatDate(agent.updatedAt || agent.createdAt)}`}
           sx={{
             ml: 1,
-            maxWidth: '200px',
+            maxWidth: isDraft ? '220px' : '180px',
             overflow: 'hidden',
             textOverflow: 'ellipsis',
             whiteSpace: 'nowrap'
           }} />
       </Box>
-      {!isDraft && <LockIcon />}
+
+      {!isDraft && (
+        <Box sx={{ marginRight: '4px' }}>
+          <LockIcon />
+        </Box>
+      )}
     </MenuItem>
   );
 };
