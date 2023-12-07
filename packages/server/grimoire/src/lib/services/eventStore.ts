056497692a53976c203beab042c925834a2844cd
Michael <m.sharpe@project89.org>
2023-12-06 23:32:39 -0800

    Refactor eventStore to use generic EventPayload type

--- packages/server/grimoire/src/lib/services/eventStore.ts
+++ packages/server/grimoire/src/lib/services/eventStore.ts
@@ -1,22 +1,22 @@
 import { EventPayload } from 'server/plugin'
 
 export interface IEventStore {
-  currentEvent: () => EventPayload<unknown> | null
-  setEvent: (event: EventPayload<unknown>) => void
+  currentEvent: () => EventPayload | null
+  setEvent: (event: EventPayload) => void
 }
 
 export class EventStore implements IEventStore {
-  private _currentEvent: EventPayload<unknown> | null
+  private _currentEvent: EventPayload | null
 
   constructor() {
     this._currentEvent = null
   }
 
-  public currentEvent(): EventPayload<unknown> | null {
+  public currentEvent(): EventPayload | null {
     return this._currentEvent
   }
 
-  public setEvent(event: EventPayload<unknown>) {
+  public setEvent(event: EventPayload) {
     this._currentEvent = event
   }
 }
