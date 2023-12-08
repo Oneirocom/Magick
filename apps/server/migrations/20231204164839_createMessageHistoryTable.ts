13a605e9af0bfe1d67cbbc5b59a7fce44f73a60f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-12-08 15:35:37 -0500

    enable uuidv4

--- apps/server/migrations/20231204164839_createMessageHistoryTable.ts
+++ apps/server/migrations/20231204164839_createMessageHistoryTable.ts
@@ -1,21 +1,20 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
   await knex.schema.createTable('chatMessages', (table) => {
-    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.uuid('agentId').unsigned().notNullable();
-    table.foreign('agentId').references('id').inTable('agents');
-    table.string('sender').notNullable();
-    table.string('connector').notNullable();
-    table.string('content').notNullable();
-    table.string('coversation_id');
-    table.timestamps(true, true);
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.uuid('agentId').unsigned().notNullable()
+    table.foreign('agentId').references('id').inTable('agents')
+    table.string('sender').notNullable()
+    table.string('connector').notNullable()
+    table.string('content').notNullable()
+    table.string('coversation_id')
+    table.timestamps(true, true)
   })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('chatMessages');
+  await knex.schema.dropTable('chatMessages')
 }
-
