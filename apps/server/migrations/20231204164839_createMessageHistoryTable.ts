13507867b2b2b5772ecd19c55f1039a592f52cf6
benbot <bbotwin@atlassian.com>
2023-12-08 15:28:36 -0500

    removed nullable constraing and unused field

--- apps/server/migrations/20231204164839_createMessageHistoryTable.ts
+++ apps/server/migrations/20231204164839_createMessageHistoryTable.ts
@@ -4,13 +4,12 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('chatMessages', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.string('message').notNullable();
     table.uuid('agentId').unsigned().notNullable();
     table.foreign('agentId').references('id').inTable('agents');
     table.string('sender').notNullable();
     table.string('connector').notNullable();
     table.string('content').notNullable();
-    table.string('coversation_id').notNullable();
+    table.string('coversation_id');
     table.timestamps(true, true);
   })
 }
