684f608ae519d0ed021e539615145cb0d8bee6f7
Michael <m.sharpe@project89.org>
2023-12-19 23:30:11 -0800

    Make sur eto dispatch tag invalidation AFTER running the next action

--- packages/client/state/src/lib/middleware/spellCacheInvalidationMiddleware.ts
+++ packages/client/state/src/lib/middleware/spellCacheInvalidationMiddleware.ts
@@ -3,13 +3,12 @@ import { spellApi } from '../api/spells'
 
 export const cacheInvalidationMiddleware: Middleware =
   store => next => action => {
-    if (
-      action.type === 'globalConfig/setCurrentSpellReleaseId' ||
-      action.type === 'globalConfig/setCurrentAgentId'
-    ) {
+    const result = next(action)
+
+    if (action.type === 'globalConfig/setCurrentSpellReleaseId') {
       // Invalidate specific query cache
       store.dispatch(spellApi.util.invalidateTags(['Spells', 'Spell']))
     }
 
-    return next(action)
+    return result
   }
