69ed1b6f3bea93ad7ec0dfc33ea0560e95c2fe95
Jakob <haruhunab1320@gmail.com>
2023-12-20 15:31:45 -0800

    add completion coreEventTypes

--- packages/server/plugin/src/lib/coreEventTypes.ts
+++ packages/server/plugin/src/lib/coreEventTypes.ts
@@ -1,1 +1,4 @@
 export const ON_MESSAGE = 'messageReceived'
+export const ON_COMPLETION = 'completionReceived'
+export const ON_STREAM_START = 'streamedCompletionStarted'
+export const ON_STREAM_END = 'streamedCompletionEnded'
