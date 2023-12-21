2be58652ee7bcac85f7ff1dd94987f3a9f844cbe
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-12-21 04:56:48 -0500

    make agent_credentials cascade

--- apps/server/migrations/20231213120000_add_agent_credentials_table.ts
+++ apps/server/migrations/20231213120000_add_agent_credentials_table.ts
@@ -4,8 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('agent_credentials', (table) => {
     table.uuid('agentId').notNullable()
     table.uuid('credentialId').notNullable()
-    table.foreign('agentId').references('id').inTable('agents')
-    table.foreign('credentialId').references('id').inTable('credentials')
+    table.foreign('agentId').references('id').inTable('agents').onDelete('CASCADE')
+    table.foreign('credentialId').references('id').inTable('credentials').onDelete('CASCADE')
     table.primary(['agentId', 'credentialId'])
     table.timestamps(true, true)
   })
