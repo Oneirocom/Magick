0dd2bec60ac3b0949913804f81ed907708b2644d
Michael <m.sharpe@project89.org>
2023-12-21 16:28:41 -0800

    Remove buildspec nx command package

--- packages/server/redis-pubsub/src/lib/redis-pubsub-koa.ts
+++ packages/server/redis-pubsub/src/lib/redis-pubsub-koa.ts
@@ -2,10 +2,13 @@ import { Application } from '@feathersjs/koa/lib'
 import { RedisPubSub } from './redis-pubsub'
 import { RedisClientOptions } from 'redis'
 
-export const koaPubSub = async (options: RedisClientOptions = {}) => {
+export const koaPubSub = async (
+  redisCloudUrl: string,
+  options: RedisClientOptions = {}
+) => {
   const pubsub = new RedisPubSub()
 
-  await pubsub.initialize(options)
+  await pubsub.initialize(redisCloudUrl, options)
 
   return (app: Application) => {
     app.set('pubsub', pubsub)
