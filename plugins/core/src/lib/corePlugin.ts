7439007b04f8cf79e619fc4b9b89d65bab31fd64
Michael <m.sharpe@project89.org>
2023-12-22 10:58:51 -0800

    Swap event queue to use central event bus

--- plugins/core/src/lib/corePlugin.ts
+++ plugins/core/src/lib/corePlugin.ts
@@ -71,7 +71,7 @@ export class CorePlugin extends CoreEventsPlugin {
   getDependencies() {
     return {
       coreActionService: new CoreActionService(
-        this.connection,
+        this.centralEventBus,
         this.actionQueueName
       ),
     }
@@ -106,16 +106,17 @@ export class CorePlugin extends CoreEventsPlugin {
     this.emitEvent(ON_MESSAGE, event)
   }
 
-  handleSendMessage(actionPayload: Job<ActionPayload>) {
-    const { actionName, event } = actionPayload.data
+  handleSendMessage(actionPayload: ActionPayload) {
+    const { actionName, event } = actionPayload
     const { plugin } = event
     const eventName = `${plugin}:${actionName}`
+    this.logger.trace(`Sending message to ${eventName} on core plugin`)
     // handle sending a message back out.
 
     if (plugin === 'Core') {
-      this.client.sendMessage(actionPayload.data)
+      this.client.sendMessage(actionPayload)
     } else {
-      this.centralEventBus.emit(eventName, actionPayload.data)
+      this.centralEventBus.emit(eventName, actionPayload)
     }
   }
 
--- plugins/core/src/lib/services/coreActionService.ts
+++ plugins/core/src/lib/services/coreActionService.ts
@@ -1,18 +1,24 @@
+import { EventEmitter } from 'events'
 import Redis from 'ioredis'
 import { BullQueue } from 'server/communication'
-import { EventPayload } from 'server/plugin'
+import { ActionPayload, EventPayload } from 'server/plugin'
 
 export class CoreActionService {
-  protected actionQueue: BullQueue
+  protected queueName: string
+  protected eventBus: EventEmitter
+  protected emitEvent: (eventName: string, payload: ActionPayload) => void
 
-  constructor(connection: Redis, queueName: string) {
-    this.actionQueue = new BullQueue(connection)
-    this.actionQueue.initialize(queueName)
+  constructor(eventBus: EventEmitter, queueName: string) {
+    this.eventBus = eventBus
+    this.queueName = queueName
+    this.emitEvent = (eventName: string, payload: ActionPayload) => {
+      this.eventBus.emit(eventName, payload)
+    }
   }
 
-  async sendMessage(event: EventPayload<unknown>, messageContent: any) {
+  async sendMessage(event: EventPayload, messageContent: any) {
     // Enqueue the message
-    await this.actionQueue.addJob('sendMessage', {
+    await this.emitEvent('sendMessage', {
       actionName: 'sendMessage',
       event,
       data: messageContent,
