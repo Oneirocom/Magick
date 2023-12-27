ac50dcd3767602e4ce3f29ac818bc858aeb33234
Michael <m.sharpe@project89.org>
2023-12-27 15:35:36 -0800

    Refactor core meassage event to use new pattern

--- plugins/core/src/lib/nodes/events/messageEvent.ts
+++ plugins/core/src/lib/nodes/events/messageEvent.ts
@@ -1,8 +1,4 @@
-import {
-  Assert,
-  NodeCategory,
-  makeEventNodeDefinition,
-} from '@magickml/behave-graph'
+import { NodeCategory } from '@magickml/behave-graph'
 import { BaseEmitter, EventPayload, ON_MESSAGE } from 'server/plugin'
 import { CorePluginEvents } from '../../corePlugin'
 import { makeMagickEventNodeDefinition } from 'server/grimoire'
@@ -11,57 +7,36 @@ type State = {
   onStartEvent?: ((event: EventPayload) => void) | undefined
 }
 
+// Define the initial state function
 const makeInitialState = (): State => ({
   onStartEvent: undefined,
 })
 
-export const messageEvent = makeMagickEventNodeDefinition({
-  typeName: 'magick/onMessage',
-  label: 'On Message',
-  category: NodeCategory.Event,
-  in: {},
-  out: {
-    flow: 'flow',
-    content: 'string',
-    event: 'object',
+// Define the specific event configuration for the message event
+const messageEventConfig = {
+  handleEvent: (event: EventPayload, args: any) => {
+    const { write, commit } = args
+    write('event', event)
+    write('content', event.content)
+    commit('flow')
   },
-  initialState: makeInitialState(),
-  init: args => {
-    const {
-      write,
-      commit,
-      node,
-      engine,
-      graph: { getDependency },
-    } = args
-    const onStartEvent = (event: EventPayload) => {
-      write('event', event)
-      write('content', event.content)
+  dependencyName: 'Core', // specify the dependency name for the Core Event Emitter
+  eventName: ON_MESSAGE, // specify the event name to listen to
+}
 
-      commit('flow')
-
-      if (!node || !engine) return
-
-      engine.onNodeExecutionEnd.emit(node)
-    }
-
-    const coreEventEmitter =
-      getDependency<BaseEmitter<CorePluginEvents>>('Core')
-
-    coreEventEmitter?.on(ON_MESSAGE, onStartEvent)
-
-    return {
-      onStartEvent,
-    }
+// Utilize the new factory function to create the message event
+export const messageEvent = makeMagickEventNodeDefinition(
+  {
+    typeName: 'magick/onMessage',
+    label: 'On Message',
+    category: NodeCategory.Event,
+    in: {},
+    out: {
+      flow: 'flow',
+      content: 'string',
+      event: 'object',
+    },
+    initialState: makeInitialState(),
   },
-  dispose: ({ state: { onStartEvent }, graph: { getDependency } }) => {
-    Assert.mustBeTrue(onStartEvent !== undefined)
-
-    const coreEventEmitter =
-      getDependency<BaseEmitter<CorePluginEvents>>('Core')
-
-    if (onStartEvent) coreEventEmitter?.removeListener(ON_MESSAGE, onStartEvent)
-
-    return {}
-  },
-})
+  messageEventConfig
+)
