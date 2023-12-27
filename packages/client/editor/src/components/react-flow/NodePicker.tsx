34b97a0c2a9332a3081ae7de60e20d3c63bb9ad0
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2023-12-27 17:13:27 +0200

    re-organized node picker menu

--- packages/client/editor/src/components/react-flow/NodePicker.tsx
+++ packages/client/editor/src/components/react-flow/NodePicker.tsx
@@ -2,7 +2,7 @@ import { NodeSpecJSON } from '@magickml/behave-graph';
 import React, { useEffect, useState } from 'react';
 import { useReactFlow, XYPosition } from 'reactflow';
 import { useOnPressKey } from '../../hooks/react-flow/useOnPressKey';
-
+import styles from './nodePicker.module.scss';
 export type NodePickerFilters = {
   handleType: 'source' | 'target';
   valueType: string;
@@ -15,6 +15,53 @@ type NodePickerProps = {
   onClose: () => void;
   specJSON: NodeSpecJSON[] | undefined;
 };
+
+type ItemType = {
+  title: string
+  type?: string
+  subItems: ItemType[]
+}
+
+type Props = {
+  item: ItemType, onPickNode: Function, position: XYPosition, index: number
+  focusedIndex: number
+  setFocusedIndex: Function
+}
+
+
+const Item = ({ focusedIndex, index, setFocusedIndex, item, position, onPickNode }: Props) => {
+  const instance = useReactFlow();
+  const [visibleSubitems, setVisibleSubitems] = useState(false);
+
+  const handleClick = () => {
+    if (item.type) {
+      onPickNode(item.type, instance.project(position));
+    }
+  }
+
+  return (
+    <div
+      className={
+        `p-2 capitalize text-base border-b border-gray-600 cursor-pointer hover:bg-gray-600 ${index === focusedIndex ? 'bg-gray-700' : 'hover:bg-gray-600'
+        } ` + styles['item'] + ' ' + (item.subItems ? styles['hasSubitems'] : '')
+      }
+      key={item.title}
+      onMouseEnter={() => setFocusedIndex(index)}
+      onClick={handleClick}
+      onMouseOver={() => setVisibleSubitems(true)}
+      onMouseLeave={() => setVisibleSubitems(false)}
+    >
+      {item.title ?? item.type.split('/')[1]}
+      {item.subItems && visibleSubitems && (
+        <div className={styles['subitems']}>
+          {item.subItems.map(subitem =>
+            <Item key={subitem.title} item={subitem} onPickNode={onPickNode} position={position} focusedIndex={focusedIndex} index={index} setFocusedIndex={setFocusedIndex} />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export const NodePicker: React.FC<NodePickerProps> = ({
   position,
@@ -103,33 +150,56 @@ export const NodePicker: React.FC<NodePickerProps> = ({
     };
   }, [filtered, focusedIndex, onPickNode, instance, position, onClose]);
 
+  // Group the nodes by category and subcategory
+  const groupedData = filtered.reduce((result, node) => {
+    const typeParts = node.type.split('/');
+    let category = node.category as string;
+    let subcategory = undefined;
+
+    if (category === 'None') {
+      category = typeParts[0].charAt(0).toUpperCase() + typeParts[0].slice(1);
+      subcategory = typeParts[2] ? typeParts[2] : typeParts[1];
+    }
+
+    let categoryIndex = result.findIndex(item => item.title === category);
+    if (categoryIndex === -1) {
+      result.push({ title: category, subItems: [] });
+      categoryIndex = result.length - 1;
+    }
+
+    if (subcategory) {
+      let subcategoryIndex = result[categoryIndex].subItems.findIndex(item => item.title === subcategory);
+      if (subcategoryIndex === -1) {
+        result[categoryIndex].subItems.push({ title: subcategory, subItems: [] });
+        subcategoryIndex = result[categoryIndex].subItems.length - 1;
+      }
+      result[categoryIndex].subItems[subcategoryIndex].subItems.push(node);
+    } else {
+      result[categoryIndex].subItems.push(node);
+    }
+
+    return result;
+  }, []);
+
   return (
     <div
-      className="node-picker absolute z-10 text-sm text-white bg-gray-800 border rounded border-gray-500"
+      className="fixed z-10 text-sm text-white bg-gray-800 border border-gray-500 rounded node-picker"
       style={{ top: position.y, left: position.x }}
     >
-      <div className="bg-gray-500 p-2">Add Node</div>
+      <div className="p-2 bg-gray-500">Add Node</div>
       <div className="p-2">
         <input
           type="text"
           autoFocus
           placeholder="Type to filter"
-          className=" bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2"
+          className="w-full px-2 py-1 bg-gray-600 disabled:bg-gray-700"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
-      <div className="max-h-48 overflow-y-scroll">
-        {filtered.map(({ type }, index) => (
-          <div
-            key={type}
-            className={`p-2 cursor-pointer border-b border-gray-600 ${index === focusedIndex ? 'bg-gray-700' : 'hover:bg-gray-600'
-              }`}
-            onMouseEnter={() => setFocusedIndex(index)}
-            onClick={() => onPickNode(type, instance.project(position))}
-          >
-            {type}
-          </div>
+      <div className={styles['context-menu']} >
+        {groupedData.map((item, index) => (
+          <Item key={item.title} item={item} onPickNode={onPickNode} position={position} index={index} setFocusedIndex={setFocusedIndex} focusedIndex={focusedIndex} />
         ))}
       </div>
     </div>
