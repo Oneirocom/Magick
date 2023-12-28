dc4ff7ce8a5bcc8fac66ea81c503c4da4f00c32c
Emmyn5600 <emmy66418@gmail.com>
2023-12-28 12:42:02 +0200

    adding userId to frigadesetup

--- packages/client/editor/src/contexts/FrigadeProvider.tsx
+++ packages/client/editor/src/contexts/FrigadeProvider.tsx
@@ -2,7 +2,7 @@ import { FrigadeProvider as FrigadeProviderOG, useUser } from "@frigade/react";
 import { useNavigate } from "react-router-dom";
 import {FRIGADE_KEY } from 'shared/config'
 import { useSelector } from "react-redux"
-// import { app } from 'server/core'
+
 
 type Props = {
   children: React.ReactNode;
@@ -10,28 +10,15 @@ type Props = {
 
 const FrigadeProvider = ({ children }: Props) => {
     const globalConfig = useSelector((state: any) => state.globalConfig)
-    const token = globalConfig?.token
     const navigate = useNavigate();
 
-    //TODO: This is a hack to get the user id from the token. We should be able to get this from token payload
-  // const verifyToken = async () => {
-  //   try {
-  //     const payload = await app
-  //       .service('authentication')
-  //       .verifyAccessToken(token, {}) // Provide the required arguments here
-  //     console.log('Token verified:', payload)
-  //   } catch (error) {
-  //     console.error('Token verification failed:', error)
-  //   }
-  // }
 
-  console.log("Ftoken", token)
 
 
   return (
     <FrigadeProviderOG
       publicApiKey={FRIGADE_KEY|| ""}
-      userId={"user.userId" || "anonymous"}
+      userId={globalConfig?.userId || "anonymous"}
       config={{
         navigate: (url, target): void => {
           if (target === "_blank") {
