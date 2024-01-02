62a8bbeecafeee98685232ad6e956a720ffa7efc
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-01-02 14:06:02 -0500

    update emitter

--- plugins/slack/src/lib/dependencies/slackEmitter.ts
+++ plugins/slack/src/lib/dependencies/slackEmitter.ts
@@ -1,18 +1,11 @@
-import { EventEmitter } from 'events'
+import { EmitterFactory, CreateMessageEvents } from 'plugins/factory'
 import TypedEmitter from 'typed-emitter'
-import { EventPayload } from 'server/plugin'
-import { SLACK_MESSAGES } from '../constants'
+import { SLACK_EVENTS } from '../constants'
 
-// Define event types with payloads conforming to EventMap
-type SlackMessageEvents = {
-  [K in keyof typeof SLACK_MESSAGES]: (event: EventPayload) => void
-} & {
-  error: (error: Error) => void
-}
+export type SlackMessageEvents = CreateMessageEvents<typeof SLACK_EVENTS>
 
-// Typed event emitter
 export type SlackEmitterType = TypedEmitter<SlackMessageEvents>
 
-// Create a single emitter for all Slack message events
-export const SlackEmitter: SlackEmitterType =
-  new EventEmitter() as unknown as SlackEmitterType
+export const SlackEmitter = new EmitterFactory<SlackMessageEvents>(
+  SLACK_EVENTS
+).getEmitter() as SlackEmitterType
