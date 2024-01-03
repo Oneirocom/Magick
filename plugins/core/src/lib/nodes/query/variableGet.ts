5e4a9bc85e048f7a095c9e950ac3c353e094e56c
Michael <m.sharpe@project89.org>
2024-01-03 14:49:02 -0800

    Use variable service in get variable node

--- plugins/core/src/lib/nodes/query/variableGet.ts
+++ plugins/core/src/lib/nodes/query/variableGet.ts
@@ -4,6 +4,7 @@ import {
   Variable,
   makeFunctionNodeDefinition,
 } from '@magickml/behave-graph'
+import { IVariableService } from '../../services/variableService'
 
 export const variableGet = makeFunctionNodeDefinition({
   typeName: 'variables/get',
@@ -29,6 +30,18 @@ export const variableGet = makeFunctionNodeDefinition({
   },
   in: {},
   out: (configuration, graph) => {
+    const variable = graph.variables[configuration.variableId]
+
+    if (!variable) return []
+
+    return [
+      {
+        key: variable.name,
+        name: variable.name,
+        valueType: variable.valueTypeName,
+      },
+    ]
+
     const socketArray = configuration?.socketOutputs.length
       ? configuration.socketOutputs
       : []
@@ -44,12 +57,29 @@ export const variableGet = makeFunctionNodeDefinition({
 
     return sockets
   },
-  exec: async ({ write, graph: { variables }, configuration }) => {
+  exec: async ({
+    write,
+    graph: { variables, getDependency },
+    configuration,
+  }) => {
+    debugger
     const variable = variables[configuration.variableId]
     const output = configuration.socketOutputs[0]
 
     if (!variable) return
 
-    write(output.name, variable.get())
+    const variableService = getDependency<IVariableService>('IVariableService')
+
+    if (!variableService) return
+
+    const value = await variableService.getVariable(variable.name)
+
+    if (!value) {
+      // set the variable to the default value
+      await variableService.setVariable(variable.name, variable.initialValue)
+      write(output.name, variable.initialValue)
+    }
+
+    write(output.name, value)
   },
 })
