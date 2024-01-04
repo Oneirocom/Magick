469f29054cb5778070523df4851022ebe43fb2d5
Michael <m.sharpe@project89.org>
2024-01-04 15:27:04 -0800

    Improve colors

--- packages/client/editor/src/utils/colors.ts
+++ packages/client/editor/src/utils/colors.ts
@@ -11,14 +11,14 @@ export type color =
   | 'orange'
 
 export const colors: Record<color, [string, string, string]> = {
-  red: ['bg-orange-700', 'border-orange-700', 'text-white'],
+  red: ['bg-rose-700', 'border-rose-700', 'text-white'],
   green: ['bg-green-600', 'border-green-600', 'text-white'],
   lime: ['bg-lime-500', 'border-lime-500', 'text-gray-900'],
-  purple: ['bg-purple-500', 'border-purple-500', 'text-white'],
-  blue: ['bg-cyan-600', 'border-cyan-600', 'text-white'],
+  purple: ['bg-violet-700', 'border-violet-700', 'text-white'],
+  blue: ['bg-sky-600', 'border-sky-600', 'text-white'],
   gray: ['bg-gray-500', 'border-gray-500', 'text-white'],
   white: ['bg-white', 'border-white', 'text-gray-700'],
-  orange: ['bg-orange-500', 'border-orange-500', 'text-white'],
+  orange: ['bg-orange-700', 'border-orange-700', 'text-white'],
 }
 
 export const valueTypeColorMap: Record<string, string> = {
