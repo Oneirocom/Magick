954a149179d74ca7a700c619422ee94939ab3329
Michael <m.sharpe@project89.org>
2024-01-03 21:29:17 -0800

    Refactor VariableNames component to use useCallback and updateConfigKeys

--- packages/client/editor/src/components/PropertiesWindow/variableNames.tsx
+++ packages/client/editor/src/components/PropertiesWindow/variableNames.tsx
@@ -1,6 +1,6 @@
 import { cx } from "class-variance-authority";
 import { ConfigurationComponentProps } from "./PropertiesWindow";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { VariableJSON } from "@magickml/behave-graph";
 import { useReactFlow } from "reactflow";
 
@@ -8,10 +8,16 @@ const inputClass = cx(
   'bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2 nodrag text-md justify-start flex',
 );
 
-export const VariableNames = ({ spell, fullConfig, updateConfigKey }: ConfigurationComponentProps) => {
-  const { variableId } = fullConfig
+export const VariableNames = ({ spell, fullConfig, updateConfigKeys }: ConfigurationComponentProps) => {
+  const { variableId, socketInputs, socketOutputs } = fullConfig
   const [selectedVariable, setSelectedVariable] = useState<VariableJSON>(null)
   const reactFlow = useReactFlow()
+
+  const setEdges = useCallback((edges) => {
+    const filtered = edges.filter(edge => edge.sourceHandle !== selectedVariable?.name && edge.targetHandle !== selectedVariable?.name)
+
+    return filtered
+  }, [selectedVariable])
 
   useEffect(() => {
     if (!spell && !fullConfig) return
@@ -21,22 +27,38 @@ export const VariableNames = ({ spell, fullConfig, updateConfigKey }: Configurat
     setSelectedVariable(variable)
   }, [spell, fullConfig])
 
-  useEffect(() => {
-    if (!selectedVariable) return
-    const variableSocket = {
-      name: selectedVariable.name,
-      valueType: selectedVariable.valueTypeName
-    }
-    updateConfigKey('socketOutputs', [variableSocket])
-  }, [selectedVariable])
-
   const updateValue = (value) => {
     const variable = spell.graph.variables.find(variable => variable.id === value) as VariableJSON
 
-    reactFlow.setEdges(edges => edges.filter(edge => edge.sourceHandle !== selectedVariable?.name))
-    updateConfigKey('variableId', value)
+    reactFlow.setEdges(setEdges)
+
     setSelectedVariable(variable)
   }
+
+  useEffect(() => {
+    if (!selectedVariable) return
+    if (selectedVariable.id === variableId) return
+    const socket = {
+      name: selectedVariable.name,
+      valueType: selectedVariable.valueTypeName
+    }
+
+    const configUpdate = {
+      variableId: selectedVariable.id,
+      label: selectedVariable.name
+    }
+
+    if (socketInputs) {
+      configUpdate['socketInputs'] = [socket]
+    }
+
+    if (socketOutputs) {
+      configUpdate['socketOutputs'] = [socket]
+    }
+
+    updateConfigKeys(configUpdate)
+  }, [selectedVariable])
+
 
   if (!spell) return null;
 
@@ -44,10 +66,11 @@ export const VariableNames = ({ spell, fullConfig, updateConfigKey }: Configurat
     <div>
       <select
         className={inputClass}
-        value={selectedVariable?.id || "select a variable"}
+        value={selectedVariable?.id || 'default'}
         onChange={(e) => updateValue(e.currentTarget.value)}
       >
         <>
+          <option disabled selected value="default">Select a variable</option>
           {(spell.graph.variables as VariableJSON[]).map((variable) => (
             <option key={variable.id} value={variable.id}>
               {variable.name}
