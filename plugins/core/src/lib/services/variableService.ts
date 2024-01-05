37634682c9e809b6da64c4a0bc5d11fb1e718dd6
Michael <m.sharpe@project89.org>
2024-01-05 11:58:03 -0800

    Add key usage to variable service

--- plugins/core/src/lib/services/variableService.ts
+++ plugins/core/src/lib/services/variableService.ts
@@ -1,7 +1,7 @@
 import Keyv from 'keyv'
 import KeyvRedis from '@keyv/redis'
 import Redis from 'ioredis'
-import { IEventStore } from 'server/grimoire'
+import { IEventStore, SpellCaster } from 'server/grimoire'
 
 // This is the interface you'll use to get and set variables.
 export interface IVariableService {
@@ -12,19 +12,31 @@ export interface IVariableService {
 // This is the class you'll use to get and set variables.
 export class IVariableService {
   private keyv: Keyv
+  private spellCaster: SpellCaster
+  private agentId: string
 
-  constructor(connection: Redis) {
+  constructor(connection: Redis, agentId: string, spellCaster: SpellCaster) {
     const keyvRedis = new KeyvRedis(connection)
     this.keyv = new Keyv({ store: keyvRedis })
+    this.agentId = agentId
+    this.spellCaster = spellCaster
+  }
+
+  getKey(name: string) {
+    // also grab event from eventstore in dependencies
+    // check
+    return `agent:${this.agentId}:variable:${name}`
   }
 
   // Set a variable in the storage.
   async setVariable(name: string, value: any): Promise<void> {
-    await this.keyv.set(name, value)
+    const key = this.getKey(name)
+    await this.keyv.set(key, value)
   }
 
   // Get a variable from the storage.
   async getVariable(name: string): Promise<any> {
-    return await this.keyv.get(name)
+    const key = this.getKey(name)
+    return await this.keyv.get(key)
   }
 }
