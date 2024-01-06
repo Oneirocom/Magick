2d12df1dbcbf5e1f43ada65f2f62b919a368cc98
Michael <m.sharpe@project89.org>
2024-01-05 20:03:37 -0800

    While loop

---
+++ plugins/core/src/lib/nodes/flow/whileLoop.ts
@@ -0,0 +1,49 @@
+import { NodeCategory, makeAsyncNodeDefinition } from '@magickml/behave-graph'
+
+const hardStop = 1000
+
+export const whileLoop = makeAsyncNodeDefinition({
+  typeName: 'flow/whileLoop',
+  otherTypeNames: ['flow/while'],
+  category: NodeCategory.Flow,
+  label: 'While Loop',
+  configuration: {
+    maxIterations: {
+      valueType: 'number',
+      defaultValue: 10,
+    },
+  },
+  in: {
+    flow: {
+      valueType: 'flow',
+    },
+    condition: {
+      valueType: 'boolean',
+      defaultValue: false,
+    },
+  },
+  out: {
+    loopBody: 'flow',
+    completed: 'flow',
+  },
+  initialState: undefined,
+  triggered: ({ configuration, finished = () => {}, commit, read }) => {
+    const { maxIterations } = configuration
+
+    const stop = maxIterations < hardStop ? maxIterations : hardStop
+
+    const loopBodyIteration = (i: number) => {
+      const condition = read('condition')
+      if (i < stop && !condition) {
+        commit('loopBody', () => {
+          loopBodyIteration(i + 1)
+        })
+      } else {
+        commit('completed')
+        finished()
+      }
+    }
+    loopBodyIteration(1)
+  },
+  dispose: () => {},
+})
