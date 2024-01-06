9aac2146d4161ac7ac8b6ef16a6b4b5bd0989973
Michael <m.sharpe@project89.org>
2024-01-05 22:39:52 -0800

    Remove console.log statement in socketsFromNum.ts

--- packages/client/editor/src/utils/socketsFromNum.ts
+++ packages/client/editor/src/utils/socketsFromNum.ts
@@ -2,7 +2,6 @@ export const socketsFromNumInputs = (numInputs: number, valueType = 'flow') => {
   const sockets = []
   if (numInputs === 0) return sockets
 
-  console.log('Adding sockets!!', numInputs)
   for (let i = 1; i <= numInputs; i++) {
     sockets.push({
       name: `${i}`,
