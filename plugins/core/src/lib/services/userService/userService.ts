d709d4961152a01368b02e59ab86ba49bfef56ac
Jakob <haruhunab1320@gmail.com>
2024-01-10 14:41:19 -0800

    add initial userservice

--- plugins/core/src/lib/services/userService/userService.ts
+++ plugins/core/src/lib/services/userService/userService.ts
@@ -2,27 +2,26 @@ import { CLOUD_AGENT_KEY, PORTAL_URL } from 'shared/config'
 
 interface User {
   id: string
+  email: string
+  name: string
 }
 interface IUserService {
-  getUserInfo(projectId: string): Promise<User>
-  // getUserBudget(userId): Promise<number>
-  // getUserBalance(): Promise<number>
-  // subtractBalance(amount: number): Promise<void>
+  getUser(projectId: string): Promise<User>
 }
 
 export class UserService implements IUserService {
-  async getUserInfo(projectId: string): Promise<User> {
-    const userData = await fetch(`${PORTAL_URL}/api/magick/user`, {
+  async getUser(projectId: string): Promise<User> {
+    const userData: User = await fetch(`${PORTAL_URL}/api/magick/user`, {
       method: 'POST',
       body: JSON.stringify({
         projectId,
         token: CLOUD_AGENT_KEY,
       }),
-    })
+    }).then(res => res.json())
 
-    if (!userData.ok) {
+    if (!userData) {
       throw new Error('User not found')
     }
-    return await userData.json()
+    return userData
   }
 }
