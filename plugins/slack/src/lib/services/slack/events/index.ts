0f97685d8dfc6610602eeb0cdb2dc35ba5e8c16e
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-01-11 14:37:31 -0500

    slack event typeguards

---
+++ plugins/slack/src/lib/services/slack/events/index.ts
@@ -0,0 +1,24 @@
+import * as events from './isEvent'
+import { MessageEvent } from '@slack/bolt'
+
+const typeGuardMap: { [key: string]: (event: MessageEvent) => boolean } = {
+  GenericMessageEvent: events.isGenericMessageEvent,
+  BotMessageEvent: events.isBotMessageEvent,
+  ChannelJoinMessageEvent: events.isChannelJoinMessageEvent,
+  ChannelLeaveMessageEvent: events.isChannelLeaveMessageEvent,
+  FileShareMessageEvent: events.isFileShareMessageEvent,
+  MeMessageEvent: events.isMeMessageEvent,
+  MessageChangedEvent: events.isMessageChangedEvent,
+  MessageDeletedEvent: events.isMessageDeletedEvent,
+  MessageRepliedEvent: events.isMessageRepliedEvent,
+}
+
+export function identifyMessageType(event: MessageEvent): string | undefined {
+  for (const [type, guard] of Object.entries(typeGuardMap)) {
+    if (guard(event)) {
+      return type
+    }
+  }
+
+  return undefined
+}
