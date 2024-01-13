b9764877638b7af26da2a65d190920b21838b3b0
Michael <m.sharpe@project89.org>
2024-01-12 21:31:58 -0800

    Add init method to EventStore

--- packages/server/grimoire/src/lib/services/eventStore.ts
+++ packages/server/grimoire/src/lib/services/eventStore.ts
@@ -1,14 +1,20 @@
+import { GraphNodes } from '@magickml/behave-graph'
 import { EventPayload } from 'server/plugin'
 
 export interface IEventStore {
   currentEvent: () => EventPayload | null
   setEvent: (event: EventPayload) => void
+  init: (nodes: GraphNodes) => void
 }
 
 export class EventStore implements IEventStore {
   private _currentEvent: EventPayload | null
 
   constructor() {
+    this._currentEvent = null
+  }
+
+  public init(nodes: GraphNodes) {
     this._currentEvent = null
   }
 
