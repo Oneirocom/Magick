7a4d86e984f3115202e3575dfec4f026e0a94e08
Michael <m.sharpe@project89.org>
2024-01-15 11:51:19 -0800

    Add "Add Knowledge" action node definition

---
+++ plugins/core/src/lib/nodes/actions/addKnowledge.ts
@@ -0,0 +1,36 @@
+import { NodeCategory, makeFlowNodeDefinition } from '@magickml/behave-graph'
+
+import { ICoreMemoryService } from '../../services/coreMemoryService/coreMemoryService'
+import { DataType } from '../../services/coreMemoryService/coreMemoryTypes'
+
+export const addKnowledge = makeFlowNodeDefinition({
+  typeName: 'action/knowledge/add',
+  category: NodeCategory.Action,
+  label: 'Add Knowledge',
+  in: {
+    flow: 'flow',
+    knowledge: 'string',
+    dataType: {
+      label: 'Data Type',
+      choices: Object.values(DataType),
+      defaultValue: DataType.AUTO,
+      valueType: 'string',
+    },
+  },
+  out: {
+    flow: 'flow',
+  },
+  initialState: undefined,
+  triggered: async ({ commit, read, graph }) => {
+    const { getDependency } = graph
+    const _dataType = read('dataType') as string
+    const dataType = _dataType === 'auto' ? undefined : _dataType
+    const coreMemoryService =
+      getDependency<ICoreMemoryService>('coreMemoryService')
+    const knowledge = read('knowledge') as string
+
+    await coreMemoryService?.add(knowledge, dataType)
+
+    commit('flow')
+  },
+})
