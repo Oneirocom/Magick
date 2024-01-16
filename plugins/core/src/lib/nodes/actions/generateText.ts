74b1c085f37b7c0bcb93aad57688aff5c1fdab5f
Michael <m.sharpe@project89.org>
2024-01-16 00:38:06 -0800

    Switch generate text to use async generator

--- plugins/core/src/lib/nodes/actions/generateText.ts
+++ plugins/core/src/lib/nodes/actions/generateText.ts
@@ -4,10 +4,7 @@ import {
   makeFlowNodeDefinition,
 } from '@magickml/behave-graph'
 import { CoreLLMService } from '../../services/coreLLMService/coreLLMService'
-import {
-  CompletionResponse,
-  LLMModels,
-} from '../../services/coreLLMService/types'
+import { LLMModels } from '../../services/coreLLMService/types'
 
 export const generateText = makeFlowNodeDefinition({
   typeName: 'magick/generateText',
@@ -64,19 +61,7 @@ export const generateText = makeFlowNodeDefinition({
     modelUsed: 'string',
   },
   initialState: undefined,
-  triggered: async ({
-    state,
-    commit,
-    read,
-    write,
-    graph: { getDependency },
-  }) => {
-    const chunkQueue = [] as any[]
-    let done = false
-    let fullResponse = ''
-    let isProcessing = false
-    let completionResponse = null as CompletionResponse | null
-
+  triggered: async ({ commit, read, write, graph: { getDependency } }) => {
     /**
      * This promise wrapper is super important. If we await the whole LLM
      * completion, it will block continued fiber execution, which will prevent
@@ -88,7 +73,7 @@ export const generateText = makeFlowNodeDefinition({
      * multiple times.  We resolve the promise after commiting the first stream
      * event to the fiber
      */
-    return new Promise((resolve, reject) => {
+    return new Promise((outerResolve, outerReject) => {
       try {
         const coreLLMService = getDependency<CoreLLMService>('coreLLMService')
 
@@ -96,7 +81,6 @@ export const generateText = makeFlowNodeDefinition({
           throw new Error('No coreLLMService provided')
         }
 
-        const stream: boolean = read('stream') || false
         const model: LLMModels = read('model') || LLMModels.GPT35Turbo
         const prompt: string = read('prompt') || ''
         const temperature: number = read('temperature') || 0.5
@@ -114,95 +98,37 @@ export const generateText = makeFlowNodeDefinition({
           },
         }
 
-        /**
-         * This is the main loop of processing chunks.  It will process a chunk
-         * and then call itself recursively until the chunk queue is empty and
-         * the isDone flag is set.  Since the work done after calling onStream
-         * could take an unknown amount of time, we need to make sure we are
-         * separating the incoming stream from the processing. We accumulate the
-         * chunks in the chunkQueue, and then process them one at a time. This is
-         * why we loop inside the commit callback
-         */
-        const processChunk = () => {
-          // If we are processing a chunk, return and don't process
-          if (isProcessing) return
-
-          // We break the loop if the queue is empty and we are done streaming
-          if (chunkQueue.length === 0 && done) {
-            write('response', fullResponse)
-            write('completionResponse', completionResponse)
+        const processNextChunk = async iterator => {
+          const result = await iterator.next()
+          if (result.done) {
+            write('response', result.value.choices[0].message.content || '')
+            write('completionResponse', result.value) // Assuming the last value is the completion response
             write('modelUsed', model)
             commit('done')
-            return
+          } else {
+            const chunk = result.value
+            write('stream', chunk.choices[0].delta.content || '')
+
+            // Commit the 'onStream' event and call the processing function recursively for the next chunk
+            commit('onStream', async () => {
+              await processNextChunk(iterator)
+            })
           }
 
-          // grab the next available chunk
-          const chunk = chunkQueue.shift()
-
-          // Important that is chunk is undefined, we keep processing the queue
-          if (!chunk) {
-            processChunk()
-          }
-
-          // set processing flag to true so we don't process another chunk
-          isProcessing = true
-
-          // Write the content out to the stream socket
-          if (chunk) write('stream', chunk.choices[0].delta.content || '')
-
-          // Here we commit the next chunk to the fiber to be processed.
-          // When the whole chain is done, we resolve with the callback.
-          commit('onStream', () => {
-            // set processsing flag to false so the next call of processChunk
-            // goes through
-            isProcessing = false
-
-            // Loop process chunk to keep processing chunks from the queue
-            processChunk()
-          })
+          // We resolve and move the engine on after we process out first chunk
+          outerResolve(undefined)
         }
 
-        /**
-         * Important that we DON'T await this in full because it blocks execution
-         * of the built of commit fiber until the whole completion comes in,
-         * defeating the point of streaming.
-         */
-        coreLLMService.completion({
+        // our iterator is a generator function that will yield a chunk of data
+        // each time it is called
+        const iterator = coreLLMService.completionGenerator({
           request,
-          callback: (chunk, isDone, _completionResponse) => {
-            // if streaming, add the chunk to the queue
-            chunkQueue.push(chunk)
-
-            // Check for done state
-            if (isDone) {
-              completionResponse = _completionResponse
-              fullResponse = _completionResponse!.choices[0].message.content
-              done = true
-
-              // If we arent streaming, we can just resolve here
-              if (!stream) {
-                write('response', fullResponse)
-                write('completionResponse', completionResponse)
-                write('modelUsed', model)
-                commit('done')
-                resolve(undefined)
-              }
-            }
-            if (stream) {
-              // Kick off the main loop of processing
-              processChunk()
-
-              /**
-               * Super important that we resolve the promise wrapper AFTER we commit
-               * the first flow to the fiber.  This will cause the engine to keep
-               * processing the fiber queue while the messages are still streaming in,
-               * causing them to be processed by the processChunk loop.
-               */
-
-              resolve(undefined)
-            }
-          },
           maxRetries,
+        })
+
+        // Start the processing loop and pass in the iterator
+        processNextChunk(iterator).catch(error => {
+          throw new Error(error)
         })
       } catch (error: any) {
         const loggerService = getDependency<ILogger>('ILogger')
@@ -211,10 +137,8 @@ export const generateText = makeFlowNodeDefinition({
         }
         loggerService?.log('error', error.toString())
         console.error('Error in generateText:', error)
-        reject(error)
+        outerReject(error)
       }
-
-      return state
     })
   },
 })
