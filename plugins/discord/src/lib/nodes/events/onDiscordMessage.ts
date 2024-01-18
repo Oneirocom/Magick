464c182faf370f9525ca0741130be429c3ba328f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-01-18 13:00:45 -0500

    onDiscordNodes

---
+++ plugins/discord/src/lib/nodes/events/onDiscordMessage.ts
@@ -0,0 +1,86 @@
+import { DISCORD_EVENTS, discordPluginName } from '../../constants'
+import { EventPayload } from 'packages/server/plugin/src'
+import { DiscordEmitterType } from '../../dependencies/discordEmitter'
+import { createEventNode } from 'plugins/shared/src'
+import { DiscordEventPayload } from '../../types'
+
+type DiscordEventProcess<K extends keyof DiscordEventPayload> = (
+  write: (key: string, value: any) => void,
+  commit: (key: string) => void,
+  event: EventPayload<DiscordEventPayload[K]>
+) => void
+
+const processDiscordEvent: DiscordEventProcess<keyof DiscordEventPayload> = (
+  write,
+  commit,
+  event
+) => {
+  write('event', event.eventName)
+  write('content', event.content)
+  write('sender', event.sender)
+  write('channel', event.channel)
+  commit('flow')
+}
+
+const createDiscordEventNode = <
+  K extends keyof DiscordEventPayload = keyof DiscordEventPayload
+>(
+  typeName: string,
+  label: string,
+  eventKey: K,
+  out: Record<string, string>,
+  process: DiscordEventProcess<K>
+) =>
+  createEventNode<DiscordEmitterType, DiscordEventPayload[K]>({
+    base: {
+      typeName,
+      label,
+      in: {},
+      out,
+    },
+    emitterDependencyKey: discordPluginName,
+    process: process,
+    event: DISCORD_EVENTS[eventKey],
+  })
+
+export const onDiscordMessage = createDiscordEventNode<'messageCreate'>(
+  'discord/onMessage',
+  'On Discord Message',
+  'messageCreate',
+  {
+    flow: 'flow',
+    content: 'string',
+    sender: 'string',
+    channel: 'string',
+    event: 'object',
+  },
+  (write, commit, event) => {
+    write('content', event.data.content)
+    write('sender', event.data.author.username)
+    write('channel', event.data.channelId)
+    commit('flow')
+  }
+)
+
+export const onDiscordReactionAdd =
+  createDiscordEventNode<'messageReactionAdd'>(
+    'discord/onReactionAdd',
+    'On Discord Reaction Add',
+    'messageReactionAdd',
+    {
+      flow: 'flow',
+      reaction: 'string',
+      count: 'number',
+      messageId: 'string',
+      event: 'event',
+    },
+    (write, commit, event) => {
+      write('reaction', event.data.emoji.name)
+      write('count', event.data.count)
+      write('messageId', event.data.messageId)
+      write('event', event.data)
+      commit('flow')
+    }
+  )
+
+export const onDiscordMessageNodes = [onDiscordMessage, onDiscordReactionAdd]
