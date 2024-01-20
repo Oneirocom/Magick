99dd1fecb92caf84fbb8883a4e7911fa212dd9e7
Michael <m.sharpe@project89.org>
2024-01-20 10:17:11 -0800

    Refactor plugin configuration code

--- scripts/config-plugins.js
+++ scripts/config-plugins.js
@@ -1,6 +1,6 @@
 try {
   const dotenv = require('dotenv-flow')
-  dotenv.config('../');
+  dotenv.config('../')
 } catch {
   console.log(
     '`npm install` is likely running with `--dry-run` or `---package-lock-only` flags. Exiting.'
@@ -11,49 +11,63 @@ try {
 console.log('Server plugins', process.env.SERVER_PLUGINS)
 console.log('Client plugins', process.env.CLIENT_PLUGINS)
 
-const serverPlugins = process.env.SERVER_PLUGINS ? process.env.SERVER_PLUGINS.split(',').map(
-  (plugin) => {
-    return `@magickml/plugin-${plugin}-server`;
-  }
-) : [];
-const clientPlugins = process.env.CLIENT_PLUGINS ? process.env.CLIENT_PLUGINS.split(',').map(
-  (plugin) => {
-    return `@magickml/plugin-${plugin}-client`;
-  }
-) : [];
+const serverPlugins = process.env.SERVER_PLUGINS
+  ? process.env.SERVER_PLUGINS.split(',').map(plugin => {
+      return `@magickml/plugin-${plugin}-server`
+    })
+  : []
+const clientPlugins = process.env.CLIENT_PLUGINS
+  ? process.env.CLIENT_PLUGINS.split(',').map(plugin => {
+      return `@magickml/plugin-${plugin}-client`
+    })
+  : []
 
-const path = require('path');
-const fs  = require('fs');
+const path = require('path')
+const fs = require('fs')
 
-const pluginsJsPathClient = path.join(__dirname, '..', 'apps', 'client', 'src/plugins.ts');
-const pluginsJsPathServer = path.join(__dirname, '..', 'apps', 'server', 'src/plugins.ts');
-const pluginsJsPathAgent = path.join(__dirname, '..', 'apps', 'agent', 'src/plugins.ts');
-const pluginsJsPathCloudAgentWorker = path.join(__dirname, '..', 'apps', 'cloud-agent-worker', 'src/plugins.ts');
+const pluginsJsPathClient = path.join(
+  __dirname,
+  '..',
+  'apps',
+  'client',
+  'src/plugins.ts'
+)
+const pluginsJsPathServer = path.join(
+  __dirname,
+  '..',
+  'apps',
+  'server',
+  'src/plugins.ts'
+)
+const pluginsJsPathCloudAgentWorker = path.join(
+  __dirname,
+  '..',
+  'apps',
+  'cloud-agent-worker',
+  'src/plugins.ts'
+)
 
 function copyExamplePluginsJson() {
+  let i = 0
 
-  let i = 0;
-
-  let importString = 'const plugins = {}\n';
+  let importString = 'const plugins = {}\n'
   for (const plugin of serverPlugins) {
-    importString += `import {default as plugin${i}} from '${plugin}';\nplugins['${plugin}'] = plugin${i};\n`;
-    i++;
+    importString += `import {default as plugin${i}} from '${plugin}';\nplugins['${plugin}'] = plugin${i};\n`
+    i++
   }
-  importString += 'export default plugins;';
+  importString += 'export default plugins;'
 
-  fs.writeFileSync(pluginsJsPathServer, importString);
-  fs.writeFileSync(pluginsJsPathAgent, importString);
-  fs.writeFileSync(pluginsJsPathCloudAgentWorker, importString);
+  fs.writeFileSync(pluginsJsPathServer, importString)
+  fs.writeFileSync(pluginsJsPathCloudAgentWorker, importString)
 
-  importString = 'const plugins = {}\n';
+  importString = 'const plugins = {}\n'
   for (const plugin of clientPlugins) {
-    importString += `import {default as plugin${i}} from '${plugin}';\nplugins['${plugin}'] = plugin${i};\n`;
-    i++;
+    importString += `import {default as plugin${i}} from '${plugin}';\nplugins['${plugin}'] = plugin${i};\n`
+    i++
   }
-  importString += 'export default plugins;';
+  importString += 'export default plugins;'
 
-  fs.writeFileSync(pluginsJsPathClient, importString);
-
+  fs.writeFileSync(pluginsJsPathClient, importString)
 }
 
-copyExamplePluginsJson();
+copyExamplePluginsJson()
