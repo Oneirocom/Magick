1cdbc605dac5ed54360a25bae9b2ba50112c0e84
Michael <m.sharpe@project89.org>
2024-01-19 23:21:27 -0800

    Fix styled divider

--- packages/client/editor/src/components/Newsidebar/StyledDivider.tsx
+++ packages/client/editor/src/components/Newsidebar/StyledDivider.tsx
@@ -1,14 +1,17 @@
 import Divider from '@mui/material/Divider'
-import styled from 'styled-components'
 
-const StyledDivider = styled(Divider)(({ theme }) => ({
-  backgroundColor: '#3D454A',
-  borderColor: '#3D454A',
-  height: '1px',
-  marginTop: '4px',
-  marginBottom: '4px',
-  marginLeft: '10px',
-  marginRight: '10px',
-}))
+const StyledDivider = (props) => {
+  const style = {
+    backgroundColor: '#3D454A',
+    borderColor: '#3D454A',
+    height: '1px',
+    marginTop: '4px',
+    marginBottom: '4px',
+    marginLeft: '10px',
+    marginRight: '10px',
+  };
 
-export default StyledDivider
+  return <Divider style={style} {...props} />;
+};
+
+export default StyledDivider;
