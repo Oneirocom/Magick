16a52326447d36fc813d845e5540e9ce77d580af
Michael <m.sharpe@project89.org>
2024-01-21 11:51:24 -0800

    setEdges now also takes a function

--- packages/client/state/src/lib/tabs/reactFlow.ts
+++ packages/client/state/src/lib/tabs/reactFlow.ts
@@ -8,7 +8,19 @@ export const setNodes = (tabId: string, nodes: Node[]) => {
   store.dispatch(graphActions.setNodes({ tabId, nodes }))
 }
 
-export const setEdges = (tabId: string, edges: Edge[]) => {
+export const setEdges = (
+  tabId: string,
+  edges: Edge[] | ((edges: Edge[]) => Edge[])
+) => {
+  if (typeof edges === 'function') {
+    const store = createStore()
+    const _edges = store.getState().graph[tabId]?.edges || []
+    const newEdges = edges(_edges)
+
+    store.dispatch(graphActions.setEdges({ tabId, edges: newEdges }))
+    return
+  }
+
   const store = createStore()
   store.dispatch(graphActions.setEdges({ tabId, edges }))
 }
@@ -28,9 +40,8 @@ export const onEdgesChange = (tabId: string) => {
 }
 
 export const onConnect = (tabId: string) => {
-  return (_connection: Connection) => {
+  return (connection: Connection) => {
     const store = createStore()
-    const connection = { ..._connection, type: 'custom-edge' }
     store.dispatch(graphActions.onConnect({ tabId, connection }))
   }
 }
