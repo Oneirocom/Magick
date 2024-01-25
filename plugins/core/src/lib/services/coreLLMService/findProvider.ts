93a4ac9138a285c19b5f0ae0b83c24070d0a1cc7
Jakob <haruhunab1320@gmail.com>
2024-01-25 15:02:09 -0800

    add findProviderKey and findProviderName

--- plugins/core/src/lib/services/coreLLMService/findProvider.ts
+++ plugins/core/src/lib/services/coreLLMService/findProvider.ts
@@ -1,12 +1,17 @@
 import { modelProviderMap } from './constants/modelProviderMap'
-import { AllModels, LLMProviderKeys } from './types'
+import { AllModels } from './types/models'
+import { LLMProviderKeys, LLMProviders } from './types/providerTypes'
 
 export function findProviderKey(model: AllModels): LLMProviderKeys | undefined {
-  return LLMProviderKeys[modelProviderMap[model].apiKey]
+  // console.log('MODEL', {
+  //   model,
+  //   providerMap: modelProviderMap[model],
+  //   apikey: modelProviderMap[model].apiKey,
+  // })
+
+  return modelProviderMap[model].apiKey
 }
 
-export function findProviderName(
-  model: AllModels
-): LLMProviderKeys | undefined {
-  return LLMProviderKeys[modelProviderMap[model].provider]
+export function findProviderName(model: AllModels): LLMProviders | undefined {
+  return modelProviderMap[model].provider
 }
