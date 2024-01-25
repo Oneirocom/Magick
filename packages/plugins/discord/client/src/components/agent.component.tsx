54b7ef5d1a175100fb0fa3dd02b590cec852347f
Michael <m.sharpe@project89.org>
2024-01-25 11:51:50 -0800

    Fix linting wrrors

--- packages/plugins/discord/client/src/components/agent.component.tsx
+++ packages/plugins/discord/client/src/components/agent.component.tsx
@@ -1,32 +1,19 @@
-import axios from 'axios'
-import { useSnackbar } from 'notistack'
-import React, { FC, useEffect, useRef, useState } from 'react'
-import { debounce } from 'lodash'
+
+import React, { FC, useEffect, useState } from 'react'
 import { Tooltip } from '@mui/material'
 
-type PluginProps = {
-  selectedAgentData: any
-  props
-}
-import { API_ROOT_URL, FILE_SERVER_URL } from 'shared/config'
 import { Switch } from 'client/core'
 import VariableModal from './VariableModal'
-import { useSelector } from 'react-redux'
 
 export const DiscordAgentWindow: FC<any> = props => {
   props = props.props
-  const { selectedAgentData, update, setSelectedAgentData } = props
-  const debouncedFunction = debounce((id, data) => update(id, data), 500)
+  const { selectedAgentData, update } = props
+
   const [editMode, setEditMode] = useState<boolean>(false)
   const [checked, setChecked] = useState(
     selectedAgentData.data?.discord_enabled || false
   )
-  const [disable, setDisable] = useState(false)
-  const { enqueueSnackbar } = useSnackbar()
-  const [playingAudio, setPlayingAudio] = useState(false)
-
-  const globalConfig = useSelector((state: any) => state.globalConfig)
-  const token = globalConfig?.token
+  const [, setDisable] = useState(false)
 
   const handleClose = () => {
     setEditMode(false)
@@ -41,55 +28,6 @@ export const DiscordAgentWindow: FC<any> = props => {
       setDisable(false)
     }
   }, [props.enable, selectedAgentData])
-  const testVoice = async () => {
-    if (
-      (selectedAgentData.data?.voice_provider &&
-        selectedAgentData.data?.voice_character) ||
-      playingAudio
-    ) {
-      if (
-        selectedAgentData.data?.voice_provider === 'tiktalknet' &&
-        selectedAgentData.data?.voice_endpoint?.length <= 0
-      ) {
-        return
-      }
-
-      const resp = await axios.get(`${API_ROOT_URL}/text_to_speech`, {
-        params: {
-          text: 'Hello there! This section has not been written yet.',
-          voice_provider: selectedAgentData.data?.voice_provider,
-          voice_character: selectedAgentData.data?.voice_character,
-          voice_language_code: selectedAgentData.data?.voice_language_code,
-          voice_endpoint: selectedAgentData.data?.voice_endpoint,
-        },
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      const url =
-        selectedAgentData.data?.voice_provider === 'google' ||
-          selectedAgentData.data?.voice_provider === 'tiktalknet'
-          ? FILE_SERVER_URL + '/' + resp.data
-          : resp.data
-      if (url && url.length > 0) {
-        setPlayingAudio(true)
-        console.log('url:', url)
-        const audio = new Audio(url)
-        audio.onended = function () {
-          setPlayingAudio(false)
-        }
-        audio.play()
-      }
-    } else {
-      enqueueSnackbar(
-        'You need to setup the voice variables to test the voice or already playing another test',
-        {
-          variant: 'error',
-        }
-      )
-    }
-  }
 
   return (
     <>
@@ -127,7 +65,6 @@ export const DiscordAgentWindow: FC<any> = props => {
       {editMode && (
         <VariableModal
           selectedAgentData={selectedAgentData}
-          testVoice={testVoice}
           editMode={editMode}
           setEditMode={handleClose}
           update={update}
