54271bfd1bd6ca823b372147208b624e9d7b6977
Jakob <haruhunab1320@gmail.com>
2024-01-26 08:12:08 -0800

    add ActiveProviders type to providers.ts

--- plugins/core/src/lib/services/coreLLMService/constants/providers.ts
+++ plugins/core/src/lib/services/coreLLMService/constants/providers.ts
@@ -1,4 +1,10 @@
-import { ActiveProviders, LLMProviders } from '../types/providerTypes'
+import { LLMProviders } from '../types/providerTypes'
+
+export type ActiveProviders =
+  | LLMProviders.OpenAI
+  | LLMProviders.GoogleAIStudio
+  | LLMProviders.TogetherAI
+// | LLMProviders.VertexAI
 
 export const activeProviders: ActiveProviders[] = [
   LLMProviders.OpenAI,
