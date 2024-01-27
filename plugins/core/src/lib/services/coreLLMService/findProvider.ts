7c8328935f4a746de2deeaea7f23662e162827a8
Jakob <haruhunab1320@gmail.com>
2024-01-26 16:04:44 -0800

    add method to findProvider

--- plugins/core/src/lib/services/coreLLMService/findProvider.ts
+++ plugins/core/src/lib/services/coreLLMService/findProvider.ts
@@ -1,17 +1,33 @@
 import { modelProviderMap } from './constants/modelProviderMap'
+import { providerModelMap } from './constants/providerModelMap'
 import { AllModels } from './types/models'
 import { LLMProviderKeys, LLMProviders } from './types/providerTypes'
 
 export function findProviderKey(model: AllModels): LLMProviderKeys | undefined {
-  // console.log('MODEL', {
-  //   model,
-  //   providerMap: modelProviderMap[model],
-  //   apikeyName: modelProviderMap[model].apiKeyName,
-  // })
-
   return modelProviderMap[model].apiKeyName
 }
 
 export function findProviderName(model: AllModels): LLMProviders | undefined {
   return modelProviderMap[model].provider
+}
+
+export function getModelsForProvider(
+  provider: LLMProviders
+): Partial<AllModels[] | undefined> {
+  return providerModelMap[provider]
+}
+
+export function getProvidersWithUserKeys(
+  keys: LLMProviderKeys[]
+): LLMProviders[] {
+  return keys
+    .map(key => {
+      const providerName = Object.keys(LLMProviderKeys).find(
+        name => LLMProviderKeys[name] === key
+      )
+      return providerName
+        ? (providerName.replace('_API_KEY', '') as LLMProviders)
+        : undefined
+    })
+    .filter(provider => provider !== undefined) as LLMProviders[]
 }
