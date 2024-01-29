2ce99157a1128fc8f65a7767cf020539d5478e0d
Jakob <haruhunab1320@gmail.com>
2024-01-29 15:54:52 -0800

    add Palm to activeProviders

--- plugins/core/src/lib/services/coreLLMService/constants/providers.ts
+++ plugins/core/src/lib/services/coreLLMService/constants/providers.ts
@@ -4,11 +4,14 @@ export type ActiveProviders =
   | LLMProviders.OpenAI
   | LLMProviders.GoogleAIStudio
   | LLMProviders.TogetherAI
+  | LLMProviders.Palm
 // | LLMProviders.VertexAI
 
 export const activeProviders: ActiveProviders[] = [
   LLMProviders.OpenAI,
   LLMProviders.TogetherAI,
   LLMProviders.GoogleAIStudio,
+  LLMProviders.Palm,
+
   // LLMProviders.VertexAI,
 ]
