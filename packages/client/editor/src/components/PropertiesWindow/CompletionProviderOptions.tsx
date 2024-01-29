fb01d7701ab4a201ecf28f4a984b983f6c939790
Jakob <haruhunab1320@gmail.com>
2024-01-29 15:48:43 -0800

    initial Subscription filtering pass finished in CompletionProviderOptions

--- packages/client/editor/src/components/PropertiesWindow/CompletionProviderOptions.tsx
+++ packages/client/editor/src/components/PropertiesWindow/CompletionProviderOptions.tsx
@@ -5,6 +5,7 @@ import {
   allOpenAICompletionModelsArray,
   googleAIStudioModelsArray,
   togetherAIModelsArray,
+  palmModelsArray,
   // ai21ModelsArray,
   // alephAlphaModelsArray,
   // anthropicModelsArray,
@@ -18,7 +19,6 @@ import {
   // nlpCloudModelsArray,
   // ollamaModelsArray,
   // openRouterModelsArray,
-  // palmModelsArray,
   // perplexityAIModelsArray,
   // petalsModelsArray,
   // replicateModelsArray,
@@ -32,11 +32,74 @@ import { CompletionModels } from "plugins/core/src/lib/services/coreLLMService/t
 import { LLMProviders } from 'plugins/core/src/lib/services/coreLLMService/types/providerTypes';
 import { ActiveProviders, activeProviders } from "plugins/core/src/lib/services/coreLLMService/constants/providers";
 
+import { useConfig } from "@magickml/providers";
+import { getProvidersWithUserKeys } from "plugins/core/src/lib/services/coreLLMService/findProvider";
+import { useListCredentialsQuery } from "client/state";
+import { useGetUserQuery } from "client/state";
+import { SubscriptionNames } from "plugins/core/src/lib/services/userService/types";
+
+
 export const CompletionProviderOptions = (props: ConfigurationComponentProps) => {
+
   const [selectedProvider, setSelectedProvider] = useState<ActiveProviders>(props.fullConfig.modelProvider);
   const [selectedModel, setSelectedModel] = useState<CompletionModels | null>(props.fullConfig.model);
-  const [filteredModels, setFilteredModels] = useState<CompletionModels[]>([]);
+  const [filteredModels, setFilteredModels] = useState<CompletionModels[]>([])
+  const [providersWithKeys, setProvidersWithKeys] = useState([])
+  const [filteredProviders, setFilteredProviders] = useState<ActiveProviders[]>([])
 
+  const config = useConfig()
+  const { data: credentials } = useListCredentialsQuery({
+    projectId: config.projectId,
+  })
+  const { data: userData } = useGetUserQuery({
+    projectId: config.projectId,
+  })
+
+
+
+  useEffect(() => {
+    if (credentials) {
+      const creds = credentials.map(cred => cred.name);
+      const providers = getProvidersWithUserKeys(creds as any);
+      setProvidersWithKeys(providers);
+    }
+  }, [credentials]);
+
+  useEffect(() => {
+    if (userData) {
+      const hasSubscription = userData.user.hasSubscription
+
+      let filteredProviders
+
+      if (hasSubscription) {
+        const userSubscriptionName = String(userData.user.subscriptionName || '').trim();
+        const wizard = String(SubscriptionNames.Wizard).trim();
+        const apprentice = String(SubscriptionNames.Apprentice).trim();
+
+        switch (userSubscriptionName) {
+          case wizard:
+            filteredProviders = activeProviders
+            break;
+          case apprentice:
+            filteredProviders = activeProviders.filter(provider => providersWithKeys.includes(provider));
+            break;
+        }
+        setFilteredProviders(filteredProviders);
+      } else {
+
+        const budget = userData.user.balance
+        if (budget > 0) {
+          filteredProviders = activeProviders
+        } else {
+          filteredProviders = []
+        }
+
+        setFilteredProviders(filteredProviders);
+      }
+    }
+  }, [userData]);
+
+  //   return providers
   useEffect(() => {
     // get all completion models for the selected provider`
     switch (selectedProvider) {
@@ -48,6 +111,9 @@ export const CompletionProviderOptions = (props: ConfigurationComponentProps) =>
         break;
       case LLMProviders.TogetherAI:
         setFilteredModels(togetherAIModelsArray);
+        break;
+      case LLMProviders.Palm:
+        setFilteredModels(palmModelsArray);
         break;
       // case LLMProviders.VertexAI:
       //   setFilteredModels(vertexAIGoogleModelsArray);
@@ -91,9 +157,6 @@ export const CompletionProviderOptions = (props: ConfigurationComponentProps) =>
       // case LLMProviders.OpenRouter:
       //   setFilteredModels(openRouterModelsArray);
       //   break;
-      // case LLMProviders.Palm:
-      //   setFilteredModels(palmModelsArray);
-      //   break;
       // case LLMProviders.PerplexityAI:
       //   setFilteredModels(perplexityAIModelsArray);
       //   break;
@@ -129,6 +192,8 @@ export const CompletionProviderOptions = (props: ConfigurationComponentProps) =>
     props.updateConfigKey("model", model);
   };
 
+
+
   return (
     <div>
       <h3>Model Provider</h3>
@@ -138,7 +203,7 @@ export const CompletionProviderOptions = (props: ConfigurationComponentProps) =>
           value={selectedProvider}
           onChange={(e) => onProviderChange(e.currentTarget.value as ActiveProviders)}
         >
-          {activeProviders.map((provider) => (
+          {filteredProviders.map((provider) => (
             <option key={provider} value={provider}>{provider}</option>
           ))}
         </select>
