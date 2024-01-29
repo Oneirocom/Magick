653f1f3f1ef44e7a5f5c60c2feb91b2afb9aadae
Jakob <haruhunab1320@gmail.com>
2024-01-29 15:53:37 -0800

    add users feathers service

---
+++ packages/server/core/src/services/users/users.ts
@@ -0,0 +1,55 @@
+import { hooks as schemaHooks } from '@feathersjs/schema'
+import {
+  userResolver,
+  userExternalResolver,
+  userQueryValidator,
+} from './users.schema'
+import type { Application } from '../../declarations'
+
+import { checkPermissions } from '../../lib/feathersPermissions'
+import { UserService } from './users.class'
+import { UserParams } from './users.class'
+
+const USER_EVENTS = ['log', 'error', 'update', 'create']
+
+export const users = (app: Application) => {
+  app.use('user', new UserService(app), {
+    methods: ['get'],
+    events: USER_EVENTS,
+  })
+
+  app.service('user').hooks({
+    around: {
+      all: [
+        schemaHooks.resolveExternal(userExternalResolver),
+        schemaHooks.resolveResult(userResolver),
+      ],
+    },
+    before: {
+      all: [
+        checkPermissions({
+          roles: ['owner', 'user'],
+        }),
+        schemaHooks.validateQuery(userQueryValidator),
+      ],
+      get: [],
+      // find: [],
+      // create: [],
+      // patch: [],
+      // update: [],
+      // remove: [],
+    },
+    after: {
+      all: [],
+    },
+    error: {
+      all: [],
+    },
+  })
+}
+
+declare module '../../declarations' {
+  interface ServiceTypes {
+    user: UserService<UserParams> // Use the UserParams type in the UserService declaration
+  }
+}
