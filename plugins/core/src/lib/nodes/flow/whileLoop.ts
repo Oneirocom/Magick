d71c9cc04a035ac759b848870fa369ec52e98afd
Michael <m.sharpe@project89.org>
2024-01-30 11:04:06 -0800

    Dispose while loop properly?

--- plugins/core/src/lib/nodes/flow/whileLoop.ts
+++ plugins/core/src/lib/nodes/flow/whileLoop.ts
@@ -26,12 +26,20 @@ export const whileLoop = makeAsyncNodeDefinition({
     completed: 'flow',
     loopBody: 'flow',
   },
-  initialState: undefined,
-  triggered: ({ configuration, finished = () => {}, commit, read }) => {
+  initialState: {
+    isDisposed: false,
+  },
+  triggered: ({ configuration, state, finished = () => {}, commit, read }) => {
     const { maxIterations } = configuration
     const stop = maxIterations < hardStop ? maxIterations : hardStop
 
     const loopBodyIteration = (i: number) => {
+      if (state.isDisposed) {
+        finished();
+        return;
+      }
+
+
       const condition = read('condition')
       if (i < stop && !condition) {
         commit('loopBody', async resolveSockets => {
@@ -44,6 +52,14 @@ export const whileLoop = makeAsyncNodeDefinition({
       }
     }
     loopBodyIteration(1)
+
+    return {
+      isDisposed: false,
+    }
   },
-  dispose: () => {},
+  dispose: () => {
+    return {
+      isDisposed: true,
+    }
+  },
 })
