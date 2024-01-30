6d0e88f925b1637a80d39215a1d2a9ff8aa585be
Michael <m.sharpe@project89.org>
2024-01-30 09:29:59 -0800

    Add AGENT_EVENT constant for server events

--- packages/shared/communication/src/lib/agentEventTypes.ts
+++ packages/shared/communication/src/lib/agentEventTypes.ts
@@ -8,6 +8,7 @@ export const AGENT_SPELL = (agentId: string) => `agent:${agentId}:server:spell`
 export const AGENT_LOG = (agentId: string) => `agent:${agentId}:server:log`
 export const AGENT_WARN = (agentId: string) => `agent:${agentId}:server:warn`
 export const AGENT_ERROR = (agentId: string) => `agent:${agentId}:server:error`
+export const AGENT_EVENT = (agentId: string) => `agent:${agentId}:server:event`
 
 export const AGENT_PONG = (agentId: string) => `agent:${agentId}:server:pong`
 
@@ -15,29 +16,16 @@ export const AGENT_SPELL_STATE = (agentId: string) =>
   `agent:${agentId}:server:spell`
 
 export const AGENT_COMMAND = (agentId: string) =>
-  `agent:${agentId}:event:command`
+  `agent:${agentId}:action:command`
 
 export const AGENT_COMMAND_PROJECT = (projectId: string) =>
-  `agent:${projectId}:command`
+  `agent:${projectId}:action:command`
 
 export const AGENT_DELETE = `agent:delete`
 
 // This cant be hardcodded
 export const AGENT_MESSAGE = (agentId: string) =>
   `agent:${agentId}:Core:${ON_MESSAGE}`
-
-// Messages for plugins
-export const AGENT_PLUGIN_ENABLE = (agentId: string, pluginName: string) =>
-  `agent:${agentId}:plugin:${pluginName}:enable`
-
-export const AGENT_PLUGIN_DISABLE = (agentId: string, pluginName: string) =>
-  `agent:${agentId}:plugin:${pluginName}:disable`
-
-export const AGENT_PLUGIN_STATUS = (agentId: string, pluginName: string) =>
-  `agent:${agentId}:plugin:${pluginName}:status`
-
-export const AGENT_PLUGIN_RESET = (agentId: string, pluginName: string) =>
-  `agent:${agentId}:plugin:${pluginName}:reset`
 
 export const AGENT_PLUGIN_REFETCH_CREDENTIALS = (
   agentId: string,
