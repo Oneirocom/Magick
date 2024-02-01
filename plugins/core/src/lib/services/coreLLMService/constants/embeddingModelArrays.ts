521882201e8b2b069735a3738688e838d571cb23
Jakob <haruhunab1320@gmail.com>
2024-02-01 09:55:05 -0800

    append prefixes to models

--- plugins/core/src/lib/services/coreLLMService/constants/embeddingModelArrays.ts
+++ plugins/core/src/lib/services/coreLLMService/constants/embeddingModelArrays.ts
@@ -5,7 +5,10 @@ import {
   VoyageEmbeddingModels,
   OpenAIEmbeddingModels,
   HuggingFaceEmbeddingModels,
+  EmbeddingModels,
 } from '../../coreEmbeddingService/types'
+import { LLMProviderPrefixes } from '../types/providerTypes'
+import { appendPrefixToModel } from './completionModelArrays'
 
 export const bedrockEmbeddingModelsArray = Object.values(BedrockEmbeddingModels)
 export const cohereEmbeddingModelsArray = Object.values(CohereEmbeddingModels)
@@ -15,3 +18,49 @@ export const openAIEmbeddingModelsArray = Object.values(OpenAIEmbeddingModels)
 export const huggingFaceEmbeddingModelsArray = Object.values(
   HuggingFaceEmbeddingModels
 )
+
+export const bedrockEmbeddingModelsWithPrefix = appendPrefixToModel(
+  bedrockEmbeddingModelsArray,
+  ''
+)
+export const cohereEmbeddingModelsWithPrefix = appendPrefixToModel(
+  cohereEmbeddingModelsArray,
+  ''
+)
+export const mistralEmbeddingModelsWithPrefix = appendPrefixToModel(
+  mistralEmbeddingModelsArray,
+  LLMProviderPrefixes.Mistral
+)
+
+export const voyageEmbeddingModelsWithPrefix = appendPrefixToModel(
+  voyageEmbeddingModelsArray,
+  LLMProviderPrefixes.VoyageAI
+)
+
+export const openAIEmbeddingModelsWithPrefix = appendPrefixToModel(
+  openAIEmbeddingModelsArray,
+  ''
+)
+
+export const huggingFaceEmbeddingModelsWithPrefix = appendPrefixToModel(
+  huggingFaceEmbeddingModelsArray,
+  LLMProviderPrefixes.HuggingFace
+)
+
+export const allEmbeddingModels: EmbeddingModels[] = [
+  ...bedrockEmbeddingModelsArray,
+  ...cohereEmbeddingModelsArray,
+  ...mistralEmbeddingModelsArray,
+  ...voyageEmbeddingModelsArray,
+  ...openAIEmbeddingModelsArray,
+  ...huggingFaceEmbeddingModelsArray,
+]
+
+export const allEmbeddingModelsWithPrefix: EmbeddingModels[] = [
+  ...bedrockEmbeddingModelsWithPrefix,
+  ...cohereEmbeddingModelsWithPrefix,
+  ...mistralEmbeddingModelsWithPrefix,
+  ...voyageEmbeddingModelsWithPrefix,
+  ...openAIEmbeddingModelsWithPrefix,
+  ...huggingFaceEmbeddingModelsWithPrefix,
+]
