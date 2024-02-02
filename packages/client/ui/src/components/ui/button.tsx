649e29db1a6a864bb7ce4fccaa37d701e8cf024d
Jakob <haruhunab1320@gmail.com>
2024-02-02 10:42:50 -0800

    fix button prop types

--- packages/client/ui/src/components/ui/button.tsx
+++ packages/client/ui/src/components/ui/button.tsx
@@ -41,17 +41,21 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
+    const buttonClass = buttonVariants({ variant: props.variant, size: props.size })
+    const combinedClass = cn(buttonClass, className)
+
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={combinedClass}
         ref={ref}
         {...props}
       />
     )
   }
 )
+
 Button.displayName = 'Button'
 
 export { Button, buttonVariants }
