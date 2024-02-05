d7fbab08b9f4ae3d69f29e0e8a6e2c196a7e3eb0
Michael <m.sharpe@project89.org>
2024-02-05 09:36:42 -0800

    Cleanup discord plugin

--- plugins/discord/src/lib/discordPlugin.ts
+++ plugins/discord/src/lib/discordPlugin.ts
@@ -1,68 +1,26 @@
-import Redis from 'ioredis'
-import { Job } from 'bullmq'
 import { ActionPayload, CoreEventsPlugin } from 'server/plugin'
 import {
-  DISCORD_ACTIONS,
   DISCORD_KEY,
   DISCORD_EVENTS,
   discordPluginCredentials,
 } from './constants'
 import { DiscordEmitter } from './dependencies/discordEmitter'
-import DiscordEventClient from './services/discordEventClient'
 import { sendDiscordMessage } from './nodes/actions/sendDiscordMessage'
-import { RedisPubSub } from 'packages/server/redis-pubsub/src'
 import { discordPluginName } from './constants'
 import { DiscordClient } from './services/discord'
 import { onDiscordMessageNodes } from './nodes/events/onDiscordMessage'
-import { DiscordCredentials } from './types'
+import { DiscordCredentials, DiscordEventPayload } from './types'
+import { EventTypes } from 'communication'
 
 export class DiscordPlugin extends CoreEventsPlugin {
   override enabled = true
-  event: DiscordEventClient
   nodes = [...onDiscordMessageNodes, sendDiscordMessage]
   values = []
   discord: DiscordClient | undefined = undefined
 
-  constructor(
-    connection: Redis,
-    agentId: string,
-    pubSub: RedisPubSub,
-    projectId: string
-  ) {
+  constructor({ connection, agentId, projectId }) {
     super({ name: discordPluginName, connection, agentId, projectId })
-    this.event = new DiscordEventClient(pubSub, agentId)
     this.setCredentials(discordPluginCredentials)
-    this.initalizeDiscord().catch(error =>
-      this.logger.error(
-        `Failed to initialize Discord Plugin for agent ${agentId} ${error}`
-      )
-    )
-  }
-
-  defineEvents(): void {
-    for (const [messageType, eventName] of Object.entries(DISCORD_EVENTS)) {
-      this.registerEvent({
-        eventName,
-        displayName: `Discord ${messageType}`,
-      })
-    }
-  }
-
-  defineActions(): void {
-    for (const [actionName] of Object.entries(DISCORD_ACTIONS)) {
-      this.registerAction({
-        actionName,
-        displayName: `Discord ${actionName}`,
-        handler: this.handleSendMessage.bind(this),
-      })
-    }
-  }
-
-  getDependencies() {
-    return {
-      [discordPluginName]: DiscordEmitter,
-      [DISCORD_KEY]: this.discord,
-    }
   }
 
   async initalizeDiscord() {
@@ -79,9 +37,55 @@ export class DiscordPlugin extends CoreEventsPlugin {
 
       await this.discord.init()
 
-      this.updateDependency('discordClient', this.discord)
+      this.logger.info('Discord client initialized.')
     } catch (error) {
-      this.logger.error('Failed during initialization:', error)
+      this.logger.error(error, 'Failed during initialization:')
+      throw error
+    }
+  }
+
+  async initializeFunctionalities() {
+    await this.initalizeDiscord().catch(error =>
+      this.logger.error(
+        `Failed to initialize Discord Plugin for agent ${this.agentId} ${error}`
+      )
+    )
+    // // handle generic message received event from discord
+    this.discord?.onMessageCreate(event => {
+      // todo fix typing here,but I am lazy.
+      this.triggerMessageReceived(event as any)
+    })
+  }
+
+  defineEvents(): void {
+    for (const [messageType, eventName] of Object.entries(DISCORD_EVENTS)) {
+      this.registerEvent({
+        eventName,
+        displayName: `Discord ${messageType}`,
+      })
+    }
+  }
+
+  defineActions(): void {
+    // for (const [actionName] of Object.entries(DISCORD_ACTIONS)) {
+    //   this.registerAction({
+    //     actionName,
+    //     displayName: `Discord ${actionName}`,
+    //     handler: this.handleSendMessage.bind(this),
+    //   })
+    // }
+    // Handler for generic event if it comes in
+    this.registerAction({
+      actionName: EventTypes.SEND_MESSAGE,
+      displayName: 'Send Message',
+      handler: this.handleSendMessage.bind(this),
+    })
+  }
+
+  getDependencies() {
+    return {
+      [discordPluginName]: DiscordEmitter,
+      [DISCORD_KEY]: this.discord,
     }
   }
 
@@ -101,25 +105,13 @@ export class DiscordPlugin extends CoreEventsPlugin {
     }
   }
 
-  initializeFunctionalities(): void {}
   handleOnMessage() {}
-  handleSendMessage(actionPayload: Job<ActionPayload>) {
-    const { actionName, event } = actionPayload.data
-    const { plugin } = event
-    const eventName = `${plugin}:${actionName}`
-    console.log(
-      'sending message from discord plugin',
-      plugin,
-      eventName,
-      actionName,
-      event
-    )
 
-    if (plugin === 'Discord') {
-      this.event.sendMessage(actionPayload.data)
-    } else {
-      this.centralEventBus.emit(eventName, actionPayload.data)
-    }
+  handleSendMessage<K extends keyof DiscordEventPayload>(
+    actionPayload: ActionPayload<DiscordEventPayload[K]>
+  ) {
+    const { event, data } = actionPayload
+    this.discord?.sendMessage<K>(data.content, event)
   }
 
   formatPayload(event, payload) {
