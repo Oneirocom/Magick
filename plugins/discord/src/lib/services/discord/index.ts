c9307faefb4e35d5ccfa4d36017c1e0707e7a6ba
Michael <m.sharpe@project89.org>
2024-02-05 09:37:58 -0800

    Add some helper functions to the discord client

--- plugins/discord/src/lib/services/discord/index.ts
+++ plugins/discord/src/lib/services/discord/index.ts
@@ -1,11 +1,41 @@
-import { Client, GatewayIntentBits } from 'discord.js'
+import { ChannelType, Client, GatewayIntentBits, TextChannel } from 'discord.js'
 import { EventPayload } from 'server/plugin'
 import { DiscordCredentials, DiscordEventPayload } from '../../types'
 import { Logger } from 'pino'
+import natural from 'natural'
 import { DISCORD_EVENTS } from '../../constants'
 
+const tokenizer = new natural.SentenceTokenizer()
+
+function tokenizeIntoSentences(content) {
+  return tokenizer.tokenize(content)
+}
+
+function splitLongSentence(sentence, maxLength) {
+  // Split the sentence at logical points, such as punctuation or conjunctions.
+  const splitRegex = /,|;|\s-\s/ // Split at commas, semicolons, or dashes. You can refine this regex based on your needs.
+  const parts = [] as string[]
+  let currentPart = ''
+
+  sentence.split(splitRegex).forEach((chunk, index, array) => {
+    if (currentPart.length + chunk.length < maxLength) {
+      currentPart +=
+        index < array.length - 1 ? chunk + array[index + 1].charAt(0) : chunk
+    } else {
+      parts.push(currentPart)
+      currentPart = chunk
+    }
+  })
+
+  if (currentPart) {
+    parts.push(currentPart)
+  }
+
+  return parts
+}
+
 export class DiscordClient {
-  private client: Client
+  private client: Client<true>
 
   constructor(
     private credentials: DiscordCredentials,
@@ -123,6 +153,80 @@ export class DiscordClient {
     Object.keys(DISCORD_EVENTS).forEach(eventName => {
       this.setupEventListener(eventName as keyof DiscordEventPayload)
     })
+  }
+
+  async onMessageCreate(
+    handler: (event: EventPayload<DiscordEventPayload['messageCreate']>) => void
+  ) {
+    this.client.on('messageCreate', (...args) => {
+      // have to cast here because of the way discord.js typings are set up
+      //@ts-ignore
+      const payload = args[0] as DiscordEventPayload['messageCreate']
+
+      if (this.checkIfBotMessage(payload)) {
+        return
+      }
+
+      const eventPayload = this.createEventPayload('messageCreate', payload)
+      handler(eventPayload)
+    })
+  }
+
+  async sendMessage<K extends keyof DiscordEventPayload>(
+    content: string,
+    event: EventPayload<DiscordEventPayload[K]>
+  ) {
+    if (!event.data.channelId) {
+      throw new Error('No channel id found')
+    }
+
+    try {
+      const channel = await this.client.channels.fetch(event.data.channelId)
+
+      if (!channel) {
+        throw new Error('No channel found')
+      }
+
+      if (channel.type !== ChannelType.GuildText) {
+        throw new Error('Channel is not a text channel')
+      }
+
+      const MAX_LENGTH = 2000
+      const sentences = tokenizeIntoSentences(content) // Function to split content into sentences
+      let currentBatch = ''
+      const batches = [] as string[]
+
+      for (const sentence of sentences) {
+        if (sentence.length > MAX_LENGTH) {
+          // If a single sentence is too long, further split it.
+          const parts = splitLongSentence(sentence, MAX_LENGTH) // Function to split long sentence
+          for (const part of parts) {
+            if (currentBatch.length + part.length <= MAX_LENGTH) {
+              currentBatch += part
+            } else {
+              batches.push(currentBatch)
+              currentBatch = part
+            }
+          }
+        } else if (currentBatch.length + sentence.length <= MAX_LENGTH) {
+          currentBatch += sentence
+        } else {
+          batches.push(currentBatch)
+          currentBatch = sentence
+        }
+      }
+
+      if (currentBatch) {
+        batches.push(currentBatch)
+      }
+
+      for (const batch of batches) {
+        await (channel as TextChannel).send(batch)
+      }
+    } catch (err) {
+      this.logger.error(err, 'ERROR IN DISCORD SEND MESSAGE')
+      throw err
+    }
   }
 
   getClient() {
