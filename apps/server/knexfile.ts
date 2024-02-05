fc4d22d83a1d89e67dad85c99e78a0e256fedccc
Michael <m.sharpe@project89.org>
2024-02-05 11:22:36 -0800

    Have knex migrate use dotenv-flow for local migrations against dev and prod

--- apps/server/knexfile.ts
+++ apps/server/knexfile.ts
@@ -1,5 +1,11 @@
-const dbURL = process.env.DATABASE_URL
+import * as dotenv from 'dotenv-flow'
+dotenv.config({
+  path: '../'
+})
 
+console.log('MIGRATING USING DATABASE URL', process.env.DATABASE_URL)
+
+const dbURL = process.env.DATABASE_URL
 module.exports = {
   client: 'pg',
   connection: dbURL
