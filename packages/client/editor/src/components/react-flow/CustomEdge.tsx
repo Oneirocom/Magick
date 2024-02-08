07eb0b430459e2c426300ed0c78fabc1b6d1054a
Michael <m.sharpe@project89.org>
2024-02-08 10:58:50 -0800

    Update custom edge to use colors

--- packages/client/editor/src/components/react-flow/CustomEdge.tsx
+++ packages/client/editor/src/components/react-flow/CustomEdge.tsx
@@ -1,11 +1,19 @@
-import {
-  BaseEdge,
-  EdgeProps,
-  getBezierPath,
-} from 'reactflow';
+import React from 'react';
+import { EdgeProps, getBezierPath } from 'reactflow';
+import { valueTypeColorMap } from '../../utils/colors';
 
-export default function CustomEdge({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition }: EdgeProps) {
-  const [path] = getBezierPath({
+export default function CustomEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  data,
+  selected
+}: EdgeProps) {
+  const edgePathData = getBezierPath({
     sourceX,
     sourceY,
     sourcePosition,
@@ -14,7 +22,12 @@ export default function CustomEdge({ id, sourceX, sourceY, targetX, targetY, sou
     targetPosition,
   });
 
+  const strokeColor = valueTypeColorMap[data.valueType as keyof typeof valueTypeColorMap] || 'white';
+
   return (
-    <BaseEdge id={id} path={path} />
+    <g>
+      <path id={id} d={edgePathData[0]} stroke={strokeColor} fill="none" strokeWidth={selected ? 6 : 3} />
+      {/* Optionally, add arrowheads or other SVG elements here */}
+    </g>
   );
 }
