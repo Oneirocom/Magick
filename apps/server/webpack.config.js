affe12ac375b5496ba0bb3872c26f4282902a450
Michael <m.sharpe@project89.org>
2024-02-10 15:14:35 -0800

    Clean up webpack

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -1,4 +1,3 @@
-const CopyPlugin = require('copy-webpack-plugin')
 const { merge } = require('webpack-merge')
 const { composePlugins, withNx } = require('@nx/webpack')
 
@@ -21,33 +20,7 @@ module.exports = composePlugins(
           }
         ]
       },
-      plugins: [
-        // Copy files pyodide.js will load asynchronously
-        new CopyPlugin({
-          patterns: [
-            {
-              from: require.resolve('pyodide/repodata.json'),
-              to: 'repodata.json'
-            },
-            {
-              from: require.resolve('pyodide/pyodide_py.tar'),
-              to: 'pyodide_py.tar'
-            },
-            {
-              from: require.resolve('pyodide/pyodide.asm.data'),
-              to: 'pyodide.asm.data'
-            },
-            {
-              from: require.resolve('pyodide/pyodide.asm.js'),
-              to: 'pyodide.asm.js'
-            },
-            {
-              from: require.resolve('pyodide/pyodide.asm.wasm'),
-              to: 'pyodide.asm.wasm'
-            }
-          ]
-        })
-      ]
+      plugins: []
     })
   }
 )
