a6bf8bbab5be8c640c7439acfd604567d48c8f22
Michael <m.sharpe@project89.org>
2024-02-10 22:59:35 -0800

    Dependency cleanup

--- packages/client/editor/postcss.config.cjs
+++ packages/client/editor/postcss.config.cjs
@@ -1,5 +1,3 @@
-const { join } = require('path')
-
 // Note: If you use library-specific PostCSS/Tailwind configuration then you should remove the `postcssConfig` build
 // option from your application's configuration (i.e. project.json).
 //
@@ -7,9 +5,6 @@ const { join } = require('path')
 
 module.exports = {
   plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
     autoprefixer: {},
   },
 }
