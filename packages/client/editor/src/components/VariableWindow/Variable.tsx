30bb3fc29f9924e3449b266609d2146d7a2ef739
Michael <m.sharpe@project89.org>
2024-02-12 00:38:47 -0800

    Fix variable window

--- packages/client/editor/src/components/VariableWindow/Variable.tsx
+++ packages/client/editor/src/components/VariableWindow/Variable.tsx
@@ -1,24 +1,32 @@
-import { VariableJSON } from "@magickml/behave-graph";
-import { cx } from "class-variance-authority";
-import { Icon } from "client/core";
-import { debounce } from "lodash";
-import { useEffect, useState } from "react";
+import { VariableJSON } from '@magickml/behave-graph'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import {
+  Input,
+  Label,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Switch,
+} from '@magickml/client-ui'
+import { cx } from 'class-variance-authority'
+import { Icon } from 'client/core'
+import { debounce } from 'lodash'
+import { useEffect, useState } from 'react'
 
-const inputClass = cx(
-  'bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2 nodrag text-md justify-start flex',
-);
+const inputClass = cx('w-full py-1 px-2 nodrag text-md justify-start flex')
 
 // todo we need to centralize these types
-const valueTypes = [
-  'boolean',
-  'number',
-  'string',
-  'float',
-  'array',
-  'object'
-]
+const valueTypes = ['boolean', 'string', 'float', 'array', 'object']
 
-const DefaultInput = ({ valueType, initialValue, onChange, choices = [], showChoices = false }) => {
+const DefaultInput = ({
+  valueType,
+  initialValue,
+  onChange,
+  choices = [],
+  showChoices = false,
+}) => {
   // determine fallback value based on type
   if (valueType === 'boolean') {
     initialValue = initialValue || false
@@ -29,20 +37,20 @@ const DefaultInput = ({ valueType, initialValue, onChange, choices = [], showCho
   } else if (valueType === 'integer') {
     initialValue = initialValue || 0
   } else if (valueType === 'string') {
-    initialValue = initialValue || ""
+    initialValue = initialValue || ''
   } else if (valueType === 'array') {
-    initialValue = initialValue || "[]"
+    initialValue = initialValue || '[]'
   } else if (valueType === 'object') {
-    initialValue = initialValue || "{}"
+    initialValue = initialValue || '{}'
   } else if (showChoices && choices.length > 0) {
     initialValue = choices[0].value
   } else {
-    initialValue = ""
+    initialValue = ''
   }
 
   const [value, setValue] = useState(initialValue)
 
-  const updateValue = (value) => {
+  const updateValue = value => {
     setValue(value)
     onChange(value)
   }
@@ -51,79 +59,62 @@ const DefaultInput = ({ valueType, initialValue, onChange, choices = [], showCho
     setValue(initialValue)
   }, [initialValue])
 
-
   return (
     <div className="w-full justify-start flex">
       {showChoices && (
-        <select
-          className={inputClass}
-          value={value || choices[0].value}
-          onChange={(e) => updateValue(e.currentTarget.value)}
-        >
-          <>
-            {choices.map((choice) => (
-              <option key={choice.text} value={choice.value}>
-                {choice.text}
-              </option>
-            ))}
-          </>
-        </select>
+        <>
+          <Select onValueChange={updateValue} defaultValue={value}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Type" />
+            </SelectTrigger>
+            <SelectContent>
+              {choices.map(choice => (
+                <SelectItem key={choice.text} value={choice.value}>
+                  {choice.text}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </>
       )}
       {valueType === 'string' && !showChoices && (
-        <input
+        <Input
           type="text"
           className={inputClass}
-          value={value || ""}
-          onChange={(e) => {
+          value={value || ''}
+          onChange={e => {
             updateValue(e.currentTarget.value)
           }}
         />
       )}
-      {valueType === 'number' && !showChoices && (
-        <input
-          type="number"
-          className={inputClass}
-          value={value || 0}
-          onChange={(e) => updateValue(e.currentTarget.value)}
-        />
-      )}
       {valueType === 'float' && !showChoices && (
-        <input
+        <Input
           type="number"
           className={inputClass}
           value={value || 0}
-          onChange={(e) => updateValue(e.currentTarget.value)}
+          onChange={e => updateValue(e.currentTarget.value)}
         />
       )}
       {valueType === 'integer' && !showChoices && (
-        <input
+        <Input
           type="number"
           className={inputClass}
           value={value || 0}
-          onChange={(e) => updateValue(e.currentTarget.value)}
+          onChange={e => updateValue(e.currentTarget.value)}
         />
       )}
       {valueType === 'boolean' && !showChoices && (
-        <input
-          type="checkbox"
-          className={inputClass + ' mr-auto justify-start flex order-first'}
-          value={value || 0}
-          onChange={(e) => updateValue(e.currentTarget.checked)}
-        />
+        <div className="flex gap-2 h-10 items-center">
+          <p>True</p>
+          <Switch value={value || 0} onChange={updateValue} />
+          <p>False</p>
+        </div>
       )}
       {valueType === 'array' && !showChoices && (
-        <input
-          disabled
-          className={inputClass}
-          value="[]"
-        />
+        <Input disabled className={inputClass} value="[]" />
       )}
       {valueType === 'object' && !showChoices && (
-        <input
-          disabled
-          className={inputClass}
-          value="{}"
-        />
+        <Input disabled className={inputClass} value="{}" />
       )}
     </div>
   )
@@ -135,64 +126,68 @@ type VariableProps = {
   deleteVariable: (variableId: string) => void
 }
 
-
-export const Variable = ({ variable, updateVariable, deleteVariable }: VariableProps) => {
-
-  const updateProperty = (property: keyof VariableJSON) => debounce((value) => {
-    updateVariable({
-      ...variable,
-      [property]: value,
-    })
-  }, 500)
+export const Variable = ({
+  variable,
+  updateVariable,
+  deleteVariable,
+}: VariableProps) => {
+  const updateProperty = (property: keyof VariableJSON) =>
+    debounce(value => {
+      updateVariable({
+        ...variable,
+        [property]: value,
+      })
+    }, 500)
 
   return (
-    <div className="border-b-2 border-b-solid border-b-[var(--background-color)] mb-2">
-      <div className="flex px-2 pl-4">
-        <div className="flex flex-column mb2 py-2 gap-2 w-full">
-          <div className="flex">
-            <p className="w-2/5">Name</p>
-            <input
-              disabled
-              className={inputClass}
-              value={variable.name}
-              onChange={(e) => {
-                updateProperty('name')(e.target.value)
-              }}
-            />
+    <div className="border-b-2 border-b-solid border-b-[var(--background-color)] mb-2 p-2 pl-4">
+      <div className="flex flex-row">
+        <div className="flex flex-col w-full gap-2">
+          <div className="flex flex-row mb2 gap-2 w-full">
+            <div className="flex-grow">
+              <p>Name</p>
+              <Input
+                disabled
+                className={inputClass}
+                value={variable.name}
+                onChange={e => updateProperty('name')(e.target.value)}
+              />
+            </div>
+
+            <div className="flex-grow">
+              <Label>Type</Label>
+              <Select
+                onValueChange={updateProperty('valueTypeName')}
+                defaultValue={variable.valueTypeName}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Type" />
+                </SelectTrigger>
+                <SelectContent>
+                  {valueTypes.map(type => (
+                    <SelectItem key={type} value={type}>
+                      {type}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-          <div className="flex">
-            <p className="w-2/5">Type</p>
-            <select
-              className={inputClass}
-              value={variable.valueTypeName}
-              onChange={(e) => {
-                updateProperty('valueTypeName')(e.target.value)
-              }}
-            >
-              <>
-                {valueTypes.map((type) => (
-                  <option key={type} value={type}>
-                    {type}
-                  </option>
-                ))}
-              </>
-            </select>
-          </div>
-          <div className="flex">
-            <p className="w-2/5">Defult value</p>
+
+          <div className="w-full">
+            <p>Default value</p>
             <DefaultInput
               valueType={variable.valueTypeName}
               initialValue={variable.initialValue}
               onChange={updateProperty('initialValue')}
             />
           </div>
-
         </div>
-        {/*  button in a flex comtainer centered vertically for trash delete */}
-        <div className="flex items-center px-4">
-          <Icon name="trash" size={16} onClick={() => {
-            deleteVariable(variable.id)
-          }} />
+        <div className="flex items-center px-2 pl-4">
+          <TrashIcon
+            className="h-6 w-6 cursor-pointer hover:text-sky-300 transition-all duration-200 ease-in-out"
+            onClick={() => deleteVariable(variable.id)}
+          />
         </div>
       </div>
     </div>
--- packages/client/editor/src/components/VariableWindow/VariableWindow.tsx
+++ packages/client/editor/src/components/VariableWindow/VariableWindow.tsx
@@ -1,25 +1,41 @@
-import { VariableJSON } from '@magickml/behave-graph';
-import { Tab, useConfig } from '@magickml/providers';
+import { VariableJSON } from '@magickml/behave-graph'
+import { Tab, useConfig } from '@magickml/providers'
 import { Window } from 'client/core'
-import { selectGraphJson, selectTabEdges, selectTabNodes, setEdges, setNodes, useGetSpellByNameQuery, useSaveSpellMutation } from 'client/state';
+import {
+  selectGraphJson,
+  selectTabEdges,
+  selectTabNodes,
+  setEdges,
+  setNodes,
+  useGetSpellByNameQuery,
+  useSaveSpellMutation,
+} from 'client/state'
 import { v4 as uuidv4 } from 'uuid'
-import { IDockviewPanelProps } from 'dockview';
-import { useCallback, useState } from 'react';
-import { enqueueSnackbar } from 'notistack';
-import { Variable } from './Variable';
-import { useSelector } from 'react-redux';
+import { IDockviewPanelProps } from 'dockview'
+import { useCallback, useState } from 'react'
+import { enqueueSnackbar } from 'notistack'
+import { Variable } from './Variable'
+import { useSelector } from 'react-redux'
+import { Button, Input, agentInputStyles } from '@magickml/client-ui'
 
-type Props = IDockviewPanelProps<{ tab: Tab, spellId: string, spellName: string }>
+type Props = IDockviewPanelProps<{
+  tab: Tab
+  spellId: string
+  spellName: string
+}>
 
 export const VariableWindow = (props: Props) => {
   const { tab, spellName } = props.params
 
-  const { spell } = useGetSpellByNameQuery({ spellName }, {
-    skip: !spellName,
-    selectFromResult: (data) => ({
-      spell: data?.data?.data[0]
-    })
-  })
+  const { spell } = useGetSpellByNameQuery(
+    { spellName },
+    {
+      skip: !spellName,
+      selectFromResult: data => ({
+        spell: data?.data?.data[0],
+      }),
+    }
+  )
 
   const nodes = useSelector(selectTabNodes(tab.id))
   const edges = useSelector(selectTabEdges(tab.id))
@@ -33,101 +49,122 @@ export const VariableWindow = (props: Props) => {
     setNewVariableName(e.target.value)
   }
 
-  const deleteAllVariableNodes = useCallback((variable: VariableJSON) => {
-    const newNodes = nodes.filter(
-      node => !node.type.includes(variable.name))
+  const deleteAllVariableNodes = useCallback(
+    (variable: VariableJSON) => {
+      const newNodes = nodes.filter(node => !node.type.includes(variable.name))
 
-    const removedNodes = nodes.filter(
-      node => node.type.includes(variable.name)
-    ).map(node => node.id)
+      const removedNodes = nodes
+        .filter(node => node.type.includes(variable.name))
+        .map(node => node.id)
 
-    const newEdges = edges.filter(
-      edge => !removedNodes.includes(edge.source) && !removedNodes.includes(edge.target)
-    )
+      const newEdges = edges.filter(
+        edge =>
+          !removedNodes.includes(edge.source) &&
+          !removedNodes.includes(edge.target)
+      )
 
-    setNodes(tab.id, newNodes)
-    setEdges(tab.id, newEdges)
-  }, [nodes, edges])
+      setNodes(tab.id, newNodes)
+      setEdges(tab.id, newEdges)
+    },
+    [nodes, edges]
+  )
 
-  const saveVariable = useCallback((variable: VariableJSON) => {
-    const graph = spell.graph
-    const variables = graph.variables.map((v) => v.id === variable.id ? variable : v);
+  const saveVariable = useCallback(
+    (variable: VariableJSON) => {
+      const graph = spell.graph
+      const variables = graph.variables.map(v =>
+        v.id === variable.id ? variable : v
+      )
 
-    const newGraph = { ...graph, variables };
-    const newSpell = { ...spell, graph: newGraph };
+      const newGraph = { ...graph, variables }
+      const newSpell = { ...spell, graph: newGraph }
 
-    return saveSpellMutation({ projectId, spell: newSpell })
-      .then(() => enqueueSnackbar('Variable saved', { variant: 'success' }))
-      .catch((err) => {
-        console.error(err);
-        enqueueSnackbar('Error saving variable', { variant: 'error' });
-      });
-  }, [spell, projectId, enqueueSnackbar, graphJson]);  // dependencies
+      return saveSpellMutation({ projectId, spell: newSpell })
+        .then(() => enqueueSnackbar('Variable saved', { variant: 'success' }))
+        .catch(err => {
+          console.error(err)
+          enqueueSnackbar('Error saving variable', { variant: 'error' })
+        })
+    },
+    [spell, projectId, enqueueSnackbar, graphJson]
+  ) // dependencies
 
-  const deleteVariable = useCallback((variableId: string) => {
-    const graph = spell.graph
-    const variable = graph.variables.find((v) => v.id === variableId);
-    const newGraph = { ...graph, variables: graph.variables.filter((v) => v.id !== variableId) };
-    const updatedGraph = {
-      ...newGraph,
-      nodes: graph.nodes.filter((node) => !node.type.includes(variable.name)),
-    }
+  const deleteVariable = useCallback(
+    (variableId: string) => {
+      const graph = spell.graph
+      const variable = graph.variables.find(v => v.id === variableId)
+      const newGraph = {
+        ...graph,
+        variables: graph.variables.filter(v => v.id !== variableId),
+      }
+      const updatedGraph = {
+        ...newGraph,
+        nodes: graph.nodes.filter(node => !node.type.includes(variable.name)),
+      }
 
-    const newSpell = { ...spell, graph: updatedGraph };
+      const newSpell = { ...spell, graph: updatedGraph }
 
-    deleteAllVariableNodes(variable)
+      deleteAllVariableNodes(variable)
 
-    return saveSpellMutation({ projectId, spell: newSpell })
-      .then(() => enqueueSnackbar('Variable deleted', { variant: 'success' }))
-      .catch((err) => {
-        console.error(err);
-        enqueueSnackbar('Error deleting variable', { variant: 'error' });
-      });
-  }, [spell, projectId, enqueueSnackbar, graphJson]);  // dependencies
+      return saveSpellMutation({ projectId, spell: newSpell })
+        .then(() => enqueueSnackbar('Variable deleted', { variant: 'success' }))
+        .catch(err => {
+          console.error(err)
+          enqueueSnackbar('Error deleting variable', { variant: 'error' })
+        })
+    },
+    [spell, projectId, enqueueSnackbar, graphJson]
+  ) // dependencies
 
   const createNewVariable = useCallback(() => {
     const newVariable: VariableJSON = {
       name: newVariableName,
       id: uuidv4(),
       valueTypeName: 'string',
-      initialValue: []
-    };
+      initialValue: [],
+    }
 
     const graph = spell.graph
-    const newGraph = { ...graph, variables: [...graph.variables, newVariable] };
-    const newSpell = { ...spell, graph: newGraph };
+    const newGraph = { ...graph, variables: [...graph.variables, newVariable] }
+    const newSpell = { ...spell, graph: newGraph }
 
     return saveSpellMutation({ projectId, spell: newSpell })
       .then(() => {
-        enqueueSnackbar('Variable created', { variant: 'success' });
-        setNewVariableName('');  // Assuming this state is managed in this component
+        enqueueSnackbar('Variable created', { variant: 'success' })
+        setNewVariableName('') // Assuming this state is managed in this component
       })
-      .catch((err) => {
-        console.error(err);
-        enqueueSnackbar('Error creating variable', { variant: 'error' });
-      });
-  }, [spell, projectId, enqueueSnackbar, newVariableName, graphJson]);  // dependencies
+      .catch(err => {
+        console.error(err)
+        enqueueSnackbar('Error creating variable', { variant: 'error' })
+      })
+  }, [spell, projectId, enqueueSnackbar, newVariableName, graphJson]) // dependencies
 
   if (!spell?.graph) return null
 
   return (
     <Window borderless>
+      <div className="flex flex-row px-2 mt-2 gap-2 border-b-2 border-b-solid border-b-[var(--background-color)] pb-2">
+        <Input
+          className={agentInputStyles}
+          onChange={onInputChange}
+          value={newVariableName}
+          placeholder="New variable"
+        />
+        <Button variant="secondary" onClick={createNewVariable}>
+          +
+        </Button>
+      </div>
       <div>
-        {spell.graph.variables.map((variable) => {
+        {spell.graph.variables.map(variable => {
           return (
-            <Variable variable={variable} updateVariable={saveVariable} deleteVariable={deleteVariable} />
+            <Variable
+              variable={variable}
+              updateVariable={saveVariable}
+              deleteVariable={deleteVariable}
+            />
           )
         })}
-        <div className="flex flex-row px-2 mt-2">
-          <input
-            className="flex-grow"
-            onChange={onInputChange}
-            value={newVariableName}
-            placeholder="New variable"
-          />
-          <button onClick={createNewVariable}>+</button>
-        </div>
-      </div >
-    </Window >
+      </div>
+    </Window>
   )
 }
