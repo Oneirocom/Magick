342e9867fbd5513fcd481beb7473133184f40ac3
Jakob <haruhunab1320@gmail.com>
2024-02-11 16:09:15 -0800

    add optional params to TxtInputField

--- packages/client/editor/src/components/react-flow/TextInputField.tsx
+++ packages/client/editor/src/components/react-flow/TextInputField.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Input } from '@magickml/ui';
-import { v4 as uuidv4 } from 'uuid'
 
 export const TextInputField = ({
   value,
@@ -9,22 +8,20 @@ export const TextInputField = ({
   handleBlur,
 }: {
   value: string
-  onChange: (e) => void
-  onFocus?: (inputId: string) => void
+  onChange?: (e) => void
+  onFocus?: () => void
   handleBlur?: () => void
 }) => {
 
-  const inputId = React.useMemo(() => `text-input::${uuidv4()}`, []);
   return (
     <Input
-      id={inputId}
       className='bg-dark-3 h-5 rounded-sm px-2'
       type="text"
       placeholder="input text..."
       value={value}
       onChange={(e) => onChange(e)}
-      onFocus={() => onFocus(inputId)}
-      onBlur={handleBlur}
+      onFocus={() => onFocus()}
+      onBlur={() => handleBlur()}
     />
   )
 };
