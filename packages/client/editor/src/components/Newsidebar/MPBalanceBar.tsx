1b395bdf1c367618a55e5c3aca162f34fd0602f3
Jakob <haruhunab1320@gmail.com>
2024-02-12 14:36:30 -0800

    adjust percentege for neophytes on mp bar

--- packages/client/editor/src/components/Newsidebar/MPBalanceBar.tsx
+++ packages/client/editor/src/components/Newsidebar/MPBalanceBar.tsx
@@ -1,7 +1,11 @@
 import { styled } from '@mui/material/styles'
 import { useFeathers } from '@magickml/providers'
-import { LinearProgress, linearProgressClasses, Typography } from '@mui/material'
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import {
+  LinearProgress,
+  linearProgressClasses,
+  Typography,
+} from '@mui/material'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 
 import styles from './menu.module.css'
 import { useState, useEffect } from 'react'
@@ -16,35 +20,34 @@ export const MPBalanceBar = ({ userData }) => {
   const [magickPowerBalance, setMagickPowerBalance] = useState(0)
 
   const [isMPVisible, setIsMPVisible] = useState(false)
-  const [isWalletVisible, setIsWalletVisible] = useState(false); // State for Wallet tooltip visibility
+  const [isWalletVisible, setIsWalletVisible] = useState(false) // State for Wallet tooltip visibility
 
-  const [isMPHovered, setIsMPHovered] = useState(false);
+  const [isMPHovered, setIsMPHovered] = useState(false)
   const [isMPTransitionIn, setIsMPTransitionIn] = useState(false)
   const [isMPTransitionOut, setIsMPTransitionOut] = useState(false)
 
-  const [isWalletHovered, setIsWalletHovered] = useState(false);
-  const [isWalletTransitionIn, setIsWalletTransitionIn] = useState(false);
-  const [isWalletTransitionOut, setIsWalletTransitionOut] = useState(false);
+  const [isWalletHovered, setIsWalletHovered] = useState(false)
+  const [isWalletTransitionIn, setIsWalletTransitionIn] = useState(false)
+  const [isWalletTransitionOut, setIsWalletTransitionOut] = useState(false)
 
-  const [isTooltipHovered, setIsTooltipHovered] = useState(false);
+  const [isTooltipHovered, setIsTooltipHovered] = useState(false)
 
-
-  const [walletColor, setWalletColor] = useState('');
+  const [walletColor, setWalletColor] = useState('')
 
   const handleWalletMouseEnter = () => {
-    setIsWalletHovered(true);
-  };
+    setIsWalletHovered(true)
+  }
 
   const handleWalletMouseLeave = () => {
-    setIsWalletHovered(false);
-  };
+    setIsWalletHovered(false)
+  }
 
   const handleMPMouseEnter = () => {
-    setIsMPHovered(true);
+    setIsMPHovered(true)
   }
 
   const handleMPMouseLeave = () => {
-    setIsMPHovered(false);
+    setIsMPHovered(false)
   }
 
   //handle setup
@@ -61,24 +64,24 @@ export const MPBalanceBar = ({ userData }) => {
   useEffect(() => {
     if (!client) return
 
-    const handler = (data) => {
-      const newCharge = data.newCharge;
-      let updatedMagickPowerBalance = magickPowerBalance;
+    const handler = data => {
+      const newCharge = data.newCharge
+      let updatedMagickPowerBalance = magickPowerBalance
 
       // Deduct from magick power balance until it reaches 0
       if (updatedMagickPowerBalance > 0) {
-        updatedMagickPowerBalance -= newCharge;
+        updatedMagickPowerBalance -= newCharge
         if (updatedMagickPowerBalance < 0) {
-          updatedMagickPowerBalance = 0;
+          updatedMagickPowerBalance = 0
         }
-        setMagickPowerBalance(updatedMagickPowerBalance);
+        setMagickPowerBalance(updatedMagickPowerBalance)
       }
 
       // Adjust wallet balance after magick power balance reaches 0
       if (updatedMagickPowerBalance === 0) {
-        const newBalance = remainingBalance - newCharge;
-        const newWalletBalance = newBalance <= 0 ? 0 : newBalance;
-        setRemainingBalance(newWalletBalance);
+        const newBalance = remainingBalance - newCharge
+        const newWalletBalance = newBalance <= 0 ? 0 : newBalance
+        setRemainingBalance(newWalletBalance)
       }
     }
 
@@ -91,54 +94,52 @@ export const MPBalanceBar = ({ userData }) => {
 
   useEffect(() => {
     if (isWalletHovered) {
-      setIsWalletVisible(true);
-      setIsWalletTransitionIn(true);
+      setIsWalletVisible(true)
+      setIsWalletTransitionIn(true)
     } else {
-      if (isTooltipHovered) return;
-      setIsWalletTransitionOut(true);
+      if (isTooltipHovered) return
+      setIsWalletTransitionOut(true)
       const timeoutId = setTimeout(() => {
-        setIsWalletVisible(false);
-        setIsWalletTransitionIn(false);
-      }, 500);
+        setIsWalletVisible(false)
+        setIsWalletTransitionIn(false)
+      }, 500)
 
-      return () => clearTimeout(timeoutId);
+      return () => clearTimeout(timeoutId)
     }
-  }, [isWalletHovered, isTooltipHovered]);
+  }, [isWalletHovered, isTooltipHovered])
 
   useEffect(() => {
     if (isMPHovered) {
-      setIsMPVisible(true);
-      setIsMPTransitionIn(true);
+      setIsMPVisible(true)
+      setIsMPTransitionIn(true)
     } else {
-      setIsMPTransitionOut(true);
+      setIsMPTransitionOut(true)
       setTimeout(() => {
-        setIsMPVisible(false);
-        setIsMPTransitionIn(false);
-      }, 500);
+        setIsMPVisible(false)
+        setIsMPTransitionIn(false)
+      }, 500)
     }
-  }, [isMPHovered]);
-
-
+  }, [isMPHovered])
 
   useEffect(() => {
     if (remainingBalance === 0 && magickPowerBalance > 0) {
-      setWalletColor('#FFFFFF'); // Set to white if wallet balance is 0 and there is remaining Magick Power
+      setWalletColor('#FFFFFF') // Set to white if wallet balance is 0 and there is remaining Magick Power
     } else if (remainingBalance >= 5) {
-      setWalletColor('#00FF85');
+      setWalletColor('#00FF85')
     } else if (remainingBalance < 5 && remainingBalance >= 2) {
-      setWalletColor('#FFD600');
+      setWalletColor('#FFD600')
     } else {
-      setWalletColor('#FF0000');
+      setWalletColor('#FF0000')
     }
-  }, [remainingBalance, magickPowerBalance]);
+  }, [remainingBalance, magickPowerBalance])
 
   const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     height: 12,
     borderRadius: 5,
     borderWidth: 2,
-    borderColor: magickPowerBalance ? "#117A91" : "#363D44",
+    borderColor: magickPowerBalance ? '#117A91' : '#363D44',
     [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: "#0b0d0e",
+      backgroundColor: '#0b0d0e',
     },
     [`& .${linearProgressClasses.bar}`]: {
       borderRadius: 5,
@@ -146,59 +147,86 @@ export const MPBalanceBar = ({ userData }) => {
     },
   }))
 
-
-
   const hasSubscription = userData && userData?.user?.hasSubscription
   const isNeophyte = userData && !hasSubscription
-  const isApprentice = userData && userData?.user?.subscriptionName === SubscriptionNames.Apprentice
-  const isWizard = userData && userData?.user?.subscriptionName === SubscriptionNames.Wizard
+  const isApprentice =
+    userData &&
+    userData?.user?.subscriptionName === SubscriptionNames.Apprentice
+  const isWizard =
+    userData && userData?.user?.subscriptionName === SubscriptionNames.Wizard
 
   const mp = Number((magickPowerBalance * 100).toFixed(0))
-  const walletBalance = Number((remainingBalance).toFixed(2))
+  const walletBalance = Number(remainingBalance.toFixed(2))
 
-  const neophyteBody = 'Your Magick Power (MP) shows how much compute power you have remaining before your free trial is over.'
-  const apprenticeBody = 'Upgrade to Wizard for access to all AI providers and 1000 MP per month.'
-  const wizardBody = 'Your Magick Power (MP) reflects your available compute power, replenished monthly with your Wizard subscription.'
-  const body = isNeophyte ? neophyteBody : isApprentice ? apprenticeBody : wizardBody
+  const neophyteBody =
+    'Your Magick Power (MP) shows how much compute power you have remaining before your free trial is over.'
+  const apprenticeBody =
+    'Upgrade to Wizard for access to all AI providers and 1000 MP per month.'
+  const wizardBody =
+    'Your Magick Power (MP) reflects your available compute power, replenished monthly with your Wizard subscription.'
+  const body = isNeophyte
+    ? neophyteBody
+    : isApprentice
+    ? apprenticeBody
+    : wizardBody
 
   const walletStyle = {
     color: walletColor,
     transition: 'color 0.5s ease',
-  };
+  }
 
   return (
     <div className={`${styles.credits} ${isApprentice ? 'opacity-40' : ''}`}>
       <div>
         <div className={styles.menuFlex}>
           <AutoAwesomeIcon sx={{ mr: 1, color: '#B7BBBE' }} />
-          <Typography variant="body1" className='text-[#B7BBBE]'>Magick Power (MP)</Typography>
-          <InfoOutlinedIcon className='text-[#B7BBBE] ml-1' style={{
-            fontSize: '1rem'
-          }}
+          <Typography variant="body1" className="text-[#B7BBBE]">
+            Magick Power (MP)
+          </Typography>
+          <InfoOutlinedIcon
+            className="text-[#B7BBBE] ml-1"
+            style={{
+              fontSize: '1rem',
+            }}
             onMouseEnter={handleMPMouseEnter}
             onMouseLeave={handleMPMouseLeave}
           />
         </div>
-        <BorderLinearProgress variant="determinate" value={Math.min((magickPowerBalance * 10), 100)} />
+        <BorderLinearProgress
+          variant="determinate"
+          value={
+            isNeophyte
+              ? Math.min(magickPowerBalance * 5 * 10, 100)
+              : Math.min(magickPowerBalance * 10, 100)
+          }
+        />
         <p className={`${styles.creditCount} mt-2 text-[#B7BBBE]`}>
-          {isNeophyte ? 'Upgrade to use MP' : `${mp} / ${isWizard || isApprentice ? '1000' : '200'} monthly MP`}
-        </p >
-      </div>
-
-      <div className='flex row'>
-        <p className='text-[#B7BBBE]' >
-          Wallet: <span className='font-medium' style={walletStyle}>${walletBalance}</span>
+          {isNeophyte
+            ? 'Upgrade to use MP'
+            : `${mp} / ${isWizard || isApprentice ? '1000' : '200'} monthly MP`}
         </p>
-        <InfoOutlinedIcon className='text-[#B7BBBE] ml-1' style={{
-          fontSize: '1rem'
-        }}
-          onMouseEnter={handleWalletMouseEnter}
-          onMouseLeave={handleWalletMouseLeave} />
       </div>
 
+      <div className="flex row">
+        <p className="text-[#B7BBBE]">
+          Wallet:{' '}
+          <span className="font-medium" style={walletStyle}>
+            ${walletBalance}
+          </span>
+        </p>
+        <InfoOutlinedIcon
+          className="text-[#B7BBBE] ml-1"
+          style={{
+            fontSize: '1rem',
+          }}
+          onMouseEnter={handleWalletMouseEnter}
+          onMouseLeave={handleWalletMouseLeave}
+        />
+      </div>
 
       <div className={`${isMPVisible ? 'visible' : 'invisible'}`}>
-        <div className={`
+        <div
+          className={`
           absolute
           transform
           left-0
@@ -215,20 +243,20 @@ export const MPBalanceBar = ({ userData }) => {
           border-[#1BC5EB]
           rounded-md
           bg-[#171b1c]
-        `}>
-          <h2 className='mb-2'>MP Bar</h2>
-          <div className="max-w-md text-wrap h-20 mt-2 mb-6">
-            {body}
-          </div>
+        `}
+        >
+          <h2 className="mb-2">MP Bar</h2>
+          <div className="max-w-md text-wrap h-20 mt-2 mb-6">{body}</div>
         </div>
       </div>
-
 
       <div
         onMouseEnter={() => setIsTooltipHovered(true)}
         onMouseLeave={() => setIsTooltipHovered(false)}
-        className={`${isWalletVisible ? 'visible' : 'invisible'}`}>
-        <div className={`
+        className={`${isWalletVisible ? 'visible' : 'invisible'}`}
+      >
+        <div
+          className={`
         absolute
         transform
         left-0
@@ -246,16 +274,21 @@ export const MPBalanceBar = ({ userData }) => {
         border-[#1BC5EB]
         rounded-md
         bg-[#171b1c]
-      `}>
-          <h2 className='mb-2'>Wallet</h2>
+      `}
+        >
+          <h2 className="mb-2">Wallet</h2>
           <div className="max-w-md text-wrap">
-            Your Wallet reflects money available for compute power, used after your monthly MP runs out. <a href="https://cloud-dev.magickml.com/account" className="text-blue-500 hover:text-blue-700">
+            Your Wallet reflects money available for compute power, used after
+            your monthly MP runs out.{' '}
+            <a
+              href="https://cloud-dev.magickml.com/account"
+              className="text-blue-500 hover:text-blue-700"
+            >
               Click to top up.
             </a>
           </div>
         </div>
       </div>
     </div>
-
   )
 }
