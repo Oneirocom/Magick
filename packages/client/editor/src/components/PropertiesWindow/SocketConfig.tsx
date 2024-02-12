d175f956ca059c99d09ec39c4a29c8f4083a598c
Jakob <haruhunab1320@gmail.com>
2024-02-12 13:51:36 -0800

    hanlde merge

--- packages/client/editor/src/components/PropertiesWindow/SocketConfig.tsx
+++ packages/client/editor/src/components/PropertiesWindow/SocketConfig.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useState } from "react"
-import { ConfigurationComponentProps } from "./PropertiesWindow"
-import SingleElement from "./SingleElement"
+import { useCallback, useState } from 'react'
+import { ConfigurationComponentProps } from './PropertiesWindow'
+import SingleElement from './SingleElement'
 
 /**
  * AddNewSocket component provides a form input to add a new socket.
@@ -53,19 +53,19 @@ const AddNewSocket = ({ addSocket, valueTypes, definedValueType }) => {
             }
           }}
         />
-        {!definedValueType &&
+        {!definedValueType && (
           <select
             value={selectedValueType}
             onChange={e => setSelectedValueType(e.target.value)}
-            className="flex flex-1 px-3 bg-[var(--background-color)] rounded-sm">
-            {
-              valueTypes.map(type => (
-                <option value={type}>{type}</option>
-              ))
-            }
+            className="flex flex-1 px-3 bg-[var(--background-color)] rounded-sm"
+          >
+            {valueTypes.map(type => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            ))}
           </select>
-
-        }
+        )}
         {/* Add button */}
         <button style={{ margin: 0 }} onClick={onAdd} type="submit">
           Add
@@ -75,27 +75,29 @@ const AddNewSocket = ({ addSocket, valueTypes, definedValueType }) => {
   )
 }
 
-
-export const SocketConfig = ({ config, updateConfigKey, fullConfig }: ConfigurationComponentProps) => {
-  const defaultValues = [
-    'string',
-    'integer',
-    'boolean'
-  ]
+export const SocketConfig = ({
+  config,
+  updateConfigKey,
+  fullConfig,
+}: ConfigurationComponentProps) => {
+  const defaultValues = ['string', 'integer', 'boolean']
   const [configKey, sockets = []] = config
   const { socketValues = defaultValues, valueType = null } = fullConfig
 
-  const addSocket = useCallback((socket) => {
-    const newValue = [
-      ...sockets,
-      {
-        name: socket.name,
-        valueType: socket.valueType
-      }
-    ]
+  const addSocket = useCallback(
+    socket => {
+      const newValue = [
+        ...sockets,
+        {
+          name: socket.name,
+          valueType: socket.valueType,
+        },
+      ]
 
-    updateConfigKey(configKey, newValue)
-  }, [sockets, configKey, updateConfigKey])
+      updateConfigKey(configKey, newValue)
+    },
+    [sockets, configKey, updateConfigKey]
+  )
 
   const deleteSocket = (name: string) => {
     const newValue = sockets.filter((socket: any) => socket.name !== name)
@@ -114,7 +116,11 @@ export const SocketConfig = ({ config, updateConfigKey, fullConfig }: Configurat
         />
       ))}
       <div>
-        <AddNewSocket addSocket={addSocket} valueTypes={socketValues} definedValueType={valueType} />
+        <AddNewSocket
+          addSocket={addSocket}
+          valueTypes={socketValues}
+          definedValueType={valueType}
+        />
         {/* <Input value={key} onChange={e => setKey(e.target.value)} />
         <select value={valueType} onChange={e => setValueType(e.target.value)}>
           <option value="string">string</option>
