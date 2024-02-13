dcb65a1431534317b0aa1ac8204011d758f849ed
Michael <m.sharpe@project89.org>
2024-02-12 23:40:59 -0800

    fix sha exctraction in script

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -14,12 +14,10 @@ fi
 set -e
 
 # get submodule commit
-output=`git submodule status --recursive` # get submodule info
-no_prefix=${output#*-} # get rid of the prefix
-COMMIT=${no_prefix% *} # get rid of the suffix
+output=$(git submodule status --recursive | grep "$GITHUB_ACCESS_TOKEN") # get specific submodule info
+COMMIT=$(echo $output | awk '{print $1}') # extract the commit SHA
 
 echo "Submodule commit: $COMMIT"
-echo "NO PREFIX: $no_prefix"
 
 # set up an empty temporary work directory
 rm -rf tmp || true # remove the tmp folder if exists
