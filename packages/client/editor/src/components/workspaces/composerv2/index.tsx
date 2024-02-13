e63b51aa65b1deb1d10830a41e4c5a1bb3ec4eb7
Jakob <haruhunab1320@gmail.com>
2024-02-13 15:32:11 -0800

    cleanup composer

--- packages/client/editor/src/components/workspaces/composerv2/index.tsx
+++ packages/client/editor/src/components/workspaces/composerv2/index.tsx
@@ -1,91 +1,27 @@
-import { GridviewReact, IDockviewPanelProps, IGridviewPanelProps, Orientation } from 'dockview';
+import { GridviewReact, IDockviewPanelProps, Orientation } from 'dockview'
 import WorkspaceProvider from '../../../contexts/WorkspaceProvider'
-import { Tab, useDockviewTheme } from 'client/state';
-import { usePubSub } from '@magickml/providers';
-import { Composer } from './composer';
+import { Tab, useDockviewTheme } from 'client/state'
+import { usePubSub } from '@magickml/providers'
+import { composerLayoutComponents } from './ComposerLayoutComponents'
 
-
-const DraggableElement = (props) => {
-  const { tab } = props.params
-  const { publish, events } = usePubSub()
-  const windows = {
-    'Console': () => {
-      publish(events.$CREATE_CONSOLE(tab.id))
-    },
-    'TextEditor': () => {
-      publish(events.$CREATE_TEXT_EDITOR(tab.id))
-    },
-    'Variables': () => {
-      publish(events.$CREATE_INSPECTOR(tab.id))
-    },
-    'Test': () => {
-      publish(events.$CREATE_PLAYTEST(tab.id))
-    }
-  }
-
-  const handleClick = () => {
-    windows[props.window]()
-  }
-
-  return (
-    <p
-      tabIndex={-1}
-      onDragStart={(event) => {
-        if (event.dataTransfer) {
-          event.dataTransfer.effectAllowed = 'move';
-
-          event.dataTransfer.setData('text/plain', 'nothing');
-          event.dataTransfer.setData('component', props.window)
-          event.dataTransfer.setData('title', props.title)
-        }
-      }}
-      onClick={handleClick}
-      draggable={true}
-      className='p-2 text-white transition-all cursor-pointer hover:bg-gray-600'
-    >
-      {props.window}
-    </p>
-  )
-};
-
-const composerLayoutComponents = {
-  WindowBar: (props: IGridviewPanelProps<{ title: string }>) => {
-    return (
-      <div>
-        <div style={{ width: '100%', display: 'inline-flex', justifyContent: 'flex-end', flexDirection: 'row', gap: '8px', padding: "0 16px" }}>
-          <p style={{ padding: 8, color: 'grey', marginRight: 50 }}>Composer V2</p>
-          <DraggableElement window="Console" {...props} />
-          <DraggableElement window="TextEditor" title="Text Editor" {...props} />
-          <DraggableElement window="Test" {...props} />
-          <DraggableElement window="Variables" {...props} />
-        </div>
-      </div >
-    )
-  },
-  Composer: (props: IGridviewPanelProps<{ tab: Tab, theme: string, spellId: string, spellName: string }>) => {
-    return <Composer
-      {...props.params}
-      spellId={props.params.spellId}
-      theme={`composer-layout ${props.params.theme}`}
-      tab={props.params.tab}
-      spellName={props.params.spellName}
-    />
-  }
-}
-
-const ComposerContainer = (props: IDockviewPanelProps<{ tab: Tab; theme: string, spellId: string, spellName: string }>) => {
+const ComposerContainer = (
+  props: IDockviewPanelProps<{
+    tab: Tab
+    theme: string
+    spellId: string
+    spellName: string
+  }>
+) => {
   const { theme } = useDockviewTheme()
   const pubSub = usePubSub()
 
-  const onReady = (event) => {
+  const onReady = event => {
     event.api.addPanel({
       id: 'WindowBar',
       component: 'WindowBar',
       maximumHeight: 30,
       minimumHeight: 30,
-      params: {
-        ...props.params
-      }
+      params: { ...props.params },
     })
 
     event.api.addPanel({
@@ -93,15 +29,18 @@ const ComposerContainer = (props: IDockviewPanelProps<{ tab: Tab; theme: string,
       component: 'Composer',
       params: {
         title: 'Composer',
-        // pass through params to main composer component
-        ...props.params
+        ...props.params,
       },
       position: { referencePanel: 'WindowBar', direction: 'below' },
     })
   }
 
   return (
-    <WorkspaceProvider tab={props.params.tab} pubSub={pubSub} spellId={props.params.spellId}>
+    <WorkspaceProvider
+      tab={props.params.tab}
+      pubSub={pubSub}
+      spellId={props.params.spellId}
+    >
       <GridviewReact
         components={composerLayoutComponents}
         disableAutoResizing={false}
@@ -109,8 +48,15 @@ const ComposerContainer = (props: IDockviewPanelProps<{ tab: Tab; theme: string,
         orientation={Orientation.VERTICAL}
         hideBorders={true}
         onReady={onReady}
-        className={`global-layout ${theme}`}
-      /></WorkspaceProvider>)
+        // className={``} // Tailwind classes for full height
+        className={`
+        global-layout
+        ${theme}
+        h-screen
+        `} // Tailwind classes for full height
+      />
+    </WorkspaceProvider>
+  )
 }
 
 export default ComposerContainer
