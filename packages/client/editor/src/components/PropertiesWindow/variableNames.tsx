7311f7c63c134515225ad361c43b6dec95627de3
Jakob <haruhunab1320@gmail.com>
2024-02-13 07:07:27 -0800

    Add ch Select

--- packages/client/editor/src/components/PropertiesWindow/variableNames.tsx
+++ packages/client/editor/src/components/PropertiesWindow/variableNames.tsx
@@ -1,81 +1,80 @@
-import { cx } from "class-variance-authority";
-import { ConfigurationComponentProps } from "./PropertiesWindow";
-import { useEffect, useState } from "react";
-import { VariableJSON } from "@magickml/behave-graph";
-import { useReactFlow } from "reactflow";
+import { useEffect, useState } from 'react'
+import { VariableJSON } from '@magickml/behave-graph'
+import { ConfigurationComponentProps } from './PropertiesWindow'
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from '@magickml/client-ui'
 
-const inputClass = cx(
-  'bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2 nodrag text-md justify-start flex',
-);
-
-export const VariableNames = ({ spell, fullConfig, updateConfigKeys, node }: ConfigurationComponentProps) => {
-  const { variableId, socketInputs, socketOutputs } = fullConfig
-  const [selectedVariable, setSelectedVariable] = useState<VariableJSON>(null)
-  const reactFlow = useReactFlow()
+export const VariableNames = ({
+  spell,
+  fullConfig,
+  updateConfigKeys,
+  node,
+}: ConfigurationComponentProps) => {
+  const [selectedVariable, setSelectedVariable] = useState<VariableJSON | null>(
+    null
+  )
 
   useEffect(() => {
-    if (!spell && !fullConfig) return
-
-    const variable = spell.graph.variables.find(variable => variable.id === variableId)
-    if (!variable) return
-    setSelectedVariable(variable)
+    if (spell && fullConfig) {
+      const variable = spell.graph.variables.find(
+        variable => variable.id === fullConfig.variableId
+      )
+      setSelectedVariable(variable || null)
+    }
   }, [spell, fullConfig])
 
-  const updateValue = (value) => {
-    const variable = spell.graph.variables.find(variable => variable.id === value) as VariableJSON
+  const updateValue = (variableId: string) => {
+    const variable = spell.graph.variables.find(
+      variable => variable.id === variableId
+    ) as VariableJSON
 
-    reactFlow.setEdges((edges) => {
-      return edges.filter((edge) => (edge.source !== node.id && edge.target !== node.id) || edge.targetHandle === 'flow')
-    })
+    if (variable) {
+      setSelectedVariable(variable)
 
-    setSelectedVariable(variable)
+      const socket = {
+        name: variable.name,
+        valueType: variable.valueTypeName,
+      }
+
+      const configUpdate: Record<string, any> = {
+        variableId: variable.id,
+        label: variable.name,
+      }
+
+      if (fullConfig.socketInputs) {
+        configUpdate['socketInputs'] = [socket]
+      }
+
+      if (fullConfig.socketOutputs) {
+        configUpdate['socketOutputs'] = [socket]
+      }
+
+      updateConfigKeys(configUpdate)
+    }
   }
 
-  useEffect(() => {
-    if (!selectedVariable) return
-    if (selectedVariable.id === variableId) return
-    const socket = {
-      name: selectedVariable.name,
-      valueType: selectedVariable.valueTypeName
-    }
-
-    const configUpdate = {
-      variableId: selectedVariable.id,
-      label: selectedVariable.name
-    }
-
-    if (socketInputs) {
-      configUpdate['socketInputs'] = [socket]
-    }
-
-    if (socketOutputs) {
-      configUpdate['socketOutputs'] = [socket]
-    }
-
-    updateConfigKeys(configUpdate)
-  }, [selectedVariable])
-
-
-  if (!spell) return null;
+  if (!spell) return null
 
   return (
     <div>
       <label htmlFor="variable">Select a variable</label>
-      <select
-        name="variable"
-        className={inputClass}
-        value={selectedVariable?.id || 'default'}
-        onChange={(e) => updateValue(e.currentTarget.value)}
-      >
-        <>
-          <option disabled selected value="default">Select a variable</option>
-          {(spell.graph.variables as VariableJSON[]).map((variable) => (
-            <option key={variable.id} value={variable.id}>
+      <Select value={selectedVariable?.id || ''} onValueChange={updateValue}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a variable" />
+        </SelectTrigger>
+        <SelectContent>
+          {spell.graph.variables.map((variable: VariableJSON) => (
+            <SelectItem key={variable.id} value={variable.id}>
               {variable.name}
-            </option>
+            </SelectItem>
           ))}
-        </>
-      </select>
+        </SelectContent>
+      </Select>
     </div>
-  );
+  )
 }
