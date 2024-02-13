c076292c02d622c9921ea5c804f163e27d91e30b
Michael <m.sharpe@project89.org>
2024-02-12 23:27:48 -0800

    update info in workaround

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -1,8 +1,7 @@
 # github submodule repo address without https:// prefix
-SUBMODULE_GITHUB=github.com/beeinger/vercel-private-submodule
-
+SUBMODULE_GITHUB=github.com/oneirocom/portal.git
 # .gitmodules submodule path
-SUBMODULE_PATH=library
+SUBMODULE_PATH=portal/cloud
 
 # github access token is necessary
 # add it to Environment Variables on Vercel
