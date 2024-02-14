1ac2e8c1f2a2c23b96927304c609f8f954cc04c2
Jakob <haruhunab1320@gmail.com>
2024-02-14 12:00:40 -0800

    add 25 width to tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -22,6 +22,9 @@ module.exports = {
       },
     },
     extend: {
+      width: {
+        25: '7rem', // Add custom width 'w-25'
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
