636ae7496ce634b124ea21aa0d5de0cfddc95e2d
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-16 14:04:13 -0500

    dialog colors

--- packages/client/ui/src/lib/core/ui/dialog.tsx
+++ packages/client/ui/src/lib/core/ui/dialog.tsx
@@ -42,13 +42,13 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[s=opetaten]:slide-in-from-top-[48%] sm:rounded-xs md:w-full',
+        'bg-ds-card fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[s=opetaten]:slide-in-from-top-[48%] sm:rounded-xs md:w-full',
         className
       )}
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="text-black dark:text-white absolute right-4 top-4 rounded-sm ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
+      <DialogPrimitive.Close className="text-ds-black dark:text-ds-white absolute right-4 top-4 rounded-sm ring-offset-ds-neutral transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
         <Cross2Icon className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
