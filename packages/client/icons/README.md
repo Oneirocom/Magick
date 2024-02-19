c2a9d14b3c4db01c9a7012fd07b6d07eddecde8f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-19 16:29:05 -0500

    rename to @magickml/icons, setup svgr

--- packages/client/icons/README.md
+++ packages/client/icons/README.md
@@ -1,7 +1,7 @@
-# @magickml/client-icons
+# @magickml/icons
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test @magickml/client-icons` to execute the unit tests via [Vitest](https://vitest.dev/).
+Run `nx test @magickml/icons` to execute the unit tests via [Vitest](https://vitest.dev/).
--- packages/client/icons/vite.config.ts
+++ packages/client/icons/vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: '@magickml/client-icons',
+      name: '@magickml/icons',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
