1a1999ef81f6ac0d50904f63a5e2b798c66068a3
Michael <m.sharpe@project89.org>
2024-02-22 10:38:44 -0800

    Bring down redis ping interval

--- packages/server/redis-pubsub/src/lib/redis-pubsub.ts
+++ packages/server/redis-pubsub/src/lib/redis-pubsub.ts
@@ -56,7 +56,7 @@ export class RedisPubSub extends EventEmitter {
     const options: RedisClientOptions = {
       ..._options,
       url: redisCloudUrl,
-      pingInterval: 5 * 60 * 1000,
+      pingInterval: 2 * 60 * 1000,
       socket: {
         ..._options?.socket,
         reconnectStrategy: this.retryStrategy,
