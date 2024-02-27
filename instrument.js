f346eece3daacdaaa61abb93a8756910650effa7
Michael <m.sharpe@project89.org>
2024-02-27 10:49:34 -0800

    Add a redis, bullMQ, Postgres OpenTelemetry instrumentations

--- instrument.js
+++ instrument.js
@@ -1,6 +1,19 @@
-const { initSDK } = require('@hyperdx/node-opentelemetry');
+const { initSDK } = require('@hyperdx/node-opentelemetry')
+const {
+  BullMQInstrumentation,
+} = require('@jenniferplusplus/opentelemetry-instrumentation-bullmq')
+const {
+  IORedisInstrumentation,
+} = require('@opentelemetry/instrumentation-ioredis')
+
+import { KoaInstrumentation } from '@opentelemetry/instrumentation-koa'
 
 initSDK({
   consoleCapture: true, // optional, default: true
   advancedNetworkCapture: true, // optional, default: false
-});
+  additionalInstrumentations: [
+    new BullMQInstrumentation(),
+    new IORedisInstrumentation(),
+    new KoaInstrumentation(),
+  ],
+})
