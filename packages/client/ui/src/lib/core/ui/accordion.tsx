c250e8372c866fe959d71737b35b6dad519a4c91
Jakob <haruhunab1320@gmail.com>
2024-02-28 11:29:41 -0800

    rebuild NodePicker

--- packages/client/ui/src/lib/core/ui/accordion.tsx
+++ packages/client/ui/src/lib/core/ui/accordion.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
-import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { ChevronDownIcon, ChevronRightIcon } from '@radix-ui/react-icons'
 
 import { cn } from '../../utils/shadcn'
 
@@ -20,19 +20,27 @@ AccordionItem.displayName = 'AccordionItem'
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
+    iconPosition?: 'start' | 'end'
+  }
+>(({ className, children, iconPosition = 'end', ...props }, ref) => (
+  <AccordionPrimitive.Header className="flex items-center">
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        `flex flex-1 items-center py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-90`,
         className
       )}
       {...props}
     >
+      {iconPosition === 'start' && (
+        // <ChevronDownIcon className="h-4 w-4 mr-1 shrink-0 text-muted-foreground transition-transform duration-200" />
+        <ChevronRightIcon className="h-4 w-4 mr-1 shrink-0 text-muted-foreground transition-transform duration-200" />
+      )}
       {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      {iconPosition === 'end' && (
+        <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      )}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
