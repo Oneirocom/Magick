544d64cdb02ab091df1679426eca3cfa5be575af
Jakob <haruhunab1320@gmail.com>
2024-03-01 14:18:26 -0800

    clean up NpodePickerMenu

--- packages/client/editor/src/components/react-flow/NodePicker/NodePickerMenu.tsx
+++ packages/client/editor/src/components/react-flow/NodePicker/NodePickerMenu.tsx
@@ -1,22 +1,17 @@
 import { Accordion } from '@magickml/client-ui'
 import { NodeItem } from './NodeItem'
+import { XYPosition } from 'reactflow'
 
 export const NodePickerMenu = ({
   groupedData,
   onPickNode,
   pickedNodePosition,
-  setActiveCategories,
-  activeCategories,
-  lastClickedCategory,
-  setLastClickedCategory,
+  instance,
 }: {
   groupedData: any
-  onPickNode: any
-  pickedNodePosition: any
-  setActiveCategories: any
-  activeCategories: string[]
-  lastClickedCategory: string
-  setLastClickedCategory: any
+  onPickNode: (type: string, position: XYPosition) => void
+  pickedNodePosition: XYPosition
+  instance: any
 }) => {
   return (
     <div className="max-h-[320px] overflow-y-scroll w-full">
@@ -27,11 +22,8 @@ export const NodePickerMenu = ({
             key={item.title + index}
             item={item}
             onPickNode={onPickNode}
-            position={pickedNodePosition}
-            setActiveCategories={setActiveCategories}
-            activeCategories={activeCategories}
-            lastClickedCategory={lastClickedCategory}
-            setLastClickedCategory={setLastClickedCategory}
+            pickedNodePosition={pickedNodePosition}
+            instance={instance}
           />
         ))}
       </Accordion>
