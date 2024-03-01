fe5bb153bbefd036bad6150cb5d08af1a375938a
Jakob <haruhunab1320@gmail.com>
2024-03-01 11:38:22 -0800

    commit NodEItem

--- packages/client/editor/src/components/react-flow/NodePicker/NodeItem.tsx
+++ packages/client/editor/src/components/react-flow/NodePicker/NodeItem.tsx
@@ -11,13 +11,19 @@ export const NodeItem = ({
   position,
   onPickNode,
   index,
-  focusedIndex,
+  setActiveCategories,
+  activeCategories,
+  // lastClickedCategory,
+  setLastClickedCategory,
 }: {
   item: ItemType
   position: XYPosition
   onPickNode: any
   index: number
-  focusedIndex: number
+  setActiveCategories: any
+  activeCategories: string[]
+  lastClickedCategory: string
+  setLastClickedCategory: any
 }) => {
   const instance = useReactFlow()
 
@@ -26,15 +32,29 @@ export const NodeItem = ({
       onPickNode(i.type, instance.project(position))
     }
   }
+
+  const categoryOpen = activeCategories.includes(item.title)
   return (
     <AccordionItem
-      key={item.title}
+      key={item.title + index}
       value={item.title}
-      className={`py-0 border-b border-black ${
-        index === focusedIndex ? 'bg-[#282d33]' : ''
-      }`}
+      className={`py-0 border-b border-black`}
     >
-      <AccordionTrigger className="py-2 px-2" iconPosition="start">
+      <AccordionTrigger
+        className={`py-2 px-2 ${
+          categoryOpen && `bg-[#282d33]`
+        } hover:bg-[#282d33] cursor-pointer transition-all border-b border-black hover:underline`}
+        iconPosition="start"
+        onClick={() => {
+          setActiveCategories([...activeCategories, item.title])
+          setLastClickedCategory(item.title)
+          if (activeCategories.includes(item.title)) {
+            setActiveCategories(
+              activeCategories.filter(category => category !== item.title)
+            )
+          }
+        }}
+      >
         {item.title ?? item?.type}
       </AccordionTrigger>
       {item?.subItems &&
@@ -46,7 +66,7 @@ export const NodeItem = ({
                 e.stopPropagation()
                 handleClick({ i: subItem })
               }}
-              className="py-2 pr-2 pl-6 border-b-0 border-t border-black hover:underline hover:bg-[#282d33] cursor-pointer"
+              className={`py-2 pr-2 pl-6 border-b-0 border-t border-black hover:underline hover:bg-[#282d33] cursor-pointer`}
             >
               {sub}
             </AccordionContent>
