3acd895b24322c68231addce1037ad9388467876
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-04 07:45:50 -0500

    getState node

---
+++ plugins/core/src/lib/nodes/_uncategorized/getState.ts
@@ -0,0 +1,51 @@
+import { makeFlowNodeDefinition, NodeCategory } from '@magickml/behave-graph'
+import { type BasePlugin } from 'server/plugin'
+import { CORE_DEP_KEYS } from '../../config'
+
+// plan is to not hardcode this and generate it dynamically by what plugins are enabled
+export enum PluginStateChoice {
+  CORE = 'core',
+  DISCORD = 'discord',
+  SLACK = 'slack',
+}
+
+export const getStateNode = makeFlowNodeDefinition({
+  typeName: 'agent/getState',
+  category: NodeCategory.Agent,
+  label: 'Get Agent State',
+  in: {
+    flow: 'flow',
+    plugin: {
+      label: 'Data Type',
+      choices: Object.values(PluginStateChoice),
+      defaultValue: PluginStateChoice.CORE,
+      valueType: 'string',
+    },
+  },
+  out: {
+    flow: 'flow',
+    state: 'object',
+  },
+  initialState: undefined,
+  triggered: async ({ commit, write, read, graph }) => {
+    const { getDependency } = graph
+    const plugin = read('plugin') as PluginStateChoice
+
+    if (!Object.values(PluginStateChoice).includes(plugin)) {
+      throw new Error(`Invalid plugin in getStateNode: ${plugin}`)
+    }
+
+    const getState = getDependency<BasePlugin['getGlobalState']>(
+      CORE_DEP_KEYS.GET_STATE
+    )
+
+    if (!getState) {
+      throw new Error(`getState dependency not found`)
+    }
+
+    const state = await getState(plugin)
+    write('state', state)
+
+    commit('flow')
+  },
+})
