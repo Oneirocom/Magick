e4a22d030c963b3d00334f5a668eb8776302426a
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-13 19:31:44 -0400

    fix s3 manager error generateClientConfig

--- packages/server/plugin/src/lib/storage/managers/s3-storage-manager.ts
+++ packages/server/plugin/src/lib/storage/managers/s3-storage-manager.ts
@@ -8,7 +8,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { PluginStorageManager } from '../plugin-storage-manager'
 
 export class S3PluginStorageManager extends PluginStorageManager {
-  private s3: S3Client
+  private s3: S3Client | undefined
 
   constructor(
     agentId: string,
@@ -19,6 +19,10 @@ export class S3PluginStorageManager extends PluginStorageManager {
     private bucket: string
   ) {
     super(agentId, plugin)
+    // this check is mainly for generateClientConfig
+    if (!accessKeyId || !secretAccessKey || !region) {
+      return
+    }
     this.s3 = new S3Client({
       credentials: { accessKeyId, secretAccessKey },
       region,
@@ -38,21 +42,28 @@ export class S3PluginStorageManager extends PluginStorageManager {
     }
 
     const command = new PutObjectCommand(params)
-    await this.s3.send(command)
+    await this.s3?.send(command)
     return this.getFileUrl(fileName)
   }
 
   async getFileUrl(fileName: string): Promise<string> {
+    if (!this.s3) {
+      throw new Error('S3 client not initialized')
+    }
     const params = {
       Bucket: this.bucket,
       Key: `${this.agentId}/${this.plugin}/${fileName}`,
     }
 
     const command = new GetObjectCommand(params)
-    return getSignedUrl(this.s3, command, { expiresIn: 900 }) // URL expires in 15 minutes
+
+    return getSignedUrl(this?.s3, command, { expiresIn: 900 }) // URL expires in 15 minutes
   }
 
   async deleteFile(fileName: string): Promise<void> {
+    if (!this.s3) {
+      throw new Error('S3 client not initialized')
+    }
     const params = {
       Bucket: this.bucket,
       Key: `${this.agentId}/${this.plugin}/${fileName}`,
