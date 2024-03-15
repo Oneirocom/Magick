f126b0c9daa0bf30d7218a919d45ac0b2c72dd0b
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-15 01:23:37 -0400

    lint

--- packages/client/ui/src/lib/fancy/input.tsx
+++ packages/client/ui/src/lib/fancy/input.tsx
@@ -10,11 +10,11 @@ export const FancyInput = React.forwardRef<HTMLInputElement, FancyInputProps>(
     const radius = 100 // change this to increase the rdaius of the hover effect
     const [visible, setVisible] = React.useState(false)
 
-    let mouseX = useMotionValue(0)
-    let mouseY = useMotionValue(0)
+    const mouseX = useMotionValue(0)
+    const mouseY = useMotionValue(0)
 
     function handleMouseMove({ currentTarget, clientX, clientY }: any) {
-      let { left, top } = currentTarget.getBoundingClientRect()
+      const { left, top } = currentTarget.getBoundingClientRect()
 
       mouseX.set(clientX - left)
       mouseY.set(clientY - top)
