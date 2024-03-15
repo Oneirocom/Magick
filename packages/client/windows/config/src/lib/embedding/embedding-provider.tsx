16a036f3dec694217f3e45f483e4dad0b6a8de47
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-15 01:17:43 -0400

    Add EmbeddingProviderDropdown component

---
+++ packages/client/windows/config/src/lib/embedding/embedding-provider.tsx
@@ -0,0 +1,30 @@
+import { Dropdown } from '@magickml/client-ui'
+import { type FC } from 'react'
+import { LLMProviders, availableEmbeddingProviders } from 'servicesShared'
+
+interface EmbeddingProviderDropdownProps {
+  selectedEmbeddingProvider?: LLMProviders
+  onChange: (provider: LLMProviders) => void
+}
+
+export const EmbeddingProviderDropdown: FC<EmbeddingProviderDropdownProps> = ({
+  selectedEmbeddingProvider,
+  onChange,
+}) => {
+  const providerOptions = availableEmbeddingProviders.map(prov => ({
+    value: prov.provider,
+    label: prov.displayName,
+  }))
+
+  return (
+    <div className="flex flex-col gap-y-1 w-full">
+      <p className='font-semibold'>Embedding Provider</p>
+      <Dropdown
+        options={providerOptions}
+        selectedValue={selectedEmbeddingProvider || ''}
+        onChange={value => onChange(value as LLMProviders)}
+        placeholder="Select a provider"
+      />
+    </div>
+  )
+}
