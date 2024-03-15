df9e7f6ae37925ab3bb8dd6313dc74dc765b6d2c
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-15 01:12:16 -0400

    fancy input

---
+++ packages/client/ui/src/lib/fancy/input.tsx
@@ -0,0 +1,57 @@
+import { useMotionTemplate, useMotionValue, motion } from 'framer-motion'
+import { cn } from '../utils/shadcn'
+import React from 'react'
+
+export interface FancyInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const FancyInput = React.forwardRef<HTMLInputElement, FancyInputProps>(
+  ({ className, type, ...props }, ref) => {
+    const radius = 100 // change this to increase the rdaius of the hover effect
+    const [visible, setVisible] = React.useState(false)
+
+    let mouseX = useMotionValue(0)
+    let mouseY = useMotionValue(0)
+
+    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+      let { left, top } = currentTarget.getBoundingClientRect()
+
+      mouseX.set(clientX - left)
+      mouseY.set(clientY - top)
+    }
+    return (
+      <motion.div
+        style={{
+          background: useMotionTemplate`
+        radial-gradient(
+          ${visible ? radius + 'px' : '0px'} circle at ${mouseX}px ${mouseY}px,
+          var(--ds-primary),
+          transparent 80%
+        )
+      `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <input
+          type={type}
+          className={cn(
+            `flex h-10 w-full border-none bg-ds-card-alt text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
+          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+           disabled:cursor-not-allowed disabled:opacity-50
+           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+           group-hover/input:shadow-none transition duration-400
+           `,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
+    )
+  }
+)
+FancyInput.displayName = 'FancyInput'
