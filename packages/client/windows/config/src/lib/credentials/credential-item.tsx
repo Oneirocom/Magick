f128fbef4d5f023aeb032497de657309ca10e85f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-15 01:18:09 -0400

    Add CredentialItem component

---
+++ packages/client/windows/config/src/lib/credentials/credential-item.tsx
@@ -0,0 +1,52 @@
+import { Avatar, AvatarImage, AvatarFallback } from '@magickml/client-ui'
+import { FC } from 'react'
+import clsx from 'clsx'
+import { type PluginCredential } from './utils'
+
+interface CredentialItemProps {
+  action?: React.ReactNode
+  isLinked: boolean
+  credential: PluginCredential
+}
+
+export const CredentialItem: FC<CredentialItemProps> = ({
+  action,
+  isLinked,
+  credential,
+}) => {
+  return (
+    <div className="inline-flex items-center justify-start space-x-4 border-2 bg-[#282d33] border-white/10 rounded-sm p-4 h-24">
+      <Avatar>
+        <AvatarImage src={credential.icon} alt={credential.clientName} />
+        <AvatarFallback>{credential.initials}</AvatarFallback>
+      </Avatar>
+      <div className="flex items-start gap-y-[2px] h-full flex-col">
+        <p className="text-xs font-semibold leading-none">
+          {credential.clientName}
+        </p>
+        <p className="text-xs">{credential.description}</p>
+        <p
+          className={clsx(
+            isLinked ? 'text-green-500' : 'text-white/60',
+            'text-sm'
+          )}
+        >
+          {isLinked ? 'Linked' : 'Not Linked'}
+        </p>
+        {credential?.helpLink && (
+          <a
+            className="text-xs text-normal link text-ds-primary-m"
+            href={credential.helpLink}
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Learn more"
+          >
+            Learn how to get this
+          </a>
+        )}
+      </div>
+      <div className="grow" />
+      {action}
+    </div>
+  )
+}
