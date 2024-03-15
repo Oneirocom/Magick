0f3a03d57c5512d6c9349bba83b7268c55c9870d
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-15 01:16:27 -0400

    fix ui exports

--- packages/client/ui/src/lib/fancy/moving-border.tsx
+++ packages/client/ui/src/lib/fancy/moving-border.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import {
   motion,
@@ -8,66 +7,6 @@ import {
   useTransform,
 } from 'framer-motion'
 import { useRef } from 'react'
-import { cn } from '../utils/shadcn'
-
-export function Button({
-  borderRadius = '1.75rem',
-  children,
-  as: Component = 'button',
-  containerClassName,
-  borderClassName,
-  duration,
-  className,
-  ...otherProps
-}: {
-  borderRadius?: string
-  children: React.ReactNode
-  as?: any
-  containerClassName?: string
-  borderClassName?: string
-  duration?: number
-  className?: string
-  [key: string]: any
-}) {
-  return (
-    <Component
-      className={cn(
-        'bg-transparent relative text-xl  h-16 w-40 p-[1px] overflow-hidden ',
-        containerClassName
-      )}
-      style={{
-        borderRadius: borderRadius,
-      }}
-      {...otherProps}
-    >
-      <div
-        className="absolute inset-0"
-        style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
-      >
-        <MovingBorder duration={duration} rx="30%" ry="30%">
-          <div
-            className={cn(
-              'h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]',
-              borderClassName
-            )}
-          />
-        </MovingBorder>
-      </div>
-
-      <div
-        className={cn(
-          'relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased',
-          className
-        )}
-        style={{
-          borderRadius: `calc(${borderRadius} * 0.96)`,
-        }}
-      >
-        {children}
-      </div>
-    </Component>
-  )
-}
 
 export const MovingBorder = ({
   children,
@@ -141,7 +80,11 @@ export const MovingBorder = ({
   )
 }
 
-export const successBorderGradient = 'bg-[radial-gradient(var(--ds-alert)_40%,transparent_60%)]'
-export const errorBorderGradient = 'bg-[radial-gradient(var(--ds-error)_40%,transparent_60%)]'
-export const processingBorderGradient = 'bg-[radial-gradient(var(--ds-primary)_40%,transparent_60%)]'
-export const selectedBorderGradient = 'bg-[radial-gradient(var(--ds-white)_40%,transparent_60%)]'
+export const successBorderGradient =
+  'bg-[radial-gradient(var(--ds-alert)_40%,transparent_60%)]'
+export const errorBorderGradient =
+  'bg-[radial-gradient(var(--ds-error)_40%,transparent_60%)]'
+export const processingBorderGradient =
+  'bg-[radial-gradient(var(--ds-primary)_40%,transparent_60%)]'
+export const selectedBorderGradient =
+  'bg-[radial-gradient(var(--ds-white)_40%,transparent_60%)]'
