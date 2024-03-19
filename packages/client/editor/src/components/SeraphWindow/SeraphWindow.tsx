93d71d579de517ca280a644cb8b8f412ac113b23
Jakob <haruhunab1320@gmail.com>
2024-03-19 14:35:38 -0700

    fix seraph typo

---
+++ packages/client/editor/src/components/SeraphWindow/SeraphWindow.tsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux'
+import ChatWindow from '../ChatWindow/ChatWindow'
+import { RootState } from 'client/state'
+
+const SeraphWindow = ({ spellName }: { spellName: string }) => {
+  const { currentTab } = useSelector((state: RootState) => state.tabLayout)
+
+  return (
+    <div className="flex flex-col h-full p-4 text-white">
+      <ChatWindow tab={currentTab} spellName={spellName} />
+    </div>
+  )
+}
+
+export default SeraphWindow
