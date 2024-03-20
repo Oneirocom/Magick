a4b62f695cd484a36842392b0c6e7bc1d992580d
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-20 17:52:42 -0400

    feathers service for plugin state

---
+++ packages/server/core/src/services/state/plugin-state-service.ts
@@ -0,0 +1,69 @@
+import {
+  Params,
+  ServiceMethods,
+  HookContext,
+  type Id,
+} from '@feathersjs/feathers'
+import type { Application } from '../../declarations'
+import { prismaCore, type Prisma } from '@magickml/server-db'
+
+type PluginStateBody = Record<string, any>
+type PluginStateParams = {
+  agentId: Id
+  plugin: string | undefined
+}
+
+export type PluginStateServiceMethods = Pick<
+  ServiceMethods<PluginStateBody>,
+  'find'
+>
+
+class PluginStateService implements PluginStateServiceMethods {
+  app: Application
+  options: any
+
+  constructor(options: any = {}, app: Application) {
+    this.app = app
+    this.options = options
+  }
+
+  async find(params: Params<PluginStateParams>): Promise<PluginStateBody> {
+    const agentId = params.query.agentId
+    const plugin = params.query.plugin
+    if (typeof agentId !== 'string') {
+      throw new Error('No agentId provided')
+    }
+
+    let where: Prisma.pluginStateWhereInput = {
+      agentId,
+    }
+
+    if (plugin) {
+      where.plugin = plugin
+    }
+
+    return await prismaCore.pluginState.findMany({
+      where,
+      select: {
+        id: true,
+        plugin: true,
+        state: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    })
+  }
+}
+
+export const pluginState = (app: Application): void => {
+  app.use('/state', new PluginStateService({}, app)),
+    {
+      methods: ['GET'],
+    }
+}
+
+declare module '../../declarations' {
+  interface ServiceTypes {
+    ['/state']: PluginStateService
+  }
+}
