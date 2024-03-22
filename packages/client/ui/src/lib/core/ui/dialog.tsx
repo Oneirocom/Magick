7fb26ef1b5a27bc3bfaada3297e4e242277bd06b
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-22 15:49:14 -0400

    Add noClose prop to MagickDialog and DialogContent components

--- packages/client/ui/src/lib/core/ui/dialog.tsx
+++ packages/client/ui/src/lib/core/ui/dialog.tsx
@@ -35,8 +35,10 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+    noClose?: boolean
+  }
+>(({ className, children, noClose = false, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -48,10 +50,12 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="text-ds-black dark:text-ds-white absolute right-4 top-4 rounded-sm ring-offset-ds-neutral transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
-        <Cross2Icon className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
+      {!noClose && (
+        <DialogPrimitive.Close className="text-ds-black dark:text-ds-white absolute right-4 top-4 rounded-sm ring-offset-ds-neutral transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
+          <Cross2Icon className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
+      )}
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
