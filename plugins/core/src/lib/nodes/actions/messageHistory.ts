a7a7eb265286054b0a1515bcfe99c25a4b5c93ff
Michael <m.sharpe@project89.org>
2024-03-21 22:40:29 -0700

    Add getMessageHistory flow node definition

---
+++ plugins/core/src/lib/nodes/actions/messageHistory.ts
@@ -0,0 +1,51 @@
+import { NodeCategory, makeFlowNodeDefinition } from '@magickml/behave-graph'
+import { IEventStore } from 'server/grimoire'
+
+export const getMessageHistory = makeFlowNodeDefinition({
+  typeName: 'queries/messages/getMessageHistory',
+  otherTypeNames: ['events/getMessageHistory'],
+  category: NodeCategory.Flow,
+  label: 'Message History',
+  configuration: {
+    hiddenProperties: {
+      valueType: 'array',
+      defaultValue: ['hiddenProperties', 'eventState'],
+    },
+    eventState: {
+      valueType: 'array',
+      defaultValue: ['channel', 'connector'] as string[],
+    },
+  },
+  in: {
+    flow: 'flow',
+    entries: {
+      valueType: 'integer',
+      defaultValue: 10,
+    },
+  },
+  out: {
+    flow: 'flow',
+    messages: 'array',
+  },
+  initialState: undefined,
+  triggered: async ({ configuration, graph, read, write, commit }) => {
+    const { eventState } = configuration
+
+    const eventStore = graph.getDependency<IEventStore>('IEventStore')
+
+    if (!eventStore) {
+      throw new Error('Event store not found')
+    }
+
+    const limit = Number(read('entries')) as number
+
+    const messages = (await eventStore.getMessages(eventState, limit)).reverse()
+
+    if (messages[0].role === 'assistant') {
+      messages.shift()
+    }
+
+    write('messages', messages)
+    commit('flow')
+  },
+})
