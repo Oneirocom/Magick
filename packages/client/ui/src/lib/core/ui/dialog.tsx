edfface7370448316dcc5725c16c103a5b19df93
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-22 14:57:08 -0400

    Update MagickDialog component styling

--- packages/client/ui/src/lib/core/ui/dialog.tsx
+++ packages/client/ui/src/lib/core/ui/dialog.tsx
@@ -92,7 +92,7 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      'text-lg font-semibold font-montAlt leading-none tracking-tight',
+      'text-lg font-semibold !font-montAlt leading-none tracking-tight',
       className
     )}
     {...props}
