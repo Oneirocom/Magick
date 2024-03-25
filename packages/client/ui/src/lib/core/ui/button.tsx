122b8c291123d2590698f92038f51b7f2962b069
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-25 17:12:51 -0400

    update button rounded styles

--- packages/client/ui/src/lib/core/ui/button.tsx
+++ packages/client/ui/src/lib/core/ui/button.tsx
@@ -28,14 +28,14 @@ const buttonVariants = cva(
         'portal-primary':
           'bg-ds-primary text-ds-black font-semibold font-montserrat',
         'portal-neutral':
-          'bg-ds-neutral text-ds-white font-medium font-montserrat border border-ds-white/40',
+          'bg-ds-neutral text-ds-white font-medium font-montserrat',
           clear: '',
       },
       size: {
         default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-lg px-3 text-xs',
-        md: 'h-10 rounded-lg px-4 text-base',
-        lg: 'h-12 rounded-lg px-4',
+        sm: 'h-8 rounded-[5px] px-3 text-xs',
+        md: 'h-10 rounded-[5px] px-4 text-base',
+        lg: 'h-12 rounded-[5px] px-4',
         icon: 'h-9 w-9',
         responsive: 'h-8 lg:h-12 rounded-md px-3 lg:px-4 text-xs lg:text-base',
       },
