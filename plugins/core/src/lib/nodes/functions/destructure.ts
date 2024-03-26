d1654ffc24c68247758579b3c3788a9460447d26
Michael <m.sharpe@project89.org>
2024-03-26 14:57:31 -0700

    Beginning of destructure node

---
+++ plugins/core/src/lib/nodes/functions/destructure.ts
@@ -0,0 +1,59 @@
+import {
+  NodeCategory,
+  SocketsList,
+  makeFunctionNodeDefinition,
+} from '@magickml/behave-graph'
+
+export const objectDestructure = makeFunctionNodeDefinition({
+  typeName: 'logic/object/destructure',
+  aliases: [],
+  category: NodeCategory.Logic,
+  label: 'Destructure',
+  configuration: {
+    hiddenProperties: {
+      valueType: 'array',
+      defaultValue: [
+        'hiddenProperties',
+        'textEditorOptions',
+        'textEditorData',
+        'socketValues',
+      ],
+    },
+    socketValues: {
+      valueType: 'array',
+      defaultValue: ['string', 'array', 'boolean', 'integer'],
+    },
+    socketOutputs: {
+      valueType: 'array',
+      defaultValue: [],
+    },
+  },
+  in: {
+    object: 'object',
+  },
+  out: configuration => {
+    const startSockets = []
+
+    const socketArray = configuration?.socketOutputs.length
+      ? configuration.socketOutputs
+      : []
+
+    const sockets: SocketsList =
+      socketArray.map(socketOutput => {
+        return {
+          key: socketOutput.name,
+          name: socketOutput.name,
+          valueType: socketOutput.valueType,
+        }
+      }) || []
+
+    return [...startSockets, ...sockets]
+  },
+  exec: ({ write, read, configuration }) => {
+    const object = (read('object') as Record<string, any>) || {}
+    configuration.socketOutputs.forEach(socketOutput => {
+      const value = object[socketOutput.name]
+      write(socketOutput.name, value)
+    })
+  },
+})
