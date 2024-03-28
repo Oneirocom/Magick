e2cb52460071d67e3a91367cb8086daf22b5dad8
Jakob <haruhunab1320@gmail.com>
2024-03-28 14:41:09 -0700

    add types to seraph api

--- packages/client/state/src/lib/api/seraph.ts
+++ packages/client/state/src/lib/api/seraph.ts
@@ -1,13 +1,17 @@
+import { ISeraphEvent } from 'servicesShared'
 import { rootApi } from './api'
 export const seraphApi = rootApi.injectEndpoints({
   endpoints: builder => ({
-    getSeraphChatHistory: builder.query({
+    getSeraphChatHistory: builder.query<ISeraphEvent[], { agentId: string }>({
       providesTags: ['SeraphChatHistory'],
       query: ({ agentId }) => ({
         url: `seraph/chatHistory?agentId=${agentId}`,
       }),
     }),
-    createSeraphRequest: builder.mutation({
+    createSeraphRequest: builder.mutation<
+      { success: boolean; id: string },
+      ISeraphEvent
+    >({
       invalidatesTags: ['SeraphChatHistory'],
       query: seraphRequest => ({
         url: `seraph/request`,
