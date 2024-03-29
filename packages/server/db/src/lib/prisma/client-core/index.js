f4c92924cc967d3027076dd5a310995f5075b0ed
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-29 17:40:27 -0400

    fix portal studio command

--- packages/server/db/src/lib/prisma/client-core/index.js
+++ packages/server/db/src/lib/prisma/client-core/index.js
@@ -523,7 +523,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/jakobgrant/Workspaces/Oneirocom/Magick/packages/server/db/src/lib/prisma/client-core",
+      "value": "/home/coffee/migrate/magick/packages/server/db/src/lib/prisma/client-core",
       "fromEnvVar": null
     },
     "config": {
@@ -533,7 +533,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "darwin-arm64",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
@@ -602,8 +602,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-path.join(process.cwd(), "src/lib/prisma/client-core/libquery_engine-darwin-arm64.dylib.node")
+path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
+path.join(process.cwd(), "src/lib/prisma/client-core/libquery_engine-debian-openssl-3.0.x.so.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "src/lib/prisma/client-core/schema.prisma")
