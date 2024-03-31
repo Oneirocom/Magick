2263d998c43a84eb47c562edc3f1df44013a7f61
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-31 02:35:35 -0400

    Update button variant styles

--- packages/client/ui/src/lib/core/ui/button.tsx
+++ packages/client/ui/src/lib/core/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border border-ds-neutral hover:border-ds-primary bg-transparent shadow-sm',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost:
