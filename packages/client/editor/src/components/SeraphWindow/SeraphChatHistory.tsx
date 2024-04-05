722d4d6a2dca7f0bb553aa90113327d19bb8a380
Jakob <haruhunab1320@gmail.com>
2024-04-05 10:09:39 -0700

    add  in user and seraph message copmponent

--- packages/client/editor/src/components/SeraphWindow/SeraphChatHistory.tsx
+++ packages/client/editor/src/components/SeraphWindow/SeraphChatHistory.tsx
@@ -1,26 +1,43 @@
 import { Scrollbars } from 'react-custom-scrollbars-2'
 
 export const SeraphChatHistory = ({ history, scrollbars }) => {
+  const UserMessage = ({ message }) => (
+    <div className="flex flex-row mb-2">
+      <div className="bg-transparent p-2 rounded text-white flex-1 text-sm font-mono whitespace-pre-line">
+        {message}
+      </div>
+    </div>
+  )
+
+  const SeraphMessage = ({ message }) => (
+    <div className="flex flex-row mb-2">
+      <div className="bg-slate-700 p-2 rounded text-white flex-1 text-sm font-mono whitespace-pre-line">
+        {message}
+      </div>
+    </div>
+  )
+
   return (
     <div className="flex-1 overflow-hidden bg-[--ds-black]">
       <Scrollbars ref={ref => (scrollbars.current = ref)}>
         <ul className="list-none m-0 p-2">
-          {history.map((message, index) => (
-            <li
-              key={index}
-              className={`flex flex-row mb-2 ${
-                message.sender === 'user' ? 'justify-end' : ''
-              }`}
-            >
-              <div
-                className={`rounded p-2 text-white flex-1 text-sm font-mono whitespace-pre-line ${
-                  message.sender === 'user' ? 'bg-blue-500' : 'bg-slate-700'
-                }`}
-              >
-                {message.content}
-              </div>
-            </li>
-          ))}
+          {history.map((message, index) => {
+            if (message.sender === 'user') {
+              return (
+                <li key={index}>
+                  <UserMessage message={message.content} />
+                </li>
+              )
+            } else if (message.sender === 'seraph') {
+              return (
+                <li key={index}>
+                  <SeraphMessage message={message.content} />
+                </li>
+              )
+            } else {
+              return null
+            }
+          })}
         </ul>
       </Scrollbars>
     </div>
