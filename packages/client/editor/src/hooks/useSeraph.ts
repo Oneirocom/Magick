dbcb07a1114475e24c520df3b10ae2c36cbb8153
Jakob <haruhunab1320@gmail.com>
2024-04-05 10:12:11 -0700

    update useSeraph hook

--- packages/client/editor/src/hooks/useSeraph.ts
+++ packages/client/editor/src/hooks/useSeraph.ts
@@ -22,16 +22,16 @@ export const useSeraph = ({ tab, projectId, agentId, history, setHistory }) => {
   // set up listeners for response, error, info,
   useEffect(() => {
     const destoryResponseListener = subscribe(
-      $SERAPH_EVENT(tab.id),
+      events.$SERAPH_EVENT(tab.id),
       (event, data) => {
-        console.log('RESPONSE', event)
+        console.log('RESPONSE', { event, data })
         setEventData(data)
       }
     )
     return () => {
       destoryResponseListener()
     }
-  }, [$SERAPH_EVENT, tab.id])
+  }, [])
 
   useEffect(() => {
     if (!eventData) return
@@ -52,19 +52,20 @@ export const useSeraph = ({ tab, projectId, agentId, history, setHistory }) => {
 
   // function to make a request
   const makeSeraphRequest = async (request: SeraphRequest) => {
-    const seraphRequest: ISeraphEvent = {
-      agentId,
-      projectId,
-      type: SeraphEvents.request,
-      spellId: tab.params.spellId,
-      data: { request },
-      createdAt: new Date().toISOString(),
-    }
     try {
+      const seraphRequest: ISeraphEvent = {
+        agentId,
+        projectId,
+        type: SeraphEvents.request,
+        spellId: tab.params.spellId,
+        data: { request },
+        createdAt: new Date().toISOString(),
+      }
       setEventData(undefined)
       const data = await createSeraphRequest(seraphRequest)
       if (!data) throw new Error('Error creating seraph request')
-      publish($SERAPH_EVENT(tab.id), { seraphRequest })
+      publish($SERAPH_EVENT(tab.id), seraphRequest)
+      return true
     } catch (error) {
       console.error('Error making seraph request', error)
     }
