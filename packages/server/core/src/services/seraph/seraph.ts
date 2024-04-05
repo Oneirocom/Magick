8e9b69066c5ac31be1c6b5bc8226ade1624c688f
Jakob <haruhunab1320@gmail.com>
2024-04-05 16:34:05 -0700

    relay seraph messages to feathers

--- packages/server/core/src/services/seraph/seraph.ts
+++ packages/server/core/src/services/seraph/seraph.ts
@@ -13,25 +13,22 @@ import { SeraphService, getOptions } from './seraph.class'
 import { jsonResolver } from '../utils'
 import { v4 as uuidv4 } from 'uuid'
 import { checkPermissions } from '../../lib/feathersPermissions'
-
+import { SeraphFunction } from 'servicesShared'
 // Re-export spellReleases.class and spells.schema
 export * from './seraph.class'
 export * from './seraph.schema'
 
-// Update these to match seraph emitter
-const SERAPH_EVENTS = [
+const SERAPH_EVENTs = [
   'error',
   'message',
   'info',
   'token',
   'request',
-  'response',
   'functionExecution',
   'functionResult',
   'middlewareExecution',
   'middlewareResult',
 ]
-
 /**
  * Configure the seraph events service by registering it, its hooks, and its options.
  * @param app - The Feathers application
@@ -40,14 +37,42 @@ export const seraph = (app: Application) => {
   // Register the seraph events service on the Feathers application
   app.use('seraph', new SeraphService(getOptions(app), app), {
     methods: ['create', 'find', 'get'],
-    events: SERAPH_EVENTS,
+    events: SERAPH_EVENTs,
   })
 
-  // const seraph = app.get('seraph')
+  const seraph = app.get('seraphCore')
 
-  // seraph.on('token', (token: string) => {
-  //   app.services('seraph').emit('token', token)
-  // })
+  seraph.on('token', (token: string) => {
+    app.emit('token', token)
+  })
+
+  seraph.on('message', (message: string) => {
+    app.emit('message', message)
+  })
+
+  seraph.on('error', (error: string) => {
+    app.emit('error', error)
+  })
+
+  seraph.on('info', (info: string) => {
+    app.emit('info', info)
+  })
+
+  seraph.on('functionExecution', (seraphFunction: SeraphFunction) => {
+    app.emit('functionExecution', seraphFunction)
+  })
+
+  seraph.on('functionResult', (seraphFunction: SeraphFunction) => {
+    app.emit('functionResult', seraphFunction)
+  })
+
+  seraph.on('middlewareExecution', (middlewareFunction: SeraphFunction) => {
+    app.emit('middlewareExecution', middlewareFunction)
+  })
+
+  seraph.on('middlewareResult', (middlewareFunction: SeraphFunction) => {
+    app.emit('middlewareResult', middlewareFunction)
+  })
 
   // Initialize hooks for the seraph events service
   app.service('seraph').hooks({
