a66019722435c23d51c4277bc94cca60c3c80ac9
Jakob <haruhunab1320@gmail.com>
2024-04-08 13:59:40 -0700

    update reference to SeraphCore

--- packages/server/seraph/src/lib/seraph_iterator.ts
+++ packages/server/seraph/src/lib/seraph_iterator.ts
@@ -1,14 +1,18 @@
-import { Seraph } from './seraph'
+import { SeraphCore } from './seraphCore'
 import { LLMManager } from './llm_manager'
 
 class SeraphIterator implements AsyncIterator<string> {
-  private seraph: Seraph
+  private seraph: SeraphCore
   private conversationId: string
   private systemPrompt: string
   private llmManager: LLMManager
   private done: boolean = false
 
-  constructor(seraph: Seraph, conversationId: string, systemPrompt: string) {
+  constructor(
+    seraph: SeraphCore,
+    conversationId: string,
+    systemPrompt: string
+  ) {
     this.seraph = seraph
     this.conversationId = conversationId
     this.systemPrompt = systemPrompt
