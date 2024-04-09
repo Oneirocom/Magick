a59c4a01742e8a44d9a31a508b2cb0d3b98a51df
Jakob <haruhunab1320@gmail.com>
2024-04-09 15:04:13 -0700

    comment out bar items

--- packages/client/editor/src/components/workspaces/composerv2/ComposerLayoutComponents.tsx
+++ packages/client/editor/src/components/workspaces/composerv2/ComposerLayoutComponents.tsx
@@ -1,16 +1,16 @@
 import { IGridviewPanelProps, Tab } from 'dockview'
-import { DraggableElement } from './DraggableElement'
+// import { DraggableElement } from './DraggableElement'
 import { Composer } from './composer'
 
 export const composerLayoutComponents = {
   WindowBar: (props: IGridviewPanelProps<{ title: string }>) => {
     return (
-      <div className="flex justify-end items-center gap-4 p-4">
-        <p className="text-gray-400 mr-12">Composer V2</p>
+      <div className="flex justify-end items-center gap-4 p-4 h-9">
+        {/* <p className="text-gray-400 mr-12">Composer V2</p>
         <DraggableElement window="Console" {...props} />
         <DraggableElement window="TextEditor" title="Text Editor" {...props} />
         <DraggableElement window="Test" {...props} />
-        <DraggableElement window="Variables" {...props} />
+        <DraggableElement window="Variables" {...props} /> */}
       </div>
     )
   },
