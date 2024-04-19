113a03ee849d90345f681047287fbcd8c7d9c32a
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 23:41:25 -0400

    Fix import statement and update envs in public-presigner.ts

--- packages/server/storage/src/lib/upload/public-presigner.ts
+++ packages/server/storage/src/lib/upload/public-presigner.ts
@@ -1,6 +1,4 @@
-// public-presigner.ts
-
-import { UploadPresigner, UploadPresignerConfig } from './upload-presigner';
+import { UploadPresigner, UploadPresignerConfig } from './upload-presigner'
 
 export enum PublicPresignType {
   projectAvatar = 'projectAvatar',
@@ -8,25 +6,33 @@ export enum PublicPresignType {
   templateAvatar = 'templateAvatar',
 }
 
-const types: UploadPresignerConfig['presignTypes'] = {
-  [PublicPresignType.projectAvatar]: {
-    folder: 'projects',
-  },
-  [PublicPresignType.agentAvatar]: {
-    folder: 'agents',
-  },
-  [PublicPresignType.templateAvatar]: {
-    folder: 'templates',
-  },
-};
-
 const config: UploadPresignerConfig = {
-  accessKeyId: process.env?.['PROJECT_AWS_ACCESS_KEY_ID'] || '',
-  secretAccessKey: process.env?.['PROJECT_AWS_SECRET_ACCESS_KEY'] || '',
-  region: process.env?.['PROJECT_AWS_REGION'] || 'east-1',
-  endpoint: process.env?.['PROJECT_AWS_BUCKET_ENDPOINT'] || '',
-  bucketName: process.env?.['PROJECT_AWS_BUCKET_NAME'] || '',
-  presignTypes: types,
-};
+  settings: {
+    type: 'public',
+    uploadConfig: {
+      [PublicPresignType.projectAvatar]: {
+        folder: 'projects',
+        fileKey: 'avatar.jpg',
+      },
+      [PublicPresignType.agentAvatar]: {
+        folder: 'agents',
+        fileKey: 'avatar.jpg',
+      },
+      [PublicPresignType.templateAvatar]: {
+        folder: 'templates',
+        fileKey: 'avatar.jpg',
+      },
+    },
+    rootFolder: null,
+  },
 
-export const publicPresigner = new UploadPresigner(config);
+  config: {
+    accessKeyId: process.env?.['NEXT_AWS_ACCESS_KEY_ID'] || '',
+    secretAccessKey: process.env?.['NEXT_AWS_SECRET_ACCESS_KEY'] || '',
+    region: process.env?.['NEXT_AWS_REGION'] || 'east-1',
+    endpoint: process.env?.['NEXT_AWS_BUCKET_ENDPOINT'] || '',
+    bucketName: process.env?.['NEXT_AWS_BUCKET_NAME'] || '',
+  },
+}
+
+export const publicPresigner = new UploadPresigner(config)
