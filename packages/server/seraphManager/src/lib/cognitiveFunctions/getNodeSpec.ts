01dbf9f6780f4c4d39809ecc016e79b757b882f9
Michael <m.sharpe@project89.org>
2024-04-19 00:55:50 -0700

    Improve node spec description

--- packages/server/seraphManager/src/lib/cognitiveFunctions/getNodeSpec.ts
+++ packages/server/seraphManager/src/lib/cognitiveFunctions/getNodeSpec.ts
@@ -4,7 +4,7 @@ import { getNodeSpec } from 'shared/nodeSpec'
 const functionDefinition = {
   name: 'getNodeSpec',
   description:
-    'You can use this function when you need to know what nodes are available in the Magick system.  It will return a large list of nodes that you can use to build your spells.  It is a basic JSON spec which tells you all the nodes, inputs, outputs, and configurations.  It will not tell you detailed descriptions of each node however.',
+    'You use this function when you need to know what nodes are available in the Magick system.  Never answer any question about Magicks nodes, or make recommendations for nodes, without calling this.  You always need this information in your context to answer anything related to spells. It will return a large list of nodes that you can use to build your spells.  It is a basic JSON spec which tells you all the nodes, inputs, outputs, and configurations.  It will not tell you detailed descriptions of each node however.',
   parameters: {},
   examples: [
     `<invoke>
