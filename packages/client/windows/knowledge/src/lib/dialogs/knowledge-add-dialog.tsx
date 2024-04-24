429d6614884801836e4649077a8ede349b70baa9
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-24 16:26:21 -0400

    use new component in add knowledge dialog

--- packages/client/windows/knowledge/src/lib/dialogs/knowledge-add-dialog.tsx
+++ packages/client/windows/knowledge/src/lib/dialogs/knowledge-add-dialog.tsx
@@ -1,14 +1,5 @@
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-  InputWithLabel,
-  PortalDialog,
-  SelectWithLabel,
-} from '@magickml/client-ui'
+import { Tabs, TabsList, TabsTrigger, PortalDialog } from '@magickml/client-ui'
 import { useState } from 'react'
-import { DataType } from './add/file-types'
 import { KnowledgeUploadContent } from './add/knowledge-upload-content'
 import { KnowledgeDialogTab } from './add/types'
 import { addKnowledgeDialogAtom, addKnowledgeFormAtom } from './add/state'
@@ -19,6 +10,7 @@ import toast from 'react-hot-toast'
 import { useCreateKnowledgeMutation } from 'client/state'
 import { useConfig } from '@magickml/providers'
 import { AddKnowledge } from 'servicesShared'
+import { KnowledgeURLContent } from './add/knowledge-url-content'
 
 export const ACCEPT =
   '.eml, .html, .json, .md, .msg, .rst, .rtf, .txt, .xml, .jpeg, .jpg, .png, .csv, .doc, .docx, .epub, .odt, .pdf, .ppt, .pptx, .tsv, .xlsx'
@@ -30,17 +22,12 @@ type AddKnowledgeDialogProps = {
 export const AddKnowledgeDialog: React.FC<AddKnowledgeDialogProps> = ({
   openState,
 }) => {
-  /* Constants */
-  const dataTypes = Object.values(DataType).map(type => ({
-    label: type,
-    value: type,
-  }))
-
   /* State */
   const [tabState, setTabState] = useState<KnowledgeDialogTab>(
-    KnowledgeDialogTab.UPLOAD
+    KnowledgeDialogTab.URL
   )
   const uploadDisabled = useState(false)
+  const urlDisabled = useState(false)
   const resetFiles = useResetAtom(addKnowledgeDialogAtom)
   const resetForm = useResetAtom(addKnowledgeFormAtom)
   const reset = () => {
@@ -58,7 +45,12 @@ export const AddKnowledgeDialog: React.FC<AddKnowledgeDialogProps> = ({
     if (tabState === KnowledgeDialogTab.UPLOAD) {
       knowledge = newKnowledge
     } else {
-      knowledge = [formKnowledge]
+      knowledge = [
+        {
+          ...formKnowledge,
+          external: true,
+        },
+      ]
     }
 
     try {
@@ -115,67 +107,12 @@ export const AddKnowledgeDialog: React.FC<AddKnowledgeDialogProps> = ({
       >
         {/* Tabs List */}
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger
-            className="opacity-50 pointer-events-none"
-            value={KnowledgeDialogTab.URL}
-          >
-            URL
-          </TabsTrigger>
+          <TabsTrigger value={KnowledgeDialogTab.URL}>URL</TabsTrigger>
           <TabsTrigger value={KnowledgeDialogTab.UPLOAD}>Upload</TabsTrigger>
         </TabsList>
 
         {/* URL Tab */}
-        <TabsContent
-          value={KnowledgeDialogTab.URL}
-          className="flex flex-col gap-y-4 pt-2"
-        >
-          <InputWithLabel
-            id="name"
-            label="Name"
-            placeholder="Enter a name for this new knowledge entry"
-            className="w-full"
-            value={formKnowledge.name}
-            onChange={e =>
-              setFormKnowledge({ ...formKnowledge, name: e.target.value })
-            }
-            required
-          />
-          <InputWithLabel
-            id="sourceUrl"
-            label="Source URL"
-            placeholder="Enter a source URL to download the knowledge from"
-            className="w-full"
-            value={formKnowledge.sourceUrl}
-            onChange={e =>
-              setFormKnowledge({ ...formKnowledge, sourceUrl: e.target.value })
-            }
-            disabled={!!formKnowledge.sourceUrl}
-          />
-
-          <div className="grid grid-cols-2 gap-x-2">
-            <InputWithLabel
-              id="tag"
-              label="Tag"
-              placeholder="Enter a tag"
-              value={formKnowledge.tag}
-              onChange={e =>
-                setFormKnowledge({ ...formKnowledge, tag: e.target.value })
-              }
-            />
-            <SelectWithLabel
-              id="dataType"
-              label="Data Type"
-              className="w-full"
-              options={dataTypes}
-              group="Data Type"
-              value={formKnowledge.dataType}
-              onValueChange={(value: string) =>
-                setFormKnowledge({ ...formKnowledge, dataType: value })
-              }
-              required
-            />
-          </div>
-        </TabsContent>
+        <KnowledgeURLContent disabledState={urlDisabled} />
 
         {/* Upload Tab */}
         <KnowledgeUploadContent disabledState={uploadDisabled} />
