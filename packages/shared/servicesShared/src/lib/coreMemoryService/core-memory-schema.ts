95723bd14213349807746760b189cdf5d704a669
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-24 16:25:46 -0400

    add external to core-memory-add schema

--- packages/shared/servicesShared/src/lib/coreMemoryService/core-memory-schema.ts
+++ packages/shared/servicesShared/src/lib/coreMemoryService/core-memory-schema.ts
@@ -102,6 +102,7 @@ export const AddKnowledgeSchema = z.object({
   name: z.string(),
   sourceUrl: z.string(),
   dataType: z.string(), // TODO: use z.enum in shared package e2e. not doing now because of circular dependency
+  external: z.boolean().optional(), // EZ way to tell if the knowledge is external url. Better handling in future
 })
 
 export type AddKnowledge = z.infer<typeof AddKnowledgeSchema>
