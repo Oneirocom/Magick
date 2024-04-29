ad024add3f98e040f29ddecc0ba24bd7aa776b92
Michael <m.sharpe@project89.org>
2024-04-29 16:43:18 -0700

    Add lifecycle test node

--- plugins/core/src/lib/nodes/lifecycle/onTick.ts
+++ plugins/core/src/lib/nodes/lifecycle/onTick.ts
@@ -1,8 +1,9 @@
 import { ILifecycleEventEmitter, NodeCategory } from '@magickml/behave-graph'
 import { makeMagickEventNodeDefinition } from 'server/grimoire'
+import { CORE_DEP_KEYS } from '../../config'
+import { Agent } from 'server/agents'
 
 type State = {
-  onTickEvent?: (() => void) | undefined
   lastTickTime: number
 }
 
@@ -12,7 +13,7 @@ const makeInitialState = (): State => ({
 
 export const LifecycleOnTick = makeMagickEventNodeDefinition(
   {
-    typeName: 'lifecycle/onTick',
+    typeName: 'lifecycles/onTick',
     label: 'On Tick',
     category: NodeCategory.Event,
     in: {},
@@ -24,18 +25,30 @@ export const LifecycleOnTick = makeMagickEventNodeDefinition(
   },
   {
     customListener: (getDependency, onStartEvent) => {
+      const agent = getDependency<Agent>(CORE_DEP_KEYS.AGENT)
+
+      console.log('I AM A LISTSNER')
+
+      if (!agent) return
+
       const lifecycleEventEmitter = getDependency<ILifecycleEventEmitter>(
         'ILifecycleEventEmitter'
       )
 
       lifecycleEventEmitter?.tickEvent.addListener(() => {
-        onStartEvent()
+        const event = agent?.formatEvent<{}>({
+          content: '',
+          eventName: 'tick',
+          sender: agent.id,
+          data: {},
+        })
+
+        onStartEvent(event)
       })
     },
     handleEvent: (event, { write, commit, state }) => {
-      const { lastTickTime } = state
       const currentTime = Date.now()
-      const deltaSeconds = (currentTime - lastTickTime) * 0.001
+      const deltaSeconds = (currentTime - state.lastTickTime) * 0.001
       write('deltaSeconds', deltaSeconds)
       commit('flow')
     },
