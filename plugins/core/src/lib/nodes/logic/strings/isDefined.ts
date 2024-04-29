a9ce70585f62f683f344c7a29ee48598ed211d2b
Michael <m.sharpe@project89.org>
2024-04-29 16:43:30 -0700

    Fix label on undefined node

--- plugins/core/src/lib/nodes/logic/strings/isDefined.ts
+++ plugins/core/src/lib/nodes/logic/strings/isDefined.ts
@@ -2,7 +2,7 @@ import { makeInNOutFunctionDesc } from '@magickml/behave-graph'
 
 export const IsDefined = makeInNOutFunctionDesc({
   name: 'logic/string/isDefined',
-  label: 'Concat',
+  label: 'Is Defined',
   in: ['string'],
   out: 'boolean',
   exec: (a: string) => a !== undefined && a !== null && a !== '',
