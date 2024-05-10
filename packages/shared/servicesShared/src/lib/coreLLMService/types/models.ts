470f819def3c0bd448c97a36250368a0628064b9
Jakob <haruhunab1320@gmail.com>
2024-05-10 16:55:29 -0700

    add model type

--- packages/shared/servicesShared/src/lib/coreLLMService/types/models.ts
+++ packages/shared/servicesShared/src/lib/coreLLMService/types/models.ts
@@ -1,4 +1,15 @@
-import { EmbeddingModel } from '../../coreEmbeddingService/types'
-import { CompletionModel } from './completionModels'
-
-export type AllModels = CompletionModel | EmbeddingModel
+export type Model = {
+  model_name: string
+  display_name: string
+  max_context_window: number
+  input_cost: number
+  output_cost: number
+  rate_limit: number
+  provider: {
+    provider_name: string
+    provider_id: string
+    api_key: string
+    moderation: 'Filtered'
+    extra_kwargs: any
+  }
+}
