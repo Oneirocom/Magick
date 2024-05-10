04612a6d0be2b7e7770445646dae0ac003365f5e
Michael <m.sharpe@project89.org>
2024-05-10 14:30:53 -0700

    Update the agent connector app to handle streaming audio

--- apps/agent-connector/src/app/app.ts
+++ apps/agent-connector/src/app/app.ts
@@ -4,6 +4,7 @@ import { initApp } from 'server/core'
 import { z } from 'zod'
 import { fastifyCors } from '@fastify/cors'
 import { fastifyWebsocket } from '@fastify/websocket'
+import { streamSpeech, elevenlabs } from 'modelfusion'
 import { v4 } from 'uuid'
 import WebSocket from 'ws'
 import axios from 'axios'
@@ -27,6 +28,64 @@ const agentInfoSchema = z.object({
   type: z.literal('agentInfo'),
 })
 
+// Define an interface to extend the AsyncGenerator with additional methods
+interface TextStreamGenerator extends AsyncGenerator<string, void, unknown> {
+  addToken: (token: string) => void
+  endStream: () => void
+}
+
+// Generate the text stream generator
+function createTextStreamGenerator(): TextStreamGenerator {
+  let resolve: ((value: IteratorResult<string, void>) => void) | null = null
+  const queue: (string | null)[] = []
+
+  async function* generator(): AsyncGenerator<string, void, undefined> {
+    while (true) {
+      if (queue.length > 0) {
+        const token = queue.shift()
+        // Explicitly check for null to allow empty strings to be yielded
+        if (token !== null) {
+          yield token
+        } else {
+          return // Finish the generator when a null token is encountered
+        }
+      } else {
+        const result: IteratorYieldResult<string> | IteratorReturnResult<void> =
+          await new Promise(r => (resolve = r))
+        // Check explicitly that result.value is not undefined to allow yielding empty strings
+        if (!result.done && result.value) {
+          console.log('result.value', result.value)
+          yield result.value
+        } else {
+          return // End the generator when done is true
+        }
+      }
+    }
+  }
+
+  const textGenerator: TextStreamGenerator = generator() as TextStreamGenerator
+
+  textGenerator.addToken = (token: string) => {
+    if (resolve) {
+      resolve({ value: token, done: false })
+      resolve = null
+    } else {
+      queue.push(token)
+    }
+  }
+
+  textGenerator.endStream = () => {
+    if (resolve) {
+      resolve({ value: undefined, done: true })
+      resolve = null
+    } else {
+      queue.push(null)
+    }
+  }
+
+  return textGenerator
+}
+
 type AgentInfo = z.infer<typeof agentInfoSchema>
 
 /* eslint-disable-next-line */
@@ -42,8 +101,7 @@ export async function app(fastify: FastifyInstance) {
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
   })
 
-  const apiKey = process.env.ELEVENLABS_API_KEY
-  const voiceId = '09mMp1DS2qzVMMRC8S2P'
+  const voiceId = 'bn5HJAJ1igu4dFplCXkQ'
   const model = 'eleven_turbo_v2'
 
   fastify.get('/ws/:agentId', { websocket: true }, async (socket, req) => {
@@ -61,6 +119,8 @@ export async function app(fastify: FastifyInstance) {
 
     const agent = new Agent(agentData, agentApp.get('pubsub'), agentApp)
 
+    let textGenerator = createTextStreamGenerator()
+
     /* USER INPUT STREAM */
     socket.on('message', message => {
       const data = messageSchema.parse(JSON.parse(message.toString()))
@@ -75,109 +135,69 @@ export async function app(fastify: FastifyInstance) {
         })
       )
 
-      agent.spellbook.refreshSpells().then(() => {
-        console.log('Emitting message', data.text)
-        agent.emit(
-          'message',
-          agent.formatEvent<{ responseId: number }>({
-            content: data.text,
-            sender: 'client',
-            eventName: 'message',
-            data: {
-              responseId: numId(),
-            },
-          })
-        )
-      })
+      agent.emit(
+        'message',
+        agent.formatEvent<{ responseId: number }>({
+          content: data.text,
+          sender: 'client',
+          eventName: 'message',
+          data: {
+            responseId: numId(),
+          },
+        })
+      )
     })
 
     /* AGENT MESSAGE OUTPUT STREAM */
     agent.on('messageStream', async actionPayload => {
-      const event = actionPayload.event
-      const responseId = (event.data as { responseId: string }).responseId
+      try {
+        const event = actionPayload.event
+        const responseId = (event.data as { responseId: string }).responseId
+        const content = actionPayload.data.content
 
-      // Send the text chunk to the client
-      socket.send(
-        JSON.stringify({
-          id: v4(),
-          sender: agentData.name,
-          text: actionPayload.data.content,
-          type: 'message',
-          responseId: responseId,
-        })
-      )
+        // Send the text chunk to the client
+        socket.send(
+          JSON.stringify({
+            id: v4(),
+            sender: agentData.name,
+            text: content,
+            type: 'message',
+            responseId: responseId,
+          })
+        )
 
-      // Open WebSocket connection to ElevenLabs API
-      const elevenLabsSocket = new WebSocket(
-        `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_id=${model}`
-      )
+        if (content === '<START>') {
+          console.log('Starting text to speech')
+          textGenerator = createTextStreamGenerator()
 
-      elevenLabsSocket.onopen = () => {
-        // Send the initial message with the API key and voice settings
-        const initialMessage = JSON.stringify({
-          text: ' ',
-          voice_settings: { stability: 0.5, similarity_boost: 0.8 },
-          xi_api_key: apiKey,
-        })
-        elevenLabsSocket.send(initialMessage)
+          const speechStream = await streamSpeech({
+            model: elevenlabs.SpeechGenerator({
+              model: model,
+              voice: voiceId,
+              optimizeStreamingLatency: 1,
+              voiceSettings: { stability: 1, similarityBoost: 0.35 },
+              generationConfig: {
+                chunkLengthSchedule: [50, 90, 120, 150, 200],
+              },
+            }),
+            text: textGenerator,
+          })
 
-        // Send the text message
-        const textMessage = JSON.stringify({
-          text: actionPayload.data.content + ' ',
-          try_trigger_generation: true,
-        })
-        elevenLabsSocket.send(textMessage)
-
-        // Send the EOS message
-        const eosMessage = JSON.stringify({ text: '' })
-        elevenLabsSocket.send(eosMessage)
-      }
-
-      elevenLabsSocket.onmessage = async event => {
-        const response = JSON.parse(event.data as any)
-        if (response.audio) {
-          const audioData = Buffer.from(response.audio, 'base64')
-          // const audioBase64 = audioData.toString('base64')
-
-          // Store the audio data in a buffer
-          const audioBuffer = Buffer.from(audioData)
-
-          // Send the audio data to the client in smaller chunks
-          const chunkSize = 1024 * 10 // 10KB chunks
-          for (let i = 0; i < audioBuffer.length; i += chunkSize) {
-            const chunk = audioBuffer.slice(i, i + chunkSize)
-            const chunkBase64 = chunk.toString('base64')
+          // part is a Uint8Array
+          for await (const part of speechStream) {
             if (socket.readyState === WebSocket.OPEN) {
-              socket.send(
-                JSON.stringify({
-                  type: 'audio',
-                  data: chunkBase64,
-                  responseId: responseId,
-                  isComplete: i + chunkSize >= audioBuffer.length,
-                })
-              )
-              await new Promise(resolve => setTimeout(resolve, 100)) // Delay between chunks to avoid overwhelming the client
+              console.log('Sending chunk')
+              socket.send(part) // Send the chunk directly as binary data
+              await new Promise(resolve => setTimeout(resolve, 10)) // Throttle sending
             }
           }
-        }
-        if (response.isFinal) {
-          elevenLabsSocket.close()
-        }
-      }
-
-      elevenLabsSocket.onerror = error => {
-        console.error('WebSocket error:', error)
-        elevenLabsSocket.close()
-      }
-
-      elevenLabsSocket.onclose = event => {
-        if (event.wasClean) {
-          console.log(
-            `WebSocket connection closed cleanly, code=${event.code} reason=${event.reason}`
-          )
+        } else if (content === '<END>') {
+          textGenerator.endStream()
         } else {
-          console.log('WebSocket connection died')
+          textGenerator.addToken(content)
         }
+      } catch (error) {
+        console.error('Error in agent.on messageStream', error)
       }
     })
 
