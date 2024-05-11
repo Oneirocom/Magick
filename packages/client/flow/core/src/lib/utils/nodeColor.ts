a8b2ed3cbb37863d30bba9e9a32cc892e83768c1
Michael <m.sharpe@project89.org>
2024-05-11 02:17:44 -0700

    Optimize mini map

--- packages/client/flow/core/src/lib/utils/nodeColor.ts
+++ packages/client/flow/core/src/lib/utils/nodeColor.ts
@@ -1,6 +1,6 @@
 import type { Node } from '@xyflow/react'
 import type { NodeSpecJSON } from '@magickml/behave-graph'
-import { categoryColorMap, colors, valueTypeColorMap } from './colors'
+import { categoryColorMap, colorHexMap, colors, valueTypeColorMap } from './colors'
 import { MagickNodeType } from '../node/base-node'
 
 function getCategory(node: Node, specJson: NodeSpecJSON[]) {
@@ -20,45 +20,13 @@ export function nodeColor(node: MagickNodeType, specJson: NodeSpecJSON[]) {
   if (colorName === undefined) {
     colorName = 'red'
   }
-  let [backgroundColor] = colors[colorName]
 
   if (configuration?.valueTypeName) {
     const colorName = valueTypeColorMap[configuration.valueTypeName]
     if (colorName) {
-      ;[backgroundColor] = colors[colorName]
+      return colorHexMap[colorName]
     }
   }
 
-  const color = getHexColorFromTailwindClass(backgroundColor)
-
-  return color
-}
-
-function getHexColorFromTailwindClass(className: string) {
-  // Create a temporary element
-  const tempElement = document.createElement('div')
-  tempElement.className = className
-
-  // Append it to the body (it won't be visible)
-  document.body.appendChild(tempElement)
-
-  // Get the computed style
-  const style = window.getComputedStyle(tempElement)
-  const rgb = style.backgroundColor
-
-  // Remove the element from the DOM
-  document.body.removeChild(tempElement)
-
-  // Convert RGB to Hex
-  const rgbMatch = rgb.match(/\d+/g)
-  if (!rgbMatch) return '#000000'
-
-  const hex = `#${rgbMatch
-    .map(x => {
-      const hex = parseInt(x).toString(16)
-      return hex.length === 1 ? '0' + hex : hex
-    })
-    .join('')}`
-
-  return hex
+  return colorHexMap[colorName]
 }
