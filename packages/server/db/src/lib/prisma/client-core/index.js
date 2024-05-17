bd8138d3cf11d4eb0e45643379c2f946c164397e
Jakob <haruhunab1320@gmail.com>
2024-05-17 11:15:40 -0700

    add keywords package

--- packages/server/db/src/lib/prisma/client-core/index.js
+++ packages/server/db/src/lib/prisma/client-core/index.js
@@ -565,7 +565,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/home/coffee/Desktop/MAGICK/packages/server/db/src/lib/prisma/client-core",
+      "value": "/Users/jakobgrant/Workspaces/Oneirocom/Magick/packages/server/db/src/lib/prisma/client-core",
       "fromEnvVar": null
     },
     "config": {
@@ -575,7 +575,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "darwin-arm64",
         "native": true
       }
     ],
@@ -644,8 +644,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "src/lib/prisma/client-core/libquery_engine-debian-openssl-3.0.x.so.node")
+path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
+path.join(process.cwd(), "src/lib/prisma/client-core/libquery_engine-darwin-arm64.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "src/lib/prisma/client-core/schema.prisma")
