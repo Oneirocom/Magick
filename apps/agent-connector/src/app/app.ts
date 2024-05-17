4ab70d1e613e8d837ee90cf4d7a27a82ca70756e
Michael <m.sharpe@project89.org>
2024-05-17 01:03:42 -0700

    Add sendAudio comment toggle

--- apps/agent-connector/src/app/app.ts
+++ apps/agent-connector/src/app/app.ts
@@ -21,6 +21,7 @@ const messageSchema = z.object({
   id: z.string(),
   text: z.string(),
   sender: z.string(),
+  type: z.string(),
 })
 
 const agentInfoSchema = z.object({
@@ -104,150 +105,175 @@ export async function app(fastify: FastifyInstance) {
   const voiceId = 'ijaOS620XRUqeVbPeOqJ'
   const model = 'eleven_turbo_v2'
 
-  fastify.get('/ws/:agentId', { websocket: true }, async (socket, req) => {
-    const agentId = req.url.split('/')[2]
-    console.log('Agent ID', agentId)
-    if (!agentId) {
-      socket.close()
-      return
-    }
+  fastify.get(
+    '/ws/:agentId/:channelId',
+    { websocket: true },
+    async (socket, req) => {
+      const agentId = req.url.split('/')[2]
+      const channelId = req.url.split('/')[3]
 
-    // startup agent
-    const agentData = await agentApp.service('agents').get(agentId)
-    const info: AgentInfo = { data: agentData, type: 'agentInfo' }
-    socket.send(JSON.stringify(info))
+      if (!agentId) {
+        socket.close()
+        return
+      }
 
-    const agent = new Agent(agentData, agentApp.get('pubsub'), agentApp)
+      let sendAudio = false
 
-    let textGenerator = createTextStreamGenerator()
+      // startup agent
+      const agentData = await agentApp.service('agents').get(agentId)
+      const info: AgentInfo = { data: agentData, type: 'agentInfo' }
+      socket.send(JSON.stringify(info))
 
-    /* USER INPUT STREAM */
-    socket.on('message', message => {
-      const data = messageSchema.parse(JSON.parse(message.toString()))
-      console.log('socket.on message', data)
+      const agent = new Agent(agentData, agentApp.get('pubsub'), agentApp)
 
-      socket.send(
-        JSON.stringify({
-          id: new Date().toISOString(),
-          text: 'processing...',
-          sender: agentData.name,
-          type: 'status',
-        })
-      )
+      let textGenerator = createTextStreamGenerator()
 
-      agent.emit(
-        'message',
-        agent.formatEvent<{ responseId: number }>({
-          content: data.text,
-          sender: 'client',
-          eventName: 'message',
-          data: {
-            responseId: numId(),
-          },
-        })
-      )
-    })
+      /* USER INPUT STREAM */
+      socket.on('message', message => {
+        const data = messageSchema.parse(JSON.parse(message.toString()))
+        console.log('socket.on message', data)
+        const { type } = data
 
-    /* AGENT MESSAGE OUTPUT STREAM */
-    agent.on('messageStream', async actionPayload => {
-      try {
-        const event = actionPayload.event
-        const responseId = (event.data as { responseId: string }).responseId
-        const content = actionPayload.data.content
+        if (type === 'message') {
+          socket.send(
+            JSON.stringify({
+              id: new Date().toISOString(),
+              text: 'processing...',
+              sender: agentData.name,
+              type: 'status',
+            })
+          )
 
-        // Send the text chunk to the client
+          agent.emit(
+            'message',
+            agent.formatEvent<{ responseId: number }>({
+              content: data.text,
+              sender: 'client',
+              channel: channelId,
+              eventName: 'message',
+              data: {
+                responseId: numId(),
+              },
+            })
+          )
+        }
+
+        if (type === 'toggleAudio') {
+          console.log('Toggling send audio!')
+          sendAudio = !sendAudio
+          console.log('sendAudio', sendAudio)
+        }
+      })
+
+      /* AGENT MESSAGE OUTPUT STREAM */
+      agent.on('messageStream', async actionPayload => {
+        if (actionPayload.event.channel !== channelId) return // Ignore messages not for this channel
+        try {
+          const event = actionPayload.event
+          const responseId = (event.data as { responseId: string }).responseId
+          const content = actionPayload.data.content
+
+          // Send the text chunk to the client
+          socket.send(
+            JSON.stringify({
+              id: v4(),
+              sender: agentData.name,
+              text: content,
+              type: 'message',
+              responseId: responseId,
+            })
+          )
+
+          if (!sendAudio) return
+
+          if (content === '<START>') {
+            console.log('Starting text to speech')
+            textGenerator = createTextStreamGenerator()
+
+            try {
+              const speechStream = await streamSpeech({
+                model: elevenlabs.SpeechGenerator({
+                  model: model,
+                  voice: voiceId,
+                  optimizeStreamingLatency: 1,
+                  voiceSettings: { stability: 1, similarityBoost: 0.35 },
+                  generationConfig: {
+                    chunkLengthSchedule: [50, 90, 120, 150, 200],
+                  },
+                }),
+                text: textGenerator,
+              })
+
+              // part is a Uint8Array
+              for await (const part of speechStream) {
+                if (socket.readyState === WebSocket.OPEN) {
+                  console.log('Sending chunk')
+                  socket.send(part) // Send the chunk directly as binary data
+                  await new Promise(resolve => setTimeout(resolve, 10)) // Throttle sending
+                }
+              }
+            } catch (err) {
+              console.error('Error streaming speech', err)
+            }
+          } else if (content === '<END>') {
+            textGenerator.endStream()
+          } else {
+            textGenerator.addToken(content)
+          }
+        } catch (error) {
+          console.error('Error in agent.on messageStream', error)
+        }
+      })
+
+      /* OTHER SOCKETS */
+      agent.on('message', actionPayload => {
+        const { content } = actionPayload
+        console.log('agent.on message', content)
+      })
+
+      agent.on('messageReceived', async actionPayload => {
+        if (actionPayload.event.channel !== channelId) return // Ignore messages not for this channel
+        console.log('agent.on messageReceived', actionPayload)
+        const text = actionPayload.data.content
         socket.send(
           JSON.stringify({
-            id: v4(),
+            id: new Date().toISOString(),
             sender: agentData.name,
-            text: content,
+            text,
             type: 'message',
-            responseId: responseId,
           })
         )
 
-        if (content === '<START>') {
-          console.log('Starting text to speech')
-          textGenerator = createTextStreamGenerator()
+        if (!sendAudio) return
 
-          try {
-            const speechStream = await streamSpeech({
-              model: elevenlabs.SpeechGenerator({
-                model: model,
-                voice: voiceId,
-                optimizeStreamingLatency: 1,
-                voiceSettings: { stability: 1, similarityBoost: 0.35 },
-                generationConfig: {
-                  chunkLengthSchedule: [50, 90, 120, 150, 200],
-                },
-              }),
-              text: textGenerator,
-            })
+        console.log('sending audio', text)
 
-            // part is a Uint8Array
-            for await (const part of speechStream) {
-              if (socket.readyState === WebSocket.OPEN) {
-                console.log('Sending chunk')
-                socket.send(part) // Send the chunk directly as binary data
-                await new Promise(resolve => setTimeout(resolve, 10)) // Throttle sending
-              }
-            }
-          } catch (err) {
-            console.error('Error streaming speech', err)
-          }
-        } else if (content === '<END>') {
-          textGenerator.endStream()
-        } else {
-          textGenerator.addToken(content)
+        try {
+          const speech = await generateSpeech({
+            model: elevenlabs.SpeechGenerator({
+              model: model,
+              voice: voiceId,
+            }),
+            text,
+          })
+          socket.send(speech)
+        } catch (err) {
+          console.error('Error generating speech', err)
         }
-      } catch (error) {
-        console.error('Error in agent.on messageStream', error)
-      }
-    })
+      })
 
-    /* OTHER SOCKETS */
-    agent.on('message', actionPayload => {
-      const { content } = actionPayload
-      console.log('agent.on message', content)
-    })
+      // on disconnect
+      socket.on('close', () => {
+        agent.removeAllListeners()
+        agent.onDestroy()
+      })
 
-    agent.on('messageReceived', async actionPayload => {
-      console.log('agent.on messageReceived', actionPayload)
-      const text = actionPayload.data.content
-      socket.send(
-        JSON.stringify({
-          id: new Date().toISOString(),
-          sender: agentData.name,
-          text,
-          type: 'message',
-        })
-      )
-
-      try {
-        const speech = await generateSpeech({
-          model: elevenlabs.SpeechGenerator({
-            model: model,
-            voice: voiceId,
-          }),
-          text,
-        })
-        socket.send(speech)
-      } catch (err) {
-        console.error('Error generating speech', err)
-      }
-    })
-
-    // on disconnect
-    socket.on('close', () => {
-      agent.removeAllListeners()
-      agent.onDestroy()
-    })
-
-    socket.on('error', () => {
-      agent.removeAllListeners()
-      agent.onDestroy()
-    })
-  })
+      socket.on('error', () => {
+        agent.removeAllListeners()
+        agent.onDestroy()
+      })
+    }
+  )
 
   fastify.get('/aai-token', async function () {
     try {
