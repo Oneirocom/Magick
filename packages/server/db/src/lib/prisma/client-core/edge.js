6b0ec9ac523212425cb28cea87f0299396d35829
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-18 02:08:15 -0400

    chore: Add portal:start script for production server deployment

--- packages/server/db/src/lib/prisma/client-core/edge.js
+++ packages/server/db/src/lib/prisma/client-core/edge.js
@@ -564,7 +564,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/parzival/workspace/oneirocom/Magick/packages/server/db/src/lib/prisma/client-core",
+      "value": "/home/coffee/Desktop/MAGICK/packages/server/db/src/lib/prisma/client-core",
       "fromEnvVar": null
     },
     "config": {
@@ -574,7 +574,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "darwin-arm64",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
