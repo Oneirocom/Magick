294c6986b4c24c0cab6902d75c825c24a3811c03
Michael <m.sharpe@project89.org>
2024-05-24 21:17:01 -0700

    Add isCommand socket to parseCommand

--- plugins/core/src/lib/nodes/logic/strings/parseCommand.ts
+++ plugins/core/src/lib/nodes/logic/strings/parseCommand.ts
@@ -18,11 +18,19 @@ export const parseCommand = makeFunctionNodeDefinition({
       label: 'Content',
       valueType: 'string',
     },
+    isCommand: {
+      label: 'isCommand',
+      valueType: 'boolean',
+    },
   },
   exec: ({ read, write }) => {
     const a = read('string') as string
 
     const [command, ...content] = a.split(' ')
+
+    // check if the command starts with a /
+    const isCommand = command.startsWith('/')
+    write('isCommand', isCommand)
     write('command', command)
     write('content', content.join(' '))
   },
