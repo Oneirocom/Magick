137ffc4e7439b10f67595761017647bf890e48b1
Michael <m.sharpe@project89.org>
2024-05-28 12:17:31 -0700

    Fis default configs

--- packages/client/editor/src/components/PropertiesWindow/DefaultConfig.tsx
+++ packages/client/editor/src/components/PropertiesWindow/DefaultConfig.tsx
@@ -1,12 +1,13 @@
-import { cx } from "class-variance-authority";
-import { ConfigurationComponentProps } from "./PropertiesWindow"
-import { useState } from "react";
+import { cx } from 'class-variance-authority'
+import { ConfigurationComponentProps } from './PropertiesWindow'
+import { useState } from 'react'
+import { Input, Switch } from '@magickml/client-ui'
 
-const inputClass = cx(
-  'bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2 nodrag text-sm',
-);
-
-export const DefaultConfig = ({ valueType, config, updateConfigKey }: ConfigurationComponentProps) => {
+export const DefaultConfig = ({
+  valueType,
+  config,
+  updateConfigKey,
+}: ConfigurationComponentProps) => {
   const [name, inputValue] = config
   const [inputVal, setInputVal] = useState(inputValue)
 
@@ -17,52 +18,49 @@ export const DefaultConfig = ({ valueType, config, updateConfigKey }: Configurat
 
   return (
     <div>
-      <label className="mr-2">{name}</label>
-      <div className="flex-1 justify-center">
+      <label className="mr-2 mb-2">{name}</label>
+      <div className="">
         {valueType === 'string' && (
-          <input
+          <Input
             type="text"
-            className={inputClass}
             value={inputVal || ''}
-            onChange={(e) => {
+            onChange={e => {
               onChange(name, e.currentTarget.value)
             }}
           />
         )}
         {valueType === 'number' && (
-          <input
+          <Input
             type="number"
-            className={inputClass}
             value={inputVal || 0}
-            onChange={(e) => onChange(name, e.currentTarget.value)}
+            onChange={e => onChange(name, e.currentTarget.value)}
           />
         )}
         {valueType === 'float' && (
-          <input
+          <Input
             type="number"
-            className={inputClass}
             value={inputVal || 0}
-            onChange={(e) => onChange(name, e.currentTarget.value)}
+            onChange={e => onChange(name, e.currentTarget.value)}
           />
         )}
         {valueType === 'integer' && (
-          <input
+          <Input
             type="number"
-            className={inputClass}
             value={inputVal || 0}
-            onChange={(e) => onChange(name, e.currentTarget.value)}
+            onChange={e => onChange(name, e.currentTarget.value)}
           />
         )}
         {valueType === 'boolean' && (
-          <input
-            type="checkbox"
-            className={inputClass}
-            value={inputVal || 0}
-            onChange={(e) => onChange(name, e.currentTarget.checked)}
-          />
+          <div className="flex gap-2 items-center red">
+            <Switch
+              defaultChecked={inputVal}
+              value={inputVal || 0}
+              onCheckedChange={value => onChange(name, value)}
+              // onFocus={() => onFocus(value)}
+            />
+          </div>
         )}
       </div>
     </div>
   )
-
 }
