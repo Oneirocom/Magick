f8e3512de4e734031c580ac7eb700768e2a25ce7
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-29 11:10:55 -0400

    logger plugin

---
+++ apps/embedder/src/server/plugins/logger.ts
@@ -0,0 +1,16 @@
+import { consola } from 'consola'
+import { defineNitroPlugin } from 'nitropack/runtime'
+
+export default defineNitroPlugin(nitroApp => {
+  nitroApp.hooks.hook('request', event => {
+    consola.start(`${event.method} ${event.path}`)
+  })
+
+  nitroApp.hooks.hook('error', (event, { body }) => {
+    consola.error(event.message, event.message, body)
+  })
+
+  nitroApp.hooks.hook('afterResponse', event => {
+    consola.success(`${event.method} ${event.path}`)
+  })
+})
