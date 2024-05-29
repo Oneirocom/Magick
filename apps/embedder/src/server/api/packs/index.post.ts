25f9fd8c290eb4425a1799024c673d9c9b0e7194
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-29 11:13:12 -0400

    feat: Add API endpoint to create a pack

---
+++ apps/embedder/src/server/api/packs/index.post.ts
@@ -0,0 +1,34 @@
+import {
+  authParse,
+  CreatePackRequestSchema,
+  CreatePackResponseSchema,
+} from '@magickml/embedder/schema'
+import { randomUUID } from 'crypto'
+import { embedderDb, Pack } from 'embedder-db-pg'
+import { defineEventHandler, readBody } from 'h3'
+
+export default defineEventHandler(async event => {
+  console.log('VALIDATING AUTH')
+  const { entity, owner } = authParse(event.context)
+
+  const paredInput = CreatePackRequestSchema.parse(await readBody(event))
+
+  const insertedPack = await embedderDb
+    .insert(Pack)
+    .values({
+      id: randomUUID(),
+      ...paredInput,
+      owner,
+      entity,
+    })
+    .returning()
+    .execute()
+    .then(results => results[0])
+
+  console.log('insertedPack', insertedPack)
+
+  const parsedOutput = CreatePackResponseSchema.parse(insertedPack)
+  console.log('parsedOutput', parsedOutput)
+
+  return parsedOutput
+})
