ac388618841be721c334a0fdd9afaa90860ad196
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-29 11:02:00 -0400

    add embedder/clientr/react package

---
+++ packages/embedder/client/react/src/lib/embedder-client-react.tsx
@@ -0,0 +1,21 @@
+import { createEmbedderClient } from '@magickml/embedder/client/ts'
+import { ZodiosOptions } from '@zodios/core'
+
+import { ZodiosHooks } from '@zodios/react'
+
+interface CreateEmbedderClientOptions {
+  tsqPrefix: string
+
+  baseUrl: string
+  options?: ZodiosOptions
+}
+
+export const createEmbedderReactClient = ({
+  tsqPrefix,
+  ...options
+}: CreateEmbedderClientOptions) => {
+  return new ZodiosHooks(
+    tsqPrefix,
+    createEmbedderClient(options.baseUrl, options.options)
+  )
+}
