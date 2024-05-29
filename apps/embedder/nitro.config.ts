83ea2f299250757ea4bc8e96810e79f5b8e7ea11
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-29 11:10:04 -0400

    init nitro

---
+++ apps/embedder/nitro.config.ts
@@ -0,0 +1,88 @@
+import { resolve } from 'path'
+import { defineNitroConfig } from 'nitropack/config'
+
+// https://nitro.unjs.io/config
+export default defineNitroConfig({
+  srcDir: 'src/server',
+
+  runtimeConfig: {
+    redis: {
+      url: process.env.NITRO_REDIS_URL,
+    },
+    pinecone: {
+      projectName: 'pat',
+    },
+  },
+
+  experimental: {
+    tasks: true,
+    database: true,
+  },
+
+  typescript: {
+    tsConfig: {
+      extends: './tsconfig.app.json',
+    },
+  },
+
+  alias: {
+    '@magickml/embedder/auth/plugin': resolve(
+      __dirname,
+      '../../packages/embedder/auth/plugin/src/index.ts'
+    ),
+    '@magickml/server-db': resolve(
+      __dirname,
+      '../../packages/server/db/src/index.ts'
+    ),
+    '@magickml/embedder/auth/token': resolve(
+      __dirname,
+      '../../packages/embedder/auth/token/src/index.ts'
+    ),
+    '@magickml/embedder/client/ts': resolve(
+      __dirname,
+      '../../packages/embedder/client/ts/src/index.ts'
+    ),
+    '@magickml/embedder/config': resolve(
+      __dirname,
+      '../../packages/embedder/config/src/index.ts'
+    ),
+    '@magickml/embedder/db/pinecone': resolve(
+      __dirname,
+      '../../packages/embedder/db/pinecone/src/index.ts'
+    ),
+    'embedder-db-pg': resolve(
+      __dirname,
+      '../../packages/embedder/db/pg/src/index.ts'
+    ),
+    '@magickml/embedder/queue': resolve(
+      __dirname,
+      '../../packages/embedder/queue/src/index.ts'
+    ),
+
+    '@magickml/embedder/loaders/core': resolve(
+      __dirname,
+      '../../packages/embedder/loaders/core/src/index.ts'
+    ),
+    '@magickml/embedder/schema': resolve(
+      __dirname,
+      '../../packages/embedder/schema/src/index.ts'
+    ),
+    '@magickml/embedder/worker': resolve(
+      __dirname,
+      '../../packages/embedder/worker/src/index.ts'
+    ),
+
+    '@magickml/embedder/api/jobs': resolve(
+      __dirname,
+      '../../packages/embedder/api/jobs/src/index.ts'
+    ),
+    '@magickml/embedder/api/loaders': resolve(
+      __dirname,
+      '../../packages/embedder/api/loaders/src/index.ts'
+    ),
+    '@magickml/embedder/api/packs': resolve(
+      __dirname,
+      '../../packages/embedder/api/packs/src/index.ts'
+    ),
+  },
+})
