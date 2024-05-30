b2287754b51de569f79954fa7f1f93330b20268e
Michael <m.sharpe@project89.org>
2024-05-30 00:37:35 -0700

    chore: Update handle functions in readonly-flow.tsx

--- packages/client/flow/core/src/lib/flow/readonly-flow.tsx
+++ packages/client/flow/core/src/lib/flow/readonly-flow.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import React, { useMemo } from 'react'
 import { type Tab } from '@magickml/providers'
@@ -83,6 +83,9 @@ export const ReadOnlyFlow: React.FC<ReadOnlyFlowProps> = ({
         nodeMenuActions: [],
         isValidConnectionHandler: () => true,
         onEdgeUpdate: () => {},
+        handleSelectionDragStart: () => {},
+        handleDelete: () => {},
+        handleNodeDragStart: () => {},
       }}
     />
   )
