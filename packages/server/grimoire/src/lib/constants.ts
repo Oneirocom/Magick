89cfbec08b74805e9b8f9a87c0849ff20ee162fc
Michael <m.sharpe@project89.org>
2024-06-03 13:55:58 -0700

    feat: Add constants for base dependency keys and input/output events

---
+++ packages/server/grimoire/src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const BASE_DEP_KEYS = {
+  I_LOGGER: 'ILogger',
+  I_STATE_SERVICE: 'IStateService',
+  I_EVENT_STORE: 'IEventStore',
+  I_SPELLCASTER: 'spellcaster',
+  I_LIFECYCLE_EMITTER: 'ILifecycleEventEmitter',
+}
+
+export const INPUT_EVENT = 'subspellInput'
+export const OUTPUT_EVENT = 'subspellOutput'
