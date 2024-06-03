f084c7a39f6db44bce3a5e5c5d6b97cc2ce40b00
Michael <m.sharpe@project89.org>
2024-06-03 13:55:07 -0700

    Handle subspell run in custom node types

--- packages/client/flow/core/src/lib/hooks/useCustomNodeTypes.tsx
+++ packages/client/flow/core/src/lib/hooks/useCustomNodeTypes.tsx
@@ -1,11 +1,47 @@
-"use client"
+'use client'
 
 import { NodeSpecJSON } from '@magickml/behave-graph'
 import { useEffect, useState } from 'react'
-import { NodeTypes } from '@xyflow/react'
+import { NodeProps, NodeTypes } from '@xyflow/react'
 
 import { CoreNode } from '../node/core-node'
+
 import { ReadOnlyNode } from '../node/readonly-node'
+import { SubspellNode } from '../nodeTypes/subspell'
+
+type Props = NodeProps & {
+  data: any
+  type: any
+}
+
+const getCoreNode = (
+  spec: NodeSpecJSON,
+  allSpecs: NodeSpecJSON[],
+  spellId: string,
+  props: Props
+) => {
+  switch (spec.type) {
+    case 'action/subspell/run':
+      return (
+        <SubspellNode
+          spec={spec}
+          allSpecs={allSpecs}
+          spellId={spellId}
+          {...props}
+        />
+      )
+    default: {
+      return (
+        <CoreNode
+          spec={spec}
+          allSpecs={allSpecs}
+          spellId={spellId}
+          {...props}
+        />
+      )
+    }
+  }
+}
 
 const getCustomNodeTypes = (
   allSpecs: NodeSpecJSON[],
@@ -15,12 +51,7 @@ const getCustomNodeTypes = (
   return allSpecs.reduce((nodes: NodeTypes, node) => {
     nodes[node.type] = props => {
       return type === 'core' ? (
-        <CoreNode
-          spec={node}
-          allSpecs={allSpecs}
-          spellId={spellId}
-          {...props}
-        />
+        getCoreNode(node, allSpecs, spellId, props)
       ) : (
         <ReadOnlyNode
           spec={node}
