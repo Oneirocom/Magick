5f1ae2c6945e1181ea41902877e0ba79f1bfdec0
Michael <m.sharpe@project89.org>
2024-06-03 13:56:46 -0700

    Add subspell input node

---
+++ packages/server/grimoire/src/lib/nodes/subspells/subspellInput.ts
@@ -0,0 +1,93 @@
+import { Assert, NodeCategory } from '@magickml/behave-graph'
+import { InputData, SpellCaster } from '../../spellCaster'
+import { makeMagickEventNodeDefinition } from '../../factories/magickEventNode'
+import { BASE_DEP_KEYS, INPUT_EVENT } from '../../constants'
+import { IEventStore } from '../../services/eventStore'
+import { CORE_DEP_KEYS } from 'plugin/core'
+
+type State = {
+  onSpellCasterEvent?: ((data: InputData) => void) | undefined
+}
+
+const makeInitialState = (): State => ({
+  onSpellCasterEvent: undefined,
+})
+
+export const SubspellInput = makeMagickEventNodeDefinition(
+  {
+    typeName: 'events/subspells/input',
+    label: 'Subspell Input',
+    category: NodeCategory.Event,
+    in: {},
+    out: (_, graph) => {
+      const spellCaster = graph.getDependency<SpellCaster>(
+        BASE_DEP_KEYS.I_SPELLCASTER
+      )
+
+      if (!spellCaster) {
+        return []
+      }
+
+      return spellCaster.inputs.map(input => ({
+        key: input.key,
+        name: input.label,
+        valueType: input.valueType,
+      }))
+    },
+    initialState: makeInitialState(),
+  },
+  {
+    init: ({ state, commit, write, graph, handleState }) => {
+      Assert.mustBeTrue(state.onSpellCasterEvent === undefined)
+
+      const spellCaster = graph.getDependency<SpellCaster>(
+        BASE_DEP_KEYS.I_SPELLCASTER
+      )
+
+      const eventStore = graph.getDependency<IEventStore>(
+        CORE_DEP_KEYS.EVENT_STORE
+      )
+
+      if (!spellCaster || !eventStore) {
+        throw new Error('SpellCaster or EventStore not found')
+      }
+
+      const onSpellCasterEvent = (data: InputData) => {
+        const { flow, inputs } = data
+
+        const event = eventStore.currentEvent()
+
+        if (!event) {
+          throw new Error('Current Event not found')
+        }
+
+        for (const input of inputs) {
+          write(input.socketName, input.value)
+        }
+
+        handleState(event, false)
+
+        commit(flow)
+      }
+
+      spellCaster.on(INPUT_EVENT, onSpellCasterEvent)
+
+      return {
+        onSpellCasterEvent,
+      }
+    },
+    dispose: ({ graph, state }) => {
+      const spellCaster = graph.getDependency<SpellCaster>(
+        BASE_DEP_KEYS.I_SPELLCASTER
+      )
+
+      if (!spellCaster || !state.onSpellCasterEvent) return {}
+
+      spellCaster.removeListener(INPUT_EVENT, state.onSpellCasterEvent)
+
+      return {
+        onSpellCasterEvent: undefined,
+      }
+    },
+  }
+)
