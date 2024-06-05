26e6e7fb243cbecd844c3f4634c3814fd7f38c88
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-06-05 00:08:09 -0400

    feat: Add new endpoint to retrieve chunks for a specific loader

---
+++ apps/embedder/src/server/api/packs/[id]/loaders/[loaderId].get.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+import { embedderDb, Loader } from 'embedder-db-pg'
+import { and, eq } from 'drizzle-orm'
+import { createLoader } from '@magickml/embedder/loaders/core'
+
+export default defineEventHandler(async event => {
+  const packId = z.string().parse(event.context.params?.id)
+  const loaderId = z.string().parse(event.context.params?.loaderId)
+
+  const loader = await embedderDb
+    .select()
+    .from(Loader)
+    .where(and(eq(Loader.packId, packId), eq(Loader.id, loaderId)))
+    .execute()
+    .then(results => results[0])
+
+  if (!loader) {
+    throw createError({
+      statusCode: 404,
+      message: 'Loader not found',
+    })
+  }
+
+  const chunkGenerator = createLoader(loader).getChunks()
+
+  const chunksArray: string[] = []
+
+  for await (const chunk of chunkGenerator) {
+    chunksArray.push(chunk.pageContent)
+  }
+
+  return { chunks: chunksArray }
+})
