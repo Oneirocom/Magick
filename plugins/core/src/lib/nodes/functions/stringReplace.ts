c46d97baaad90015cafa4a39ad19d0b3cbef3a73
Michael <m.sharpe@project89.org>
2024-06-10 16:54:18 -0700

    Fix string replace

--- plugins/core/src/lib/nodes/functions/stringReplace.ts
+++ plugins/core/src/lib/nodes/functions/stringReplace.ts
@@ -6,12 +6,7 @@ export const stringReplace = makeInNOutFunctionDesc({
   category: NodeCategory.Logic,
   label: 'Replace',
   in: [{ string: 'string' }, { search: 'string' }, { replace: 'string' }],
-  out: [
-    {
-      name: 'newString',
-      valueType: 'string',
-    },
-  ],
+  out: [{ newString: 'string' }],
   exec: (string: string, search: string, replace: string) =>
     string.replace(search, replace),
 })
