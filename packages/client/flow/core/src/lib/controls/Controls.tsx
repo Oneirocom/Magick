5e9ca50d06c517c7e66cbe0f1fb8e9858b5b954e
Jakob <haruhunab1320@gmail.com>
2024-06-14 16:22:56 -0700

    refactor: Remove unused togglePlay method from CustomControls component

--- packages/client/flow/core/src/lib/controls/Controls.tsx
+++ packages/client/flow/core/src/lib/controls/Controls.tsx
@@ -1,13 +1,7 @@
-"use client"
+'use client'
 
 import { GraphJSON, NodeSpecJSON } from '@magickml/behave-graph'
-import {
-  faPause,
-  faPlay,
-  faSitemap,
-  faBug,
-  faBugSlash,
-} from '@fortawesome/free-solid-svg-icons'
+import { faSitemap } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import React from 'react'
@@ -16,10 +10,6 @@ import { ControlButton, Controls } from '@xyflow/react'
 import { ClearModal } from './modals/ClearModal'
 
 export type CustomControlsProps = {
-  playing: boolean
-  togglePlay: () => void
-  toggleDebug: () => void
-  isDebug: boolean
   setBehaviorGraph: (value: GraphJSON) => void
   specJson: NodeSpecJSON[] | undefined
   miniMapOpen: boolean
@@ -27,10 +17,6 @@ export type CustomControlsProps = {
 }
 
 export const CustomControls: React.FC<CustomControlsProps> = ({
-  playing,
-  isDebug,
-  togglePlay,
-  toggleDebug,
   toggleMiniMap,
 }) => {
   const [clearModalOpen, setClearModalOpen] = useState(false)
@@ -38,14 +24,8 @@ export const CustomControls: React.FC<CustomControlsProps> = ({
   return (
     <>
       <Controls>
-        {/* <ControlButton title="Help" onClick={() => setHelpModalOpen(true)}>
-          <FontAwesomeIcon icon={faQuestion} />
-        </ControlButton> */}
         <ControlButton title="Minimap" onClick={toggleMiniMap}>
           <FontAwesomeIcon icon={faSitemap} />
-        </ControlButton>
-        <ControlButton title="Debug" onClick={toggleDebug}>
-          <FontAwesomeIcon icon={isDebug ? faBug : faBugSlash} />
         </ControlButton>
         {/* <ControlButton title="Load" onClick={() => setLoadModalOpen(true)}>
           <FontAwesomeIcon icon={faUpload} />
@@ -53,9 +33,6 @@ export const CustomControls: React.FC<CustomControlsProps> = ({
         {/* <ControlButton title="Clear" onClick={() => setClearModalOpen(true)}>
           <FontAwesomeIcon icon={faTrash} />
         </ControlButton> */}
-        <ControlButton title="Run" onClick={togglePlay}>
-          <FontAwesomeIcon icon={playing ? faPlay : faPause} />
-        </ControlButton>
       </Controls>
       <ClearModal
         open={clearModalOpen}
