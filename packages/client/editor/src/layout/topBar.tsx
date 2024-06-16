93aa75fd15ae96af3b0511cef8f6d39062c8e235
Jakob <haruhunab1320@gmail.com>
2024-06-15 17:08:58 -0700

    add tool tips to top bar

--- packages/client/editor/src/layout/topBar.tsx
+++ packages/client/editor/src/layout/topBar.tsx
@@ -1,19 +1,22 @@
 'use client'
 
 import ViewSidebarOutlinedIcon from '@mui/icons-material/ViewSidebarOutlined'
-import {
-  faBug,
-  // faBugSlash
-} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useConfig, usePubSub } from '@magickml/providers'
+import { faBug } from '@fortawesome/free-solid-svg-icons'
 import useEditorSession from '../hooks/useEditorSession'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from '@magickml/client-ui'
 import { enqueueSnackbar } from 'notistack'
 import toast from 'react-hot-toast'
 import posthog from 'posthog-js'
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from '@magickml/client-ui'
 import {
   useLazyGetAgentByIdQuery,
   useSelectAgentsState,
@@ -174,78 +177,108 @@ const TopBar: React.FC<TopBarProps> = ({
   const isDraftAgent = agent.isDraft
 
   return (
-    <div className="bg-gray-800 text-white py-4 px-2 flex items-center justify-between w-full relative h-12 border-b-2 border-[--background-color]">
-      <div className="flex items-center space-x-2">
-        <Button
-          onClick={toggleFileDrawer}
-          className="text-white font-bold py-2 px-2 rounded bg-transparent"
-        >
-          <ViewSidebarOutlinedIcon className="transform scale-x-[-1] hover:text-[#06c9f0] transition duration-300" />
-        </Button>
+    <TooltipProvider>
+      <div className="bg-gray-800 text-white py-4 px-2 flex items-center justify-between w-full relative h-12 border-b-2 border-[--background-color]">
+        <div className="flex items-center space-x-2">
+          <Button
+            onClick={toggleFileDrawer}
+            className="text-white font-bold py-2 px-2 rounded bg-transparent"
+          >
+            <ViewSidebarOutlinedIcon className="transform scale-x-[-1] hover:text-[#06c9f0] transition duration-300" />
+          </Button>
 
-        {leftTopBarItems?.map((item, index) => (
-          <React.Fragment key={index}>{item}</React.Fragment>
-        ))}
-      </div>
-      <div className="flex items-center space-x-4 absolute left-1/2 transform -translate-x-1/2">
-        <Button
-          className="h-6"
-          variant="secondary"
-          size="sm"
-          onClick={onRefresh}
-        >
-          Reset
-        </Button>
-        {isDraftAgent && (
-          <Button
-            onClick={toggleRun}
-            className={`${
-              engineRunning
-                ? 'bg-[#363d42] hover:bg-[#565c62]'
-                : 'bg-[#fe980a] hover:bg-[#d7820b]'
-            } text-white font-bold py-2 px-4 rounded h-[30px]`}
-          >
-            {engineRunning ? 'Stop' : 'Run'}
-          </Button>
-        )}
-        <Button
-          className="h-6"
-          variant="destructive"
-          size="sm"
-          onClick={onKill}
-        >
-          Kill
-        </Button>
-      </div>
-      <div className="flex items-center space-x-2">
-        <Button
-          className={`h-[26px] w-[28px] bg-transparent shadow-sm 0`}
-          size="sm"
-          onClick={toggleDebug}
-        >
-          <FontAwesomeIcon
-            icon={faBug}
-            className={`text-white  ${
-              isDebug
-                ? 'text-[#3acd44] hover:text-[#21f343]'
-                : 'text-[#0b0d0e] hover:text-[#3acd44]'
-            }`}
-            size="lg"
-          />
-        </Button>
-        {rightTopBarItems?.map((item, index) => (
-          <React.Fragment key={index}>{item}</React.Fragment>
-        ))}
-        <div className="flex items-center space-x-2 p-1 rounded-md">
-          <Button
-            className="text-white font-bold rounded px-1 bg-transparent w-[28px] h-[28px]"
-            onClick={toggleRightPanel}
-          >
-            <ViewSidebarOutlinedIcon className="hover:text-[#06c9f0] transition duration-300" />
-          </Button>
+          {leftTopBarItems?.map((item, index) => (
+            <React.Fragment key={index}>{item}</React.Fragment>
+          ))}
+        </div>
+        <div className="flex items-center space-x-4 absolute left-1/2 transform -translate-x-1/2 z-50">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                className="h-6"
+                variant="secondary"
+                size="sm"
+                onClick={onRefresh}
+              >
+                Reset
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top" sideOffset={8} className="z-50">
+              Refresh the application
+            </TooltipContent>
+          </Tooltip>
+          {isDraftAgent && (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  onClick={toggleRun}
+                  className={`${
+                    engineRunning
+                      ? 'bg-[#363d42] hover:bg-[#565c62]'
+                      : 'bg-[#fe980a] hover:bg-[#d7820b]'
+                  } text-white font-bold py-2 px-4 rounded h-[30px]`}
+                >
+                  {engineRunning ? 'Stop' : 'Run'}
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={8} className="z-50">
+                {engineRunning ? 'Stop the engine' : 'Start the engine'}
+              </TooltipContent>
+            </Tooltip>
+          )}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                className="h-6"
+                variant="destructive"
+                size="sm"
+                onClick={onKill}
+              >
+                Kill
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top" sideOffset={8} className="z-50">
+              Kill all spells
+            </TooltipContent>
+          </Tooltip>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                className={`h-[26px] w-[28px] bg-transparent shadow-sm 0`}
+                size="sm"
+                onClick={toggleDebug}
+              >
+                <FontAwesomeIcon
+                  icon={faBug}
+                  className={`text-white  ${
+                    isDebug
+                      ? 'text-[#3acd44] hover:text-[#21f343]'
+                      : 'text-[#0b0d0e] hover:text-[#3acd44]'
+                  }`}
+                  size="lg"
+                />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top" sideOffset={8} className="z-50">
+              Toggle debug mode
+            </TooltipContent>
+          </Tooltip>
+          {rightTopBarItems?.map((item, index) => (
+            <React.Fragment key={index}>{item}</React.Fragment>
+          ))}
+          <div className="flex items-center space-x-2 p-1 rounded-md">
+            <Button
+              className="text-white font-bold rounded px-1 bg-transparent w-[28px] h-[28px]"
+              onClick={toggleRightPanel}
+            >
+              <ViewSidebarOutlinedIcon className="hover:text-[#06c9f0] transition duration-300" />
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </TooltipProvider>
   )
 }
 
