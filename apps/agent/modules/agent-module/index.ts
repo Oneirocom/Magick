789fa8cfbc9afe0f6a3c58a3e9608d90e6ba938a
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-06-18 17:58:58 -0400

    agent module

---
+++ apps/agent/modules/agent-module/index.ts
@@ -0,0 +1,64 @@
+import 'nitropack'
+
+import { NitroModule } from 'nitropack/types'
+import { fileURLToPath } from 'node:url'
+import consola from 'consola'
+
+import { getMagickNodes } from './helpers/scan'
+import { NodeHandler } from './types/shared'
+
+declare module 'nitropack' {
+  interface NitroOptions {
+    grimoireOptions?: {}
+  }
+
+  interface Nitro {
+    magick: {
+      scannedNodes: NodeHandler[]
+    }
+  }
+}
+
+export default <NitroModule>{
+  async setup(nitro) {
+    const resolve = (path: string) =>
+      fileURLToPath(new URL(path, import.meta.url))
+
+    console.log('Hello from my module!')
+    // 1: Initalize magick object
+    nitro.magick = {
+      scannedNodes: [],
+    }
+
+    // Scan the folders for Magick dependencies
+    const nodes = await getMagickNodes(nitro)
+
+    // Store the scanned nodes
+    nitro.magick.scannedNodes = nodes
+
+    nitro?.magick?.scannedNodes?.forEach(node => {
+      consola.info(`Registered node: ${node.name.split('.')[0]}`)
+    })
+
+    if (nitro.options.imports) {
+      nitro.options.imports.dirs ??= []
+      nitro.options.imports.dirs.push(resolve('runtimes/utils/*'))
+      consola.info('Added utils to imports', nitro.options.imports.dirs)
+    }
+
+    nitro.options.virtual['#magick/nodes'] = () => {
+      const imports = nitro.magick.scannedNodes.map(n => n.handler)
+
+      const code = `
+${imports.map(handler => `import ${handler} from '${handler}';`).join('\n')}
+export const magickNodes = [
+${nitro.magick.scannedNodes
+  .map(n => `  { name: '${n.name}', handler: ${n.handler} }`)
+  .join(',\n')}
+];
+`
+
+      return code
+    }
+  },
+}
---
+++ apps/agent/modules/agent-module/runtime/plugins/logger.ts
@@ -0,0 +1,17 @@
+import { consola } from 'consola'
+import { defineNitroPlugin } from 'nitropack/runtime'
+
+export default defineNitroPlugin(nitroApp => {
+  consola.info('Agent Logger Plugin Loaded!')
+  nitroApp.hooks.hook('request', event => {
+    consola.start(`${event.method} ${event.path}`)
+  })
+
+  nitroApp.hooks.hook('error', (event, { body }) => {
+    consola.error(event.message, event.message, body)
+  })
+
+  nitroApp.hooks.hook('afterResponse', event => {
+    consola.success(`${event.method} ${event.path}`)
+  })
+})
