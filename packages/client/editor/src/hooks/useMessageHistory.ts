84a2ef6f5013731cf68f03cf6888e6b9ec5a002b
Jakob <haruhunab1320@gmail.com>
2024-06-20 11:31:51 -0700

    make chat window autoscrol blocked when user scrolls up

--- packages/client/editor/src/hooks/useMessageHistory.ts
+++ packages/client/editor/src/hooks/useMessageHistory.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import Scrollbars from 'react-custom-scrollbars-2'
 
 export type Message = {
   sender: string
@@ -6,16 +7,48 @@ export type Message = {
   id?: string
 }
 
-export const useMessageHistory = ({ seraph }: { seraph?: boolean }) => {
+export const useMessageHistory = ({
+  seraph,
+  userScrolled = false,
+  setUserScrolled,
+}: {
+  seraph?: boolean
+  userScrolled?: boolean
+  setUserScrolled: (userScrolled: boolean) => void
+}) => {
   const [history, setHistory] = useState<Message[]>([])
-  const scrollbars = useRef<any>()
+  const scrollbarsRef = useRef<Scrollbars>(null)
   const [autoScroll, setAutoScroll] = useState<boolean>(true)
 
+  const isAtBottom = useCallback(() => {
+    if (!scrollbarsRef.current) return true
+    const { scrollTop, scrollHeight, clientHeight } =
+      scrollbarsRef.current.getValues()
+    // Consider "at bottom" if within 1px of the bottom
+    console.log(
+      scrollHeight,
+      scrollTop,
+      clientHeight,
+      Math.abs(scrollHeight - scrollTop - clientHeight)
+    )
+    return Math.abs(scrollHeight - scrollTop - clientHeight) <= 1
+  }, [scrollbarsRef.current])
+
+  const handleScroll = useCallback(() => {
+    const atBottom = isAtBottom()
+    if (atBottom) {
+      setUserScrolled(false)
+    } else {
+      setUserScrolled(true)
+    }
+  }, [isAtBottom, setUserScrolled])
+
   useEffect(() => {
-    if (!scrollbars.current) return
+    if (!scrollbarsRef.current) return
     if (!autoScroll) return
-    scrollbars.current.scrollToBottom()
-  }, [history, autoScroll])
+    if (userScrolled) return
+    scrollbarsRef.current.scrollToBottom()
+  }, [history, autoScroll, userScrolled])
 
   const printToConsole = (text: string) => {
     if (typeof text !== 'string')
@@ -30,15 +63,18 @@ export const useMessageHistory = ({ seraph }: { seraph?: boolean }) => {
 
   const onClear = () => {
     setHistory([])
+    setUserScrolled(false)
   }
 
   return {
     history,
-    scrollbars,
+    scrollbars: scrollbarsRef,
     autoscroll: autoScroll,
     setAutoscroll: setAutoScroll,
     printToConsole,
     setHistory,
     onClear,
+    isAtBottom,
+    handleScroll,
   }
 }
