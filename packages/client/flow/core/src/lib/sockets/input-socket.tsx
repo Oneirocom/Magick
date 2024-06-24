51b7b2d49e8982e465af2e942162d00c680e1139
Jakob <haruhunab1320@gmail.com>
2024-06-23 20:43:01 -0700

    update subscription crreation and deletion

--- packages/client/flow/core/src/lib/sockets/input-socket.tsx
+++ packages/client/flow/core/src/lib/sockets/input-socket.tsx
@@ -118,39 +118,32 @@ const InputFieldForValue = ({
     // setIsFocused(false)
   }
   useEffect(() => {
-    if (!currentTab) return
+    if (!currentTab || !isActive) return
+
     // Subscription function
     const subscribeToEvents = () => {
       return subscribe(
         events.$CHAT_TO_INPUT(currentTab.id),
         (eventName, { value, nodeId: incomingNodeId, name: incomingName }) => {
           if (nodeId !== incomingNodeId) return
+
           onChange(name, value)
           setInputVal(value || '')
         }
       )
     }
 
-    // Initial subscription
-    let unsubscribe: () => void
-    if (isActive) {
-      unsubscribe = subscribeToEvents()
-    }
+    // Subscribe only if the input is active
+    const unsubscribe = subscribeToEvents()
 
-    // Effect cleanup and conditional resubscription
+    // Cleanup function
     return () => {
+      // Always unsubscribe in the cleanup
       if (unsubscribe) {
         unsubscribe()
       }
-
-      // If isActive changes to false, unsubscribe
-      if (isActive) {
-        unsubscribe = subscribeToEvents()
-      }
     }
-  }, [currentTab, name, isActive, nodeId])
-
-  useEffect(() => {}, [isActive])
+  }, [currentTab, name, isActive, nodeId, onChange, setInputVal])
 
   return (
     <div className={containerClass}>
