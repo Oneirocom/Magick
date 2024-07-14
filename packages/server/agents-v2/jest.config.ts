d41c73bda9756714738bdd151442c206a6c15c92
Jakob <haruhunab1320@gmail.com>
2024-07-13 23:20:28 -0700

    update jes config

--- packages/server/agents-v2/jest.config.ts
+++ packages/server/agents-v2/jest.config.ts
@@ -1,11 +1,19 @@
-/* eslint-disable */
-export default {
+import type { Config } from 'jest'
+
+const config: Config = {
   displayName: 'agents-v2',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/packages/server/agents-v2',
+  testMatch: ['**/src/**/*.spec.ts', '**/__tests__/integration/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/unit/'],
 }
+
+export default config
