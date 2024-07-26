dd9dea5ad84c12915997e4b7a462192b9fa431fc
Michael <m.sharpe@project89.org>
2024-07-26 01:40:06 -0700

    Clean up dependencies

--- instrument.js
+++ instrument.js
@@ -1,20 +1,20 @@
-const { initSDK } = require('@hyperdx/node-opentelemetry')
-const {
-  BullMQInstrumentation,
-} = require('@jenniferplusplus/opentelemetry-instrumentation-bullmq')
-const { KnexInstrumentation } = require('@opentelemetry/instrumentation-knex')
-const {
-  IORedisInstrumentation,
-} = require('@opentelemetry/instrumentation-ioredis')
-const { KoaInstrumentation } = require('@opentelemetry/instrumentation-koa')
+// const { initSDK } = require('@hyperdx/node-opentelemetry')
+// const {
+//   BullMQInstrumentation,
+// } = require('@jenniferplusplus/opentelemetry-instrumentation-bullmq')
+// const { KnexInstrumentation } = require('@opentelemetry/instrumentation-knex')
+// const {
+//   IORedisInstrumentation,
+// } = require('@opentelemetry/instrumentation-ioredis')
+// const { KoaInstrumentation } = require('@opentelemetry/instrumentation-koa')
 
-initSDK({
-  consoleCapture: true, // optional, default: true
-  advancedNetworkCapture: true, // optional, default: false
-  additionalInstrumentations: [
-    new BullMQInstrumentation(),
-    new IORedisInstrumentation(),
-    new KoaInstrumentation(),
-    new KnexInstrumentation(),
-  ],
-})
+// initSDK({
+//   consoleCapture: true, // optional, default: true
+//   advancedNetworkCapture: true, // optional, default: false
+//   additionalInstrumentations: [
+//     new BullMQInstrumentation(),
+//     new IORedisInstrumentation(),
+//     new KoaInstrumentation(),
+//     new KnexInstrumentation(),
+//   ],
+// })
