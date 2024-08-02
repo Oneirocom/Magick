93c1e11b917cadccc362a01f99525a44c3cad115
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-01 22:59:33 -0400

    feat: Add Grimoire playground

---
+++ grimoire/playground/grimoire.config.ts
@@ -0,0 +1,11 @@
+import { defineGrimoireConfig } from 'grimoire/config'
+
+export default defineGrimoireConfig({
+  agent: {
+    id: '123',
+    name: 'my-agent',
+  },
+  nitro: {
+    compatibilityDate: '2024-08-01',
+  },
+})
