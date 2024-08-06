90a2ce2651303f074cda5b1ecf0e6786e73e36e1
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 09:47:26 -0400

    feat: grimoire tools feature def and module

---
+++ grimoire/modules/tools/src/index.ts
@@ -0,0 +1,17 @@
+import { defineNovaModule } from '@gtc-nova/kit'
+import { toolFeatures, type ToolFeatures } from './features'
+
+export const toolModule = defineNovaModule<ToolFeatures>({
+  name: 'tools',
+  features: toolFeatures,
+  featureTypeFunctions: {
+    tools: () => {
+      console.log('tools')
+    },
+  },
+  pluginsDir: './../src/runtime/plugins',
+  metaUrl: import.meta.url,
+  hooks: [],
+})
+
+export type * from './types'
