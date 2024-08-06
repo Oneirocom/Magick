369bc0eb48786df3f45b96a54f35c699dadd0b02
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 10:59:37 -0400

    feat: Add @magickml/portal and @magickml/knowledge modules to frimoire framework

--- grimoire/framework/src/cli/mod.ts
+++ grimoire/framework/src/cli/mod.ts
@@ -1,5 +1,13 @@
 import { nodeModule } from '@magickml/nodes'
 import { schemasModule } from '@magickml/schemas'
 import { spellsModule } from '@magickml/spells'
+import { portalModule } from '@magickml/portal'
+import { knowledgeModule } from '@magickml/knowledge'
 
-export const modules = [nodeModule, schemasModule, spellsModule]
+export const modules = [
+  nodeModule,
+  schemasModule,
+  spellsModule,
+  portalModule,
+  knowledgeModule,
+] as const
