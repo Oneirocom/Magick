75db6a3a3079fbf2eec329e9038d3995a1cb8ad9
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 09:18:15 -0400

    chore: fix broken virtual import

--- grimoire/modules/spells/src/runtime/exports.ts
+++ grimoire/modules/spells/src/runtime/exports.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { handlers as spells } from "#spells-virtual/definitions";
+import { handlers as spells } from "#spells-virtual/spells";
 
 import type { BaseVirtualHandler } from "@gtc-nova/kit/runtime";
 import type { Spell } from "../types";
