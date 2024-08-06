728d32af1d0e933f5fdf5e49be913600b8f30b11
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 10:20:13 -0400

    refactor: Update weatherTool to use @magickml/tools/runtime/utils

--- grimoire/playground/tools/weatherTool.ts
+++ grimoire/playground/tools/weatherTool.ts
@@ -1,16 +1,22 @@
-import { defineTool } from '@magickml/tools'
+import { defineTool } from '@magickml/tools/runtime/utils'
 import { z } from 'zod'
 
-export default {
-  name: 'weather',
-  definition: defineTool({
-    description: 'Get the weather in a location',
-    parameters: z.object({
-      location: z.string().describe('The location to get the weather for'),
-    }),
-    execute: async ({ location }) => ({
-      location,
-      temperature: 72 + Math.floor(Math.random() * 21) - 10,
-    }),
+export default defineTool({
+  description: 'A tool to get weather information for a given city',
+  parameters: z.object({
+    city: z.string(),
   }),
+  execute: async args => {
+    const { city } = args
+    // Make an API call to get weather data for the city
+    const weatherData = await fetchWeatherData(city)
+    return `The current weather in ${city} is ${weatherData.description} with a temperature of ${weatherData.temperature}°C.`
+  },
+})
+const fetchWeatherData = async (city: string) => {
+  // Dummy
+  return {
+    description: 'Sunny',
+    temperature: 23,
+  }
 }
