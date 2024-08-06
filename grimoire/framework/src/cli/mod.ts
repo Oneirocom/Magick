e052011fba4bf97fb64e13c05970c8b7126910ad
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 09:15:48 -0400

    chore: rexport modules as one

---
+++ grimoire/framework/src/cli/mod.ts
@@ -0,0 +1,5 @@
+import { nodeModule } from '@magickml/nodes'
+import { schemasModule } from '@magickml/schemas'
+import { spellsModule } from '@magickml/spells'
+
+export const modules = [nodeModule, schemasModule, spellsModule]
