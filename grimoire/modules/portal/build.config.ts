fab1c3df0d28fb4fa2181afa8c4aad38ede0f682
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-08-06 10:32:03 -0400

    chore: init portal module

---
+++ grimoire/modules/portal/build.config.ts
@@ -0,0 +1,51 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  failOnWarn: false,
+  declaration: true,
+
+  entries: [
+    { input: "src/index.ts", outDir: "dist" },
+
+    { input: "src/runtime/utils/index.ts", outDir: "dist/runtime/utils" },
+  ],
+
+  externals: [
+    "dot-prop",
+    "globby",
+    "ohash",
+    "pathe",
+    "rollup",
+    "ufo",
+    "#nitro-internal-virtual/error-handler",
+    "#nitro-internal-virtual/plugins",
+    "#nitro-internal-virtual/server-handlers",
+    "#nitro-internal-virtual/storage",
+    "#nitro-internal-virtual/tasks",
+    "#nitro-internal-virtual/database",
+    "nitro/kit",
+    "nitro/runtime",
+    "consola",
+    "consola/utils",
+    "klona",
+    "destr",
+    "h3",
+    "hookable",
+    "ofetch",
+    "unenv/runtime/fetch/index",
+    "unstorage",
+    "defu",
+    "radix3",
+    "unctx",
+    "croner",
+    "std-env",
+    "db0",
+    "scule",
+    "cookie-es",
+    "uncrypto",
+    "iron-webcrypto",
+    "unenv",
+    "nitro",
+    "@llm-tools/embedjs",
+  ],
+});
