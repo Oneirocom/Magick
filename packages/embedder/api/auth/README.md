23ec1d3109490eb9a38c83120795a9b2841b5520
Jakob <haruhunab1320@gmail.com>
2024-08-08 14:21:11 -0700

    add auth and token package

--- packages/embedder/api/auth/README.md
+++ packages/embedder/api/auth/README.md
@@ -1,11 +1,11 @@
-# auth
+# embedder-api-auth
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Building
 
-Run `nx build auth` to build the library.
+Run `nx build embedder-api-auth` to build the library.
 
 ## Running unit tests
 
-Run `nx test auth` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test embedder-api-auth` to execute the unit tests via [Jest](https://jestjs.io).
--- packages/embedder/auth/token/jest.config.ts
+++ packages/embedder/auth/token/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'embedder-api-auth',
+  displayName: 'token',
   preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/packages/embedder/api/auth',
+  coverageDirectory: '../../../../coverage/packages/embedder/auth/token',
 }
