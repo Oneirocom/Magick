cdc89f0a5d01444fcf90e36f101f0f0d04e56b48
Jakob <haruhunab1320@gmail.com>
2024-08-15 12:26:32 -0700

    add try catch to index.post

--- apps/embedder/src/server/api/packs/index.post.ts
+++ apps/embedder/src/server/api/packs/index.post.ts
@@ -6,29 +6,36 @@ import {
 import { randomUUID } from 'crypto'
 import { embedderDb, Pack } from '@magickml/embedder-db-pg'
 import { defineEventHandler, readBody } from 'h3'
-
 export default defineEventHandler(async event => {
   console.log('VALIDATING AUTH')
-  const { entity, owner } = authParse(event.context)
+  try {
+    const { entity, owner } = authParse(event.context)
 
-  const paredInput = CreatePackRequestSchema.parse(await readBody(event))
+    const parsedInput = CreatePackRequestSchema.parse(await readBody(event))
 
-  const insertedPack = await embedderDb
-    .insert(Pack)
-    .values({
-      id: randomUUID(),
-      ...paredInput,
-      owner,
-      entity,
+    const insertedPack = await embedderDb
+      .insert(Pack)
+      .values({
+        id: randomUUID(),
+        ...parsedInput,
+        owner,
+        entity,
+      })
+      .returning()
+      .execute()
+      .then(results => results[0])
+
+    console.log('insertedPack', insertedPack)
+
+    const parsedOutput = CreatePackResponseSchema.parse(insertedPack)
+
+    return parsedOutput
+  } catch (error) {
+    console.error('Error in handler:', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error',
+      data: error,
     })
-    .returning()
-    .execute()
-    .then(results => results[0])
-
-  console.log('insertedPack', insertedPack)
-
-  const parsedOutput = CreatePackResponseSchema.parse(insertedPack)
-  console.log('parsedOutput', parsedOutput)
-
-  return parsedOutput
+  }
 })
